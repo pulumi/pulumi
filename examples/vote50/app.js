d6b25750089ba0e0f2b6df239c812eadba728218
joeduffy <joeduffy@acm.org>
2016-11-01 10:31:21 -0700

    Add a few lingering changes to the Mu voting example
    Some of this is out of date, however I'm archiving work in progress,
    and will freshen it up momentarily.

--- examples/vote50/app.js
+++ examples/vote50/app.js
@@ -1,10 +1,11 @@
 var mu = require("mu");
 
 // Define a Service that creates a Stack, wires up Functions to Triggers, and exposes an API:
-class VotingService {
+class VotingService extends mu.Service {
     constructor() {
-        this.votes = new mu.Table();
-        this.voteCounts = new mu.Table();
+        super();
+        this.votes = new mu.services.Table();
+        this.voteCounts = new mu.services.Table();
         this.votes.forEach(vote => {
             // Keep our aggregated counts up-to-date:
             this.voteCounts.updateIncrement(vote.color, vote.count);
@@ -17,10 +18,10 @@ class VotingService {
 }
 
 // Create a HTTP endpoint Service that receives votes from an API:
-var voteAPI = new mu.HTTPGateway();
+var voteAPI = new mu.services.HTTPGateway();
 
-for (var state of [ "AL", "AK", ... "WI", "WY" ]) {
+for (var state of [ "AL", "AK", /*...,*/ "WI", "WY" ]) {
     var votingService = new VotingService();
-    voteAPI.register(`/${state}`, votingService);
+    voteAPI.register(`/${state}/vote`, votingService);
 }
 
