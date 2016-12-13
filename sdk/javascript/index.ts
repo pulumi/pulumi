6160d1a4c88aa11c55db462d3356a9890b9799e8
joeduffy <joeduffy@acm.org>
2016-12-12 16:28:39 -0800

    Export all submodules

--- sdk/javascript/index.ts
+++ sdk/javascript/index.ts
@@ -1,6 +1,10 @@
 // Copyright 2016 Marapongo, Inc. All rights reserved.
 
+export * from './arch';
+export * from './cluster';
 export * from './context';
 export * from './extension';
+export * from './json';
+export * from './options';
 export * from './stack';
 
