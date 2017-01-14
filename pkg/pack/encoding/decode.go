ab2d0ae6cbe86f7e2c89799aae09a271fbf641bf
joeduffy <joeduffy@acm.org>
2017-01-14 09:42:05 -0800

    Implement tag-directed decoding
    This change eliminates boilerplate decoding logic in all the different
    data structures, and instead uses a new tag-directed decoding scheme.
    This works a lot like the JSON deserializers, in that it recognizes the
    `json:"name"` tags, except that we permit annotation of fields that
    require custom deserialization, as `json:"name,custom"`.  The existing
    `json:"name,omitempty"` tag is recognized for optional fields.

--- pkg/pack/encoding/decode.go
+++ pkg/pack/encoding/decode.go
@@ -5,14 +5,14 @@
 package encoding
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/marapongo/mu/pkg/encoding"
 	"github.com/marapongo/mu/pkg/pack"
 	//"github.com/marapongo/mu/pkg/pack/ast"
-	"github.com/marapongo/mu/pkg/pack/symbols"
+	//"github.com/marapongo/mu/pkg/pack/symbols"
 	"github.com/marapongo/mu/pkg/util"
 )
 
@@ -47,20 +47,30 @@ func newMissing(ty string, field string) error {
 	return fmt.Errorf("Missing required %v field `%v`", ty, field)
 }
 
-func newWrongType(ty string, field string, expect string, actual string) error {
-	msg := fmt.Sprintf("%v `%v` must be a `%v`", ty, field, expect)
-	if actual != "" {
-		msg += fmt.Sprintf(", got `%v`", actual)
+func newWrongType(ty string, field string, expect reflect.Type, actual reflect.Type) error {
+	return fmt.Errorf("%v `%v` must be a `%v`, got `%v`", ty, field, expect, actual)
+}
+
+// adjustValue converts if possible to produce the target type.
+func adjustValue(val reflect.Value, to reflect.Type) reflect.Value {
+	for !val.Type().AssignableTo(to) {
+		if val.Type().ConvertibleTo(to) {
+			val = val.Convert(to)
+		} else if val.Kind() == reflect.Interface && to.Kind() != reflect.Interface {
+			val = val.Elem()
+		} else {
+			break
+		}
 	}
-	return errors.New(msg)
+	return val
 }
 
 // decodeField decodes primitive fields.  For fields of complex types, we use custom deserialization.
-func decodeField(tree object, ty string, field string, target interface{}, req bool) error {
+func decodeField(tree object, ty string, key string, target interface{}, optional bool) error {
 	vdst := reflect.ValueOf(target)
 	util.AssertM(vdst.Kind() == reflect.Ptr && !vdst.IsNil() && vdst.Elem().CanSet(),
 		"Target must be a non-nil, settable pointer")
-	if v, has := tree[field]; has {
+	if v, has := tree[key]; has {
 		// The field exists; okay, try to map it to the right type.
 		vsrc := reflect.ValueOf(v)
 		vsrcType := vsrc.Type()
@@ -79,78 +89,156 @@ func decodeField(tree object, ty string, field string, target interface{}, req b
 			}
 		}
 
-		// If the source and destination types don't match, after depointerizing the type above, bail right away.
-		if vsrcType != vdstType {
-			return newWrongType(ty, field, vdstType.Name(), vsrcType.Name())
+		// If the source is an interface, convert it to its element type.
+		if !vsrcType.AssignableTo(vdstType) && vsrcType.Kind() == reflect.Interface {
+			vsrc = vsrc.Elem()
+			vsrcType = vsrc.Type()
 		}
 
-		// Otherwise, go ahead and copy the value from source to the target.
-		vdst.Elem().Set(vsrc)
-	} else if req {
+		if !vsrcType.AssignableTo(vdstType) {
+			// If the source and destination types don't match, after depointerizing the type above, something's up.
+			if vsrcType.Kind() == vdstType.Kind() {
+				switch vsrcType.Kind() {
+				case reflect.Slice:
+					// If a slice, everything's ok so long as the elements are compatible.
+					arr := reflect.New(vdstType).Elem()
+					for i := 0; i < vsrc.Len(); i++ {
+						elem := vsrc.Index(i)
+						if !elem.Type().AssignableTo(vdstType.Elem()) {
+							elem = adjustValue(elem, vdstType.Elem())
+							if !elem.Type().AssignableTo(vdstType.Elem()) {
+								return newWrongType(ty, fmt.Sprintf("%v[%v]", key, i), vdstType.Elem(), elem.Type())
+							}
+						}
+						arr = reflect.Append(arr, elem)
+					}
+					vsrc = arr
+					vsrcType = vsrc.Type()
+				case reflect.Map:
+					// Similarly, if a map, everything's ok so long as elements and keys are compatible.
+					m := reflect.New(vdstType).Elem()
+					for _, k := range vsrc.MapKeys() {
+						val := vsrc.MapIndex(k)
+						if !k.Type().AssignableTo(vdstType.Key()) {
+							k = adjustValue(k, vdstType.Key())
+							if !k.Type().AssignableTo(vdstType.Key()) {
+								return newWrongType(ty,
+									fmt.Sprintf("%v[%v] key", key, k.Interface()), vdstType.Key(), k.Type())
+							}
+						}
+						if !val.Type().AssignableTo(vdstType.Elem()) {
+							val = adjustValue(val, vdstType.Elem())
+							if !val.Type().AssignableTo(vdstType.Elem()) {
+								return newWrongType(ty,
+									fmt.Sprintf("%v[%v] value", key, k.Interface()), vdstType.Elem(), val.Type())
+							}
+						}
+						m.SetMapIndex(k, val)
+					}
+					vsrc = m
+					vsrcType = vsrc.Type()
+				}
+			}
+		}
+
+		// Finally, provided everything is kosher, go ahead and store the value; otherwise, issue an error.
+		if vsrcType.AssignableTo(vdstType) {
+			vdst.Elem().Set(vsrc)
+		} else {
+			return newWrongType(ty, key, vdstType, vsrcType)
+		}
+	} else if !optional {
 		// The field doesn't exist and yet it is required; issue an error.
-		return newMissing(ty, field)
+		return newMissing(ty, key)
 	}
 	return nil
 }
 
-func decodeMetadata(tree object) (*pack.Metadata, error) {
-	var meta pack.Metadata
-	if err := decodeField(tree, "Metadata", "name", &meta.Name, true); err != nil {
-		return nil, err
-	}
-	if err := decodeField(tree, "Metadata", "description", &meta.Description, false); err != nil {
-		return nil, err
-	}
-	if err := decodeField(tree, "Metadata", "author", &meta.Author, false); err != nil {
-		return nil, err
-	}
-	if err := decodeField(tree, "Metadata", "website", &meta.Website, false); err != nil {
-		return nil, err
-	}
-	if err := decodeField(tree, "Metadata", "license", &meta.License, false); err != nil {
-		return nil, err
-	}
-	return &meta, nil
-}
+// decode decodes an entire map into a target object, using tag-directed mappings.
+func decode(tree object, target interface{}) error {
+	vdst := reflect.ValueOf(target)
+	util.AssertM(vdst.Kind() == reflect.Ptr && !vdst.IsNil() && vdst.Elem().CanSet(),
+		"Target must be a non-nil, settable pointer")
+	vdstType := vdst.Type().Elem()
+	util.AssertM(vdstType.Kind() == reflect.Struct && !vdst.IsNil(),
+		"Target must be a struct type with `json:\"x\"` tags to direct decoding")
 
-func decodePackage(tree object) (*pack.Package, error) {
-	// Ensure only recognized fields are present.
-	if err := noExcess(tree,
-		"Package", "name", "description", "author", "website", "license", "dependencies", "modules"); err != nil {
-		return nil, err
-	}
+	// For each field in the struct that has a `json:"name"`, look it up in the map by that `name`, issuing an error if
+	// it is missing or of the wrong type.  For each field that has a tag with omitempty specified, i.e.,
+	// `json:"name,omitempty"`, do the same, but permit it to be missing without issuing an error.
 
-	// Deserialize the informational metadata portion.
-	meta, err := decodeMetadata(tree)
-	if err != nil {
-		return nil, err
-	}
-
-	// Now deserialize the dependencies and modules section.
-	pack := pack.Package{Metadata: *meta}
-
-	if deps, has := tree["dependencies"]; has {
-		if adeps, ok := deps.([]interface{}); ok {
-			var tokens []symbols.ModuleToken
-			for i, dep := range adeps {
-				if sdep, ok := dep.(string); ok {
-					tokens = append(tokens, symbols.ModuleToken(sdep))
-				} else {
-					return nil, newWrongType(
-						"Package", fmt.Sprintf("dependencies[%v]", i),
-						"string", reflect.ValueOf(dep).Type().Name())
-				}
+	// We need to pass over the struct first to build up the set of infos, so we can dig into embedded structs.
+	fldtypes := []reflect.Type{vdstType}
+	var fldinfos []reflect.StructField
+	for len(fldtypes) > 0 {
+		fldtype := fldtypes[0]
+		fldtypes = fldtypes[1:]
+		for i := 0; i < fldtype.NumField(); i++ {
+			fldinfo := fldtype.Field(i)
+			if fldinfo.Anonymous {
+				// If an embedded struct, push it onto the queue to visit.
+				fldtypes = append(fldtypes, fldinfo.Type)
+			} else {
+				// Otherwise, we will go ahead and consider this field in our decoding.
+				fldinfos = append(fldinfos, fldinfo)
 			}
-			pack.Dependencies = &tokens
-		} else {
-			return nil, newWrongType(
-				"Package", "dependencies",
-				"[]ModuleToken", reflect.ValueOf(deps).Type().Name())
 		}
 	}
 
-	// TODO: dependencies.
-	// TODO: modules.
+	// Now go through, read and parse the "json" tags, and actually perform the decoding for each one.
+	flds := make(map[string]bool)
+	for _, fldinfo := range fldinfos {
+		if tag := fldinfo.Tag.Get("json"); tag != "" {
+			var key string    // the JSON key name.
+			var optional bool // true if this can be missing.
+			var custom bool   // true if custom marshaling is used.
+
+			// Decode the tag.
+			tagparts := strings.Split(tag, ",")
+			util.AssertMF(len(tagparts) > 0,
+				"Expected >0 tagparts on field %v.%v; got %v", vdstType.Name(), fldinfo.Name, len(tagparts))
+			key = tagparts[0]
+			for i := 1; i < len(tagparts); i++ {
+				switch tagparts[i] {
+				case "omitempty":
+					optional = true
+				case "custom":
+					custom = true
+				default:
+					util.FailMF("Unrecognized tagpart on field %v.%v: %v", vdstType.Name(), fldinfo.Name, tagparts[i])
+				}
+			}
+
+			// Now use the tag to direct unmarshaling.
+			fld := vdst.Elem().FieldByName(fldinfo.Name)
+			if !custom {
+				if err := decodeField(tree, vdstType.Name(), key, fld.Addr().Interface(), optional); err != nil {
+					return err
+				}
+			}
+
+			// Remember this key so we can be sure not to reject it later when checking for unrecognized fields.
+			flds[key] = true
+		}
+	}
+
+	// Afterwards, if there are any unrecognized fields, issue an error.
+	for k := range tree {
+		if !flds[k] {
+			return fmt.Errorf("Unrecognized %v field `%v`", vdstType.Name(), k)
+		}
+	}
+
+	return nil
+}
+
+func decodePackage(tree object) (*pack.Package, error) {
+	var pack pack.Package
+
+	// First use tag-directed decoding for the simple parts of the struct.
+	if err := decode(tree, &pack); err != nil {
+		return nil, err
+	}
 
 	return &pack, nil
 }
--- pkg/pack/encoding/decode_test.go
+++ pkg/pack/encoding/decode_test.go
@@ -15,53 +15,128 @@ type bag struct {
 	StringP  *string
 	Float64  float64
 	Float64P *float64
+	Strings  []string
+	StringsP *[]string
 }
 
 func TestDecodeField(t *testing.T) {
-	tree := make(object)
-	tree["b"] = true
-	tree["s"] = "hello"
-	tree["f"] = float64(3.14159265359)
+	tree := object{
+		"b":  true,
+		"s":  "hello",
+		"f":  float64(3.14159265359),
+		"ss": []string{"a", "b", "c"},
+	}
 
 	// Try some simple primitive decodes.
 	var s bag
 	var err error
-	err = decodeField(tree, "bag", "b", &s.Bool, true)
+	err = decodeField(tree, "bag", "b", &s.Bool, false)
 	assert.Nil(t, err)
 	assert.Equal(t, tree["b"], s.Bool)
-	err = decodeField(tree, "bag", "b", &s.BoolP, true)
+	err = decodeField(tree, "bag", "b", &s.BoolP, false)
 	assert.Nil(t, err)
 	assert.Equal(t, tree["b"], *s.BoolP)
-	err = decodeField(tree, "bag", "s", &s.String, true)
+	err = decodeField(tree, "bag", "s", &s.String, false)
 	assert.Nil(t, err)
 	assert.Equal(t, tree["s"], s.String)
-	err = decodeField(tree, "bag", "s", &s.StringP, true)
+	err = decodeField(tree, "bag", "s", &s.StringP, false)
 	assert.Nil(t, err)
 	assert.Equal(t, tree["s"], *s.StringP)
-	err = decodeField(tree, "bag", "f", &s.Float64, true)
+	err = decodeField(tree, "bag", "f", &s.Float64, false)
 	assert.Nil(t, err)
 	assert.Equal(t, tree["f"], s.Float64)
-	err = decodeField(tree, "bag", "f", &s.Float64P, true)
+	err = decodeField(tree, "bag", "f", &s.Float64P, false)
 	assert.Nil(t, err)
 	assert.Equal(t, tree["f"], *s.Float64P)
+	err = decodeField(tree, "bag", "ss", &s.Strings, false)
+	assert.Nil(t, err)
+	assert.Equal(t, tree["ss"], s.Strings)
+	err = decodeField(tree, "bag", "ss", &s.StringsP, false)
+	assert.Nil(t, err)
+	assert.Equal(t, tree["ss"], *s.StringsP)
+
+	// Ensure interface{} conversions work:
+	var sif string
+	err = decodeField(object{"x": interface{}("hello")}, "bag", "x", &sif, false)
+	assert.Nil(t, err)
+	assert.Equal(t, "hello", sif)
+
+	var sifs []string
+	err = decodeField(object{"arr": []interface{}{"a", "b", "c"}}, "bag", "arr", &sifs, false)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"a", "b", "c"}, sifs)
 
 	// Ensure missing optional fields are ignored:
 	s.String = "x"
-	err = decodeField(tree, "bag", "missing", &s.String, false)
+	err = decodeField(tree, "bag", "missing", &s.String, true)
 	assert.Nil(t, err)
 	assert.Equal(t, "x", s.String)
 
 	// Try some error conditions; first, wrong type:
 	s.String = "x"
-	err = decodeField(tree, "bag", "b", &s.String, true)
+	err = decodeField(tree, "bag", "b", &s.String, false)
 	assert.NotNil(t, err)
 	assert.Equal(t, "bag `b` must be a `string`, got `bool`", err.Error())
 	assert.Equal(t, "x", s.String)
 
 	// Next, missing required field:
 	s.String = "x"
-	err = decodeField(tree, "bag", "missing", &s.String, true)
+	err = decodeField(tree, "bag", "missing", &s.String, false)
 	assert.NotNil(t, err)
 	assert.Equal(t, "Missing required bag field `missing`", err.Error())
 	assert.Equal(t, "x", s.String)
+}
+
+type bagtag struct {
+	String        string `json:"s"`
+	StringCust    string `json:"sc,custom"`
+	StringOpt     string `json:"so,omitempty"`
+	StringCustOpt string `json:"sco,custom,omitempty"`
+}
+
+func TestDecode(t *testing.T) {
+	var err error
+
+	// First, test the fully populated case.
+	var b1 bagtag
+	err = decode(object{
+		"s":   "something",
+		"sc":  "nothing",
+		"so":  "ohmy",
+		"sco": "ohmynada",
+	}, &b1)
+	assert.Nil(t, err)
+	assert.Equal(t, "something", b1.String)
+	assert.Equal(t, "", b1.StringCust)
+	assert.Equal(t, "ohmy", b1.StringOpt)
+	assert.Equal(t, "", b1.StringCustOpt)
+
+	// Now let optional fields go missing.
+	var b2 bagtag
+	err = decode(object{
+		"s":  "something",
+		"sc": "nothing",
+	}, &b2)
+	assert.Nil(t, err)
+	assert.Equal(t, "something", b2.String)
+	assert.Equal(t, "", b2.StringCust)
+	assert.Equal(t, "", b2.StringOpt)
+	assert.Equal(t, "", b2.StringCustOpt)
+
+	// Try some error conditions; first, wrong type:
+	var b3 bagtag
+	err = decode(object{
+		"s":  true,
+		"sc": "",
+	}, &b3)
+	assert.NotNil(t, err)
+	assert.Equal(t, "bagtag `s` must be a `string`, got `bool`", err.Error())
+	assert.Equal(t, "", b3.String)
+
+	// Next, missing required field:
+	var b4 bagtag
+	err = decode(object{}, &b4)
+	assert.NotNil(t, err)
+	assert.Equal(t, "Missing required bagtag field `s`", err.Error())
+	assert.Equal(t, "", b4.String)
 }
