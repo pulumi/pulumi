e14ca5f4ffb42880a99cf98e3d00e6c706b434fc
joeduffy <joeduffy@acm.org>
2017-02-09 14:50:41 -0800

    Manually print the stack-trace when --logtostderr is enabled
    Glog doesn't actually print out the stack traces for all goroutines,
    when --logtostderr is enabled, the same way it normally does.  This
    makes debugging more complex in some cases.  So, we'll manually do it.

--- pkg/util/contract/failfast.go
+++ pkg/util/contract/failfast.go
@@ -3,10 +3,23 @@
 package contract
 
 import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime/debug"
+
 	"github.com/golang/glog"
 )
 
 // failfast logs and panics the process in a way that is friendly to debugging.
 func failfast(msg string) {
+	v := flag.Lookup("logtostderr").Value
+	if g, isgettable := v.(flag.Getter); isgettable {
+		if enabled := g.Get().(bool); enabled {
+			// Print the stack to stderr anytime glog verbose logging is enabled, since glog won't.
+			fmt.Fprintf(os.Stderr, "fatal: %v\n", msg)
+			debug.PrintStack()
+		}
+	}
 	glog.Fatal(msg)
 }
