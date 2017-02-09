e8f54f3f4d92e73e11b1e1e164895b7c99de1625
joeduffy <joeduffy@acm.org>
2017-02-09 13:36:18 -0800

    Add a minimal MuJS blueprint example

---
+++ examples/minimal/index.ts
@@ -0,0 +1,12 @@
+// Copyright 2016 Marapongo, Inc. All rights reserved.
+
+import * as mu from "@mu/mu";
+
+class SimpleResource extends mu.Resource {
+    constructor() {
+        super();
+    }
+}
+
+let simple = new SimpleResource();
+
