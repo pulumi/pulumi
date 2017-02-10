432b9666fec5c699f3df07833ffe1847d1f3de53
joeduffy <joeduffy@acm.org>
2017-02-09 16:36:53 -0800

    Add an install script for the AWS library
    Eventually this won't be required, but for now, it will simplify
    bringing up our first end-to-end working example.

---
+++ lib/aws/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# A simple installation script for the Mu standard library.
+
+set -e                    # bail on errors
+
+echo Compiling:
+mujs                      # compile the package
+
+echo Sharing NPM links:
+yarn link                 # let NPM references resolve easily.
+
+MULIB=/usr/local/mu/lib
+echo Installing Mu standard library to $MULIB:
+mkdir -p $MULIB           # ensure the target library directory exists
+cp -Rv ./bin/ $MULIB/aws/ # copy to the standard library location
+
+echo Done.
+
