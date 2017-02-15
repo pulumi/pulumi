f22514fc74b5aab2947d5c4dd82545d06d064286
joeduffy <joeduffy@acm.org>
2017-02-15 12:47:20 -0800

    Strengthen the super test
    This change adds runtime verification of the super test's expected
    behavior.  This has proven useful in testing out some of the recent
    runtime/interpreter logic around super, etc.  Most likely we will
    start testing all of the MuJS tests as real MuIL/etc. tests soon.

--- tools/mujs/tests/output/basic/super/index.ts
+++ tools/mujs/tests/output/basic/super/index.ts
@@ -16,9 +16,28 @@ class C extends B {
     constructor() {
         // This `super` should resolve to the constructor function:
         super(42);
+    }
 
+    public gy(): number {
         // This `super` should just be an object reference to the base object:
         let y = super.gx();
+        return y;
     }
+}
+
+let b = new B(18);
+let bgx = b.gx();
+if (bgx !== 18) {
+    throw new Error("Expected b.gx == 18; got " + bgx);
+}
+
+let c = new C();
+let cgx = c.gx();
+if (cgx !== 42) {
+    throw new Error("Expected c.gx == 42; got " + cgx);
+}
+let cgy = c.gy();
+if (cgy !== 42) {
+    throw new Error("Expecred c.gy == 42; got " + cgy);
 }
 
