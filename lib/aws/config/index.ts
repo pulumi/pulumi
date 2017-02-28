679a4bf479a416f8c5109e5b714ed9d16e8b4cb9
joeduffy <joeduffy@acm.org>
2017-02-28 11:12:34 -0800

    Export a first class Region type

--- lib/aws/config/index.ts
+++ lib/aws/config/index.ts
@@ -1,23 +1,10 @@
 // Copyright 2016 Pulumi, Inc. All rights reserved.
 
+import {Region} from "../types";
+
 // region configures the target region for a deployment.  The provider explicitly does not recognize AWS_REGION,
 // to minimize the possibility of accidental deployment differences due to a changing environment variable.
-export let region:
-    "us-east-1"      | // US East (N. Virginia)
-    "us-east-2"      | // US East (Ohio)
-    "us-west-1"      | // US West (N. California)
-    "us-west-2"      | // US West (Oregon)
-    "ca-central"     | // Canada (Central)
-    "ap-south-1"     | // Asia Pacific (Mumbai)
-    "ap-northeast-1" | // Asia Pacific (Tokyo)
-    "ap-northeast-2" | // Asia Pacific (Seoul)
-    "ap-southeast-1" | // Asia Pacific (Singapore)
-    "ap-southeast-2" | // Asia Pacific (Sydney)
-    "eu-central-1"   | // EU (Frankfurt)
-    "eu-west-1"      | // EU (Ireland)
-    "eu-west-2"      | // EU (London)
-    "sa-east-1"      | // South America (Sao Paulo)
-    undefined;
+export let region: Region | undefined;
 
 // accessKeyId configures the access key ID used to access AWS.  This is a secret.  If not provided, the
 // provider will look in the standard places (~/.aws/credentials, AWS_ACCESS_KEY_ID, etc).
--- lib/aws/index.ts
+++ lib/aws/index.ts
@@ -1,5 +1,7 @@
 // Copyright 2016 Pulumi, Inc. All rights reserved.
 
+export * from "./types";
+
 import * as config from "./config";
 import * as ec2 from "./ec2";
 export { config, ec2 };
