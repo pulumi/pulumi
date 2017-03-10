cfd06083f5947318cabdd38ea9a8616078eb82a9
joeduffy <joeduffy@acm.org>
2017-03-10 09:49:20 -0800

    Add a basic Riffmart sample
    This checks in part of the Riffmart sample.
    
    warning: this is a work-in-progress, and is incomplete.

---
+++ examples/scenarios/aws/riffmart/index.ts
@@ -0,0 +1,16 @@
+// Copyright 2016 Pulumi, Inc. All rights reserved.
+
+import * as cluster from "./cluster";
+import * as infra from "./infra";
+
+export let cloud: boolean | undefined;
+
+// Now create some useful variables that are used throughout the below logic.
+let prefix = "riffmart-" + coconut.env();
+let availabilityZone = aws.config.region + "a";
+
+// First spin up the basic cluster infrastructure.
+infra.new();
+
+// TODO: the rest.
+
