d23d3a5f873b0e1a38ecbc970cd113abc13f617b
joeduffy <joeduffy@acm.org>
2017-03-15 19:54:29 -0700

    Add the instance maps to the AWS library
    For now, this makes the demo nicer; eventually, we probably want
    to consider factoring this out into a separate "awsutils" package.

--- lib/aws/ec2/index.ts
+++ lib/aws/ec2/index.ts
@@ -1,14 +1,15 @@
 // Copyright 2017 Pulumi, Inc. All rights reserved.
 
-export * from './instance';
-export * from './internetGateway';
-export * from './route';
-export * from './routeTable';
-export * from './securityGroup';
-export * from './securityGroupEgress';
-export * from './securityGroupIngress';
-export * from './subnet';
-export * from './vpc';
-export * from './vpcGatewayAttachment';
-export * from './vpcPeeringConnection';
+export * from "./instance";
+export * from "./instanceMaps";
+export * from "./internetGateway";
+export * from "./route";
+export * from "./routeTable";
+export * from "./securityGroup";
+export * from "./securityGroupEgress";
+export * from "./securityGroupIngress";
+export * from "./subnet";
+export * from "./vpc";
+export * from "./vpcGatewayAttachment";
+export * from "./vpcPeeringConnection";
 
