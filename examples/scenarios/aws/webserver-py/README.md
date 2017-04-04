f0c1100ad6b0889b09082a9a60d6df6f281eb669
joeduffy <joeduffy@acm.org>
2017-04-03 17:22:37 -0700

    Add basic web server example in Python

---
+++ examples/scenarios/aws/webserver-py/README.md
@@ -0,0 +1,7 @@
+# examples/scenarios/aws/webserver-py
+
+An example based on the basic Amazon EC2 Instance sample at:
+http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/deploying.applications.html.
+
+This is written in Python.  A JavaScript version is available at examples/scenarios/aws/webserver/.
+
---
+++ examples/scenarios/aws/webserver-py/index.py
@@ -0,0 +1,20 @@
+# Copyright 2017 Pulumi, Inc. All rights reserved.
+
+import coconut.aws
+
+size = "t2.micro"
+
+def main():
+    group = aws.ec2.SecurityGroup("web-secgrp",
+            group_description="Enable HTTP access",
+            security_group_ingress=[
+                aws.ec2.SecurityGroupIngressRule("tcp", 80, 80, "0.0.0.0/0")
+            ])
+    server = aws.ec2.Instance("web-server-www",
+            instance_type=size,
+            security_groups=[ group ],
+            image_id=aws.ec2.get_linux_ami(size))
+
+if __name__ == "__main__":
+    main()
+
