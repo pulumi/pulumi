7878a44ef546763593a7af673884229e950064c8
joeduffy <joeduffy@acm.org>
2017-04-17 18:06:12 -0700

    Export inlinePolicy fields

--- lib/aws/provider/iam/policy.go
+++ lib/aws/provider/iam/policy.go
@@ -4,6 +4,6 @@ package iam
 
 // inlinePolicy represents a policy attached to a Policy, Group, and/or User resource.
 type inlinePolicy struct {
-	policyDocument map[string]interface{} `json:"policyDocument"` // a description of which actions are allowed.
-	policyName     string                 `json:"policyName"`     // the unique name of this policy.
+	PolicyDocument map[string]interface{} `json:"policyDocument"` // a description of which actions are allowed.
+	PolicyName     string                 `json:"policyName"`     // the unique name of this policy.
 }
