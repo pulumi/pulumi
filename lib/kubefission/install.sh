70e75063c2c69cd4e0d408e50860bfbb06ac327a
joeduffy <joeduffy@acm.org>
2017-05-01 17:43:50 -0700

    Remove superfluous output from install.sh

--- lib/kubefission/install.sh
+++ lib/kubefission/install.sh
@@ -4,22 +4,22 @@
 set -e                    # bail on errors
 
 echo Compiling:
-pushd pack/ > /dev/null && # compile the package
+pushd pack/ > /dev/null &&    # compile the package
     cocojs &&
-    coco pack verify &&   # ensure the package verifies
-    yarn link &&          # let NPM references resolve easily.
+    coco pack verify &&       # ensure the package verifies
+    yarn link &&              # let NPM references resolve easily.
     cp -R ./.coconut/bin ../bin &&
     popd > /dev/null
-pushd provider/ &&        # compile the resource provider
+pushd provider/ >/dev/null && # compile the resource provider
     go build -o ../bin/coco-resource-kubefission &&
-    popd
+    popd > /dev/null
 
 COCOLIB=/usr/local/coconut/lib
 THISLIB=$COCOLIB/kubefission/
 echo Installing Coconut Fission library to $THISLIB:
-mkdir -p $COCOLIB         # ensure the target library directory exists
-rm -rf $THISLIB           # clean the target
-cp -Rv ./bin/ $THISLIB    # copy to the standard library location
+mkdir -p $COCOLIB             # ensure the target library directory exists
+rm -rf $THISLIB               # clean the target
+cp -R ./bin/ $THISLIB         # copy to the standard library location
 
 echo Done.
 
