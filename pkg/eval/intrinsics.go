71855c7c4484d55df83bf59f6b4a0fd819e0cacf
joeduffy <joeduffy@acm.org>
2017-05-13 19:22:52 -0400

    Split intrinsics
    This change just refactors out the dynamic intrinsics functions, from the general
    intrinsics file, in preparation for some new ones.

--- pkg/eval/intrinsics.go
+++ pkg/eval/intrinsics.go
@@ -8,7 +8,6 @@ import (
 	"github.com/pulumi/coconut/pkg/diag"
 	"github.com/pulumi/coconut/pkg/eval/rt"
 	"github.com/pulumi/coconut/pkg/tokens"
-	"github.com/pulumi/coconut/pkg/util/contract"
 )
 
 // Invoker implements an intrinsic function's functionality.
@@ -82,34 +81,4 @@ func MaybeIntrinsic(tree diag.Diagable, sym symbols.Symbol) symbols.Symbol {
 		}
 	}
 	return sym
-}
-
-func isFunction(intrin *Intrinsic, e *evaluator, this *rt.Object, args []*rt.Object) *rt.Unwind {
-	contract.Assert(this == nil)    // module function
-	contract.Assert(len(args) == 1) // just one arg: the object to inquire about functionness
-	_, isfunc := args[0].Type().(*symbols.FunctionType)
-	ret := e.alloc.NewBool(intrin.Node, isfunc)
-	return rt.NewReturnUnwind(ret)
-}
-
-func dynamicInvoke(intrin *Intrinsic, e *evaluator, this *rt.Object, args []*rt.Object) *rt.Unwind {
-	contract.Assert(this == nil)    // module function
-	contract.Assert(len(args) == 3) // three args: obj, thisArg, and args
-
-	// First ensure the target is a function.
-	t := args[0].Type()
-	if _, isfunc := t.(*symbols.FunctionType); !isfunc {
-		return e.NewIllegalInvokeTargetException(intrin.Node, t)
-	}
-
-	// Fetch the function stub information (ignoring `this`, since we will pass our own).
-	stub := args[0].FunctionValue()
-
-	// Next, simply call through to the evalCall function, which will do all additional verification.
-	stub.This = this // adjust this before the call (note this doesn't mutate the source stub; it's by-value).
-	obj, uw := e.evalCallFuncStub(intrin.Node, stub, args...)
-	if uw != nil {
-		return uw
-	}
-	return rt.NewReturnUnwind(obj)
 }
---
+++ pkg/eval/intrinsics_dynamic.go
@@ -0,0 +1,39 @@
+// Copyright 2017 Pulumi, Inc. All rights reserved.
+
+package eval
+
+import (
+	"github.com/pulumi/coconut/pkg/compiler/symbols"
+	"github.com/pulumi/coconut/pkg/eval/rt"
+	"github.com/pulumi/coconut/pkg/util/contract"
+)
+
+func isFunction(intrin *Intrinsic, e *evaluator, this *rt.Object, args []*rt.Object) *rt.Unwind {
+	contract.Assert(this == nil)    // module function
+	contract.Assert(len(args) == 1) // just one arg: the object to inquire about functionness
+	_, isfunc := args[0].Type().(*symbols.FunctionType)
+	ret := e.alloc.NewBool(intrin.Node, isfunc)
+	return rt.NewReturnUnwind(ret)
+}
+
+func dynamicInvoke(intrin *Intrinsic, e *evaluator, this *rt.Object, args []*rt.Object) *rt.Unwind {
+	contract.Assert(this == nil)    // module function
+	contract.Assert(len(args) == 3) // three args: obj, thisArg, and args
+
+	// First ensure the target is a function.
+	t := args[0].Type()
+	if _, isfunc := t.(*symbols.FunctionType); !isfunc {
+		return e.NewIllegalInvokeTargetException(intrin.Node, t)
+	}
+
+	// Fetch the function stub information (ignoring `this`, since we will pass our own).
+	stub := args[0].FunctionValue()
+
+	// Next, simply call through to the evalCall function, which will do all additional verification.
+	stub.This = this // adjust this before the call (note this doesn't mutate the source stub; it's by-value).
+	obj, uw := e.evalCallFuncStub(intrin.Node, stub, args...)
+	if uw != nil {
+		return uw
+	}
+	return rt.NewReturnUnwind(obj)
+}
