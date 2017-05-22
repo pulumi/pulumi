3bad4fde98d985e246a87a9187ab5c58f837892f
joeduffy <joeduffy@acm.org>
2017-05-22 13:20:57 -0700

    Disable storing output properties
    The storing of output properties won't work correctly until pulumi/lumi#90
    is completed.  The update logic sees properties that weren't supplied by the
    developer and thinks this means an update is required; this is easy to fix
    but better to just roll into the overall pending change that will land soon.

--- pkg/resource/plan.go
+++ pkg/resource/plan.go
@@ -680,12 +680,13 @@ func (s *step) Apply() (State, error) {
 		contract.Assert(s.old == nil)
 		contract.Assert(s.new != nil)
 		contract.Assertf(!s.new.HasID(), "Resources being created must not have IDs already")
-		id, outs, rst, err := prov.Create(s.new.Type(), s.new.Properties())
+		id, _, rst, err := prov.Create(s.new.Type(), s.new.Properties())
 		if err != nil {
 			return rst, err
 		}
 		s.new.SetID(id)
-		s.new.SetPropertiesFrom(outs)
+		// TODO[pulumi/lumi#90]: reenable setting outputs when this is fully functional.
+		// s.new.SetPropertiesFrom(outs)
 
 	case OpDelete, OpReplaceDelete:
 		// Invoke the Delete RPC function for this provider:
