84d4840fb917733246cf795ecb08ef55f9937974
joeduffy <joe@pulumi.com>
2017-07-14 09:44:08 -0700

    Rename PID type to ID
    We want to change the property name, but not the type.

--- lib/lumi/resource.ts
+++ lib/lumi/resource.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
 
-export type PID = string;
+export type ID = string;
 export type UPN = string;
 
 // Resource represents a class whose CRUD operations are implemented by a provider plugin.
 export abstract class Resource {
-    public readonly pid: PID; // the provider-assigned ID (initialized by the runtime).
+    public readonly pid: ID;  // the provider-assigned ID (initialized by the runtime).
     public readonly upn: UPN; // the Universal Pulumi Name (initialized by the runtime).
 }
 
