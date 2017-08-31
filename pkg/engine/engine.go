cda0fd9bca50ec80df793ec13495e93f34b741cd
Matt Ellis <matt@pulumi.com>
2017-08-31 10:28:02 -0700

    Add godoc comments

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -35,8 +35,12 @@ func (e *Engine) InitDiag(opts diag.FormatOptions) {
 	e.snk = diag.DefaultSink(opts)
 }
 
+// EnvironmentProvider abstracts away retriving and storing environments
 type EnvironmentProvider interface {
+	// GetEnvironment returns the environment named by `name` or a non nil error
 	GetEnvironment(name tokens.QName) (*deploy.Target, *deploy.Snapshot, *environment.Checkpoint, error)
+	// SaveEnvironment saves an environment o be retrieved later by GetEnvironment
 	SaveEnvironment(env *deploy.Target, snap *deploy.Snapshot) error
+	// RemoveEnvironment removes an environment from the system
 	RemoveEnvironment(env *deploy.Target) error
 }
