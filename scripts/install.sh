aa0eb41769677a98a1f716adeb19cc7b6e6dfcb5
joeduffy <joe@pulumi.com>
2017-09-07 10:42:56 -0700

    Update the publish scripts for the new package structure

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# install.sh will download and install the current bits from the usual share location and binplace them.
-# The first argument is the Git commit hash to fetch and the second is the target location to install them into.
-
+# install.sh installs an existing release.
 set -e
 
-./install_release.sh pulumi-fabric ${1} ${2}
+INSTALL=$GOPATH/src/github.com/pulumi/pulumi/scripts/install.sh
+if [ ! -f $PUBLISH ]; then
+    >&2 echo "error: Missing publish script at $INSTALL"
+    exit 1
+fi
+
+${INSTALL} pulumi-fabric $1 $2
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,43 +1,13 @@
 #!/bin/bash
-# publish.sh will publish the current bits from the usual build location to an S3 build share.
-
+# publish.sh builds and publishes a release.
 set -e
 
-PUBDIR=$(mktemp -du)
-GITVER=$(git rev-parse HEAD)
-PUBFILE=$(dirname ${PUBDIR})/${GITVER}.tgz
-PUBPREFIX=s3://eng.pulumi.com/releases/pulumi-fabric
-declare -a PUBTARGETS=(${GITVER} $(git describe --tags) $(git rev-parse --abbrev-ref HEAD))
+PUBLISH=$GOPATH/src/github.com/pulumi/pulumi/scripts/publish.sh
+if [ ! -f $PUBLISH ]; then
+    >&2 echo "error: Missing publish script at $PUBLISH"
+    exit 1
+fi
 
-ROOT=$(dirname $0)/..
-
-# Ensure the repo isn't dirty.
-git diff-index --quiet HEAD -- || \
-    test -n "${PUBFORCE}" || \
-    (echo "error: Cannot publish a dirty repo; set PUBFORCE=true to override" && exit 99)
-
-# Copy the binaries and packs.
-mkdir -p ${PUBDIR}/bin/
-cp ${GOPATH}/bin/lumi ${PUBDIR}/bin/
-mkdir -p ${PUBDIR}/sdk/nodejs/
-cp ${ROOT}/sdk/nodejs/pulumi-langhost-nodejs ${PUBDIR}/sdk/nodejs/
-cp -R ${ROOT}/sdk/nodejs/package.json ${PUBDIR}/sdk/nodejs/package.json
-cp -R ${ROOT}/sdk/nodejs/bin/. ${PUBDIR}/sdk/nodejs/bin/
-cp -R ${ROOT}/sdk/nodejs/node_modules/. ${PUBDIR}/sdk/nodejs/node_modules/
-echo sdk/nodejs/ >> ${PUBDIR}/packs.txt
-
-# Tar up the release and upload it to our S3 bucket.
-tar -czf ${PUBFILE} -C ${PUBDIR} .
-for target in ${PUBTARGETS[@]}; do
-    PUBTARGET=${PUBPREFIX}/${target}.tgz
-    echo Publishing ${GITVER} to: ${PUBTARGET}
-    if [ -z "${FIRSTTARGET}" ]; then
-        # Upload the first one for real.
-        aws s3 cp ${PUBFILE} ${PUBTARGET}
-        FIRSTTARGET=${PUBTARGET}
-    else
-        # For all others, reuse the first target to avoid re-uploading.
-        aws s3 cp ${FIRSTTARGET} ${PUBTARGET}
-    fi
-done
+RELEASE_INFO=($(./make_release.sh))
+${PUBLISH} ${RELEASE_INFO[0]} pulumi-fabric ${RELEASE_INFO[@]:1}
 
