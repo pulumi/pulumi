527aee7b15d2c6b9965509fe08f8c6e1a08590b1
joeduffy <joe@pulumi.com>
2017-09-07 21:52:58 -0700

    Clone pulumi

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -2,18 +2,17 @@
 # make_release.sh will create a build package ready for publishing.
 set -e
 
+ROOT=$(dirname $0)/..
 PUBDIR=$(mktemp -du)
 GITVER=$(git rev-parse HEAD)
 PUBFILE=$(dirname ${PUBDIR})/${GITVER}.tgz
 PUBPREFIX=s3://eng.pulumi.com/releases/pulumi-fabric
 declare -a PUBTARGETS=(${GITVER} $(git describe --tags) $(git rev-parse --abbrev-ref HEAD))
 
-ROOT=$(dirname $0)/..
-
 # Copy the binaries and packs.
 mkdir -p ${PUBDIR}/bin/
 cp ${GOPATH}/bin/lumi ${PUBDIR}/bin/
-mkdir -p ${PUBDIR}/sdk/nodejs/
+mkdir -p ${PUBDIR}/sdk/
 cp -R ${ROOT}/sdk/nodejs/bin/. ${PUBDIR}/sdk/nodejs/
 cp -R ${ROOT}/sdk/nodejs/node_modules/. ${PUBDIR}/sdk/nodejs/node_modules/
 cp ${ROOT}/sdk/nodejs/pulumi-langhost-nodejs ${PUBDIR}/sdk/nodejs/
