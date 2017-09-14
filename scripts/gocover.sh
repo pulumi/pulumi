a5706e06625c8693aa6341d52ac038cddea53413
joeduffy <joe@pulumi.com>
2017-09-13 18:52:06 -0700

    Update the gocover commands

--- scripts/gocover.sh
+++ scripts/gocover.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-export PKGS='./pkg/... ./cmd/... ./lib/aws/provider/...'
-export PKGS_COMMA='./pkg/...,./cmd/...,./lib/aws/provider/...'
+export PKGS='./pkg/... ./cmd/...'
+export PKGS_COMMA='./pkg/...,./cmd/...'
 go test -i $PKGS
 go list -f '{{if gt (len .TestGoFiles) 0}}"go test -covermode count -coverprofile {{.Name}}.coverprofile -coverpkg $PKGS_COMMA {{.ImportPath}}"{{end}}' $PKGS | xargs -P100 -I {} bash -c {} 2>&1 | grep -v '^warning: no packages being tested depend on '
 gocovmerge `ls *.coverprofile` > coverage.cov
