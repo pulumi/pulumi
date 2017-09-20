56d36986a6e9e4c1aff2948ff836c49066726529
joeduffy <joe@pulumi.com>
2017-09-20 09:57:18 -0700

    Add back Computed<T> as a short-hand
    This adds back Computed<T> as a short-hand for Promise<T | undefined>.
    Subtly, all resource properties need to permit undefined flowing through
    during planning  Rather than forcing the long-hand version, which is easy
    to forget, we'll keep the convention of preferring Computed<T>.  It's
    just a typedef and the runtime type is just a Promise.

--- sdk/nodejs/asset/archive.ts
+++ sdk/nodejs/asset/archive.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
 
-import { Property, PropertyValue } from "../resource";
+import { Computed, ComputedValue } from "../resource";
 import { Asset } from "./asset";
 
 // An Archive represents a collection of named assets.
@@ -12,9 +12,9 @@ export type AssetMap = {[name: string]: Asset};
 
 // An AssetArchive is an archive created with a collection of named assets.
 export class AssetArchive extends Archive {
-    public readonly assets: Property<AssetMap>; // a map of name to asset.
+    public readonly assets: Computed<AssetMap>; // a map of name to asset.
 
-    constructor(assets: PropertyValue<AssetMap>) {
+    constructor(assets: ComputedValue<AssetMap>) {
         super();
         this.assets = Promise.resolve<AssetMap | undefined>(assets);
     }
@@ -22,9 +22,9 @@ export class AssetArchive extends Archive {
 
 // A FileArchive is an archive in a file-based archive in one of the supported formats (.tar, .tar.gz, or .zip).
 export class FileArchive extends Archive {
-    public readonly path: Property<string>; // the path to the asset file.
+    public readonly path: Computed<string>; // the path to the asset file.
 
-    constructor(path: PropertyValue<string>) {
+    constructor(path: ComputedValue<string>) {
         super();
         this.path = Promise.resolve<string | undefined>(path);
     }
@@ -34,9 +34,9 @@ export class FileArchive extends Archive {
 // protocol for fetching the archive's contents: `file://` is a local file (just like a FileArchive), `http://` and
 // `https://` specify HTTP and HTTPS, respectively, and specific providers may recognize custom schemes.
 export class RemoteArchive extends Archive {
-    public readonly uri: Property<string>; // the URI where the archive lives.
+    public readonly uri: Computed<string>; // the URI where the archive lives.
 
-    constructor(uri: PropertyValue<string>) {
+    constructor(uri: ComputedValue<string>) {
         super();
         this.uri = Promise.resolve<string | undefined>(uri);
     }
