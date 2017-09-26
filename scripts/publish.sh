9660da4d146b07b506c1d2544c1f30c9a80cd359
Matt Ellis <matt@pulumi.com>
2017-09-25 17:00:08 -0700

    Run Travis on both macOS and Linux
    Unlike go binaries (where we can cross compile) the node module that
    we publish needs to be built on the platform we publish for. Update
    our `.travis.yml` file to also build on macOS and fix the publishing
    script so we don't don't cross publish Darwin from Linux. Once we have
    CI working for Windows, we'll remove the loop over PUBLISH_GOOS and
    each build will publish just the artifacts for the host OS.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,6 @@
 set -o nounset -o errexit -o pipefail
 
 PUBLISH=$GOPATH/src/github.com/pulumi/home/scripts/publish.sh
-PUBLISH_GOOS=("linux" "darwin")
 PUBLISH_GOARCH=("amd64")
 PUBLISH_PROJECT="pulumi"
 
@@ -12,15 +11,14 @@ if [ ! -f $PUBLISH ]; then
     exit 1
 fi
 
-for OS in "${PUBLISH_GOOS[@]}"
-do
-    for ARCH in "${PUBLISH_GOARCH[@]}"
-    do
-        export GOOS=${OS}
-        export GOARCH=${ARCH}
+OS=$(go env GOOS)
 
-        RELEASE_INFO=($($(dirname $0)/make_release.sh))
-        ${PUBLISH} ${RELEASE_INFO[0]} "${PUBLISH_PROJECT}/${OS}/${ARCH}" ${RELEASE_INFO[@]:1}
-    done
+for ARCH in "${PUBLISH_GOARCH[@]}"
+do
+    export GOARCH=${ARCH}
+
+    RELEASE_INFO=($($(dirname $0)/make_release.sh))
+    ${PUBLISH} ${RELEASE_INFO[0]} "${PUBLISH_PROJECT}/${OS}/${ARCH}" ${RELEASE_INFO[@]:1}
 done
+
 
