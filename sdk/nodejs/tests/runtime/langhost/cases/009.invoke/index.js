2e9e0d2a98c8e87d827eef059535c58ea4d350ae
joeduffy <joe@pulumi.com>
2017-09-30 14:53:09 -0400

    Add a simple invoke test case

---
+++ sdk/nodejs/tests/runtime/langhost/cases/009.invoke/index.js
@@ -0,0 +1,15 @@
+// Test the ability to invoke provider functions via RPC.
+
+let assert = require("assert");
+let pulumi = require("../../../../../");
+
+let args = {
+    a: "hello",
+    b: true,
+    c: [ 0.99, 42, { z: "x" } ],
+};
+let result = pulumi.runtime.invoke("invoke:index:echo", args);
+result.then((v) => {
+    assert.deepEqual(v, args);
+});
+
