71f44f40b45bf81b832571ad106d1a050250ea0f
Chris Smith <chris@pulumi.com>
2017-10-30 18:25:15 -0700

    Add 'pulumi ls'

--- cmd/stack_ls.go
+++ cmd/stack_ls.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 
 	"github.com/pkg/errors"
+	"github.com/pulumi/pulumi/pkg/apitype"
 	"github.com/pulumi/pulumi/pkg/encoding"
 
 	"github.com/pulumi/pulumi/pkg/tokens"
@@ -20,6 +21,13 @@ import (
 )
 
 func newStackLsCmd() *cobra.Command {
+	if usePulumiCloudCommands() {
+		return newCloudStackLsCmd()
+	}
+	return newFAFStackLsCmd()
+}
+
+func newFAFStackLsCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "ls",
 		Short: "List all known stacks",
@@ -36,27 +44,25 @@ func newStackLsCmd() *cobra.Command {
 				return err
 			}
 
-			fmt.Printf("%-20s %-48s %-12s\n", "NAME", "LAST UPDATE", "RESOURCE COUNT")
+			var summaries []stackSummary
 			for _, stack := range stacks {
-				_, _, snapshot, err := getStack(stack)
-				if err != nil {
-					continue
+				summary := stackSummary{
+					Name:          stack,
+					LastDeploy:    "n/a",
+					ResourceCount: "n/a",
 				}
 
-				// Now print out the name, last deployment time (if any), and resources (if any).
-				lastDeploy := "n/a"
-				resourceCount := "n/a"
-				if snapshot != nil {
-					lastDeploy = snapshot.Time.String()
-					resourceCount = strconv.Itoa(len(snapshot.Resources))
+				// Ignore errors, just leave display settings as "n/a".
+				_, _, snapshot, err := getStack(stack)
+				if err == nil && snapshot != nil {
+					summary.LastDeploy = snapshot.Time.String()
+					summary.ResourceCount = strconv.Itoa(len(snapshot.Resources))
 				}
-				display := stack.String()
-				if stack == currentStack {
-					display += "*" // fancify the current stack.
-				}
-				fmt.Printf("%-20s %-48s %-12s\n", display, lastDeploy, resourceCount)
+
+				summaries = append(summaries, summary)
 			}
 
+			displayStacks(summaries, currentStack)
 			return nil
 		}),
 	}
@@ -102,4 +108,81 @@ func getStacks() ([]tokens.QName, error) {
 	}
 
 	return stacks, nil
+}
+
+func newCloudStackLsCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "ls",
+		Short: "List all known stacks",
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			stacks, err := getCloudStacks()
+			if err != nil {
+				return nil
+			}
+
+			// Map to a summary slice.
+			var summaries []stackSummary
+			for _, stack := range stacks {
+				summary := stackSummary{
+					Name:          stack.StackName,
+					LastDeploy:    "n/a", // TODO(pulumi-service/issues#249): Make this info available.
+					ResourceCount: strconv.Itoa(len(stack.Resources)),
+				}
+				// If the stack hasn't been pushed to, it's resource count doesn't matter.
+				if stack.ActiveUpdate == "" {
+					summary.ResourceCount = "n/a"
+				}
+				summaries = append(summaries, summary)
+			}
+
+			// Ignore the error, since current stack is just cosmetic for the display.
+			currentStack, err := getCurrentStack()
+			if err != nil {
+				currentStack = tokens.QName("")
+			}
+
+			displayStacks(summaries, currentStack)
+			return nil
+		}),
+	}
+}
+
+// getCloudStacks returns all stacks for the current repository x workspace on the Pulumi Cloud.
+func getCloudStacks() ([]apitype.Stack, error) {
+	// Look up the owner, repository, and project from the workspace and nearest package.
+	w, err := newWorkspace()
+	if err != nil {
+		return nil, err
+	}
+	projID, err := getCloudProjectIdentifier(w)
+	if err != nil {
+		return nil, err
+	}
+
+	// Query all stacks for the project on Pulumi.
+	var stacks []apitype.Stack
+	path := fmt.Sprintf("/orgs/%s/programs/%s/%s/stacks", projID.Owner, projID.Repository, projID.Project)
+	if err := pulumiRESTCall("GET", path, nil, &stacks); err != nil {
+		return nil, err
+	}
+	return stacks, nil
+}
+
+type stackSummary struct {
+	Name tokens.QName
+	// May be "n/a" for an undeployed stack.
+	LastDeploy    string
+	ResourceCount string
+}
+
+// displayStacks prints the list of stacks to STDOUT. An optional current stack name,
+// if present, will have a star by it.
+func displayStacks(stacks []stackSummary, current tokens.QName) {
+	fmt.Printf("%-20s %-48s %-12s\n", "NAME", "LAST UPDATE", "RESOURCE COUNT")
+	for _, stack := range stacks {
+		if stack.Name == current {
+			stack.Name += "*"
+		}
+		fmt.Printf("%-20s %-48s %-12s\n", stack.Name, stack.LastDeploy, stack.ResourceCount)
+	}
 }
