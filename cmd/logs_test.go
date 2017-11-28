507f52fd83339d79ce9269c3bed3de83c6fd3e32
Luke Hoban <luke@pulumi.com>
2017-11-28 12:21:24 -0800

    Ensure tests are not time-zone sensitive.

--- cmd/logs_test.go
+++ cmd/logs_test.go
@@ -20,8 +20,8 @@ func TestParseSince(t *testing.T) {
 	fmt.Printf("Res: %v\n", b)
 
 	c, _ := parseSince("2006-01-02T15:04:05")
-	assert.Equal(t, "2006-01-02T15:04:05-08:00", c.Format(time.RFC3339))
+	assert.Equal(t, "Mon Jan  2 15:04:05 2006", c.Format(time.ANSIC))
 
 	d, _ := parseSince("2006-01-02")
-	assert.Equal(t, "2006-01-02T00:00:00-08:00", d.Format(time.RFC3339))
+	assert.Equal(t, "Mon Jan  2 00:00:00 2006", d.Format(time.ANSIC))
 }
