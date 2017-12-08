816e639ac6c0e117101b83a767185277f97683a8
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-12-08 12:29:06 -0800

    Revert "Edits needs to continuously pass along the new directory. (#668)"
    This reverts commit 8bd18227220c6bfc2ba8175c7adfb2903e610e23.

--- pkg/testing/integration/program.go
+++ pkg/testing/integration/program.go
@@ -343,17 +343,16 @@ func TestEdits(t *testing.T, opts *ProgramTestOptions, dir string) error {
 	for i, edit := range opts.EditDirs {
 		fmt.Fprintf(opts.Stdout, "Applying edit '%v' and rerunning preview and update\n", edit)
 
-		var err error
-		dir, err = prepareProject(t, opts, edit.Dir, dir, edit.Additive)
-		if !assert.NoError(t, err, "Expected to apply edit %v atop %v, but got an error %v", edit, dir, err) {
+		edir, err := prepareProject(t, opts, edit.Dir, dir, edit.Additive)
+		if !assert.NoError(t, err, "Expected to apply edit %v atop %v, but got an error %v", edit, edir, err) {
 			return err
 		}
 
-		if err = TestPreviewAndUpdate(t, opts, dir, fmt.Sprintf("edit%d", i)); err != nil {
+		if err = TestPreviewAndUpdate(t, opts, edir, fmt.Sprintf("edit%d", i)); err != nil {
 			return err
 		}
 
-		if err = PerformExtraRuntimeValidation(t, opts, edit.ExtraRuntimeValidation, dir); err != nil {
+		if err = PerformExtraRuntimeValidation(t, opts, edit.ExtraRuntimeValidation, edir); err != nil {
 			return err
 		}
 	}
