db4b50ee1d05b837581ebe9549b5c9b25065e3bd
Chris Smith <chris@pulumi.com>
2018-01-20 18:42:03 -0800

    Address PR feedback

--- pkg/backend/cloud/backend.go
+++ pkg/backend/cloud/backend.go
@@ -613,8 +613,6 @@ func Login(cloudURL string) error {
 // isValidAccessToken tries to use the provided Pulumi access token and returns if it is accepted
 // or not. Returns error on any unexpected error.
 func isValidAccessToken(cloud, accessToken string) (bool, error) {
-	errValidatingToken := errors.New("error validating access token. Is cloud URL correct?")
-
 	// Make a request to get the authenticated user. If it returns a successful response,
 	// we know the access token is legit. We also parse the response as JSON and confirm
 	// it has a name field that is non-empty (like the Pulumi Service would return).
@@ -625,12 +623,11 @@ func isValidAccessToken(cloud, accessToken string) (bool, error) {
 		if errResp, ok := err.(*apitype.ErrorResponse); ok && errResp.Code == 401 {
 			return false, nil
 		}
-		glog.V(1).Infof("Error response testing access token: %v", err)
-		return false, errValidatingToken
+		return false, errors.Wrapf(err, "getting user info from %v", cloud)
 	}
 
 	if respObj.Name == "" {
-		return false, errValidatingToken
+		return false, errors.New("unexpected response from cloud API")
 	}
 
 	return true, nil
