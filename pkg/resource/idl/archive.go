4b850dcb15ddb74a6c04ba9203275905e8952923
Matt Ellis <matt@pulumi.com>
2018-02-01 20:31:51 -0800

    Remove pkg/resource/idl
    A hold-over from a previous experiment (LumiIDL) which we don't use
    anymore. If we decide to bring that back, we can easily restore these
    types, but for now, let's just remove this dead code.

--- pkg/resource/idl/archive.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
-
-package idl
-
-// Archive is a marker struct to indicate that an IDL struct is an archive.
-type Archive struct {
-}
--- pkg/resource/idl/resource.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
-
-package idl
-
-// Resource is a marker struct to indicate that an IDL struct is a resource.
-type Resource struct {
-}
