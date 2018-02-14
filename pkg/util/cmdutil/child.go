618cec439b601ebc269fd49b864cba24f2d8dc68
Sean Gillespie <sean@pulumi.com>
2018-02-14 10:58:51 -0800

    Don't use shebangs to launch providers and correctly kill child process trees on Unix

--- pkg/util/cmdutil/child.go
+++ pkg/util/cmdutil/child.go
@@ -4,12 +4,22 @@
 package cmdutil
 
 import (
-	"github.com/pulumi/pulumi/pkg/util/contract"
+	"os/exec"
+	"syscall"
 )
 
 // KillChildren calls os.Process.Kill() on every child process of `pid`'s, stoping after the first error (if any). It
 // also only kills direct child process, not any children they may have.
 func KillChildren(pid int) error {
-	contract.Failf("KillChidren only implemented on windows")
-	return nil
+	// A subprocess that was launched after calling `RegisterProcessGroup` below will
+	// belong to a process group whose ID is the same as the PID. Passing the negation
+	// of our PID (same as the PGID) sends a SIGKILL to all processes in our group.
+	return syscall.Kill(-pid, syscall.SIGKILL)
+}
+
+// RegisterProcessGroup informs the OS that it needs to call `setpgid` on this
+// child process. When it comes time to kill this process, we'll kill all processes
+// in the same process group.
+func RegisterProcessGroup(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 }
