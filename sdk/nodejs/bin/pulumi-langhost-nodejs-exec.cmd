4b2441ac22080f93d6a011e5b0b7bffcac8e3563
Matt Ellis <matt@pulumi.com>
2018-02-14 16:05:13 -0800

    Use relative path in langhost launcher
    We no longer have a node_modules folder in the SDK (since all
    packages now come from NPM) so we need to adjust the shell script we
    use to launch our runner to use a relative path.

--- sdk/nodejs/bin/pulumi-langhost-nodejs-exec.cmd
+++ sdk/nodejs/bin/pulumi-langhost-nodejs-exec.cmd
@@ -1,7 +1,5 @@
 @echo off
-SET REQUIRE_ROOT=%~dp0
-SET REQUIRE_ROOT=%REQUIRE_ROOT:\=/%
 REM We depend on a custom node build that has exposed some internal state
 REM This node is downloaded and extracted via the EnsureCustomNode target
 REM in the root build.proj
-"%~dp0..\custom_node\node\node.exe" "%REQUIRE_ROOT%./cmd/run" %*
+"%~dp0..\custom_node\node\node.exe" "./node_modules/@pulumi/pulumi/cmd/run" %*
