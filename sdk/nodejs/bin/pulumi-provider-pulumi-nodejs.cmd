2d0ca1992e0dfd700a4fc3e1c66f45cecec74376
Matt Ellis <matt@pulumi.com>
2018-02-15 16:17:47 -0800

    Cleanup provider launch scripts and fix some windows build oddities
    The windows build was still on the old plan from way back when where
    we had binaries littered in the build tree and you had to add parts of
    your build-tree to the `%PATH%` for the integration tests to work.
    
    This cleans that up and moves all of our scripts that invoke
    javascript to be on the same plan. They invoke our specially named
    node with a relative path to the JS code we want to run.

--- sdk/nodejs/bin/pulumi-provider-pulumi-nodejs.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-SET REQUIRE_ROOT=%~dp0
-SET REQUIRE_ROOT=%REQUIRE_ROOT:\=/%
-REM We depend on a custom node build that has exposed some internal state
-REM This node is downloaded and extracted via the EnsureCustomNode target
-REM in the root build.proj
-"%~dp0..\custom_node\node\node.exe" "%REQUIRE_ROOT%./cmd/dynamic-provider" %*
