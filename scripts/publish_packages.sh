4f822b19fd4a20bade883a111f674e79b8c4be7a
joeduffy <joe@pulumi.com>
2018-03-07 11:57:38 -0800

    Publish to production PyPI

--- scripts/publish_packages.sh
+++ scripts/publish_packages.sh
@@ -12,7 +12,7 @@ if [[ "${TRAVIS_OS_NAME:-}" == "linux" ]]; then
 
     echo "Publishing Pip package to pulumi.com:"
     twine upload \
-        --repository-url https://pypi-dot-testing.moolumi.io?token=${PULUMI_API_TOKEN} \
+        --repository-url https://pypi.pulumi.com?token=${PULUMI_API_TOKEN} \
         -u pulumi -p pulumi \
         ${ROOT}/sdk/python/bin/dist/*.whl
 fi
