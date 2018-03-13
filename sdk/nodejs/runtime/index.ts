304af9cdf192f484ffd7cc29d980524172009093
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2018-03-12 18:12:49 -0700

    Split closure serialization into separate files containing the different concerns. (#1045)
    The four concerns are:
    
        parsing a v8 function string so we can figure out captured variables.
        walkgin the function/object graph producing the graph we will serialize.
        rewriting constructors and methods so that 'super' works.
        serializing graph to text.

--- sdk/nodejs/runtime/index.ts
+++ sdk/nodejs/runtime/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
 
-export * from "./closure";
+export { serializeFunctionAsync }  from "./closure/serializeClosure";
 export * from "./config";
 export * from "./invoke";
 export * from "./resource";
