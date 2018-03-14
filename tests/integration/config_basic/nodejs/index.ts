cca831b8449f7342ce4f77929e13ec8ef1caecaf
joeduffy <joe@pulumi.com>
2018-03-14 11:36:06 -0700

    Add basic config tests
    This adds a basic config test, for both Node.js and Python runtimes,
    that simply reads back and checks configuration and secret values.

---
+++ tests/integration/config_basic/nodejs/index.ts
@@ -0,0 +1,15 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+import * as assert from "assert";
+import { Config } from "@pulumi/pulumi";
+
+// Just test that basic config works.
+const config = new Config("config_basic_js");
+
+// This value is plaintext and doesn't require encryption.
+const value = config.require("aConfigValue");
+assert.equal(value, "this value is a value", "aConfigValue not the expected value");
+
+// This value is a secret and is encrypted using the passphrase `supersecret`.
+const secret = config.require("bEncryptedSecret");
+assert.equal(secret, "this super secret is encrypted", "bEncryptedSecret not the expected value");
---
+++ tests/integration/config_basic/python/__main__.py
@@ -0,0 +1,14 @@
+# Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+import pulumi
+
+# Just test that basic config works.
+config = pulumi.Config('config_basic_py')
+
+# This value is plaintext and doesn't require encryption.
+value = config.require('aConfigValue')
+assert value == 'this value is a Pythonic value'
+
+# This value is a secret and is encrypted using the passphrase `supersecret`.
+secret = config.require('bEncryptedSecret')
+assert secret == 'this super Pythonic secret is encrypted'
