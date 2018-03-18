d719e7966e58c65ab2b1ca7574f3cc9cebca4260
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2018-03-18 00:15:22 -0700

    Only avoid running transformations on outputs we truly do not have values for. (#921)

--- sdk/nodejs/runtime/closure/createClosure.ts
+++ sdk/nodejs/runtime/closure/createClosure.ts
@@ -180,6 +180,7 @@ interface ContextFrame {
 // transform during deployment, to something you call .get on to get the raw underlying value from
 // inside a cloud callback.
 class SerializedOutput<T> implements resource.Output<T> {
+    /* @internal */ public performApply: Promise<boolean>;
     /* @internal */ public readonly promise: () => Promise<T>;
     /* @internal */ public readonly resources: () => Set<resource.Resource>;
 
