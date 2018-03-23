063c9ccd05258ddb8b5fa4f9fa19f213d933d1ec
Cyrus Najmabadi <cyrus@pulumi.com>
2018-03-23 16:23:36 -0700

    Working live update.

--- cmd/pulumi.go
+++ cmd/pulumi.go
@@ -90,6 +90,7 @@ func NewPulumiCmd() *cobra.Command {
 	cmd.AddCommand(newPreviewCmd())
 	cmd.AddCommand(newStackCmd())
 	cmd.AddCommand(newUpdateCmd())
+	cmd.AddCommand(newUpdate2Cmd())
 	cmd.AddCommand(newVersionCmd())
 
 	// Commands specific to the Pulumi Cloud Management Console.
---
+++ cmd/update2.go
@@ -0,0 +1,231 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+	//	"time"
+	// "github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/progress"
+	"github.com/docker/docker/pkg/streamformatter"
+	"github.com/docker/docker/pkg/term"
+	// "github.com/pulumi/pulumi/pkg/backend"
+	// "github.com/pulumi/pulumi/pkg/engine"
+	// "github.com/pulumi/pulumi/pkg/tokens"
+	"github.com/pulumi/pulumi/pkg/util/cmdutil"
+)
+
+// copied from: https://github.com/docker/cli/blob/master/cli/command/out.go
+// replace with usage of that library when we can figure out hte right version story
+
+type commonStream struct {
+	fd         uintptr
+	isTerminal bool
+	state      *term.State
+}
+
+// FD returns the file descriptor number for this stream
+func (s *commonStream) FD() uintptr {
+	return s.fd
+}
+
+// IsTerminal returns true if this stream is connected to a terminal
+func (s *commonStream) IsTerminal() bool {
+	return s.isTerminal
+}
+
+// RestoreTerminal restores normal mode to the terminal
+func (s *commonStream) RestoreTerminal() {
+	if s.state != nil {
+		term.RestoreTerminal(s.fd, s.state)
+	}
+}
+
+// SetIsTerminal sets the boolean used for isTerminal
+func (s *commonStream) SetIsTerminal(isTerminal bool) {
+	s.isTerminal = isTerminal
+}
+
+type outStream struct {
+	commonStream
+	out io.Writer
+}
+
+func (o *outStream) Write(p []byte) (int, error) {
+	return o.out.Write(p)
+}
+
+// SetRawTerminal sets raw mode on the input terminal
+func (o *outStream) SetRawTerminal() (err error) {
+	if os.Getenv("NORAW") != "" || !o.commonStream.isTerminal {
+		return nil
+	}
+	o.commonStream.state, err = term.SetRawTerminalOutput(o.commonStream.fd)
+	return err
+}
+
+// GetTtySize returns the height and width in characters of the tty
+func (o *outStream) GetTtySize() (uint, uint) {
+	if !o.isTerminal {
+		return 0, 0
+	}
+	ws, err := term.GetWinsize(o.fd)
+	if err != nil {
+		if ws == nil {
+			return 0, 0
+		}
+	}
+	return uint(ws.Height), uint(ws.Width)
+}
+
+// NewOutStream returns a new OutStream object from a Writer
+func newOutStream(out io.Writer) *outStream {
+	fd, isTerminal := term.GetFdInfo(out)
+	return &outStream{commonStream: commonStream{fd: fd, isTerminal: isTerminal}, out: out}
+}
+
+func writeDistributionProgress(outStream io.Writer, progressChan <-chan progress.Progress) {
+	progressOutput := streamformatter.NewJSONStreamFormatter().NewProgressOutput(outStream, false)
+
+	for prog := range progressChan {
+		// fmt.Printf("Received progress")
+		progressOutput.WriteProgress(prog)
+	}
+}
+
+// end of copy
+
+// func newOutStream(out io.Writer) *OutStream {
+// 	fd, isTerminal := term.GetFdInfo(out)
+// 	return &OutStream{CommonStream: CommonStream{fd: fd, isTerminal: isTerminal}, out: out}
+// }
+
+func newUpdate2Cmd() *cobra.Command {
+	var debug bool
+	var stack string
+
+	var message string
+
+	// Flags for engine.UpdateOptions.
+	var analyzers []string
+	var color colorFlag
+	var parallel int
+	var preview bool
+	var showConfig bool
+	var showReplacementSteps bool
+	var showSames bool
+	var summary bool
+
+	var cmd = &cobra.Command{
+		Use:     "update2",
+		Aliases: []string{"up2"},
+		Short:   "New update",
+		Long:    "New update",
+		Args:    cmdutil.NoArgs,
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+
+			_, stdout, _ := term.StdStreams()
+
+			pipeReader, pipeWriter := io.Pipe()
+			progressChan := make(chan progress.Progress, 100)
+			writesDone := make(chan struct{})
+
+			chanOutput := progress.ChanOutput(progressChan)
+			// ctx, cancelFunc := context.WithCancel(ctx)
+
+			go func() {
+				writeDistributionProgress(pipeWriter, progressChan)
+				fmt.Printf("Done writing distribution.  closing")
+				close(writesDone)
+			}()
+
+			go func() {
+				for {
+					id := strconv.Itoa(rand.Int() % 5)
+					r := rand.Int()
+					action := fmt.Sprintf("Updating... %v", r)
+					chanOutput.WriteProgress(progress.Progress{
+						ID:     id,
+						Action: action,
+					})
+					// fmt.Printf("Done writing message")
+					time.Sleep(10 * time.Millisecond)
+				}
+			}()
+
+			return jsonmessage.DisplayJSONMessagesToStream(pipeReader, newOutStream(stdout), nil)
+			// s, err := requireStack(tokens.QName(stack), true)
+			// if err != nil {
+			// 	return err
+			// }
+			// proj, root, err := readProject()
+			// if err != nil {
+			// 	return err
+			// }
+
+			// m, err := getUpdateMetadata(message, root)
+			// if err != nil {
+			// 	return errors.Wrap(err, "gathering environment metadata")
+			// }
+
+			// return s.Update(proj, root, debug, m, engine.UpdateOptions{
+			// 	Analyzers: analyzers,
+			// 	DryRun:    preview,
+			// 	Parallel:  parallel,
+			// 	Debug:     debug,
+			// }, backend.DisplayOptions{
+			// 	Color:                color.Colorization(),
+			// 	ShowConfig:           showConfig,
+			// 	ShowReplacementSteps: showReplacementSteps,
+			// 	ShowSames:            showSames,
+			// 	Summary:              summary,
+			// })
+		}),
+	}
+
+	cmd.PersistentFlags().BoolVarP(
+		&debug, "debug", "d", false,
+		"Print detailed debugging output during resource operations")
+	cmd.PersistentFlags().StringVarP(
+		&stack, "stack", "s", "",
+		"Choose a stack other than the currently selected one")
+
+	cmd.PersistentFlags().StringVarP(
+		&message, "message", "m", "",
+		"Optional message to associate with the update operation")
+
+	// Flags for engine.UpdateOptions.
+	cmd.PersistentFlags().VarP(
+		&color, "color", "c", "Colorize output. Choices are: always, never, raw, auto")
+	cmd.PersistentFlags().StringSliceVar(
+		&analyzers, "analyzer", []string{},
+		"Run one or more analyzers as part of this update")
+	cmd.PersistentFlags().IntVarP(
+		&parallel, "parallel", "p", 0,
+		"Allow P resource operations to run in parallel at once (<=1 for no parallelism)")
+	cmd.PersistentFlags().BoolVarP(
+		&preview, "preview", "n", false,
+		"Don't create/delete resources; just preview the planned operations")
+	cmd.PersistentFlags().BoolVar(
+		&showConfig, "show-config", false,
+		"Show configuration keys and variables")
+	cmd.PersistentFlags().BoolVar(
+		&showReplacementSteps, "show-replacement-steps", false,
+		"Show detailed resource replacement creates and deletes instead of a single step")
+	cmd.PersistentFlags().BoolVar(
+		&showSames, "show-sames", false,
+		"Show resources that needn't be updated because they haven't changed, alongside those that do")
+	cmd.PersistentFlags().BoolVar(
+		&summary, "summary", false,
+		"Only display summarization of resources and operations")
+
+	return cmd
+}
