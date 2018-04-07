5673cda88450610d603ee80d0f7bfcff2590e67f
Cyrus Najmabadi <cyrus@pulumi.com>
2018-04-07 01:25:12 -0700

    Break out helpers into separate file.

--- pkg/backend/local/display.go
+++ pkg/backend/local/display.go
@@ -6,7 +6,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"os"
 	"regexp"
 	"sort"
 	"strconv"
@@ -14,7 +13,6 @@ import (
 
 	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/pkg/progress"
-	"github.com/docker/docker/pkg/streamformatter"
 	"github.com/docker/docker/pkg/term"
 
 	"github.com/pulumi/pulumi/pkg/backend"
@@ -27,84 +25,6 @@ import (
 	"github.com/pulumi/pulumi/pkg/util/cmdutil"
 	"github.com/pulumi/pulumi/pkg/util/contract"
 )
-
-// copied from: https://github.com/docker/cli/blob/master/cli/command/out.go
-// replace with usage of that library when we can figure out hte right version story
-
-type commonStream struct {
-	fd         uintptr
-	isTerminal bool
-	state      *term.State
-}
-
-// FD returns the file descriptor number for this stream
-func (s *commonStream) FD() uintptr {
-	return s.fd
-}
-
-// IsTerminal returns true if this stream is connected to a terminal
-func (s *commonStream) IsTerminal() bool {
-	return s.isTerminal
-}
-
-// RestoreTerminal restores normal mode to the terminal
-func (s *commonStream) RestoreTerminal() {
-	if s.state != nil {
-		err := term.RestoreTerminal(s.fd, s.state)
-		contract.IgnoreError(err)
-	}
-}
-
-// SetIsTerminal sets the boolean used for isTerminal
-func (s *commonStream) SetIsTerminal(isTerminal bool) {
-	s.isTerminal = isTerminal
-}
-
-type outStream struct {
-	commonStream
-	out io.Writer
-}
-
-func (o *outStream) Write(p []byte) (int, error) {
-	return o.out.Write(p)
-}
-
-// SetRawTerminal sets raw mode on the input terminal
-func (o *outStream) SetRawTerminal() (err error) {
-	if os.Getenv("NORAW") != "" || !o.commonStream.isTerminal {
-		return nil
-	}
-	o.commonStream.state, err = term.SetRawTerminalOutput(o.commonStream.fd)
-	return err
-}
-
-// GetTtySize returns the height and width in characters of the tty
-func (o *outStream) GetTtySize() (uint, uint) {
-	if !o.isTerminal {
-		return 0, 0
-	}
-	ws, err := term.GetWinsize(o.fd)
-	if err != nil {
-		if ws == nil {
-			return 0, 0
-		}
-	}
-	return uint(ws.Height), uint(ws.Width)
-}
-
-// NewOutStream returns a new OutStream object from a Writer
-func newOutStream(out io.Writer) *outStream {
-	fd, isTerminal := term.GetFdInfo(out)
-	return &outStream{commonStream: commonStream{fd: fd, isTerminal: isTerminal}, out: out}
-}
-
-func writeDistributionProgress(outStream io.Writer, progressChan <-chan progress.Progress) {
-	progressOutput := streamformatter.NewJSONStreamFormatter().NewProgressOutput(outStream, false)
-
-	for prog := range progressChan {
-		writeProgress(progressOutput, prog)
-	}
-}
 
 type Status struct {
 	ID   string
