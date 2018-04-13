e0cb470d3b043d1d488b9d0f7fa2851b41dcdb3b
Cyrus Najmabadi <cyrus@pulumi.com>
2018-04-13 12:09:28 -0700

    Remove comments.

--- pkg/engine/plan.go
+++ pkg/engine/plan.go
@@ -219,12 +219,6 @@ func printPlan(result *planResult) (ResourceChanges, error) {
 		return nil, errors.New("an error occurred while advancing the preview")
 	}
 
-	// if !result.Options.Diag.Success() {
-	// 	// If any error occurred while walking the plan, be sure to let the developer know.  Otherwise,
-	// 	// although error messages may have spewed to the output, the final lines of the plan may look fine.
-	// 	return nil, errors.New("one or more errors occurred during this preview")
-	// }
-
 	// Emit an event with a summary of operation counts.
 	changes := ResourceChanges(actions.Ops)
 	result.Options.Events.previewSummaryEvent(changes)
