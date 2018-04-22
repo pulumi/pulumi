d04b68c193980276c4f95c4e66340e69e70a8f0e
Cyrus Najmabadi <cyrus@pulumi.com>
2018-04-21 18:03:51 -0700

    Actual ascii grid.

--- pkg/backend/local/progress.go
+++ pkg/backend/local/progress.go
@@ -442,29 +442,43 @@ func (display *ProgressDisplay) generateTreeNodes() []*treeNode {
 	return result
 }
 
-func (display *ProgressDisplay) addIndentations(treeNodes []*treeNode, isRoot bool, indentation string) {
-	prefix := indentation
-	if !isRoot {
-		prefix += " `--"
-	}
+/*
+http://www.randygaul.net/2015/06/15/printing-pretty-ascii-trees/
 
-	childIndentation := indentation + " |  "
+
+0 1 2 3 4 5 6 7 8 9 A B C D E F
+U+250x ─ ━ │ ┃ ┄ ┅ ┆ ┇ ┈ ┉ ┊ ┋ ┌ ┍ ┎ ┏
+U+251x ┐ ┑ ┒ ┓ └ ┕ ┖ ┗ ┘ ┙ ┚ ┛ ├ ┝ ┞ ┟
+U+252x ┠ ┡ ┢ ┣ ┤ ┥ ┦ ┧ ┨ ┩ ┪ ┫ ┬ ┭ ┮ ┯
+U+253x ┰ ┱ ┲ ┳ ┴ ┵ ┶ ┷ ┸ ┹ ┺ ┻ ┼ ┽ ┾ ┿
+U+254x ╀ ╁ ╂ ╃ ╄ ╅ ╆ ╇ ╈ ╉ ╊ ╋ ╌ ╍ ╎ ╏
+U+255x ═ ║ ╒ ╓ ╔ ╕ ╖ ╗ ╘ ╙ ╚ ╛ ╜ ╝ ╞ ╟
+U+256x ╠ ╡ ╢ ╣ ╤ ╥ ╦ ╧ ╨ ╩ ╪ ╫ ╬ ╭ ╮ ╯
+U+257x ╰ ╱ ╲ ╳ ╴ ╵ ╶ ╷ ╸ ╹ ╺ ╻ ╼ ╽ ╾ ╿
+*/
+
+func (display *ProgressDisplay) addIndentations(treeNodes []*treeNode, isRoot bool, indentation string) {
+
+	childIndentation := indentation + " │  "
 	lastChildIndentation := indentation + "    "
 
 	for i, node := range treeNodes {
-
-		node.colorizedColumns[typeColumn] = prefix + node.colorizedColumns[typeColumn]
-
 		isLast := i == len(treeNodes)-1
+
+		prefix := indentation
+
 		var nestedIndentation string
 		if !isRoot {
 			if isLast {
+				prefix += " └─ "
 				nestedIndentation = lastChildIndentation
 			} else {
+				prefix += " ├─ "
 				nestedIndentation = childIndentation
 			}
 		}
 
+		node.colorizedColumns[typeColumn] = prefix + node.colorizedColumns[typeColumn]
 		display.addIndentations(node.childNodes, false /*isRoot*/, nestedIndentation)
 	}
 }
@@ -524,7 +538,7 @@ func (display *ProgressDisplay) refreshAllRowsIfInTerminal() {
 		rootNodes := display.generateTreeNodes()
 		rootNodes = filterOutUnnecessaryNodes(rootNodes)
 		sortNodes(rootNodes)
-		display.addIndentations(rootNodes, true, "")
+		display.addIndentations(rootNodes, true /*isRoot*/, "")
 
 		var rows [][]string
 		display.addColorizedColumnsFromTreeNodes(rootNodes, &rows)
