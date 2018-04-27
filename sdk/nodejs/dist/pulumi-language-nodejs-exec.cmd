5decc10cbb58cb71f7606059ccfdae1bc534cff9
Sean Gillespie <sean@pulumi.com>
2018-04-27 16:55:41 -0700

    Improve the error message if Pulumi runtime SDK isn't installed (#1286)
    * Improve the error message when npm/yarn install hasn't been run
    
    * Same thing, but for Python
    
    * Use PULUMI_RUN in batch script
    
    * Use -e, -f doesn't work for symlinked paths (e.g. yarn link)

--- sdk/nodejs/dist/pulumi-language-nodejs-exec.cmd
+++ sdk/nodejs/dist/pulumi-language-nodejs-exec.cmd
@@ -1,2 +1,10 @@
-@set NODE_PATH=%NODE_PATH%;%~dp0\v6.10.2
-@node ./node_modules/@pulumi/pulumi/cmd/run %*
+@echo off
+
+set NODE_PATH=%NODE_PATH%;%~dp0\v6.10.2
+set PULUMI_RUN=./node_modules/@pulumi/pulumi/cmd/run
+if not exist %PULUMI_RUN% (
+    echo It looks like the Pulumi SDK has not been installed. Have you run npm install or yarn install?
+    exit /b 1
+)
+
+node %PULUMI_RUN% %*
