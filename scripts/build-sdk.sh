213c9c3c080ca01365581591a8f274511cd0e054
Matt Ellis <matt@pulumi.com>
2018-04-30 20:44:01 -0700

    Use newer versions of the build-sdk.sh scripts

--- scripts/build-sdk.sh
+++ scripts/build-sdk.sh
@@ -1,144 +1,104 @@
 #!/bin/bash
-# Usage CreatePkg.sh [version-tag] [ref-name]
-# CreatePkg.sh will package the latest packages from S3 release bucket based on the git commit associated with ref-name
-# for main components  namely pulumi, pulumi-aws, pulumi-azure, pulumi-kubernetes, pulumi-cloud and then package them together along with install.sh to
-# create a distributable SDK package.
+# Usage build-sdk.sh [version-tag] [pulumi-cloud-ref-name]
 #
 # version-tag defaults to current date and time
 # ref-name defaults to master (can be a branch or tag name)
 set -o nounset -o errexit -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# This function does npm install of the extracted packages
-# usage npm_install path_to_module
-function npm_install()
+S3_PROD_BUCKET_ROOT="s3://rel.pulumi.com/releases/"
+S3_ENG_BUCKET_ROOT="s3://eng.pulumi.com/releases/"
+S3_PUBLISH_FOLDER_SDK="${S3_PROD_BUCKET_ROOT}sdk/"
+
+# This function downloads a specific release and into the current working directory
+# usage: download_release <repo-name> <commitish>
+function download_release()
 {
-    echo Installing Node modules from ${1}
-    ( cd ${1} ; npm install --only=production )
-}
+    local repo_name=${1}
+    local repo_commit=${2}
 
-# This function does the following
-# Usage process_package <component_name> <s3_bucket_package_folder>
-# 1. Gets the last commit for the component identified by the repo
-# 2. Get the tgz package corresponding to the commit from the S3 bucket
-# 3. Extracts the package locally 
-# 4. Deletes the package after successful extraction
+    echo "downloading ${repo_name}@${repo_commit}"
 
-function process_package()
-{
-    echo Getting Packages for repo: ${1}
-    commit=$(git ls-remote -h -t git@github.com:pulumi/$1.git ${ref_name} | cut -f1)
-
-    # check for empty commit hash
-    if [ -z "${commit}" ]; then 
-        echo Failed to get last commit hash for ${1}
-        exit 1
-    fi
-
-    file=${commit}.tgz
-    s3_file=${s3_bucket_root}${1}/${OS}/${platform}/${file}
+    local file=${repo_commit}.tgz
+    local s3_file=${S3_ENG_BUCKET_ROOT}${repo_name}/${OS}/amd64/${file}
 
     # Use AWS CLI to download the package corresponding to the component from S3 bucket
-    if aws s3 cp --only-show-errors "${s3_file}" './'"${file}" 2>/dev/null; then
-        echo Successfully downloaded: ${s3_file}
-    else
-        echo Failed to get package: ${s3_file} from AWS S3
+    if ! aws s3 cp --only-show-errors "${s3_file}" "./${file}" 2>/dev/null; then
+        >&2 echo "failed to download ${s3_file}"
         exit 1
-    fi
-
-    if tar -xzf "${file}" 2>/dev/null; then
-        echo Successfully unzipped file:${file}. Now deleting
-        rm -f ${file}
-    else
-        echo Failed to unzip package: ${file}
-        exit 1   
     fi
 }
 
-# Main script processing the workload. Following steps are performed
-# 1. Get the OS to find out if we should build for Darwin(Mac) or Linux
-# 2. Check if a Version Input is passed. If not, get current date/time to create a version stamp
-# 3. Check if a tag or head name was passed, otherwise use master
-# 4. Verify dependencies such as npm, tar and aws cli are installed
-# 5. Process the generated packages for each component (pulumi, pulumi-aws, pulumi-cloud) based on the last 
-#    commit sha from master branch and extract them to the temporary folder
-# 6. run npm for each component
-# 7. Build the tar file and upload to S3 bucket
+# This function downloads and extracts a specific release and into the current working directory
+# usage: download_and_extract_release <repo-name> <commitish>
+function download_and_extract_release()
+{
+    local repo_name=${1}
+    local repo_commit=${2}
 
-platform=amd64
-s3_bucket_root="s3://eng.pulumi.com/releases/"
-s3_publish_folder="${s3_bucket_root}sdk/"
-components=("pulumi" "pulumi-aws" "pulumi-azure" "pulumi-kubernetes" "pulumi-cloud")
-dependencies=("npm" "tar" "aws")
+    local file=${repo_commit}.tgz
 
-# Get the OS version
+    download_release "${repo_name}" "${repo_commit}"
+
+    if ! tar -xzf "${file}" 2>/dev/null; then
+        >&2 echo "failed to untar ${file}"
+        exit 1
+    fi
+
+    rm "./${file}"
+}
+
+# get the OS version
 OS=""
 case $(uname) in
-    "Linux") OS="linux";;
-    "Darwin") OS="darwin";;
+    "Linux") OS=linux;;
+    "Darwin") OS=darwin;;
     *) echo "error: unknown host os $(uname)" ; exit 1;;
 esac
 
-sdkfilename=pulumi-${1:-$(date +"%Y%m%d_%H%M%S")}-${OS}.x64.tar.gz
-ref_name="${2:-master}"
-
-echo Upon completion package: ${sdkfilename} will be uploaded to S3 location: ${s3_publish_folder}
-
-# Verify the required dependencies are already installed
-for dependency in ${dependencies[@]}
-do
-    if ! command -v ${dependency} >/dev/null; then
-        echo "required dependency '${dependency}' is not installed"
-        exit 1
-    fi
-done
+SDK_FILENAME=pulumi-${1:-$(date +"%Y%m%d_%H%M%S")}-${OS}.x64.tar.gz
+PULUMI_REF=${2:-master}
 
 # setup temporary folder to process the package
-pulumi_folder=$(mktemp -d)/pulumi
-mkdir -p ${pulumi_folder}
+PULUMI_FOLDER=$(mktemp -d)/pulumi
+mkdir -p "${PULUMI_FOLDER}"
 
-echo ${pulumi_folder}
-cd ${pulumi_folder}
+cd "${PULUMI_FOLDER}"
 
-cp ${SCRIPT_DIR}/../dist/install.sh .
+cp "${SCRIPT_DIR}/../dist/install.sh" .
 
-# Process each component from the components list
-for component in ${components[@]}
-do
-    echo Processing Component: $component
-    process_package $component
-done
+download_and_extract_release pulumi "${PULUMI_REF}"
 
-echo npm install pulumi and its components
-if [ -d "${pulumi_folder}/node_modules/pulumi" ]; then
-    npm_install "${pulumi_folder}/node_modules/pulumi"
-fi
+# All node packages are now delivered via npm, so remove the node_modules folder.
+rm -rf "${PULUMI_FOLDER}/node_modules"
 
-if [ -d "${pulumi_folder}/node_modules/@pulumi" ]; then
-    for packageDir in "${pulumi_folder}/node_modules/@pulumi"/*; do
-        npm_install "$packageDir"
-    done
-fi
+SDK_PACKAGE_PATH=$(mktemp)
 
-echo zip contents of the folder in a package
-sdk_package_path=$(mktemp)
+echo "compressing package to ${SDK_PACKAGE_PATH}"
 
 cd ..
 
-if tar -zcf ${sdk_package_path} pulumi; then
-    echo Successfully created the package: ${sdk_package_path}
-else
-    echo Failed to zip the contents of the folder to file: ${sdk_package_path}
+if ! tar -zcf ${SDK_PACKAGE_PATH} pulumi; then
+    >&2 echo "failed to compress package"
     exit 1
 fi
 
-echo Upload ${sdk_package_path} to aws s3 bucket ${s3_publish_folder}${sdkfilename}
-aws s3 cp --only-show-errors ${sdk_package_path} ${s3_publish_folder}${sdkfilename}
+echo "uploading SDK to ${S3_PUBLISH_FOLDER_SDK}${SDK_FILENAME}"
+echo "${SDK_PACKAGE_PATH}"
+# rel.pulumi.com is in our production account, so assume that role first
+CREDS_JSON=$(aws sts assume-role \
+                 --role-arn "arn:aws:iam::058607598222:role/UploadPulumiReleases" \
+                 --role-session-name "upload-sdk" \
+                 --external-id "upload-pulumi-release")
 
-echo Successfully created: ${s3_publish_folder}${sdkfilename}
+# Use these new credentials to create the PPC user account.
+export AWS_ACCESS_KEY_ID=$(echo ${CREDS_JSON}     | jq ".Credentials.AccessKeyId" --raw-output)
+export AWS_SECRET_ACCESS_KEY=$(echo ${CREDS_JSON} | jq ".Credentials.SecretAccessKey" --raw-output)
+export AWS_SECURITY_TOKEN=$(echo ${CREDS_JSON}    | jq ".Credentials.SessionToken" --raw-output)
 
-echo Deleting Temporary folder
-rm ${sdk_package_path}
-rm -rf  ${pulumi_folder}/
+aws s3 cp --only-show-errors "${SDK_PACKAGE_PATH}" "${S3_PUBLISH_FOLDER_SDK}${SDK_FILENAME}"
 
-echo Success.
+rm "${SDK_PACKAGE_PATH}"
+rm -rf "${PULUMI_FOLDER}/"
+
+echo "done"
