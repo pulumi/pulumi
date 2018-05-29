46f83f07957bae142047c4b02e520ffcf9742c3d
Sean Gillespie <sean@pulumi.com>
2018-05-29 10:58:24 -0700

    Provide copy-pasteable output on CLI panics (#1431)

--- main.go
+++ main.go
@@ -5,15 +5,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"runtime/debug"
 
 	"github.com/pulumi/pulumi/cmd"
 	"github.com/pulumi/pulumi/pkg/util/contract"
+	"github.com/pulumi/pulumi/pkg/version"
 )
 
+func panicHandler() {
+	if panicPayload := recover(); panicPayload != nil {
+		stack := string(debug.Stack())
+		fmt.Fprintln(os.Stderr, "================================================================================")
+		fmt.Fprintln(os.Stderr, "The Pulumi CLI encountered a fatal error. This is a bug!")
+		fmt.Fprintln(os.Stderr, "We would appreciate a report: https://github.com/pulumi/pulumi/issues/")
+		fmt.Fprintln(os.Stderr, "Please provide all of the below text in your report.")
+		fmt.Fprintln(os.Stderr, "================================================================================")
+		fmt.Fprintf(os.Stderr, "Pulumi Version:   %s\n", version.Version)
+		fmt.Fprintf(os.Stderr, "Go Version:       %s\n", runtime.Version())
+		fmt.Fprintf(os.Stderr, "Go Compiler:      %s\n", runtime.Compiler)
+		fmt.Fprintf(os.Stderr, "Architecture:     %s\n", runtime.GOARCH)
+		fmt.Fprintf(os.Stderr, "Operating System: %s\n", runtime.GOOS)
+		fmt.Fprintf(os.Stderr, "Panic:            %s\n\n", panicPayload)
+		fmt.Fprintln(os.Stderr, stack)
+		os.Exit(1)
+	}
+}
+
 func main() {
+	defer panicHandler()
 	if err := cmd.NewPulumiCmd().Execute(); err != nil {
 		_, err = fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
 		contract.IgnoreError(err)
-		os.Exit(-1)
+		os.Exit(1)
 	}
 }
