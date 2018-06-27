279282f1e1314f72aac020fd270a64399c206dc2
Sean Gillespie <sean@pulumi.com>
2018-06-27 15:39:39 -0700

    Implementation of Assets for Python

--- sdk/python/lib/pulumi/runtime/rpc.py
+++ sdk/python/lib/pulumi/runtime/rpc.py
@@ -22,12 +22,16 @@ from six.moves import map
 
 from google.protobuf import struct_pb2
 from .unknown import Unknown
+from . import known_types
 
 UNKNOWN = "04da6b54-80e4-46f7-96ec-b56ff0331ba9"
 """If a value is None, we serialize as UNKNOWN, which tells the engine that it may be computed later."""
 
-_custom_resource_type = None
-"""The type of CustomResource. Filled-in as the Pulumi package is initializing."""
+_special_sig_key = "4dabf18193072939515e22adb298388d"
+"""specialSigKey is sometimes used to encode type identity inside of a map.  See pkg/resource/properties.go."""
+
+_special_asset_sig = "c44067f5952c0a294b673a41bacd8c17"
+"""specialAssetSig is a randomly assigned hash used to identify assets in maps.  See pkg/resource/asset.go."""
 
 def serialize_resource_props(props):
     """
@@ -44,7 +48,7 @@ def serialize_resource_value(value):
     """
 
     assert _custom_resource_type is not None, "failed to set CustomResource type"
-    if isinstance(value, _custom_resource_type):
+    if known_types.is_custom_resource(value):
         # Resource objects aren't serializable.  Instead, serialize them as references to their IDs.
         return serialize_resource_value(value.id)
     elif isinstance(value, dict):
@@ -56,6 +60,23 @@ def serialize_resource_value(value):
     elif isinstance(value, Unknown):
         # Serialize instances of Unknown as the UNKNOWN guid
         return UNKNOWN
+    elif known_types.is_asset(value):
+        # Serializing an asset or archive requires the use of a magical signature key, since otherwise it would look
+        # like any old weakly typed object/map when received by the other side of the RPC boundary.
+        obj = {
+            _special_sig_key: _special_asset_sig
+        }
+
+        if hasattr(value, "path"):
+            obj["path"] = value.path
+        elif hasattr(value, "text"):
+            obj["text"] = value.text
+        elif hasattr(value, "uri"):
+            obj["uri"] = value.uri
+        else:
+            raise AssertionError("unknown asset type: " + str(value))
+
+        return obj
     else:
         # All other values are directly serializable.
         # TODO[pulumi/pulumi#1063]: eventually, we want to think about Output, Properties, and so on.
@@ -72,6 +93,20 @@ def deserialize_resource_props(props_struct):
     # We assume that we are deserializing properties that we got from a Resource RPC endpoint,
     # which has type `Struct` in our gRPC proto definition.
     assert isinstance(props_struct, struct_pb2.Struct)
+
+    if _special_sig_key in props_struct:
+        if props_struct[_special_sig_key] == _special_asset_sig:
+            # This is an asset. Re-hydrate this object into an Asset.
+            if "path" in props_struct:
+                return known_types.new_file_asset(props_struct["path"])
+            if "text" in props_struct:
+                return known_types.new_string_asset(props_struct["text"])
+            if "uri" in props_struct:
+                return known_types.new_remote_asset(props_struct["uri"])
+            raise AssertionError("Invalid asset encountered when unmarshaling resource property")
+
+        raise AssertionError("Unrecognized signature when unmarshaling resource property")
+
 
     # Struct is duck-typed like a dictionary, so we can iterate over it in the normal ways.
     return {k: deserialize_property(v) for (k, v) in list(props_struct.items())}
@@ -95,12 +130,3 @@ def deserialize_property(prop):
 
     # Everything else is identity projected.
     return prop
-
-def register_custom_resource_type(class_obj):
-    """
-    Registers the given class object as the CustomResource type,
-    for use in serialization.
-    """
-    assert isinstance(class_obj, six.class_types), "class_obj is not a Class"
-    global _custom_resource_type
-    _custom_resource_type = class_obj
