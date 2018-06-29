5d1a3db4aaebb81bc4c3d12a6acd4ef8758620f4
Sean Gillespie <sean@pulumi.com>
2018-06-29 14:08:58 -0700

    Python Language Host Tests (#1577)
    * Test the Python language host end-to-end
    
    This commit introduces an end-to-end language host testing framework for
    the Python SDK, similar to what already exists for the Node SDK. The
    real language host is used to run Pulumi programs written in Python
    while mocking out the resource monitor.
    
    * Add new tests
    
    * Print out better diagnostics when the langhost fails to launch
    
    * Use the in-tree executor for testing
    
    * CR: Place tests and code being tested in the same directory for ease of understanding, add a README
    
    * Turns out I misunderstood the semantics of resource registration - fix two tests so that they pass now and fix a few bugs in the test harness

---
+++ sdk/python/lib/test/langhost/one_resource/test_one_resource.py
@@ -0,0 +1,30 @@
+# Copyright 2016-2018, Pulumi Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from os import path
+from ..util import LanghostTest
+
+
+class OneResourceTest(LanghostTest):
+    def test_one_resource(self):
+        self.run_test(
+            program=path.join(self.base_path(), "one_resource"),
+            expected_resource_count=1)
+
+    def register_resource(self, _ctx, _dry_run, ty, name, _resource,
+                          _dependencies):
+        self.assertEqual(ty, "test:index:MyResource")
+        self.assertEqual(name, "testResource1")
+        return {
+            "urn": self.make_urn(ty, name),
+        }
---
+++ sdk/python/lib/test/langhost/runtime_settings/test_runtime_settings.py
@@ -0,0 +1,31 @@
+# Copyright 2016-2018, Pulumi Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from os import path
+from ..util import LanghostTest
+
+
+class RuntimeSettingsTest(LanghostTest):
+    def test_runtime_settings(self):
+        self.run_test(program=path.join(self.base_path(), "runtime_settings"),
+                      project="myproject",
+                      stack="mystack",
+                      config={
+                          "test:known": "knownkey",
+                          "test:lowercase_true": "true",
+                          "test:uppercase_true": "True",
+                          "test:lowercase_false": "false",
+                          "test:uppercase_false": "False",
+                          "test:not_a_bool": "DBBool",
+                      },
+                      expected_resource_count=0)
