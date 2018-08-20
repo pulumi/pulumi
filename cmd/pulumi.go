258d849dd68ad836088a58905e0f19f081b230f8
Matt Ellis <matt@pulumi.com>
2018-08-20 11:05:49 -0700

    Fix wierd interactions due to Cobra and glog
    The glog package force the use of golang's underyling flag package,
    which Cobra does not use. To work around this, we had a complicated
    dance around defining flags in multiple places, calling flag.Parse
    explicitly and then stomping values in the flag package with values we
    got from Cobra.
    
    Because we ended up parsing parts of the command line twice, each with
    a different set of semantics, we ended up with bad UX in some
    cases. For example:
    
    `$ pulumi -v=10 --logflow update`
    
    Would fail with an error message that looked nothing like normal CLI
    errors, where as:
    
    `$ pulumi -v=10 update --logflow`
    
    Would behave as you expect. To address this, we now do two things:
    
    - We never call flag.Parse() anymore. Wacking the flags with values we
      got from Cobra is sufficent for what we care about.
    
    - We use a forked copy of glog which does not complain when
      flag.Parse() is not called before logging.
    
    Fixes #301
    Fixes #710
    Fixes #968

--- cmd/pulumi.go
+++ cmd/pulumi.go
@@ -17,7 +17,6 @@ package cmd
 import (
 	"bufio"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -165,12 +164,6 @@ func NewPulumiCmd() *cobra.Command {
 		cmd.PersistentFlags().StringVar(&tracingHeaderFlag, "tracing-header", "",
 			"Include the tracing header with the given contents.")
 	}
-
-	// Tell flag about -C, so someone can do pulumi -C <working-directory> stack and the call to cmdutil.InitLogging
-	// which calls flag.Parse under the hood doesn't yell at you.
-	//
-	// TODO[pulumi/pulumi#301]: when we move away from using glog, it should be safe to remove this.
-	flag.StringVar(&cwd, "C", "", "Run pulumi as if it had been started in another directory")
 
 	return cmd
 }
