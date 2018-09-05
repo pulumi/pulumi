51c8f5c90a6de42f46355be3029780e1f0264521
Pat Gavlin <pat@pulumi.com>
2018-09-05 12:19:15 -0700

    PR feedback

--- pkg/resource/deploy/step.go
+++ pkg/resource/deploy/step.go
@@ -607,8 +607,6 @@ func (s *RefreshStep) Apply(preview bool) (resource.Status, StepCompleteFunc, er
 			return rst, nil, err
 		}
 		if initErr, isInitErr := err.(*plugin.InitError); isInitErr {
-			// We clear error in this case because we do not want the refresh to fail in the face of initialization
-			// errors.
 			initErrors = initErr.Reasons
 		}
 	}
