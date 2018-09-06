d0610fe51144c9ca542556cec77bd4c44b6cd16b
Cyrus Najmabadi <cyrus@pulumi.com>
2018-09-06 15:43:39 -0700

    Update test.

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -5,9 +5,15 @@ import * as runtime from "@pulumi/pulumi/runtime"
 
 (async function() {
     const config = new Config();
+
+    // Ensure we get the right set of dependencies back.  For example, read-package-json merged
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
+    // behavior.
     const deps = await runtime.computeCodePaths();
+
     const actual = JSON.stringify([...deps.keys()].sort());
-    const expected = "";
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
+
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
     }
