b73498386bbc90123613631f753beabcbb418b4d
Cyrus Najmabadi <cyrus@pulumi.com>
2018-09-18 13:25:32 -0700

    Remove diff test.  It has outlived its original purpose (just validating that colors worked.)

--- tests/integration/diff/step5/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-// Step 5: Delete everything:
-// * Elide a (Delete(a4)).
-// * Elide c (Delete(c)).
-// * Elide e (Delete(e)).
-// * Pending delete (Delete(a3)).
-// Checkpoint: empty
