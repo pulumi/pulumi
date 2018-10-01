33ab82e9ace68d3de6911ee2de2e46e9ddad8fd7
joeduffy <joe@pulumi.com>
2018-10-01 11:38:03 -0700

    Set up some CI variables for GitHub App support

--- scripts/docker-entry.sh
+++ scripts/docker-entry.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # This is an entrypoint for our Docker image that does some minimal bootstrapping before executing.
 
-# First, lazily install packages if required.
+# Detect the CI system and configure variables so that we get good Pulumi workflow and GitHub App support.
+if [ ! -z "${GITHUB_WORKFLOW}" ]; then
+    export PULUMI_CI_SYSTEM="GitHub"
+    export PULUMI_CI_BUILD_ID=
+    export PULUMI_CI_BUILD_TYPE=
+    export PULUMI_CI_BUILD_URL=
+    export PULUMI_CI_PULL_REQUEST_SHA="${GITHUB_SHA}"
+fi
+
+# Next, lazily install packages if required.
 if [ -e package.json ] && [ ! -d node_modules ]; then
     npm install
 fi
