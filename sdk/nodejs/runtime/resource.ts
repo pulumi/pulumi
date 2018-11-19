a337fd73793c8fba7db7b602fec9cf32aab1c72f
Joe Duffy <joe@pulumi.com>
2018-11-19 11:22:55 -0500

    Permit dependsOn that are Outputs (of Outputs) (#2198)
    This changes the input type for dependsOn from simply
    `Resource[] | Resource` to `Input<Input<Resource>[]> | Input<Resource>`.
    This permits `Output<Resource>`s, etc in addition to
    `Promise<Resource>`s. The logic for dynamically unpicking the right
    types and recursing through the data structures isn't straightforward,
    but I've written a test for all of the interesting permutations.
    
    This fixes pulumi/pulumi#991.

--- sdk/nodejs/runtime/resource.ts
+++ sdk/nodejs/runtime/resource.ts
@@ -216,14 +216,7 @@ async function prepareResource(label: string, res: Resource, custom: boolean,
     /** IMPORTANT!  We should never await prior to this line, otherwise the Resource will be partly uninitialized. */
 
     // Before we can proceed, all our dependencies must be finished.
-    let dependsOn: Resource[] = [];
-    if (Array.isArray(opts.dependsOn)) {
-        dependsOn = opts.dependsOn;
-    } else if (opts.dependsOn) {
-        dependsOn = [opts.dependsOn];
-    }
-    const explicitURNDeps = await debuggablePromise(
-        Promise.all(dependsOn.map(d => d.urn.promise())), `dependsOn(${label})`);
+    const explicitDependencies: URN[] = await gatherExplicitDependencies(opts.dependsOn);
 
     // Serialize out all our props to their final values.  In doing so, we'll also collect all
     // the Resources pointed to by any Dependency objects we encounter, adding them to 'propertyDependencies'.
@@ -246,7 +239,7 @@ async function prepareResource(label: string, res: Resource, custom: boolean,
         providerRef = `${providerURN}::${providerID}`;
     }
 
-    const dependencies: Set<URN> = new Set<URN>(explicitURNDeps);
+    const dependencies: Set<URN> = new Set<URN>(explicitDependencies);
     for (const implicitDep of implicitDependencies) {
         dependencies.add(await implicitDep.urn.promise());
     }
@@ -260,6 +253,35 @@ async function prepareResource(label: string, res: Resource, custom: boolean,
         providerRef: providerRef,
         dependencies: dependencies,
     };
+}
+
+/**
+ * Gathers explicit dependency URNs from a list of Resources (possibly Promises and/or Outputs).
+ */
+async function gatherExplicitDependencies(
+    dependsOn: Input<Input<Resource>[]> | Input<Resource> | undefined): Promise<URN[]> {
+
+    if (dependsOn) {
+        if (Array.isArray(dependsOn)) {
+            const dos: URN[] = [];
+            for (const d of dependsOn) {
+                dos.push(...(await gatherExplicitDependencies(d)));
+            }
+            return dos;
+        } else if (dependsOn instanceof Promise) {
+            return gatherExplicitDependencies(await dependsOn);
+        } else if (Output.isInstance(dependsOn)) {
+            // Recursively gather dependencies, await the promise, and append the output's dependencies.
+            const dos = (dependsOn as Output<Input<Resource>[] | Input<Resource>>).apply(gatherExplicitDependencies);
+            const urns = await dos.promise();
+            const implicits = await gatherExplicitDependencies([...dos.resources()]);
+            return urns.concat(implicits);
+        } else {
+            return [await (dependsOn as Resource).urn.promise()];
+        }
+    }
+
+    return [];
 }
 
 /**
