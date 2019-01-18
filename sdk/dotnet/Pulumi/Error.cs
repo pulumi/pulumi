9f262dce967375fcadf2f84893de177b8aad4ab5
Fraser Waters <frassle@gmail.com>
2019-01-18 18:42:46 +0000

    Inherit from Exception not ApplicationException

--- sdk/dotnet/Pulumi/Error.cs
+++ sdk/dotnet/Pulumi/Error.cs
@@ -5,7 +5,7 @@ namespace Pulumi {
     /// RunError can be used for terminating a program abruptly, but resulting in a clean exit rather than the usual
     /// verbose unhandled error logic which emits the source program text and complete stack trace.
     /// </summary>
-    public class RunError : ApplicationException {
+    public class RunError : Exception {
         public RunError() : base() {}
         public RunError(string message) : base(message) {}
         public RunError(string message, Exception innerException) : base(message, innerException) {}
