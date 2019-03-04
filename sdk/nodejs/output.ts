856493d49663f9247b46b689c88030b9cb5200ab
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-03-04 11:44:31 -0800

    update

--- sdk/nodejs/output.ts
+++ sdk/nodejs/output.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as log from "./log";
 import { Resource } from "./resource";
 import * as runtime from "./runtime";
 import * as utils from "./utils";
