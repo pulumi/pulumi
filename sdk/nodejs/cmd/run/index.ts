4c7ab90a8810ed7722f10e56531542f38146e049
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-03-12 15:35:10 -0700

    Update index.ts

--- sdk/nodejs/cmd/run/index.ts
+++ sdk/nodejs/cmd/run/index.ts
@@ -113,9 +113,13 @@ function main(args: string[]): void {
             programRunning = true;
         });
 
-        // .finally is not available.  So we call into .then with the same value for both
-        // resolved and rejected.
-        promise.then(() => { programRunning = false; }, () => { programRunning = false; });
+        // when the user's program completes successfully, set programRunning back to false.  That way, if the Pulumi
+        // scaffolding code ends up throwing an exception during teardown, it will get printed directly to the console.
+        //
+        // Note: we only do this in the 'resolved' arg of '.then' (not the 'rejected' arg).  If the users code throws
+        // an exception, this promise will get rejected, and we don't want touch or otherwise intercept the exception
+        // or change the programRunning state here at all.
+        promise.then(() => { programRunning = false; });
     });
 }
 
