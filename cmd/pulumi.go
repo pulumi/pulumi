8d725854c1bb90f9194027c27bc9926a3e4127a0
Alex Clemmer <clemmer.alexander@gmail.com>
2019-04-30 11:49:02 -0700

    Add `pulumi query` command
    This commit implements the `pulumi query` as a dedicated command in the
    CLI. The semantics of this command are to run a program in "query mode",
    which disallows all resource operations, but allows access to stack
    snapshots, as well as various query primitives.
    
    By default `query` will run the query program in the currend directory,
    but as with `up`, you can us the `--cwd` or `-C` flags to customize
    this.

--- cmd/pulumi.go
+++ cmd/pulumi.go
@@ -176,6 +176,7 @@ func NewPulumiCmd() *cobra.Command {
 	cmd.AddCommand(newPluginCmd())
 	cmd.AddCommand(newVersionCmd())
 	cmd.AddCommand(newHistoryCmd())
+	cmd.AddCommand(newQueryCmd())
 
 	// Less common, and thus hidden, commands:
 	cmd.AddCommand(newGenCompletionCmd(cmd))
---
+++ cmd/query.go
@@ -0,0 +1,90 @@
+// Copyright 2016-2019, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+
+	"github.com/pulumi/pulumi/pkg/backend"
+	"github.com/pulumi/pulumi/pkg/backend/display"
+	"github.com/pulumi/pulumi/pkg/engine"
+	"github.com/pulumi/pulumi/pkg/util/cmdutil"
+	"github.com/pulumi/pulumi/pkg/util/result"
+)
+
+// nolint: vetshadow, intentionally disabling here for cleaner err declaration/assignment.
+func newQueryCmd() *cobra.Command {
+	var stack string
+
+	var cmd = &cobra.Command{
+		Use:   "query",
+		Short: "Run query program against cloud resources",
+		Long: "Run query program against cloud resources.\n" +
+			"\n" +
+			"This command loads a Pulumi query program and executes it. In \"query mode\", Pulumi provides various\n" +
+			"useful data sources for querying, such as the resource outputs for a stack. Query mode also disallows\n" +
+			"all resource operations, so users cannot declare resource definitions as they would in normal Pulumi\n" +
+			"programs.\n" +
+			"\n" +
+			"The program to run is loaded from the project in the current directory by default. Use the `-C` or\n" +
+			"`--cwd` flag to use a different directory.",
+		Args: cmdutil.NoArgs,
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
+			interactive := cmdutil.Interactive()
+
+			opts := backend.UpdateOptions{}
+			opts.Display = display.Options{
+				Color:         cmdutil.GetGlobalColorization(),
+				IsInteractive: interactive,
+				Type:          display.DisplayQuery,
+			}
+
+			s, err := requireStack(stack, true, opts.Display, true /*setCurrent*/)
+			if err != nil {
+				return result.FromError(err)
+			}
+
+			proj, root, err := readProject()
+			if err != nil {
+				return result.FromError(err)
+			}
+
+			opts.Engine = engine.UpdateOptions{}
+
+			res := s.Query(commandContext(), backend.UpdateOperation{
+				Proj:   proj,
+				Root:   root,
+				Opts:   opts,
+				Scopes: cancellationScopes,
+			})
+			switch {
+			case res != nil && res.Error() == context.Canceled:
+				return nil
+			case res != nil:
+				return PrintEngineResult(res)
+			default:
+				return nil
+			}
+		}),
+	}
+
+	cmd.PersistentFlags().StringVarP(
+		&stack, "stack", "s", "",
+		"The name of the stack to operate on. Defaults to the current stack")
+
+	return cmd
+}
