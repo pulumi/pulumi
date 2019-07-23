799fc01d8ec53163bf282fe3c756fb76215f3ee2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-23 14:56:08 -0700

    Add typing

--- sdk/python/lib/pulumi/resource.py
+++ sdk/python/lib/pulumi/resource.py
@@ -52,20 +52,14 @@ class CustomTimeouts:
 
 
 def create_urn(
-        name: Input[str],
-        typ: Input[str],
-        parent: Optional[Union[Resource, Input[str]]] = None,
+        name: 'Input[str]',
+        typ: 'Input[str]',
+        parent: Optional[Union[Resource, 'Input[str]']] = None,
         project: str = None,
-        stack: str = None) -> Output[str]:
+        stack: str = None) -> 'Output[str]':
     """
     createUrn computes a URN from the combination of a resource name, resource type, optional
     parent, optional project and optional stack.
-
-    :param Input[str] name
-    :param Input[str] typ
-    :param Optional[Union[Resource,Input[str]]] parent
-    :param Optional[str] project
-    :param Optional[str] stack
     """
     parent_prefix = None
     if parent is not None:
@@ -91,32 +85,44 @@ def create_urn(
         lambda arr:
         arr[0] + arr[1] + "::" + arr[2])
 
-# def inherited_child_alias(child_name, parent_name, parent_alias, child_type)
-# // inheritedChildAlias computes the alias that should be applied to a child based on an alias applied to it's parent.
-# // This may involve changing the name of the resource in cases where the resource has a named derived from the name of
-# // the parent, and the parent name changed.
-# function inheritedChildAlias(childName: string, parentName: string, parentAlias: Input<string>, childType: string): Output<string> {
-#     // If the child name has the parent name as a prefix, then we make the assumption that it was
-#     // constructed from the convention of using `{name}-details` as the name of the child resource.  To
-#     // ensure this is aliased correctly, we must then also replace the parent aliases name in the prefix of
-#     // the child resource name.
-#     //
-#     // For example:
-#     // * name: "newapp-function"
-#     // * opts.parent.__name: "newapp"
-#     // * parentAlias: "urn:pulumi:stackname::projectname::awsx:ec2:Vpc::app"
-#     // * parentAliasName: "app"
-#     // * aliasName: "app-function"
-#     // * childAlias: "urn:pulumi:stackname::projectname::aws:s3/bucket:Bucket::app-function"
-#     let aliasName = output(childName);
-#     if (childName.startsWith(parentName)) {
-#         aliasName = output(parentAlias).apply(parentAliasUrn => {
-#             const parentAliasName = parentAliasUrn.substring(parentAliasUrn.lastIndexOf("::") + 2);
-#             return parentAliasName + childName.substring(parentName.length);
-#         });
-#     }
-#     return createUrn(aliasName, childType, parentAlias);
-# }
+
+# def inherited_child_alias(
+#         child_name: str,
+#         parent_name: str,
+#         parent_alias: 'Input[str]',
+#         child_type: str) -> 'Output[str]':
+#     """
+#     inheritedChildAlias computes the alias that should be applied to a child based on an alias
+#     applied to it's parent. This may involve changing the name of the resource in cases where the
+#     resource has a named derived from the name of the parent, and the parent name changed.
+#     """
+
+# #   If the child name has the parent name as a prefix, then we make the assumption that it was
+# #   constructed from the convention of using `{name}-details` as the name of the child resource.  To
+# #   ensure this is aliased correctly, we must then also replace the parent aliases name in the prefix of
+# #   the child resource name.
+# #
+# #   For example:
+# #   * name: "newapp-function"
+# #   * opts.parent.__name: "newapp"
+# #   * parentAlias: "urn:pulumi:stackname::projectname::awsx:ec2:Vpc::app"
+# #   * parentAliasName: "app"
+# #   * aliasName: "app-function"
+# #   * childAlias: "urn:pulumi:stackname::projectname::aws:s3/bucket:Bucket::app-function"
+#     alias_name = Output.from_input(child_name)
+#     if child_name.startswith(parent_name):
+#         alias_name = Output.from_input(parent_alias).apply(
+#             lambda u:
+#             u.
+# #     let aliasName = output(childName);
+# #     if (childName.startsWith(parentName)) {
+# #         aliasName = output(parentAlias).apply(parentAliasUrn => {
+# #             const parentAliasName = parentAliasUrn.substring(parentAliasUrn.lastIndexOf("::") + 2);
+# #             return parentAliasName + childName.substring(parentName.length);
+# #         });
+# #     }
+# #     return createUrn(aliasName, childType, parentAlias);
+# # }
 
 
 class ResourceOptions:
