a6bb84e01e18b2f5675b60c9bf3b41f79ed6b477
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-24 00:30:50 -0700

    Fix

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -2,9 +2,9 @@
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource(ComponentResource):
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
-res1 = Resource("res1")
+res1 = Resource1("res1")
--- tests/integration/aliases/python/rename/step2/__main__.py
+++ tests/integration/aliases/python/rename/step2/__main__.py
@@ -2,11 +2,11 @@
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource(ComponentResource):
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
 # This resource was previously named `res1`, we'll alias to the old name.
-res1 = Resource("newres1", ResourceOptions(
+res1 = Resource1("newres1", ResourceOptions(
     aliases=[Alias(name="res1")]))
