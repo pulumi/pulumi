b0055787e6a1e839d4688ec7ef1ad79d07259ea5
Chris Smith <chrsmith@users.noreply.github.com>
2019-07-25 07:58:19 -0700

    Fix issue when logging out of local backend (#2951)
    * Fix issue when logging out of local backend
    
    * Put actual cloud URL into credentials.json
    
    * Fix test on Windows; disable newly-enabled tests on Appveyor
    
    * Keep track of the URL used to initialize local backend

--- pkg/backend/filestate/backend.go
+++ pkg/backend/filestate/backend.go
@@ -60,8 +60,14 @@ type Backend interface {
 }
 
 type localBackend struct {
-	d      diag.Sink
-	url    string
+	d diag.Sink
+
+	// originalURL is the URL provided when the localBackend was initialized, for example
+	// "file://~". url is a canonicalized version that should be used when persisting data.
+	// (For example, replacing ~ with the home directory, making an absolute path, etc.)
+	originalURL string
+	url         string
+
 	bucket Bucket
 }
 
@@ -88,13 +94,13 @@ func IsFileStateBackendURL(urlstr string) bool {
 
 const FilePathPrefix = "file://"
 
-func New(d diag.Sink, u string) (Backend, error) {
-	if !IsFileStateBackendURL(u) {
+func New(d diag.Sink, originalURL string) (Backend, error) {
+	if !IsFileStateBackendURL(originalURL) {
 		return nil, errors.Errorf("local URL %s has an illegal prefix; expected one of: %s",
-			u, strings.Join(blob.DefaultURLMux().BucketSchemes(), ", "))
+			originalURL, strings.Join(blob.DefaultURLMux().BucketSchemes(), ", "))
 	}
 
-	u, err := massageBlobPath(u)
+	u, err := massageBlobPath(originalURL)
 	if err != nil {
 		return nil, err
 	}
@@ -120,9 +126,10 @@ func New(d diag.Sink, u string) (Backend, error) {
 	}
 
 	return &localBackend{
-		d:      d,
-		url:    u,
-		bucket: &wrappedBucket{bucket: bucket},
+		d:           d,
+		originalURL: originalURL,
+		url:         u,
+		bucket:      &wrappedBucket{bucket: bucket},
 	}, nil
 }
 
@@ -131,11 +138,11 @@ func New(d diag.Sink, u string) (Backend, error) {
 // file:// paths which have a few oddities around them that we want to ensure work properly.
 func massageBlobPath(path string) (string, error) {
 	if !strings.HasPrefix(path, FilePathPrefix) {
-		// not a file:// path.  Keep this untouched and pass directly to gocloud.
+		// Not a file:// path.  Keep this untouched and pass directly to gocloud.
 		return path, nil
 	}
 
-	// Strip off the "file://"" portion so we can examine and determine what to do with the rest.
+	// Strip off the "file://" portion so we can examine and determine what to do with the rest.
 	path = strings.TrimPrefix(path, FilePathPrefix)
 
 	// We need to specially handle ~.  The shell doesn't take care of this for us, and later
@@ -162,7 +169,7 @@ func massageBlobPath(path string) (string, error) {
 	}
 
 	// Using example from https://godoc.org/gocloud.dev/blob/fileblob#example-package--OpenBucket
-	// On Windows, convert "\" to "/" and add a leading "/":
+	// On Windows, convert "\" to "/" and add a leading "/". (See https://gocloud.dev/howto/blob/#local)
 	path = filepath.ToSlash(path)
 	if os.PathSeparator != '/' && !strings.HasPrefix(path, "/") {
 		path = "/" + path
@@ -176,7 +183,7 @@ func Login(d diag.Sink, url string) (Backend, error) {
 	if err != nil {
 		return nil, err
 	}
-	return be, workspace.StoreAccessToken(url, "", true)
+	return be, workspace.StoreAccessToken(be.URL(), "", true)
 }
 
 func (b *localBackend) local() {}
@@ -191,7 +198,7 @@ func (b *localBackend) Name() string {
 }
 
 func (b *localBackend) URL() string {
-	return b.url
+	return b.originalURL
 }
 
 func (b *localBackend) StateDir() string {
@@ -648,7 +655,7 @@ func (b *localBackend) ImportDeployment(ctx context.Context, stackRef backend.St
 }
 
 func (b *localBackend) Logout() error {
-	return workspace.DeleteAccessToken(b.url)
+	return workspace.DeleteAccessToken(b.originalURL)
 }
 
 func (b *localBackend) CurrentUser() (string, error) {
