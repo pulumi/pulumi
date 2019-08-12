33e4ee3bf961df739ec35f33659f1905bfc7fb46
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-08-12 14:59:41 -0700

    Update sdk/nodejs/runtime/stack.ts

--- sdk/nodejs/runtime/stack.ts
+++ sdk/nodejs/runtime/stack.ts
@@ -96,7 +96,6 @@ async function massage(prop: any, seenObjects: Set<any>): Promise<any> {
     }
 
     if (Output.isInstance(prop)) {
-        console.log("hitting this");
         return prop.apply(v => massage(v, seenObjects));
     }
 
