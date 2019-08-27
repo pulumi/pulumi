a687f815751a627d2fe605ab9b8da8de2bf25b0f
Joe Duffy <joe@pulumi.com>
2019-08-26 20:24:00 -0700

    Update the tagline in our CLI (#3141)

--- cmd/pulumi.go
+++ cmd/pulumi.go
@@ -60,7 +60,7 @@ func NewPulumiCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "pulumi",
 		Short: "Pulumi command line",
-		Long: "Pulumi - Cloud Native Infrastructure as Code\n" +
+		Long: "Pulumi - Modern Infrastructure as Code\n" +
 			"\n" +
 			"To begin working with Pulumi, run the 'pulumi new' command:\n" +
 			"\n" +
