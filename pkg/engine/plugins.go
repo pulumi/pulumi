015bada975e737208c49fb1e29f52b8e0f5475d9
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-08-28 11:28:04 -0700

    Add more details to an assert (#3129)
    Since we don't fully understand what was causing the assert to file,
    instead of making changes to return an error when there are equal
    plugins, keep the shape of the existing code and just do the work we
    need to do to ensure that the assert message has actionable data for
    us to look at.

--- pkg/engine/plugins.go
+++ pkg/engine/plugins.go
@@ -217,7 +217,7 @@ func computeDefaultProviderPlugins(languagePlugins, allPlugins pluginSet) map[to
 		sourceSet = allPlugins
 	}
 
-	defaultProviderVersions := make(map[tokens.Package]*semver.Version)
+	defaultProviderPlugins := make(map[tokens.Package]workspace.PluginInfo)
 
 	// Sort the set of source plugins by version, so that we iterate over the set of plugins in a deterministic order.
 	// Sorting by version gets us two properties:
@@ -239,37 +239,44 @@ func computeDefaultProviderPlugins(languagePlugins, allPlugins pluginSet) map[to
 			continue
 		}
 
-		if seenVersion, has := defaultProviderVersions[tokens.Package(p.Name)]; has {
-			if seenVersion == nil {
+		if seenPlugin, has := defaultProviderPlugins[tokens.Package(p.Name)]; has {
+			if seenPlugin.Version == nil {
 				logging.V(preparePluginLog).Infof(
 					"computeDefaultProviderPlugins(): plugin %s selected for package %s (override, previous was nil)",
 					p, p.Name)
-				defaultProviderVersions[tokens.Package(p.Name)] = p.Version
+				defaultProviderPlugins[tokens.Package(p.Name)] = p
 				continue
 			}
 
 			contract.Assertf(p.Version != nil, "p.Version should not be nil if sorting is correct!")
-			if p.Version != nil && p.Version.GT(*seenVersion) {
+			if p.Version != nil && p.Version.GT(*seenPlugin.Version) {
 				logging.V(preparePluginLog).Infof(
 					"computeDefaultProviderPlugins(): plugin %s selected for package %s (override, newer than previous %s)",
-					p, p.Name, seenVersion)
-				defaultProviderVersions[tokens.Package(p.Name)] = p.Version
+					p, p.Name, seenPlugin.Version)
+				defaultProviderPlugins[tokens.Package(p.Name)] = p
 				continue
 			}
 
-			contract.Failf("Should not have seen an older plugin if sorting is correct!")
+			contract.Failf("Should not have seen an older plugin if sorting is correct!\n  %s-%s %s\n  %s-%s - %s",
+				p.Name, p.Version.String(), p.Path,
+				seenPlugin.Name, seenPlugin.Version.String(), seenPlugin.Path)
 		}
 
 		logging.V(preparePluginLog).Infof(
 			"computeDefaultProviderPlugins(): plugin %s selected for package %s (first seen)", p, p.Name)
-		defaultProviderVersions[tokens.Package(p.Name)] = p.Version
+		defaultProviderPlugins[tokens.Package(p.Name)] = p
 	}
 
 	if logging.V(preparePluginLog) {
 		logging.V(preparePluginLog).Infoln("computeDefaultProviderPlugins(): summary of default plugins:")
-		for pkg, version := range defaultProviderVersions {
-			logging.V(preparePluginLog).Infof("  %-15s = %s", pkg, version)
+		for pkg, info := range defaultProviderPlugins {
+			logging.V(preparePluginLog).Infof("  %-15s = %s", pkg, info.Version)
 		}
+	}
+
+	defaultProviderVersions := make(map[tokens.Package]*semver.Version)
+	for name, plugin := range defaultProviderPlugins {
+		defaultProviderVersions[name] = plugin.Version
 	}
 
 	return defaultProviderVersions
