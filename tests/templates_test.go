d7bd9736497829a41e13cac1605803367a0f9321
Mikhail Shilkov <shilkov@gmail.com>
2019-09-12 14:45:55 -0700

    Enable openstack tests

--- tests/templates_test.go
+++ tests/templates_test.go
@@ -95,14 +95,12 @@ func TestTemplates(t *testing.T) {
 			run :=
 				// Skip packet templates for now - waiting for credentials
 				!strings.Contains(template.Name, "packet") &&
-					// Skip go templates for now
+					// Skip go templates for now - not suppored yet
 					!strings.Contains(template.Name, "go") &&
 					// Skip kubernetes templates - no kubeconfig to run them with
 					!strings.Contains(template.Name, "kubernetes") &&
 					// Skip Digital Ocean templates - they all try to create a domain which is occupied
 					!strings.Contains(template.Name, "digitalocean") &&
-					// Skip Open Stack templates for now - waiting for credentials
-					!strings.Contains(template.Name, "openstack") &&
 					// Skip gcp templates for now - waiting for gcp
 					!strings.Contains(template.Name, "gcp")
 
