0ea50f458e633441f16850f03b9021751dd889e9
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-09-24 17:15:46 -0700

    Print destroyed outputs when a stack is destroyed. (#3261)

--- pkg/backend/display/progress.go
+++ pkg/backend/display/progress.go
@@ -739,14 +739,12 @@ func (display *ProgressDisplay) processEndSteps() {
 
 	// If we get stack outputs, display them at the end.
 	var wroteOutputs bool
-	if display.stackUrn != "" && display.seenStackOutputs && !display.opts.SuppressOutputs {
+	if display.stackUrn != "" && !display.opts.SuppressOutputs {
 		stackStep := display.eventUrnToResourceRow[display.stackUrn].Step()
 
-		// We want to hide same outputs if we're doing a read and the user didn't ask to see
-		// things that are the same.
-		hideSames := stackStep.Op == deploy.OpRead && !display.opts.ShowSameResources
 		props := engine.GetResourceOutputsPropertiesString(
-			stackStep, 1, display.isPreview, display.opts.Debug, false /* refresh */, !hideSames)
+			stackStep, 1, display.isPreview, display.opts.Debug,
+			false /* refresh */, display.opts.ShowSameResources)
 		if props != "" {
 			if !wroteDiagnosticHeader {
 				display.writeBlankLine()
