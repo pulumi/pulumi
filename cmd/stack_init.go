cdc3c7995db1c1447164260a8dce19c9cff11fde
Ken Fukuyama <kenfdev@gmail.com>
2019-10-06 12:38:03 +0900

    Fix invalid bullet points in `stack init`
    This fixes the issue where the bullet points aren't rendered correctly
    by hugo.
    
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- cmd/stack_init.go
+++ cmd/stack_init.go
@@ -47,9 +47,11 @@ func newStackInitCmd() *cobra.Command {
 			"`--secrets-provider` flag.\n" +
 			"\n" +
 			"To use the `passphrase` secrets provider with the pulumi.com backend, use:\n" +
+			"\n" +
 			"* `pulumi stack init --secrets-provider=passphrase`\n" +
 			"\n" +
 			"To use a cloud secrets provider with any backend, use one of the following:\n" +
+			"\n" +
 			"* `pulumi stack init --secrets-provider=\"awskms://alias/ExampleAlias?region=us-east-1\"`\n" +
 			"* `pulumi stack init --secrets-provider=\"awskms://1234abcd-12ab-34cd-56ef-1234567890ab?region=us-east-1\"`\n" +
 			"* `pulumi stack init --secrets-provider=\"azurekeyvault://mykeyvaultname.vault.azure.net/keys/mykeyname\"`\n" +
