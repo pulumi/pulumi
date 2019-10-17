53ab8a1ad9a05cf9cd9f778ed08f58643b881318
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-17 15:00:35 -0700

    Remove

--- sdk/dotnet/Pulumirpc/generate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-protoc -I=. --csharp_out ../dotnet/ --grpc_out ../dotnet/ --plugin=protoc-gen-grpc=/home/matell/.nuget/packages/grpc.tools/1.10.0/tools/linux_x64/grpc_csharp_plugin *.proto
