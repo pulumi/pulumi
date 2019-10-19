de146b05582da5e607aca4481faed3c0d6434e23
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-19 12:25:20 -0700

    tweaks

--- sdk/dotnet/examples/bucket/Program.cs
+++ sdk/dotnet/examples/bucket/Program.cs
@@ -1,31 +1,33 @@
-﻿using AWS.S3;
-using Pulumi;
+﻿// Copyright 2016-2018, Pulumi Corporation
+
 using System;
 using System.Text;
+using Pulumi;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Deployment.Run(() => {
+        Deployment.Run(() =>
+        {
             Config config = new Config("hello-dotnet");
 
             // Create the bucket, and make it public.
-            var bucket = new Bucket(config["name"], new BucketArgs {
-                    Acl = "public-read"
-                }
-            );
+            var bucket = new Bucket(config["name"], new BucketArgs
+            {
+                Acl = "public-read"
+            });
 
             // Add some content.  We can use contentBase64 for now, but next we'll want to build out the Assets pipeline so we
             // can do a natural thing.
-            var content = new BucketObject($"{config["name"]}-content", new BucketObjectArgs {
-                    Acl = "public-read",
-                    Bucket = bucket,
-                    ContentBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes("Made with \u2764, Pulumi, and .NET")),
-                    ContentType = "text/plain; charset=utf8",
-                    Key = "hello.txt"
-                }
-            );
+            var content = new BucketObject($"{config["name"]}-content", new BucketObjectArgs
+            {
+                Acl = "public-read",
+                Bucket = bucket,
+                ContentBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes("Made with \u2764, Pulumi, and .NET")),
+                ContentType = "text/plain; charset=utf8",
+                Key = "hello.txt"
+            });
 
             // In addition to the logging here being nice, it actually forces us to block until the Tasks that represent the RPC
             // calls to create the resources complete.
