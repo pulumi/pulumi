bb24e36739a7b6230c583139b65761478cd7782d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-19 18:26:42 -0700

    Docs

--- sdk/dotnet/Pulumi/Core/Input.cs
+++ sdk/dotnet/Pulumi/Core/Input.cs
@@ -8,13 +8,29 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Pulumi
 {
+    /// <summary>
+    /// Internal interface to allow our code to operate on inputs in an untyped manner. Necessary as
+    /// there is no reasonable way to write algorithms over heterogeneous instantiations of generic
+    /// types.
+    /// </summary>
     internal interface IInput
     {
         IOutput ToOutput();
     }
 
+    /// <summary>
+    /// <see cref="Input{T}"/> is a property input for a <see cref="Resource"/>.  It may be a promptly
+    /// available T, or the output from a existing <see cref="Resource"/>.
+    /// </summary>
     public class Input<T> : IInput
     {
+        /// <summary>
+        /// Technically, in .net we can represent Inputs entirely using the Output type (since
+        /// Outputs can wrap values and promises).  However, it would look very weird to state that
+        /// the inputs to a resource *had* to be Outputs. So we basically just come up with this
+        /// wrapper type so things look sensible, even though under the covers we implement things
+        /// using the exact same type
+        /// </summary>
         private readonly Output<T> _outputValue;
 
         private Input(Output<T> outputValue)
@@ -36,53 +52,3 @@ namespace Pulumi
             => ToOutput();
     }
 }
-
-
-//using Google.Protobuf.WellKnownTypes;
-//using System;
-//using System.Threading.Tasks;
-
-//namespace Pulumi {
-//    interface IInput {
-//        Task<OutputState<object>> GetValueAsOutputStateAsync();
-//    }
-
-//    public sealed class Input<T> : IInput{
-//        T m_rawValue;
-//        Task<T> m_task;
-//        Output<T> m_output;
-
-//        private Input() {}
-
-//        public static implicit operator Input<T>(T rawValue) {
-//            return new Input<T> {
-//                m_rawValue = rawValue,
-//            };
-//        }
-
-//        public static implicit operator Input<T>(Task<T> task) {
-//            return new Input<T> {
-//                m_task = task,
-//            };
-//        }
-
-//        public static implicit operator Input<T>(Output<T> output) {
-//            return new Input<T> {
-//                m_output = output,
-//            };
-//        }
-
-//        public async Task<OutputState<object>> GetValueAsOutputStateAsync() {
-//            if (m_task != null) {
-//                return new OutputState<object>(await m_task, true, Array.Empty<Resource>());
-//            } else if (m_output != null) {
-//                return await ((IOutput)m_output).GetOutputStateAsync();
-//            } else {
-//                // If the underlying value is a resource, ensure we flow the resource as a dependency in the synthetic output state.
-//                // TODO(ellismg): Doing this here feels wrong for some reason.
-//                Resource r = m_rawValue as Resource;
-//                return new OutputState<object>(m_rawValue, true, r != null ? new Resource[] { r } : Array.Empty<Resource>());
-//            }
-//        }
-//    }
-//}
