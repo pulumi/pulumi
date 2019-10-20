e4131f99121cbcc6a46be643dcaa8be61c6c63f7
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-19 19:05:54 -0700

    Docs

--- sdk/dotnet/Pulumi/Exceptions/LogException.cs
+++ sdk/dotnet/Pulumi/Exceptions/LogException.cs
@@ -6,6 +6,11 @@ using System;
 
 namespace Pulumi
 {
+    /// <summary>
+    /// Special exception we throw if we had a problem actually logging a message to the engine
+    /// error rpc endpoint. In this case, we have no choice but to tear ourselves down reporting
+    /// whatever we can to the console instead.
+    /// </summary>
     internal class LogException : Exception
     {
         public LogException(Exception exception)
