2897b487ba36fcea2ce117b56633ce49ca250288
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-20 22:22:22 -0700

    Add tests

--- sdk/dotnet/Pulumi.Tests/Core/OutputTests.cs
+++ sdk/dotnet/Pulumi.Tests/Core/OutputTests.cs
@@ -9,13 +9,13 @@ using Xunit;
 
 namespace Pulumi.Tests.Core
 {
-    public class OutputTests
+    public partial class OutputTests
     {
         private static Output<T> CreateOutput<T>(T value, bool isKnown, bool isSecret = false)
             => new Output<T>(ImmutableHashSet<Resource>.Empty,
                 Task.FromResult(new OutputData<T>(value, isKnown, isSecret)));
 
-        private async Task Run(Func<Task> func, bool dryRun)
+        private static async Task Run(Func<Task> func, bool dryRun)
         {
             var originalValue = Deployment.DryRun;
             try
@@ -29,56 +29,108 @@ namespace Pulumi.Tests.Core
             }
         }
 
-        private Task RunInPreview(Func<Task> func)
-            => Run(func, dryRun: true);
+        public class PreviewTests
+        {
+            private static Task RunInPreview(Func<Task> func)
+                => Run(func, dryRun: true);
 
-        private Task RunInNormal(Func<Task> func)
-            => Run(func, dryRun: false);
+            [Fact]
+            public Task ApplyCanRunOnKnownValue()
+                => RunInPreview(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: true);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.True(data.IsKnown);
+                    Assert.Equal(1, data.Value);
+                });
 
-        [Fact]
-        public Task ApplyCanRunOnKnownValueInPreview()
-            => RunInPreview(async () =>
-            {
-                var o1 = CreateOutput(0, isKnown: true);
-                var o2 = o1.Apply(a => a + 1);
-                var data = await o2.DataTask.ConfigureAwait(false);
-                Assert.True(data.IsKnown);
-                Assert.Equal(1, data.Value);
-            });
+            [Fact]
+            public Task ApplyProducesUnknownDefaultOnUnknown()
+                => RunInPreview(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: false);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.False(data.IsKnown);
+                    Assert.Equal(0, data.Value);
+                });
 
-        [Fact]
-        public Task ApplyProducesUnknownDefaultOnUnknownInPreview()
-            => RunInPreview(async () =>
-            {
-                var o1 = CreateOutput(0, isKnown: false);
-                var o2 = o1.Apply(a => a + 1);
-                var data = await o2.DataTask.ConfigureAwait(false);
-                Assert.False(data.IsKnown);
-                Assert.Equal(0, data.Value);
-            });
+            [Fact]
+            public Task ApplyPreservesSecretOnKnown()
+                => RunInPreview(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: true, isSecret: true);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.True(data.IsKnown);
+                    Assert.True(data.IsSecret);
+                    Assert.Equal(1, data.Value);
+                });
 
-        [Fact]
-        public Task ApplyPreservesSecretOnKnownInPreview()
-            => RunInPreview(async () =>
-            {
-                var o1 = CreateOutput(0, isKnown: true, isSecret: true);
-                var o2 = o1.Apply(a => a + 1);
-                var data = await o2.DataTask.ConfigureAwait(false);
-                Assert.True(data.IsKnown);
-                Assert.True(data.IsSecret);
-                Assert.Equal(1, data.Value);
-            });
+            [Fact]
+            public Task ApplyPreservesSecretOnUnknown()
+                => RunInPreview(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: false, isSecret: true);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.False(data.IsKnown);
+                    Assert.True(data.IsSecret);
+                    Assert.Equal(0, data.Value);
+                });
+        }
 
-        [Fact]
-        public Task ApplyPreservesSecretEvenForUnknownInPreview()
-            => RunInPreview(async () =>
-            {
-                var o1 = CreateOutput(0, isKnown: false, isSecret: true);
-                var o2 = o1.Apply(a => a + 1);
-                var data = await o2.DataTask.ConfigureAwait(false);
-                Assert.False(data.IsKnown);
-                Assert.True(data.IsSecret);
-                Assert.Equal(0, data.Value);
-            });
+        public class NormalTests
+        {
+            private static Task RunInNormal(Func<Task> func)
+                => Run(func, dryRun: false);
+
+            [Fact]
+            public Task ApplyCanRunOnKnownValue()
+                => RunInNormal(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: true);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.True(data.IsKnown);
+                    Assert.Equal(1, data.Value);
+                });
+
+            [Fact]
+            public Task ApplyProducesKnownOnUnknown()
+                => RunInNormal(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: false);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.False(data.IsKnown);
+                    Assert.Equal(1, data.Value);
+                });
+
+            [Fact]
+            public Task ApplyPreservesSecretOnKnown()
+                => RunInNormal(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: true, isSecret: true);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.True(data.IsKnown);
+                    Assert.True(data.IsSecret);
+                    Assert.Equal(1, data.Value);
+                });
+
+            [Fact]
+            public Task ApplyPreservesSecretOnUnknown()
+                => RunInNormal(async () =>
+                {
+                    var o1 = CreateOutput(0, isKnown: false, isSecret: true);
+                    var o2 = o1.Apply(a => a + 1);
+                    var data = await o2.DataTask.ConfigureAwait(false);
+                    Assert.False(data.IsKnown);
+                    Assert.True(data.IsSecret);
+                    Assert.Equal(1, data.Value);
+                });
+        }
     }
 }
