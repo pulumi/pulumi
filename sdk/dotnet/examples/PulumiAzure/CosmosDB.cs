864147e1ee760f4ad26fd378b58f19cf3835ea9a
Mikhail Shilkov <shilkov@gmail.com>
2019-10-23 00:09:52 +0200

    Cosmos

--- sdk/dotnet/examples/PulumiAzure/CosmosDB.cs
+++ sdk/dotnet/examples/PulumiAzure/CosmosDB.cs
@@ -8,41 +8,51 @@ namespace Pulumi.Azure.CosmosDB
         private readonly StringOutputCompletionSource _name;
         public Output<string> Name => _name.Output;
 
-        [ResourceField("primaryAccessKey")]
-        private readonly StringOutputCompletionSource _primaryAccessKey;
-        public Output<string> PrimaryAccessKey => _primaryAccessKey.Output;
-
-        [ResourceField("primaryConnectionString")]
-        private readonly StringOutputCompletionSource _primaryConnectionString;
-        public Output<string> PrimaryConnectionString => _primaryConnectionString.Output;
-
         public Account(string name, AccountArgs args = default, ResourceOptions opts = default)
-            : base("azure:storage/account:Account", name, args, opts)
+            : base("azure:cosmosdb/account:Account", name, args, opts)
         {
             _name = new StringOutputCompletionSource(this);
-            _primaryAccessKey = new StringOutputCompletionSource(this);
-            _primaryConnectionString = new StringOutputCompletionSource(this);
             this.OnConstructorCompleted();
         }
     }
 
     public class AccountArgs : ResourceArgs
     {
-        public Input<string> AccessTier { get; set; }
-        public Input<string> AccountKind { get; set; }
-        public Input<string> AccountTier { get; set; }
+        public Input<AccountConsistencyPolicy> ConsistencyPolicy { get; set; }
+        public InputList<AccountGeoLocation> GeoLocations { get; set; }
         public Input<string> Location { get; set; }
         public Input<string> OfferType { get; set; }
         public Input<string> ResourceGroupName { get; set; }
 
         protected override void AddProperties(PropertyBuilder builder)
         {
-            builder.Add("accessTier", AccessTier);
-            builder.Add("accountKind", AccountKind);
-            builder.Add("accountTier", AccountTier);
+            builder.Add("consistencyPolicy", ConsistencyPolicy);
+            builder.Add("geoLocations", GeoLocations);
             builder.Add("location", Location);
             builder.Add("offerType", OfferType);
             builder.Add("resourceGroupName", ResourceGroupName);
+        }
+    }
+
+    public class AccountGeoLocation : ResourceArgs
+    {
+        public Input<string> Location { get; set; }
+        public Input<int> FailoverPriority { get; set; }
+        
+        protected override void AddProperties(PropertyBuilder builder)
+        {
+            builder.Add("location", Location);
+            builder.Add("failoverPriority", FailoverPriority);
+        }
+    }
+
+    public class AccountConsistencyPolicy : ResourceArgs
+    {
+        public Input<string> ConsistencyLevel { get; set; }
+
+        protected override void AddProperties(PropertyBuilder builder)
+        {
+            builder.Add("consistencyLevel", ConsistencyLevel);
         }
     }
 }
