ab4bc456f8c3e261995171f0e5fcb0a450b8ab83
Alex Clemmer <clemmer.alexander@gmail.com>
2019-10-23 14:40:02 -0700

    Temporarily disable localbackend-based `query` tests

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -4,31 +4,34 @@ package ints
 
 import (
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/testing/integration"
 )
 
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.
-		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail.
-			{
-				Dir:           "step2",
-				Additive:      true,
-				QueryMode:     true,
-				ExpectFailure: true,
-			},
-			// Run a query during `pulumi query`. Should succeed.
-			{
-				Dir:           "step3",
-				Additive:      true,
-				QueryMode:     true,
-				ExpectFailure: false,
-			},
-		},
-	})
+	//
+	// TODO[hausdorff, #3396]: Enable once we allow query against local backend stacks.
+	//
+
+	// integration.ProgramTest(t, &integration.ProgramTestOptions{
+	// 	// Create Pulumi resources.
+	// 	Dir:          "step1",
+	// 	StackName:    "query-stack",
+	// 	Dependencies: []string{"@pulumi/pulumi"},
+	// 	EditDirs: []integration.EditDir{
+	// 		// Try to create resources during `pulumi query`. This should fail.
+	// 		{
+	// 			Dir:           "step2",
+	// 			Additive:      true,
+	// 			QueryMode:     true,
+	// 			ExpectFailure: true,
+	// 		},
+	// 		// Run a query during `pulumi query`. Should succeed.
+	// 		{
+	// 			Dir:           "step3",
+	// 			Additive:      true,
+	// 			QueryMode:     true,
+	// 			ExpectFailure: false,
+	// 		},
+	// 	},
+	// })
 }
