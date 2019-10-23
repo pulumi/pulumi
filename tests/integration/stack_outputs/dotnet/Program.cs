82af108e8d088026b42bbbb829bf6fac3177359d
Mikhail Shilkov <shilkov@gmail.com>
2019-10-23 14:41:51 +0200

    Add the first .NET integration tests

---
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => 
+        {
+            return new Dictionary<string, object>
+            {
+                {  "xyz", "ABC" },
+                {  "foo", 42 },
+            };
+        });
+    }
+}
---
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
+using Pulumi;
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => 
+        {
+            var config = new Config();
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            //TODO var a = new StackReference(slug);
+        });
+    }
+}
