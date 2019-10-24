7189d3ea045fb8def3c6e31b8ff241ad720ad8a0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-24 11:25:16 -0700

    Remove unused exceptions

--- sdk/dotnet/Pulumi/Config_Exceptions.cs
+++ sdk/dotnet/Pulumi/Config_Exceptions.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation
 
 using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json.Linq;
 
 namespace Pulumi
 {
@@ -21,63 +18,6 @@ namespace Pulumi
 
             public ConfigTypeException(string key, object? v, string expectedType, Exception? innerException)
                 : base($"Configuration '{key}' value '{v}' is not a valid {expectedType}", innerException)
-            {
-            }
-        }
-
-        /// <summary>
-        /// ConfigEnumException is used when a configuration value isn't a correct enum value.
-        /// </summary>
-        private class ConfigEnumException : RunException
-        {
-            public ConfigEnumException(string key, object v, ISet<string> values)
-                : base($"Configuration '{key}' value '{v}' is not a legal enum value ({new JArray(values)}")
-            {
-            }
-        }
-
-        /// <summary>
-        /// ConfigRangeException is used when a configuration value is outside of the range of legal sizes.
-        /// </summary>
-        private class ConfigRangeException : RunException
-        {
-            public ConfigRangeException(string key, object v, int? min, int? max)
-                : base($"Configuration '{key}' value '{v}' is outside of the legal range({GetRange(v, min, max)}, inclusive)")
-            {
-            }
-
-            private static object GetRange(object v, int? min, int? max)
-            {
-                string range;
-                if (max == null)
-                {
-                    range = $"min {min}";
-                }
-                else if (min == null)
-                {
-                    range = $"max {max}";
-                }
-                else
-                {
-                    range = $"{min}-{max}";
-                }
-
-                if (v is string)
-                {
-                    range += " chars";
-                }
-
-                return range;
-            }
-        }
-
-        /// <summary>
-        /// ConfigPatternException is used when a configuration value does not match the given regular expression.
-        /// </summary>
-        private class ConfigPatternException : RunException
-        {
-            public ConfigPatternException(string key, string v, Regex pattern)
-                    : base($"Configuration '{key}' value '{v}' does not match the regular expression '{pattern}'")
             {
             }
         }
