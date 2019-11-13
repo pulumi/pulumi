33a0882014440c6e7af4546b2490fa6b8c40dd40
Mikhail Shilkov <shilkov@gmail.com>
2019-11-13 21:12:32 +0100

    Input index and length

--- sdk/dotnet/Pulumi/Core/OutputExtensions.cs
+++ sdk/dotnet/Pulumi/Core/OutputExtensions.cs
@@ -17,8 +17,11 @@ namespace Pulumi
         /// <param name="array">An array wrapped into <see cref="Output{T}"/>.</param>
         /// <param name="index">An index to get an element at.</param>
         /// <returns>An <see cref="Output{T}"/> containing an array element.</returns>
-        public static Output<T> GetAt<T>(this Output<ImmutableArray<T>> array, int index)
-            => array.Apply(xs => xs[index]);
+        public static Output<T> GetAt<T>(this Output<ImmutableArray<T>> array, Input<int> index)
+        {
+            var inputArray = (Input<ImmutableArray<T>>)array;
+            return Output.Tuple(inputArray, index).Apply(v => v.Item1[v.Item2]);
+        }
 
         /// <summary>
         /// Convert an output containing an array to an output containing its first element.
@@ -27,5 +30,14 @@ namespace Pulumi
         /// <param name="array">An array wrapped into <see cref="Output{T}"/>.</param>
         /// <returns>An <see cref="Output{T}"/> containing the first array element.</returns>
         public static Output<T> First<T>(this Output<ImmutableArray<T>> array) => array.GetAt(0);
+
+        /// <summary>
+        /// Convert an output containing an array to an output containing the length of the array.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the array.</typeparam>
+        /// <param name="array">An array wrapped into <see cref="Output{T}"/>.</param>
+        /// <returns>An <see cref="Output{T}"/> containing the array length.</returns>
+        public static Output<int> Length<T>(this Output<ImmutableArray<T>> array)
+            => array.Apply(xs => xs.Length);
     } 
 }
