7e5014667d96d5aa83270797375007e09ee6e6b8
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-11-20 16:31:44 -0800

    Add extensions

--- sdk/dotnet/Pulumi/Core/InputOneOf.cs
+++ sdk/dotnet/Pulumi/Core/InputOneOf.cs
@@ -4,6 +4,11 @@ using OneOf;
 
 namespace Pulumi.Core
 {
+    /// <summary>
+    /// Represents an <see cref="Input{T}"/> value that can be one of two different types. For
+    /// example, it might potentially be an <see cref="int"/> some of the time or a <see
+    /// cref="string"/> in other cases.
+    /// </summary>
     public sealed class InputOneOf<T0, T1> : Input<OneOf<T0, T1>>
     {
         public InputOneOf() : this(Output.Create(default(OneOf<T0, T1>)))
@@ -14,6 +19,8 @@ namespace Pulumi.Core
             : base(oneOf)
         {
         }
+
+        #region common conversions
 
         public static implicit operator InputOneOf<T0, T1>(T0 value)
             => Output.Create(value);
@@ -26,5 +33,7 @@ namespace Pulumi.Core
 
         public static implicit operator InputOneOf<T0, T1>(Output<T1> value)
             => new InputOneOf<T0, T1>(value.Apply(v => OneOf<T0, T1>.FromT1(v)));
+
+        #endregion
     }
 }
