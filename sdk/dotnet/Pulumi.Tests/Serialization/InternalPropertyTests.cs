d7e93472b35c7a6104bf7c24263aebcecaae7c36
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-11-21 17:05:39 -0500

    Filter out internal properties when deserializing data (#3560)

---
+++ sdk/dotnet/Pulumi.Tests/Serialization/InternalPropertyTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2016-2019, Pulumi Corporation
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Pulumi.Serialization;
+using Xunit;
+
+namespace Pulumi.Tests.Serialization
+{
+    public class InternalPropertyTests : ConverterTests
+    {
+        [Fact]
+        public void IgnoreInternalProperty()
+        {
+            var data = Converter.ConvertValue<ImmutableDictionary<string, string>>("", new Value
+            {
+                StructValue = new Struct
+                {
+                    Fields =
+                    {
+                        { "a", new Value { StringValue = "b" } },
+                        { "__defaults", new Value { BoolValue = true } },
+                    }
+                }
+            });
+            Assert.True(data.IsKnown);
+            Assert.True(data.Value.ContainsKey("a"));
+            Assert.Equal("b", data.Value["a"]);
+            Assert.False(data.Value.ContainsKey("__defaults"));
+        }
+    }
+}
