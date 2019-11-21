ba5ff9c4ff272ddd898d2aec66ec21c32c4a4835
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-11-20 16:39:49 -0800

    Add test

--- sdk/dotnet/Pulumi.Tests/Serialization/OneOfConverterTests.cs
+++ sdk/dotnet/Pulumi.Tests/Serialization/OneOfConverterTests.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
 using OneOf;
 using Pulumi.Serialization;
@@ -26,6 +29,21 @@ namespace Pulumi.Tests.Serialization
             Assert.True(data.Value.IsT1);
             Assert.True(data.IsKnown);
             Assert.Equal("foo", data.Value.AsT1);
+        }
+
+        [Fact]
+        public async Task MixedList()
+        {
+            var data = Converter.ConvertValue<ImmutableArray<OneOf<int, string>>>("",
+                await SerializeToValueAsync(new List<object> { 1, "foo" }));
+            Assert.True(data.IsKnown);
+            Assert.Equal(2, data.Value.Length);
+
+            Assert.True(data.Value[0].IsT0);
+            Assert.Equal(1, data.Value[0].AsT0);
+
+            Assert.True(data.Value[1].IsT1);
+            Assert.Equal("foo", data.Value[1].AsT1);
         }
 
         [Fact]
