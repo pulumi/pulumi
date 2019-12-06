238f58d4fc9146e3c094f8c4a73bdc2f4a5a12c7
Mikhail Shilkov <shilkov@gmail.com>
2019-12-06 14:14:00 +0100

    Fix serialization of InvokeArgs

---
+++ sdk/dotnet/Pulumi.Tests/Serialization/ArgsConverterTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright 2016-2019, Pulumi Corporation
+
+using System.Text.Json;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Pulumi.Serialization;
+using Xunit;
+
+namespace Pulumi.Tests.Serialization
+{
+    public class ArgsConverterTests : ConverterTests
+    {
+        public class SimpleInvokeArgs1 : InvokeArgs
+        {
+            [Input("s")]
+            public string? S { get; set; }
+        }
+
+        public class ComplexInvokeArgs1 : InvokeArgs
+        {
+            [Input("v")]
+            public SimpleInvokeArgs1? V { get; set; }
+        }
+
+        public class SimpleResourceArgs1 : ResourceArgs
+        {
+            [Input("s")]
+            public Input<string>? S { get; set; }
+        }
+
+        public class ComplexResourceArgs1 : ResourceArgs
+        {
+            [Input("v")]
+            public Input<SimpleResourceArgs1>? V { get; set; }
+        }
+
+        private async Task Test(object args)
+        {
+            var serialized = await SerializeToValueAsync(args);
+            var converted = Converter.ConvertValue<JsonElement>("", serialized);
+            var s = converted.Value.GetProperty("v").GetProperty("s").GetString();
+            Assert.Equal("value", s);
+        }
+
+        [Fact]
+        public async Task InvokeArgs()
+        {
+            var args = new ComplexInvokeArgs1 { V = new SimpleInvokeArgs1 { S = "value" } };
+            await Test(args);
+        }
+
+        [Fact]
+        public async Task ResourceArgs()
+        {
+            var args = new ComplexResourceArgs1 { V = new SimpleResourceArgs1 { S = "value" } };
+            await Test(args);
+        }
+    }
+}
