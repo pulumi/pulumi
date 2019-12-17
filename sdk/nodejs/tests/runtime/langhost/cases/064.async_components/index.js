a837458843904fe6591bacb43a0423133e271f64
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-17 13:35:59 -0800

    Add testing

--- sdk/nodejs/tests/runtime/langhost/cases/064.async_components/index.js
+++ sdk/nodejs/tests/runtime/langhost/cases/064.async_components/index.js
@@ -12,16 +12,26 @@ class CustResource extends pulumi.CustomResource {
 class CompResource extends pulumi.ComponentResource {
     constructor(name, opts) {
         super("test:index:CompResource", name, {}, opts)
+        const data = this.getData();
+        this.a = pulumi.output(data.then(d => d.a));
+        this.b = pulumi.output(data.then(d => d.b));
     }
 
     /** @override */
     async initialize() {
         new CustResource("a", { parent: this });
         new CustResource("b", { parent: this });
+        return { a: 1, b: 2 }
     }
 }
 
 const comp = new CompResource("comp", {});
+comp.a.apply(v => {
+    assert.equal(v, 1);
+});
+comp.b.apply(v => {
+    assert.equal(v, 2);
+});
 
 // Have a custom resource depend on the async component.  We should still pick up 'a' and 'b' as
 // dependencies.
