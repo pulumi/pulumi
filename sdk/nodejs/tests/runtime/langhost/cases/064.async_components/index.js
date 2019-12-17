7508c8fb02701ed7d4092b67af1be3d4b78b3dbc
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-17 14:25:54 -0800

    test more

--- sdk/nodejs/tests/runtime/langhost/cases/064.async_components/index.js
+++ sdk/nodejs/tests/runtime/langhost/cases/064.async_components/index.js
@@ -15,13 +15,15 @@ class CompResource extends pulumi.ComponentResource {
         const data = this.getData();
         this.a = pulumi.output(data.then(d => d.a));
         this.b = pulumi.output(data.then(d => d.b));
+        this.cust1 = pulumi.output(data.then(d => d.cust1));
+        this.cust2 = pulumi.output(data.then(d => d.cust2));
     }
 
     /** @override */
     async initialize() {
-        new CustResource("a", { parent: this });
-        new CustResource("b", { parent: this });
-        return { a: 1, b: 2 }
+        const cust1 = new CustResource("a", { parent: this });
+        const cust2 = new CustResource("b", { parent: this });
+        return { a: 1, b: 2, cust1, cust2 }
     }
 }
 
@@ -36,3 +38,7 @@ comp.b.apply(v => {
 // Have a custom resource depend on the async component.  We should still pick up 'a' and 'b' as
 // dependencies.
 const c = new CustResource("c", { dependsOn: comp });
+
+// Have another depend on the child resources that are exposed through Output wrappers of async
+// computation.   We should still pick up 'a' and 'b' as dependencies.
+const d = new CustResource("d", { dependsOn: [comp.cust1, comp.cust2] });
--- sdk/nodejs/tests/runtime/langhost/run.spec.ts
+++ sdk/nodejs/tests/runtime/langhost/run.spec.ts
@@ -1120,10 +1120,17 @@ describe("rpc", () => {
         },
         "async_components": {
             program: path.join(base, "064.async_components"),
-            expectResourceCount: 4,
+            expectResourceCount: 5,
             registerResource: (ctx: any, dryrun: boolean, t: string, name: string, res: any, dependencies?: string[],
                                custom?: boolean, protect?: boolean, parent?: string, provider?: string) => {
                 if (name === "c") {
+                    dependencies = dependencies || [];
+                    dependencies.sort();
+                    // resource 'c' should see resources 'a' and 'b' as dependencies (even though
+                    // they are async constructed by the component)
+                    assert.deepEqual(dependencies, ["test:index:CustResource::a", "test:index:CustResource::b"]);
+                }
+                else if (name === "d") {
                     dependencies = dependencies || [];
                     dependencies.sort();
                     // resource 'c' should see resources 'a' and 'b' as dependencies (even though
