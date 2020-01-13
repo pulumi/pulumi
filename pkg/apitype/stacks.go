dacd37756f4898ad0a6fd1f48c38d79533e3661b
Jamie Kinkead <jamiekinkead@Jamies-MBP.lan>
2020-01-13 15:58:42 -0800

    f

--- pkg/apitype/stacks.go
+++ pkg/apitype/stacks.go
@@ -23,11 +23,8 @@ type StackSummary struct {
 	// StackName is the name of the stack.
 	StackName string `json:"stackName"`
 
-	// LastUpdate is a Unix timestamp of the stack's last update, as applicable.
+	// LastUpdate is a Unix timestamp of the start time of the stack's last update, as applicable.
 	LastUpdate *int64 `json:"lastUpdate,omitempty"`
-
-	// CurentUpdate provides information about the current stack update in-progress as applicable.
-	CurrentUpdate *UpdateStatus `json:"currentUpdate,omitempty"`
 
 	// ResourceCount is the number of resources associated with this stack, as applicable.
 	ResourceCount *int `json:"resourceCount,omitempty"`
