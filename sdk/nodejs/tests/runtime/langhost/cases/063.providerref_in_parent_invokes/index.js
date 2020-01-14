2ec1825a1530c99f07c84fd431f3eeabeb97212b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-01-14 14:22:34 -0800

    Make  the default for  calls

--- sdk/nodejs/tests/runtime/langhost/cases/063.providerref_in_parent_invokes/index.js
+++ sdk/nodejs/tests/runtime/langhost/cases/063.providerref_in_parent_invokes/index.js
@@ -29,17 +29,17 @@ let pulumi = require("../../../../../");
         urn: "some-urn",
     };
 
-    let result1 = pulumi.runtime.invoke("test:index:echo", args, { parent });
+    let result1 = pulumi.runtime.invoke("test:index:echo", args, { parent, async: false });
     for (const key in args) {
         assert.deepEqual(result1[key], args[key]);
     }
 
-    let result2 = pulumi.runtime.invoke("test:index:echo", args, { parent });
+    let result2 = pulumi.runtime.invoke("test:index:echo", args, { parent, async: false });
     result2.then((v) => {
         assert.deepEqual(v, args);
     });
 
-    let result3 = pulumi.runtime.invoke("test:index:echo", args, { parent, async: true });
+    let result3 = pulumi.runtime.invoke("test:index:echo", args, { parent });
     result3.then((v) => {
         assert.deepEqual(v, args);
     });
