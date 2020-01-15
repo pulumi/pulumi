e2299845d40b536b6a9e294030307fbfe2e36a19
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-01-15 15:44:10 -0800

    Remove

--- sdk/nodejs/tests/runtime/langhost/cases/060.provider_invokes/index.js
+++ sdk/nodejs/tests/runtime/langhost/cases/060.provider_invokes/index.js
@@ -20,20 +20,6 @@ let args = {
     urn: "some-urn",
 };
 
-if (semver.lt(process.version, "12.11.0")) {
-	// These tests hang on runtimes later than 12.10.x due to their use of deasync.
-
-	let result1 = pulumi.runtime.invoke("test:index:echo", args, { provider });
-	for (const key in args) {
-		assert.deepEqual(result1[key], args[key]);
-	}
-
-	let result2 = pulumi.runtime.invoke("test:index:echo", args, { provider });
-	result2.then((v) => {
-		assert.deepEqual(v, args);
-	});
-}
-
 let result3 = pulumi.runtime.invoke("test:index:echo", args, { provider, async: true });
 result3.then((v) => {
     assert.deepEqual(v, args);
--- sdk/nodejs/tests/runtime/langhost/cases/061.provider_in_parent_invokes/index.js
+++ sdk/nodejs/tests/runtime/langhost/cases/061.provider_in_parent_invokes/index.js
@@ -27,20 +27,6 @@ let args = {
     urn: "some-urn",
 };
 
-if (semver.lt(process.version, "12.11.0")) {
-	// These tests hang on runtimes later than 12.10.x due to their use of deasync.
-
-	let result1 = pulumi.runtime.invoke("test:index:echo", args, { parent });
-	for (const key in args) {
-		assert.deepEqual(result1[key], args[key]);
-	}
-
-	let result2 = pulumi.runtime.invoke("test:index:echo", args, { parent });
-	result2.then((v) => {
-		assert.deepEqual(v, args);
-	});
-}
-
 let result3 = pulumi.runtime.invoke("test:index:echo", args, { parent, async: true });
 result3.then((v) => {
     assert.deepEqual(v, args);
