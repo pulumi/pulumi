bf3dc1cd0cecd4a5222216756da9343af66b8fd5
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-01-24 12:37:13 -0800

    Ensure that resources not specified by --target do not get added to checkpoint file. (#3797)

---
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -0,0 +1,32 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import * as pulumi from "@pulumi/pulumi";
+
+let currentID = 0;
+
+class Provider implements pulumi.dynamic.ResourceProvider {
+    public static instance = new Provider();
+
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+
+    constructor() {
+        this.create = async (inputs: any) => {
+            return {
+                id: (currentID++) + "",
+                outs: undefined,
+            };
+        };
+    }
+}
+
+class Resource extends pulumi.dynamic.Resource {
+    constructor(name: string, opts?: pulumi.ResourceOptions) {
+        super(Provider.instance, name, {}, opts);
+    }
+}
+
+// Create a resource using the default dynamic provider instance.
+let a = new Resource("a");
+let b = new Resource("b");
+
+export const urn = a.urn;
