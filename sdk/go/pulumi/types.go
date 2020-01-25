9c898e7e2b11d920d972ba06e4c4f1b250769116
Pat Gavlin <pat@pulumi.com>
2020-01-24 17:30:12 -0800

    Make primitive input types implement pointer types
    For example, pulumi.String also implements pulumi.StringPtr. This is
    consistent with the output of the code generator, and makes optional
    inputs much more ergonomic.

--- sdk/go/pulumi/types.go
+++ sdk/go/pulumi/types.go
@@ -766,12 +766,44 @@ func (AnyOutput) ElementType() reflect.Type {
 	return anyType
 }
 
+func (in ID) ToStringPtrOutput() StringPtrOutput {
+	return in.ToStringPtrOutputWithContext(context.Background())
+}
+
+func (in ID) ToStringPtrOutputWithContext(ctx context.Context) StringPtrOutput {
+	return in.ToStringOutputWithContext(ctx).ToStringPtrOutputWithContext(ctx)
+}
+
+func (o IDOutput) ToStringPtrOutput() StringPtrOutput {
+	return o.ToStringPtrOutputWithContext(context.Background())
+}
+
+func (o IDOutput) ToStringPtrOutputWithContext(ctx context.Context) StringPtrOutput {
+	return o.ToStringOutputWithContext(ctx).ToStringPtrOutputWithContext(ctx)
+}
+
 func (o IDOutput) awaitID(ctx context.Context) (ID, bool, error) {
 	id, known, err := o.await(ctx)
 	if !known || err != nil {
 		return "", known, err
 	}
 	return ID(convert(id, stringType).(string)), true, nil
+}
+
+func (in URN) ToStringPtrOutput() StringPtrOutput {
+	return in.ToStringPtrOutputWithContext(context.Background())
+}
+
+func (in URN) ToStringPtrOutputWithContext(ctx context.Context) StringPtrOutput {
+	return in.ToStringOutputWithContext(ctx).ToStringPtrOutputWithContext(ctx)
+}
+
+func (o URNOutput) ToStringPtrOutput() StringPtrOutput {
+	return o.ToStringPtrOutputWithContext(context.Background())
+}
+
+func (o URNOutput) ToStringPtrOutputWithContext(ctx context.Context) StringPtrOutput {
+	return o.ToStringOutputWithContext(ctx).ToStringPtrOutputWithContext(ctx)
 }
 
 func (o URNOutput) awaitURN(ctx context.Context) (URN, bool, error) {
