f69426da5b7861414ca79fef0c63bbb26c87da46
evanboyle <evan@pulumi.com>
2020-01-30 15:56:13 -0800

    alias test setup

---
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -0,0 +1,21 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+)
+
+// FooComponent is a component resource
+type FooComponent struct {
+	pulumi.ResourceState
+}
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		fooComponent := &FooComponent{}
+		ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
+
+		return nil
+	})
+}
