1a918ec5fd361cff6a0b56f4e399cb3d5390729e
evanboyle <evan@pulumi.com>
2020-02-03 15:54:13 -0800

    alias rename component test

---
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -0,0 +1,57 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+)
+
+// FooComponent is a component resource
+type FooResource struct {
+	pulumi.ResourceState
+}
+
+type FooComponent struct {
+	pulumi.ResourceState
+}
+
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
+	fooRes := &FooResource{}
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return fooRes, nil
+}
+
+// Scenario #3 - rename a component (and all it's children)
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
+	fooComp := &FooComponent{}
+	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
+	if err != nil {
+		return nil, err
+	}
+	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+	parentOpt := pulumi.Parent(fooComp)
+	_, err = NewFooResource(ctx, name+"-child", parentOpt)
+	if err != nil {
+		return nil, err
+	}
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	if err != nil {
+		return nil, err
+	}
+	return fooComp, nil
+}
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		_, err := NewFooComponent(ctx, "comp3")
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
