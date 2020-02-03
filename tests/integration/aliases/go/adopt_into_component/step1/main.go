de2b38c8fc879b3d45a7898550fa751e5aa5def5
evanboyle <evan@pulumi.com>
2020-02-03 13:30:20 -0800

    fix opts

--- tests/integration/aliases/go/adopt_into_component/step1/main.go
+++ tests/integration/aliases/go/adopt_into_component/step1/main.go
@@ -27,43 +27,43 @@ type FooComponent4 struct {
 	pulumi.ResourceState
 }
 
-func NewFooResource(ctx *pulumi.Context, name string, opt pulumi.ResourceOption) *FooResource {
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) *FooResource {
 	fooRes := &FooResource{}
-	ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opt)
+	ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	return fooRes
 }
 
-func NewFooComponent(ctx *pulumi.Context, name string, opt pulumi.ResourceOption) *FooComponent {
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) *FooComponent {
 	fooComp := &FooComponent{}
-	ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opt)
+	ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
 	return fooComp
 }
 
-func NewFooComponent2(ctx *pulumi.Context, name string, opt pulumi.ResourceOption) *FooComponent2 {
+func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) *FooComponent2 {
 	fooComp := &FooComponent2{}
-	ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opt)
+	ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
 	return fooComp
 }
 
-func NewFooComponent3(ctx *pulumi.Context, name string, opt pulumi.ResourceOption) *FooComponent3 {
+func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) *FooComponent3 {
 	fooComp := &FooComponent3{}
-	ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opt)
-	NewFooComponent2(ctx, name+"-child", opt)
+	ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
+	NewFooComponent2(ctx, name+"-child", opts...)
 	return fooComp
 }
 
-func NewFooComponent4(ctx *pulumi.Context, name string, opt pulumi.ResourceOption) *FooComponent4 {
+func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) *FooComponent4 {
 	fooComp := &FooComponent4{}
-	ctx.RegisterComponentResource("my:module:FooComponent4", name, fooComp, opt)
+	ctx.RegisterComponentResource("my:module:FooComponent4", name, fooComp, opts...)
 	return fooComp
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_ = NewFooResource(ctx, "res2", nil)
-		comp2 := NewFooComponent(ctx, "comp2", nil)
-		_ = NewFooComponent2(ctx, "unparented", nil)
-		_ = NewFooComponent3(ctx, "parentedbystack", nil)
+		_ = NewFooResource(ctx, "res2")
+		comp2 := NewFooComponent(ctx, "comp2")
+		_ = NewFooComponent2(ctx, "unparented")
+		_ = NewFooComponent3(ctx, "parentedbystack")
 		pbcOpt := pulumi.Parent(comp2)
 		_ = NewFooComponent3(ctx, "parentedbycomponent", pbcOpt)
 		dupeOpt := pulumi.Parent(comp2)
