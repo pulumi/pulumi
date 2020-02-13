0ed73364a362a78cf2af81e9bd03a0bc553cfab0
Lee-Ming Zen <lee@pulumi.com>
2020-02-12 23:04:07 -0800

    Skip test on Windows as stack tests seem to all be having issues.

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -1161,6 +1161,9 @@ func TestStackReferencePython(t *testing.T) {
 }
 
 func TestMultiStackReferencePython(t *testing.T) {
+	if runtime.GOOS == WindowsOS {
+		t.Skip("Temporarily skipping test on Windows - pulumi/pulumi#3811")
+	}
 	if owner := os.Getenv("PULUMI_TEST_OWNER"); owner == "" {
 		t.Skipf("Skipping: PULUMI_TEST_OWNER is not set")
 	}
