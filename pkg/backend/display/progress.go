53084ad4c92c371e0e8453850c7b54a78a3ff5ce
Justin Van Patten <jvp@justinvp.com>
2020-02-21 15:58:30 -0800

    Fix panic on failed stack policy
    Avoif panic during stack validations. This regressed recently when the PAC error output was modified.

--- pkg/backend/display/progress.go
+++ pkg/backend/display/progress.go
@@ -812,12 +812,18 @@ func (display *ProgressDisplay) printPolicyViolations() bool {
 			c = colors.SpecError
 		}
 
+		urn := policyEvent.ResourceURN
+		if urn == "" {
+			// An empty URN is associated with the stack itself.
+			urn = resource.DefaultRootStackURN(display.stack, display.proj)
+		}
+
 		policyNameLine := fmt.Sprintf("    %s[%s]  %s v%s %s %s (%s)",
 			c, policyEvent.EnforcementLevel,
 			policyEvent.PolicyPackName,
 			policyEvent.PolicyPackVersion, colors.Reset,
 			policyEvent.PolicyName,
-			policyEvent.ResourceURN.Name())
+			urn.Name())
 		display.writeSimpleMessage(policyNameLine)
 
 		// The message may span multiple lines, so we massage it so it will be indented properly.
