549ce57c66dedcdbb574ad24537dd2e68b797d9f
Lee-Ming Zen <lee@pulumi.com>
2020-02-22 00:23:45 -0800

    Fix a regression for CustomTimeouts in Python SDK * Accept dict as well as CustomTimeouts object

--- sdk/python/lib/pulumi/runtime/resource.py
+++ sdk/python/lib/pulumi/runtime/resource.py
@@ -343,16 +343,30 @@ def register_resource(res: 'Resource', ty: str, name: str, custom: bool, props: 
                 additional_secret_outputs = map(
                     res.translate_input_property, opts.additional_secret_outputs)
 
-            # translate the CustomTimeouts object
+            # Translate the CustomTimeouts object.
             custom_timeouts = None
             if opts.custom_timeouts is not None:
                 custom_timeouts = resource_pb2.RegisterResourceRequest.CustomTimeouts()
-                if opts.custom_timeouts.create is not None:
-                    custom_timeouts.create = opts.custom_timeouts.create
-                if opts.custom_timeouts.update is not None:
-                    custom_timeouts.update = opts.custom_timeouts.update
-                if opts.custom_timeouts.delete is not None:
-                    custom_timeouts.delete = opts.custom_timeouts.delete
+                # It could be an actual CustomTimeouts object.
+                if known_types.is_custom_timeouts(opts.custom_timeouts):
+                    print('CustomTimeouts for real')
+                    if opts.custom_timeouts.create is not None:
+                        custom_timeouts.create = opts.custom_timeouts.create
+                    if opts.custom_timeouts.update is not None:
+                        custom_timeouts.update = opts.custom_timeouts.update
+                    if opts.custom_timeouts.delete is not None:
+                        custom_timeouts.delete = opts.custom_timeouts.delete
+                # Or, it could be a workaround passing in a dict.
+                elif isinstance(opts.custom_timeouts, dict):
+                    print('CustomTimeouts dict')
+                    if 'create' in opts.custom_timeouts:
+                        custom_timeouts.create = opts.custom_timeouts['create']
+                    if 'update' in opts.custom_timeouts:
+                        custom_timeouts.update = opts.custom_timeouts['update']
+                    if 'delete' in opts.custom_timeouts:
+                        custom_timeouts.delete = opts.custom_timeouts['delete']
+                else:
+                    raise Exception("Expected custom_timeouts to be a CustomTimeouts object")
 
             req = resource_pb2.RegisterResourceRequest(
                 type=ty,
