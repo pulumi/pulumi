81b6afa3c7af28b6398f3d17228d38ce181fe898
Evan Boyle <EvanBoyle@users.noreply.github.com>
2020-03-06 17:50:18 -0800

    Revert "GetRequiredPlugins for Go (#3830)" (#4034)
    This reverts commit 3a2890c0cdc6f248dd0b5ce7a7360aaed7b9ef26.

--- sdk/go/pulumi/run.go
+++ sdk/go/pulumi/run.go
@@ -27,8 +27,6 @@ import (
 	"github.com/pulumi/pulumi/pkg/util/contract"
 )
 
-var ErrPlugins = errors.New("pulumi: plugins requested")
-
 // A RunOption is used to control the behavior of Run and RunErr.
 type RunOption func(*RunInfo)
 
@@ -37,13 +35,8 @@ type RunOption func(*RunInfo)
 // If the program fails, the process will be terminated and the function will not return.
 func Run(body RunFunc, opts ...RunOption) {
 	if err := RunErr(body, opts...); err != nil {
-		if err != ErrPlugins {
-			fmt.Fprintf(os.Stderr, "error: program failed: %v\n", err)
-			os.Exit(1)
-		}
-
-		printRequiredPlugins()
-		os.Exit(0)
+		fmt.Fprintf(os.Stderr, "error: program failed: %v\n", err)
+		os.Exit(1)
 	}
 }
 
@@ -53,9 +46,6 @@ func RunErr(body RunFunc, opts ...RunOption) error {
 	// Parse the info out of environment variables.  This is a lame contract with the caller, but helps to keep
 	// boilerplate to a minimum in the average Pulumi Go program.
 	info := getEnvInfo()
-	if info.getPlugins {
-		return ErrPlugins
-	}
 
 	for _, o := range opts {
 		o(&info)
@@ -131,7 +121,6 @@ type RunInfo struct {
 	MonitorAddr string
 	EngineAddr  string
 	Mocks       MockResourceMonitor
-	getPlugins  bool
 }
 
 // getEnvInfo reads various program information from the process environment.
@@ -139,7 +128,6 @@ func getEnvInfo() RunInfo {
 	// Most of the variables are just strings, and we can read them directly.  A few of them require more parsing.
 	parallel, _ := strconv.Atoi(os.Getenv(EnvParallel))
 	dryRun, _ := strconv.ParseBool(os.Getenv(EnvDryRun))
-	getPlugins, _ := strconv.ParseBool(os.Getenv(envPlugins))
 
 	var config map[string]string
 	if cfg := os.Getenv(EnvConfig); cfg != "" {
@@ -154,7 +142,6 @@ func getEnvInfo() RunInfo {
 		DryRun:      dryRun,
 		MonitorAddr: os.Getenv(EnvMonitor),
 		EngineAddr:  os.Getenv(EnvEngine),
-		getPlugins:  getPlugins,
 	}
 }
 
@@ -173,28 +160,4 @@ const (
 	EnvMonitor = "PULUMI_MONITOR"
 	// EnvEngine is the envvar used to read the current Pulumi engine RPC address.
 	EnvEngine = "PULUMI_ENGINE"
-	// envPlugins is the envvar used to request that the Pulumi program print its set of required plugins and exit.
-	envPlugins = "PULUMI_PLUGINS"
 )
-
-type PackageInfo struct {
-	Name    string `json:"name"`
-	Version string `json:"version,omitempty"`
-	Server  string `json:"server,omitempty"`
-}
-
-var packageRegistry = map[PackageInfo]struct{}{}
-
-func RegisterPackage(info PackageInfo) {
-	packageRegistry[info] = struct{}{}
-}
-
-func printRequiredPlugins() {
-	plugins := []PackageInfo{}
-	for info := range packageRegistry {
-		plugins = append(plugins, info)
-	}
-
-	err := json.NewEncoder(os.Stdout).Encode(map[string]interface{}{"plugins": plugins})
-	contract.IgnoreError(err)
-}
