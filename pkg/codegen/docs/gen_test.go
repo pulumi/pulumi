6ea790a1aaf87962cf49607ab82d6dfbe5a2ce84
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-03-19 18:43:54 -0700

    Baseline test for testing Python casing is complete.

--- pkg/codegen/docs/gen_test.go
+++ pkg/codegen/docs/gen_test.go
@@ -19,6 +19,8 @@
 package docs
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/codegen/python"
@@ -26,9 +28,171 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	simpleProperties = map[string]schema.PropertySpec{
+		"stringProp": {
+			Description: "A string prop.",
+			TypeSpec: schema.TypeSpec{
+				Type: "string",
+			},
+		},
+		"boolProp": {
+			Description: "A bool prop.",
+			TypeSpec: schema.TypeSpec{
+				Type: "boolean",
+			},
+		},
+	}
+
+	// testPackageSpec represents a fake package spec for a Provider used for testing.
+	testPackageSpec schema.PackageSpec
+)
+
+func initTestPackageSpec(t *testing.T) {
+	t.Helper()
+
+	pythonMapCase := map[string]json.RawMessage{
+		"python": json.RawMessage(`{"mapCase":false}`),
+	}
+	testPackageSpec = schema.PackageSpec{
+		Name:        providerPackage,
+		Description: "A fake provider package used for testing.",
+		Meta: &schema.MetadataSpec{
+			ModuleFormat: "(.*)(?:/[^/]*)",
+		},
+		Types: map[string]schema.ObjectTypeSpec{
+			// Package-level types.
+			"prov:/getPackageResourceOptions:getPackageResourceOptions": {
+				Description: "Options object for the package-level function getPackageResource.",
+				Type:        "object",
+				Properties:  simpleProperties,
+			},
+
+			// Module-level types.
+			"prov:module/getModuleResourceOptions:getModuleResourceOptions": {
+				Description: "Options object for the module-level function getModuleResource.",
+				Type:        "object",
+				Properties:  simpleProperties,
+			},
+			"prov:module/ResourceOptions:ResourceOptions": {
+				Description: "The resource options object.",
+				Type:        "object",
+				Properties: map[string]schema.PropertySpec{
+					"stringProp": {
+						Description: "A string prop.",
+						Language:    pythonMapCase,
+						TypeSpec: schema.TypeSpec{
+							Type: "string",
+						},
+					},
+					"boolProp": {
+						Description: "A bool prop.",
+						Language:    pythonMapCase,
+						TypeSpec: schema.TypeSpec{
+							Type: "boolean",
+						},
+					},
+				},
+			},
+			"prov:module/ResourceOptions2:ResourceOptions2": {
+				Description: "The resource options object.",
+				Type:        "object",
+				Properties: map[string]schema.PropertySpec{
+					"uniqueProp": {
+						Description: "This is a property unique to this type.",
+						Language:    pythonMapCase,
+						TypeSpec: schema.TypeSpec{
+							Type: "number",
+						},
+					},
+				},
+			},
+		},
+		Resources: map[string]schema.ResourceSpec{
+			"prov:module/resource:Resource": {
+				InputProperties: map[string]schema.PropertySpec{
+					"integerProp": {
+						Description: "This is integerProp's description.",
+						TypeSpec: schema.TypeSpec{
+							Type: "integer",
+						},
+					},
+					"stringProp": {
+						Description: "This is stringProp's description.",
+						TypeSpec: schema.TypeSpec{
+							Type: "string",
+						},
+					},
+					"boolProp": {
+						Description: "A bool prop.",
+						TypeSpec: schema.TypeSpec{
+							Type: "boolean",
+						},
+					},
+					"optionsProp": {
+						TypeSpec: schema.TypeSpec{
+							Ref: "#/types/prov:module/ResourceOptions:ResourceOptions",
+						},
+					},
+					"options2Prop": {
+						TypeSpec: schema.TypeSpec{
+							Ref: "#/types/prov:module/ResourceOptions2:ResourceOptions2",
+						},
+					},
+				},
+			},
+		},
+		Functions: map[string]schema.FunctionSpec{
+			// Package-level Functions.
+			"prov:/getPackageResource:getPackageResource": {
+				Description: "A package-level function.",
+				Inputs: &schema.ObjectTypeSpec{
+					Description: "Inputs for getPackageResource.",
+					Type:        "object",
+					Properties: map[string]schema.PropertySpec{
+						"options": {
+							TypeSpec: schema.TypeSpec{
+								Ref: "#/types/prov:/getPackageResourceOptions:getPackageResourceOptions",
+							},
+						},
+					},
+				},
+				Outputs: &schema.ObjectTypeSpec{
+					Description: "Outputs for getPackageResource.",
+					Properties:  simpleProperties,
+					Type:        "object",
+				},
+			},
+
+			// Module-level Functions.
+			"prov:module/getModuleResource:getModuleResource": {
+				Description: "A module-level function.",
+				Inputs: &schema.ObjectTypeSpec{
+					Description: "Inputs for getModuleResource.",
+					Type:        "object",
+					Properties: map[string]schema.PropertySpec{
+						"options": {
+							TypeSpec: schema.TypeSpec{
+								Ref: "#/types/prov:module/getModuleResource:getModuleResource",
+							},
+						},
+					},
+				},
+				Outputs: &schema.ObjectTypeSpec{
+					Description: "Outputs for getModuleResource.",
+					Properties:  simpleProperties,
+					Type:        "object",
+				},
+			},
+		},
+	}
+}
+
 // TestResourceNestedPropertyPythonCasing tests that the properties
 // of a nested object have the expected casing.
 func TestResourceNestedPropertyPythonCasing(t *testing.T) {
+	initTestPackageSpec(t)
+
 	schemaPkg, err := schema.ImportSpec(testPackageSpec)
 	assert.NoError(t, err, "importing spec")
 
@@ -41,26 +205,66 @@ func TestResourceNestedPropertyPythonCasing(t *testing.T) {
 			return
 		}
 
-		n := nestedTypes[0]
-		assert.Equal(t, "SomeResourceOptions", n.Name, "got %v instead of SomeResourceOptions", n.Name)
+		t.Run("InputPropertiesAreSnakeCased", func(t *testing.T) {
+			props := mod.getProperties(r.InputProperties, "python", true, false)
+			for _, p := range props {
+				assert.True(t, strings.Contains(p.Name, "_"), "input property name in python must use snake_case")
+			}
+		})
 
-		pyProps := n.Properties["python"]
-		nestedObject, ok := testPackageSpec.Types["prov:module/SomeResourceOptions:SomeResourceOptions"]
-		if !ok {
-			t.Error("sample schema package spec does not contain known object type")
-			return
-		}
+		// Non-unique nested properties are ones that have names that occur as direct input properties
+		// of the resource or elsewhere in the package and are mapped as snake_case even if the property
+		// itself has a "Language" spec with the `MapCase` value of `false`.
+		t.Run("NonUniqueNestedProperties", func(t *testing.T) {
+			n := nestedTypes[0]
+			assert.Equal(t, "ResourceOptions", n.Name, "got %v instead of ResourceOptions", n.Name)
 
-		for name := range nestedObject.Properties {
-			found := false
-			for _, prop := range pyProps {
-				if prop.Name == python.PyName(name) {
-					found = true
-					break
-				}
+			pyProps := n.Properties["python"]
+			nestedObject, ok := testPackageSpec.Types["prov:module/ResourceOptions:ResourceOptions"]
+			if !ok {
+				t.Error("sample schema package spec does not contain known object type")
+				return
 			}
 
-			assert.True(t, found, "expected to find %q", name)
-		}
+			for name := range nestedObject.Properties {
+				found := false
+				pyName := python.PyName(name)
+				for _, prop := range pyProps {
+					if prop.Name == pyName {
+						found = true
+						break
+					}
+				}
+
+				assert.True(t, found, "expected to find %q", pyName)
+			}
+		})
+
+		// Unique nested properties are those that only appear inside a nested object and therefore
+		// are never mapped to their snake_case. Therefore, such properties must be rendered with a
+		// camelCase.
+		t.Run("UniqueNestedProperties", func(t *testing.T) {
+			n := nestedTypes[1]
+			assert.Equal(t, "ResourceOptions2", n.Name, "got %v instead of ResourceOptions2", n.Name)
+
+			pyProps := n.Properties["python"]
+			nestedObject, ok := testPackageSpec.Types["prov:module/ResourceOptions2:ResourceOptions2"]
+			if !ok {
+				t.Error("sample schema package spec does not contain known object type")
+				return
+			}
+
+			for name := range nestedObject.Properties {
+				found := false
+				for _, prop := range pyProps {
+					if prop.Name == name {
+						found = true
+						break
+					}
+				}
+
+				assert.True(t, found, "expected to find %q", name)
+			}
+		})
 	}
 }
--- pkg/codegen/docs/testmain_test.go
+++ pkg/codegen/docs/testmain_test.go
@@ -21,8 +21,6 @@ package docs
 import (
 	"os"
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/codegen/schema"
 )
 
 const (
@@ -30,123 +28,6 @@ const (
 	providerPackage = "prov"
 )
 
-var (
-	simpleProperties = map[string]schema.PropertySpec{
-		"stringProp": {
-			Description: "A string prop.",
-			TypeSpec: schema.TypeSpec{
-				Type: "string",
-			},
-		},
-		"boolProp": {
-			Description: "A bool prop.",
-			TypeSpec: schema.TypeSpec{
-				Type: "boolean",
-			},
-		},
-	}
-
-	// testPackageSpec represents a fake package spec for a Provider used for testing.
-	testPackageSpec schema.PackageSpec
-)
-
-func initTestPackageSpec() {
-	testPackageSpec = schema.PackageSpec{
-		Name:        providerPackage,
-		Description: "A fake provider package used for testing.",
-		Meta: &schema.MetadataSpec{
-			ModuleFormat: "(.*)(?:/[^/]*)",
-		},
-		Types: map[string]schema.ObjectTypeSpec{
-			// Package-level types.
-			"prov:/getPackageResourceOptions:getPackageResourceOptions": {
-				Description: "Options object for the package-level function getPackageResource.",
-				Type:        "object",
-				Properties:  simpleProperties,
-			},
-
-			// Module-level types.
-			"prov:module/getModuleResourceOptions:getModuleResourceOptions": {
-				Description: "Options object for the module-level function getModuleResource.",
-				Type:        "object",
-				Properties:  simpleProperties,
-			},
-			"prov:module/SomeResourceOptions:SomeResourceOptions": {
-				Description: "The resource options object.",
-				Type:        "object",
-				Properties:  simpleProperties,
-			},
-		},
-		Resources: map[string]schema.ResourceSpec{
-			"prov:module/resource:Resource": {
-				InputProperties: map[string]schema.PropertySpec{
-					"integerProp": {
-						Description: "This is integerProp's description.",
-						TypeSpec: schema.TypeSpec{
-							Type: "integer",
-						},
-					},
-					"stringProp": {
-						Description: "This is stringProp's description.",
-						TypeSpec: schema.TypeSpec{
-							Type: "string",
-						},
-					},
-					"optionsProp": {
-						TypeSpec: schema.TypeSpec{
-							Ref: "#/types/prov:module/SomeResourceOptions:SomeResourceOptions",
-						},
-					},
-				},
-			},
-		},
-		Functions: map[string]schema.FunctionSpec{
-			// Package-level Functions.
-			"prov:/getPackageResource:getPackageResource": {
-				Description: "A package-level function.",
-				Inputs: &schema.ObjectTypeSpec{
-					Description: "Inputs for getPackageResource.",
-					Type:        "object",
-					Properties: map[string]schema.PropertySpec{
-						"options": {
-							TypeSpec: schema.TypeSpec{
-								Ref: "#/types/prov:/getPackageResourceOptions:getPackageResourceOptions",
-							},
-						},
-					},
-				},
-				Outputs: &schema.ObjectTypeSpec{
-					Description: "Outputs for getPackageResource.",
-					Properties:  simpleProperties,
-					Type:        "object",
-				},
-			},
-
-			// Module-level Functions.
-			"prov:module/getModuleResource:getModuleResource": {
-				Description: "A module-level function.",
-				Inputs: &schema.ObjectTypeSpec{
-					Description: "Inputs for getModuleResource.",
-					Type:        "object",
-					Properties: map[string]schema.PropertySpec{
-						"options": {
-							TypeSpec: schema.TypeSpec{
-								Ref: "#/types/prov:module/getModuleResource:getModuleResource",
-							},
-						},
-					},
-				},
-				Outputs: &schema.ObjectTypeSpec{
-					Description: "Outputs for getModuleResource.",
-					Properties:  simpleProperties,
-					Type:        "object",
-				},
-			},
-		},
-	}
-}
-
 func TestMain(m *testing.M) {
-	initTestPackageSpec()
 	os.Exit(m.Run())
 }
