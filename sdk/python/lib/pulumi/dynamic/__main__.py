28d69e963e9d109dc3385acf6852908cd2e72a2c
komal <komal@pulumi.com>
2020-04-21 11:08:22 -0700

    add tests for node and python

--- sdk/python/lib/pulumi/dynamic/__main__.py
+++ sdk/python/lib/pulumi/dynamic/__main__.py
@@ -26,9 +26,12 @@ from pulumi.runtime.proto import provider_pb2_grpc, ResourceProviderServicer
 from pulumi.dynamic import ResourceProvider
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+PROVIDER_KEY = "__provider"
+
 # _MAX_RPC_MESSAGE_SIZE raises the gRPC Max Message size from `4194304` (4mb) to `419430400` (400mb)
 _MAX_RPC_MESSAGE_SIZE = 1024 * 1024 * 400
-PROVIDER_KEY = "__provider"
+_GRPC_CHANNEL_OPTIONS = [('grpc.max_receive_message_length', _MAX_RPC_MESSAGE_SIZE)]
+
 
 def get_provider(props) -> ResourceProvider:
     byts = base64.b64decode(props[PROVIDER_KEY])
@@ -172,7 +175,7 @@ def main():
     monitor = DynamicResourceProviderServicer()
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=4),
-        options=[('grpc.max_receive_message_length', _MAX_RPC_MESSAGE_SIZE)]
+        options=_GRPC_CHANNEL_OPTIONS
     )
     provider_pb2_grpc.add_ResourceProviderServicer_to_server(monitor, server)
     port = server.add_insecure_port(address="0.0.0.0:0")
