1a7a1a8c9aac51b76924edfee687b9e56cb65405
Justin Van Patten <jvp@justinvp.com>
2020-05-21 17:50:56 -0700

    Use REM for comments
    Avoids `The system cannot find the drive specified.` due to an obscure effect in batch code blocks when there is a second line that starts with `::` that is treated as a drive separator.

--- sdk/python/dist/pulumi-analyzer-policy-python.cmd
+++ sdk/python/dist/pulumi-analyzer-policy-python.cmd
@@ -1,15 +1,15 @@
 @echo off
 
-:: Save the first two arguments.
+REM Save the first two arguments.
 set "pulumi_policy_python_engine_address=%1"
 set "pulumi_policy_python_program=%2"
 
-:: Parse the -virtualenv command line argument.
+REM Parse the -virtualenv command line argument.
 set pulumi_policy_python_virtualenv=
 :parse
 if "%~1"=="" goto endparse
 if "%~1"=="-virtualenv" (
-    :: Get the value as a fully-qualified path.
+    REM Get the value as a fully-qualified path.
     set "pulumi_policy_python_virtualenv=%~f2"
     goto endparse
 )
@@ -18,13 +18,13 @@ goto parse
 :endparse
 
 if defined pulumi_policy_python_virtualenv (
-    :: If python exists in the virtual environment, set PATH and run it.
+    REM If python exists in the virtual environment, set PATH and run it.
     if exist "%pulumi_policy_python_virtualenv%\Scripts\python.exe" (
-        :: Update PATH and unset PYTHONHOME.
+        REM Update PATH and unset PYTHONHOME.
         set "PATH=%pulumi_policy_python_virtualenv%\Scripts;%PATH%"
         set PYTHONHOME=
 
-        :: Run python from the virtual environment.
+        REM Run python from the virtual environment.
         "%pulumi_policy_python_virtualenv%\Scripts\python.exe" -u -m pulumi.policy %pulumi_policy_python_engine_address% %pulumi_policy_python_program%
         exit /B
     ) else (
@@ -32,7 +32,7 @@ if defined pulumi_policy_python_virtualenv (
         exit 1
     )
 ) else (
-    :: Otherwise, just run python. We use `python` instead of `python3` because Windows
-    :: Python installers install only `python.exe` by default.
+    REM Otherwise, just run python. We use `python` instead of `python3` because Windows
+    REM Python installers install only `python.exe` by default.
     @python -u -m pulumi.policy %pulumi_policy_python_engine_address% %pulumi_policy_python_program%
 )
