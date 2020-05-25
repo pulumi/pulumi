33db0971763dd65451fd06ec747f8eb3a3400966
Paul Stack <public@paulstack.co.uk>
2020-05-25 14:36:20 +0100

    Fixup broken bash check for npmshrink-wrap.json file

--- dist/actions/entrypoint.sh
+++ dist/actions/entrypoint.sh
@@ -100,7 +100,7 @@ if [ -e package.json ]; then
         if [ ! -z "$NPM_AUTH_TOKEN" ]; then
             echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
         fi
-        if [ -f package-lock.json ] || [ -f npm-shrinkwrap.json]; then
+        if [ -f package-lock.json ] || [ -f npm-shrinkwrap.json ]; then
           npm ci
         else
           npm install
