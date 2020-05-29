a6ecd5c58099c388233173456d0b06a34a7f3b0b
Lee Briggs <lee@leebriggs.co.uk>
2020-05-29 11:03:23 -0700

    improve dev version detection
    Currently the `isDevVersion` function only tests for "dev" versions.
    When installing pulumi from Homebrew with the `--HEAD` flag we get an
    alpha version locally. This can mean the user gets the upgrade prompt
    when it's not valid.
    
    This change changes the logic to check against a regex for all the
    possible semver `pre` versions.
    
    It also adds a unit test to ensure we're getting the right result

---
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -0,0 +1,35 @@
+// Copyright 2016-2020, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"testing"
+
+	"github.com/blang/semver"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsDevVersion(t *testing.T) {
+
+	// This function primarily focuses on the "Pre" section of the semver string
+	// So we'll focus on testing that
+	stableVer, _ := semver.ParseTolerant("1.0.0")
+	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
+	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
+
+	assert.False(t, isDevVersion(stableVer))
+	assert.True(t, isDevVersion(devVer))
+	assert.True(t, isDevVersion(alphaVer))
+
+}
