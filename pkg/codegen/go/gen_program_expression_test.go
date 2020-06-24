910ba581530cbfa09b104038f5401a9720f6cb3f
evanboyle <evan@pulumi.com>
2020-06-24 12:35:10 -0700

    skip transient test

--- pkg/codegen/go/gen_program_expression_test.go
+++ pkg/codegen/go/gen_program_expression_test.go
@@ -45,6 +45,7 @@ func TestLiteralExpression(t *testing.T) {
 }
 
 func TestBinaryOpExpression(t *testing.T) {
+	t.Skip("Skipping test due to https://github.com/pulumi/pulumi/issues/4885")
 	env := environment(map[string]interface{}{
 		"a": model.BoolType,
 		"b": model.BoolType,
