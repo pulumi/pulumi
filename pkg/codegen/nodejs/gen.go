de2a6500cdceccfd9c35856722a0c8c60ca8231c
Mikhail Shilkov <github@mikhail.io>
2020-07-01 21:47:46 +0200

    Do not import non-existing inputs or outputs in Node.js codegen (#4937)
    Do not import non-existing inputs or outputs in Node.js codegen

--- pkg/codegen/nodejs/gen.go
+++ pkg/codegen/nodejs/gen.go
@@ -878,9 +878,9 @@ func (mod *modContext) configGetter(v *schema.Property) (string, string) {
 
 func (mod *modContext) genConfig(w io.Writer, variables []*schema.Property) error {
 	imports := map[string]codegen.StringSet{}
-	mod.getImports(variables, imports)
+	referencesNestedTypes := mod.getImports(variables, imports)
 
-	mod.genHeader(w, mod.sdkImports(true, true), imports)
+	mod.genHeader(w, mod.sdkImports(referencesNestedTypes, true), imports)
 
 	// Create a config bag for the variables to pull from.
 	fmt.Fprintf(w, "let __config = new pulumi.Config(\"%v\");\n", mod.pkg.Name)
