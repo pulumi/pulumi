e47280607dbf4c91e8e5c1e55fdb9d614fb6dc90
komal <komal@pulumi.com>
2020-07-02 14:02:16 -0700

    Move docstrings

--- sdk/python/lib/pulumi/asset.py
+++ sdk/python/lib/pulumi/asset.py
@@ -31,14 +31,14 @@ class Asset:
 
 @known_types.file_asset
 class FileAsset(Asset):
-    path: str
-
     """
     A FileAsset is a kind of asset produced from a given path to a file on
     the local filesysetm.
     """
+    path: str
 
     def __init__(self, path: Union[str, PathLike]) -> None:
+
         if not isinstance(path, (str, PathLike)):
             raise TypeError("FileAsset path must be a string or os.PathLike")
         self.path = fspath(path)
@@ -46,11 +46,11 @@ class FileAsset(Asset):
 
 @known_types.string_asset
 class StringAsset(Asset):
-    text: str
-
     """
     A StringAsset is a kind of asset produced from an in-memory UTF-8 encoded string.
     """
+    text: str
+
     def __init__(self, text: str) -> None:
         if not isinstance(text, str):
             raise TypeError("StringAsset data must be a string")
@@ -59,14 +59,14 @@ class StringAsset(Asset):
 
 @known_types.remote_asset
 class RemoteAsset(Asset):
-    uri: str
-
     """
     A RemoteAsset is a kind of asset produced from a given URI string. The URI's scheme
     dictates the protocol for fetching contents: "file://" specifies a local file, "http://"
     and "https://" specify HTTP and HTTPS, respectively. Note that specific providers may recognize
     alternative schemes; this is merely the base-most set that all providers support.
     """
+    uri: str
+
     def __init__(self, uri: str) -> None:
         if not isinstance(uri, str):
             raise TypeError("RemoteAsset URI must be a string")
@@ -76,17 +76,17 @@ class RemoteAsset(Asset):
 @known_types.archive
 class Archive:
     """
-    Asset represents a collection of named assets.
+    Archive represents a collection of named assets.
     """
 
 
 @known_types.asset_archive
 class AssetArchive(Archive):
-    assets: Dict[str, Union[Asset, Archive]]
-
     """
     An AssetArchive is an archive created from an in-memory collection of named assets or other archives.
     """
+    assets: Dict[str, Union[Asset, Archive]]
+
     def __init__(self, assets: Dict[str, Union[Asset, Archive]]) -> None:
         if not isinstance(assets, dict):
             raise TypeError("AssetArchive assets must be a dictionary")
@@ -100,12 +100,12 @@ class AssetArchive(Archive):
 
 @known_types.file_archive
 class FileArchive(Archive):
-    path: str
-
     """
     A FileArchive is a file-based archive, or collection of file-based assets.  This can be
     a raw directory or a single archive file in one of the supported formats (.tar, .tar.gz, or .zip).
     """
+    path: str
+
     def __init__(self, path: str) -> None:
         if not isinstance(path, str):
             raise TypeError("FileArchive path must be a string")
@@ -114,13 +114,13 @@ class FileArchive(Archive):
 
 @known_types.remote_archive
 class RemoteArchive(Archive):
-    uri: str
-
     """
     A RemoteArchive is a file-based archive fetched from a remote location.  The URI's scheme dictates
     the protocol for fetching contents: "file://" specifies a local file, "http://" and "https://"
     specify HTTP and HTTPS, respectively, and specific providers may recognize custom schemes.
     """
+    uri: str
+
     def __init__(self, uri: str) -> None:
         if not isinstance(uri, str):
             raise TypeError("RemoteArchive URI must be a string")
