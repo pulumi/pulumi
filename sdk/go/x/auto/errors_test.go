41d6767e3224a41ed09abb1c6139904e2f6847ad
evanboyle <evan@pulumi.com>
2020-07-26 12:52:59 -0700

    host error handling, inline concurrent update detection

--- sdk/go/x/auto/errors_test.go
+++ sdk/go/x/auto/errors_test.go
@@ -5,6 +5,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,6 +17,72 @@ func TestConcurrentUpdateError(t *testing.T) {
 	ps := ProjectSpec{
 		Name:       "conflict_error",
 		SourcePath: filepath.Join(".", "test", "errors", "conflict_error"),
+	}
+	ss := StackSpec{
+		Name:    sName,
+		Project: ps,
+	}
+
+	// initialize
+	s, err := NewStack(ss)
+	if err != nil {
+		t.Errorf("failed to initialize stack, err: %v", err)
+		t.FailNow()
+	}
+
+	c := make(chan error)
+
+	// parallel updates to cause conflict
+	for i := 0; i < 5; i++ {
+		go func() {
+			_, err := s.Up()
+			c <- err
+		}()
+	}
+
+	conflicts := 0
+
+	for i := 0; i < 5; i++ {
+		err := <-c
+		if IsConcurrentUpdateError(err) {
+			conflicts++
+		}
+	}
+
+	// -- pulumi destroy --
+
+	dRes, err := s.Destroy()
+	if err != nil {
+		t.Errorf("destroy failed, err: %v", err)
+		t.FailNow()
+	}
+
+	assert.Equal(t, "destroy", dRes.Summary.Kind)
+	assert.Equal(t, "succeeded", dRes.Summary.Result)
+
+	// -- pulumi stack rm --
+
+	err = s.Remove()
+	assert.Nil(t, err, "failed to remove stack. Resources have leaked.")
+
+	// should have at least one conflict
+	assert.Greater(t, conflicts, 0)
+}
+
+func TestInlineConcurrentUpdateError(t *testing.T) {
+	sName := fmt.Sprintf("int_test%d", rangeIn(10000000, 99999999))
+	ps := ProjectSpec{
+		Name: "inline_conflict_error",
+		InlineSource: func(ctx *pulumi.Context) error {
+			ctx.Export("exp_static", pulumi.String("foo"))
+			return nil
+		},
+		Overrides: &ProjectOverrides{
+			Project: &workspace.Project{
+				Name:    "inline_conflict_error",
+				Runtime: workspace.NewProjectRuntimeInfo("go", map[string]interface{}{} /*options*/),
+			},
+		},
 	}
 	ss := StackSpec{
 		Name:    sName,
--- sdk/go/x/auto/up.go
+++ sdk/go/x/auto/up.go
@@ -1,15 +1,12 @@
 package auto
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
-	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func (s *stack) Up() (UpResult, error) {
@@ -276,62 +273,4 @@ func (s *stack) runCmd(name string, arg ...string) (string, string, int, error) 
 		code = exitError.ExitCode()
 	}
 	return stdout.String(), stderr.String(), code, err
-}
-
-func (s *stack) host(isPreview bool) (string, string, error) {
-	var stdout bytes.Buffer
-	var errBuff bytes.Buffer
-	args := []string{"host"}
-	if isPreview {
-		args = append(args, "preview")
-	}
-	cmd := exec.Command("pulumi", args...)
-	cmd.Dir = s.SourcePath
-	cmd.Stdout = &stdout
-	stderr, _ := cmd.StderrPipe()
-	cmd.Start()
-	scanner := bufio.NewScanner(stderr)
-	scanner.Split(bufio.ScanLines)
-
-	addrChan := make(chan string)
-	go func() {
-		for scanner.Scan() {
-			m := scanner.Text()
-			errBuff.WriteString(m)
-			if strings.HasPrefix(m, "127.0.0.1:") {
-				addrChan <- m
-			}
-		}
-	}()
-	monitorAddr := <-addrChan
-
-	os.Setenv(pulumi.EnvMonitor, monitorAddr)
-	os.Setenv(pulumi.EnvProject, s.ProjectName)
-	os.Setenv(pulumi.EnvStack, s.Name)
-	cfg, err := s.rawConfig()
-	if err != nil {
-		return stdout.String(), errBuff.String(), errors.Wrap(err, "failed to serialize config for inline program")
-	}
-	cfgStr, err := json.Marshal(cfg)
-	if err != nil {
-		return stdout.String(), errBuff.String(), errors.Wrap(err, "unable to marshal config")
-	}
-	os.Setenv(pulumi.EnvConfig, string(cfgStr))
-	err = pulumi.RunErr(s.InlineSource)
-	if err != nil {
-		cmd.Process.Signal(os.Interrupt)
-		err = cmd.Process.Kill()
-		if err != nil {
-			return stdout.String(), errBuff.String(), errors.Wrap(err, "failed to run inline program and shutdown gracefully")
-		}
-		return stdout.String(), errBuff.String(), errors.Wrap(err, "error running inline pulumi program")
-	}
-
-	err = cmd.Process.Signal(os.Interrupt)
-	if err != nil {
-		return stdout.String(), errBuff.String(), errors.Wrap(err, "failed to shutdown host gracefully")
-	}
-	cmd.Wait()
-
-	return stdout.String(), errBuff.String(), nil
 }
