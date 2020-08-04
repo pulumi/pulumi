7ca04ad355686d49c3ea6348cb802b67890f9826
komal <komal@pulumi.com>
2020-08-04 15:39:42 -0700

    Add more tests

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -5,14 +5,16 @@ import "testing"
 var pyNameTests = []struct {
 	input    string
 	expected string
+	legacy   string
 }{
-	{"kubeletConfigKey", "kubelet_config_key"},
-	{"podCIDR", "pod_cidr"},
-	{"podCidr", "pod_cidr"},
-	{"podCIDRs", "pod_cidrs"},
-	{"podCIDRSet", "pod_cidr_set"},
-	{"Sha256Hash", "sha256_hash"},
-	{"SHA256Hash", "sha256_hash"},
+	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},
+	{"podCIDR", "pod_cidr", "pod_cidr"},
+	{"podCidr", "pod_cidr", "pod_cidr"},
+	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},
+	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},
+	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},
+	{"Sha256Hash", "sha256_hash", "sha256_hash"},
+	{"SHA256Hash", "sha256_hash", "sha256_hash"},
 }
 
 func TestPyName(t *testing.T) {
@@ -20,6 +22,17 @@ func TestPyName(t *testing.T) {
 		t.Run(tt.input, func(t *testing.T) {
 			result := PyName(tt.input)
 			if result != tt.expected {
+				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestPyNameLegacy(t *testing.T) {
+	for _, tt := range pyNameTests {
+		t.Run(tt.input, func(t *testing.T) {
+			result := PyNameLegacy(tt.input)
+			if result != tt.legacy {
 				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
 			}
 		})
