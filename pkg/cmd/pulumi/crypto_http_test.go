40b29b7b113bcb7ac4a71ffb7d4ed1bd90ee8807
stack72 <public@paulstack.co.uk>
2020-08-07 13:34:16 +0100

    Ensure Project Stack is not loaded and saved unnecessariliy for default secrets manager
    Fixes: #5126
    
    In the newServiceSecretsManager, we were loading and saving the project stack
    without understanding if any changes had actually been made - e.g. changing the
    stack to be a new secrets provider
    
    This has now been guarded against, tests added to understand when it will
    actually be saved and we no longer get unnecessary config sorting when interacting
    with a default stack AND no longer get an empty config map in our config
    file when we initiate a new stack with a default secrets provider

---
+++ pkg/cmd/pulumi/crypto_http_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChangeProjectStackSecretDetails(t *testing.T) {
+	//projStack := &workspace.ProjectStack{
+	//	Name:    tokens.PackageName("test-package"),
+	//	Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
+	//}
+	//
+	//assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
+	//assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
+	tests := []struct {
+		TestName     string
+		ProjectStack workspace.ProjectStack
+		Expected     bool
+	}{
+		{
+			TestName: "Expects to save stack when existing secrets manager is cloud",
+			ProjectStack: workspace.ProjectStack{
+				Config:          make(config.Map),
+				SecretsProvider: "awskms://alias/TestProvider?region=us-west-2",
+				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
+			},
+			Expected: true,
+		},
+		{
+			TestName: "Expects to save stack when existing secrets manager is passphrase",
+			ProjectStack: workspace.ProjectStack{
+				Config:         make(config.Map),
+				EncryptionSalt: "v1:/AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
+			},
+			Expected: true,
+		},
+		{
+			TestName: "Does not expect to save stack when existing secrets manager is service",
+			ProjectStack: workspace.ProjectStack{
+				Config: make(config.Map),
+			},
+			Expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.TestName, func(t *testing.T) {
+			requiresProjectSave := changeProjectStackSecretDetails(&test.ProjectStack)
+			assert.Equal(t, test.Expected, requiresProjectSave)
+		})
+	}
+}
