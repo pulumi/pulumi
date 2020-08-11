56e3984d86f2d56cbee3a6efcc5c4509ca46edd3
evanboyle <evan@pulumi.com>
2020-08-11 09:58:12 -0700

    fix dotnet test

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -5,27 +5,27 @@ class MyStack : Stack
 {
     public MyStack()
     {
-        var argocd_serverDeployment = new Kubernetes.Apps.v1.Deployment("argocd_serverDeployment", new Kubernetes.Apps.v1.DeploymentArgs
+        var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
         {
             ApiVersion = "apps/v1",
             Kind = "Deployment",
-            Metadata = new Kubernetes.Meta.Inputs.ObjectMetaArgs
+            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
                 Name = "argocd-server",
             },
-            Spec = new Kubernetes.Apps.Inputs.DeploymentSpecArgs
+            Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
             {
-                Template = new Kubernetes.Core.Inputs.PodTemplateSpecArgs
+                Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
                 {
-                    Spec = new Kubernetes.Core.Inputs.PodSpecArgs
+                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
                     {
                         Containers = 
                         {
-                            new Kubernetes.Core.Inputs.ContainerArgs
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                             {
-                                ReadinessProbe = new Kubernetes.Core.Inputs.ProbeArgs
+                                ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
-                                    HttpGet = new Kubernetes.Core.Inputs.HTTPGetActionArgs
+                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
                                     },
