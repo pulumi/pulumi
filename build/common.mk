7a0189176500b8d2a973f3d03483492aad1a1c27
Lee Briggs <lee@leebriggs.co.uk>
2020-08-14 22:43:01 -0700

    back to yarn

--- build/common.mk
+++ build/common.mk
@@ -153,7 +153,7 @@ all:: build install lint test_all
 
 ensure::
 	$(call STEP_MESSAGE)
-	@if [ -e 'package.json' ]; then echo "npm ci"; npm ci; fi
+	@if [ -e 'package.json' ]; then echo "yarn instal"; rm -rf node_moduled & yarn install; fi
 build::
 	$(call STEP_MESSAGE)
 
