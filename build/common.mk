42bbb0a035c3d9e2182beb5ecfe6b04fec0a9cfa
Lee Briggs <lee@leebriggs.co.uk>
2020-08-18 16:54:13 -0700

    don't lint when running only_test

--- build/common.mk
+++ build/common.mk
@@ -192,8 +192,8 @@ install::
 endif
 
 only_build:: build install
-only_test:: lint test_all
-only_test_fast:: lint test_fast
+only_test:: test_all
+only_test_fast:: test_fast
 
 # Generate targets for each sub project. This project's default and
 # all targets will depend on the sub project's targets, and the
