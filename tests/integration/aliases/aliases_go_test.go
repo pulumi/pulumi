ae996153b8ded79d8cd01e1c909c144b16c29cf5
Lee Briggs <lee@leebriggs.co.uk>
2020-08-27 08:03:33 -0700

    fix incorrect gopath

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -25,7 +25,7 @@ func TestGoAliases(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
-					"github.com/pulumi/pulumi/sdk/v2",
+					"github.com/pulumi/pulumi/sdk/v2/go",
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
