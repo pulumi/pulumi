e5b6c612188c926fb61a2df43116727d194e1223
evanboyle <evan@pulumi.com>
2020-09-14 09:10:02 -0700

    LocalWorkspace.saveProjectSettings()

--- sdk/nodejs/x/automation/localWorkspace.ts
+++ sdk/nodejs/x/automation/localWorkspace.ts
@@ -4,6 +4,7 @@ import { StackSettings } from "./stackSettings";
 import { settings } from "cluster";
 import * as fs from "fs";
 import * as upath from "upath";
+import { promises } from "dns";
 
 
 export class LocalWorkspace /*implements Workspace TODO */ {
@@ -32,6 +33,25 @@ export class LocalWorkspace /*implements Workspace TODO */ {
             return Promise.resolve(ProjectSettings.fromYAML(contents))
         }
         return Promise.reject(new Error(`failed to find project settings file in workdir: ${this.workDir}`));
+    }
+    saveProjectSettings(settings: ProjectSettings): Promise<void> {
+        let foundExt = ".yaml";
+        for (let ext of settingsExtensions) {
+           const path = upath.joinSafe(this.workDir, `Pulumi${ext}`);
+           if (fs.existsSync(path)) {
+               foundExt = ext;
+               break;
+           }
+        }
+        const path = upath.joinSafe(this.workDir, `Pulumi${foundExt}`);
+        let contents;
+        if (foundExt === ".json") {
+            contents = JSON.stringify(settings, null, 4);
+        }
+        else {
+            contents = settings.toYAML();
+        }
+        return Promise.resolve(fs.writeFileSync(path, contents))
     }
 }
 
