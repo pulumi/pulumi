fa388380c48e6b621ddcb57c9dd27034b5d4865e
evanboyle <evan@pulumi.com>
2020-09-14 07:55:06 -0700

    LocalWorkspace.projectSettings()

--- sdk/nodejs/x/automation/localWorkspace.ts
+++ sdk/nodejs/x/automation/localWorkspace.ts
@@ -1,6 +1,10 @@
 import { Workspace } from "./workspace";
 import { ProjectSettings } from "./projectSettings";
 import { StackSettings } from "./stackSettings";
+import { settings } from "cluster";
+import * as fs from "fs";
+import * as upath from "upath";
+
 
 export class LocalWorkspace /*implements Workspace TODO */ {
     private workDir?: string;
@@ -17,14 +21,25 @@ export class LocalWorkspace /*implements Workspace TODO */ {
         }
     }
     projectSettings(): Promise<ProjectSettings>{
-        // TODO
-        return Promise.resolve(new ProjectSettings());
+        for (let ext of settingsExtensions) {
+            const isJSON = ext === ".json";
+            const path = upath.joinSafe(this.workDir, `Pulumi${ext}`);
+            if (!fs.existsSync(path)) continue;
+            const contents = fs.readFileSync(path).toString();
+            if (isJSON) {
+                return Promise.resolve(ProjectSettings.fromJSON(JSON.parse(contents)))
+            }
+            return Promise.resolve(ProjectSettings.fromYAML(contents))
+        }
+        return Promise.reject(new Error(`failed to find project settings file in workdir: ${this.workDir}`));
     }
 }
 
 export type LocalWorkspaceOpts = {
-    workDir: string,
-    pulumiHome: string,
-    program: ()=>void,
-    envVars: Map<string,string>,
+    workDir?: string,
+    pulumiHome?: string,
+    program?: ()=>void,
+    envVars?: Map<string,string>,
 }
+
+export const settingsExtensions = [".yaml", ".yml", ".json"];
