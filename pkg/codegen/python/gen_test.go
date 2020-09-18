236bc8e870b689ef17ef1c2350bfb035b3660b7f
Levi Blackstone <levi@pulumi.com>
2020-09-18 11:33:36 -0600

    Remove python test for now

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -1,14 +1,6 @@
 package python
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"path/filepath"
-	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"
-)
+import "testing"
 
 var pathTests = []struct {
 	input    string
@@ -33,30 +25,4 @@ func TestRelPathToRelImport(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestGeneratePackage(t *testing.T) {
-	// Read in, decode, and import the schema.
-	schemaBytes, err := ioutil.ReadFile(filepath.Join("..", "internal", "test", "testdata", "schema-simple.json"))
-	if err != nil {
-		panic(err)
-	}
-
-	var pkgSpec schema.PackageSpec
-	if err = json.Unmarshal(schemaBytes, &pkgSpec); err != nil {
-		panic(err)
-	}
-
-	pkg, err := schema.ImportSpec(pkgSpec, nil)
-	if err != nil {
-		t.Errorf("ImportSpec() error = %v", err)
-	}
-
-	files, err := GeneratePackage("test", pkg, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	assert.Contains(t, files, filepath.Join("pulumi_example", "resource.py"))
-	assert.Contains(t, files, filepath.Join("pulumi_example", "other_resource.py"))
 }
