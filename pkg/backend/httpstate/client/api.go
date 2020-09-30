bcac6cf1d772c2a0bfbba45df821d0a1a7e6c331
Julien Champseix <julien.champseix@gmail.com>
2020-09-30 21:52:35 +0200

    only close the gzip reader if a valid pointer was returned

--- pkg/backend/httpstate/client/api.go
+++ pkg/backend/httpstate/client/api.go
@@ -336,10 +336,10 @@ func readBody(resp *http.Response) ([]byte, error) {
 	case "gzip":
 		logging.V(apiRequestDetailLogLevel).Infoln("decompressing gzipped response from service")
 		reader, err := gzip.NewReader(resp.Body)
-		defer contract.IgnoreClose(reader)
 		if err != nil {
 			return nil, errors.Wrap(err, "reading gzip-compressed body")
 		}
+		defer contract.IgnoreClose(reader)
 
 		return ioutil.ReadAll(reader)
 	default:
