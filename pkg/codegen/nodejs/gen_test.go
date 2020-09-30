38172999e7dbcb2c555dc9c8b0757c8bae389130
Levi Blackstone <levi@pulumi.com>
2020-09-30 14:41:52 -0600

    [codegen] Extract common test logic (#5480)

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -1,12 +1,10 @@
 package nodejs
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,8 +13,6 @@ func TestGeneratePackage(t *testing.T) {
 		name          string
 		schemaDir     string
 		expectedFiles []string
-		wantErr       bool
-		validator     func(files, expectedFiles map[string][]byte)
 	}{
 		{
 			"Simple schema with local resource properties",
@@ -26,46 +22,19 @@ func TestGeneratePackage(t *testing.T) {
 				"otherResource.ts",
 				"argFunction.ts",
 			},
-			false,
-			func(files, expectedFiles map[string][]byte) {
-				for name, file := range expectedFiles {
-					assert.Contains(t, files, name)
-					assert.Equal(t, file, files[name])
-				}
-			},
 		},
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Read in, decode, and import the schema.
-			schemaBytes, err := ioutil.ReadFile(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"))
+			files, err := test.GeneratePackageFilesFromSchema(
+				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
 
-			expectedFiles := map[string][]byte{}
-			for _, file := range tt.expectedFiles {
-				fileBytes, err := ioutil.ReadFile(filepath.Join(testDir, tt.schemaDir, file))
-				assert.NoError(t, err)
-
-				expectedFiles[file] = fileBytes
-			}
-
-			var pkgSpec schema.PackageSpec
-			err = json.Unmarshal(schemaBytes, &pkgSpec)
+			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), tt.expectedFiles)
 			assert.NoError(t, err)
 
-			pkg, err := schema.ImportSpec(pkgSpec, nil)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("ImportSpec() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-
-			files, err := GeneratePackage("test", pkg, nil)
-			if err != nil {
-				panic(err)
-			}
-			tt.validator(files, expectedFiles)
+			test.ValidateFileEquality(t, files, expectedFiles)
 		})
 	}
 }
