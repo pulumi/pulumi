15ba5d83c63abca32904de8654afbbb5408c8204
komal <komal@pulumi.com>
2020-10-10 13:46:17 -0700

    Dotnet enums example

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -23,6 +23,16 @@ func TestGeneratePackage(t *testing.T) {
 				"ArgFunction.cs",
 			},
 		},
+		{
+			"Simple schema with enum types",
+			"simple-enum-schema",
+			[]string{
+				"Tree/V1/RubberTree.cs",
+				"Tree/V1/Enums.cs",
+				"Enums.cs",
+				"Inputs/ContainerArgs.cs",
+			},
+		},
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
