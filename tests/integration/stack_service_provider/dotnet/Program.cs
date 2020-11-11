45626093b0cf7307b4a2828a12835e39c165b3c1
Nathan Winder <nathan.winder@perficient.com>
2020-11-10 19:29:20 -0600

    Added integration test

---
+++ tests/integration/stack_service_provider/dotnet/Program.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+
+class MyStack : Stack
+{
+    [Output("abc")]
+    public Output<string> Abc { get; private set; }
+
+    [Output]
+    public Output<int> Foo { get; private set; }
+
+    // This should NOT be exported as stack output due to the missing attribute
+    public Output<string> Bar { get; private set; }
+
+    public MyStack(Dependancy dependancy)
+    {
+        this.Abc = Output.Create(dependancy.Abc);
+        this.Foo = Output.Create(dependancy.Foo);
+        this.Bar = Output.Create(dependancy.Bar);
+    }
+}
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        Deployment.RunAsync<MyStack>(new TestServiceProvider());
+    }
+}
+
+class Dependancy
+{
+    public string Abc { get; set; } = "ABC";
+    public int Foo { get; set; } = 42;
+    public string Bar { get; set; } = "this should not come to output";
+}
+
+class TestServiceProvider : IServiceProvider
+{
+    public object GetService(Type serviceType)
+    {
+        if (serviceType == typeof(MyStack))
+        {
+            return new MyStack(new Dependancy()); 
+        }
+
+        return null;
+    }
+}
