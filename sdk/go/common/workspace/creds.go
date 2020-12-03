9e955241fc3e06ba0aa0b9c96a94907bc74f94db
Luke Hoban <luke@pulumi.com>
2020-12-03 15:07:55 -0800

    Make credentials.json file writes concurrency safe (#5857)
    Running `pulumi` operations in parallel could occasionally result in truncating the `~/.pulumi/credentials.json` file and reading that truncated file from another process before the content could be written.
    
    Instead, use `os.Rename` to atomically replace the file contents.
    
    Concurrent `pulumi` operations could still compete for who gets to write the file first, and could lead to surprising results in some extreme cases.   But we should not see the corrupted file contents any longer.
    
    Fixes #3877.

--- sdk/go/common/workspace/creds.go
+++ sdk/go/common/workspace/creds.go
@@ -18,11 +18,13 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"time"
 
 	"github.com/pkg/errors"
 
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 )
 
@@ -213,5 +215,26 @@ func StoreCredentials(creds Credentials) error {
 	if err != nil {
 		return errors.Wrapf(err, "marshalling credentials object")
 	}
-	return ioutil.WriteFile(credsFile, raw, 0600)
+
+	// Use a temporary file and atomic os.Rename to ensure the file contents are
+	// updated atomically to ensure concurrent `pulumi` CLI operations are safe.
+	tempCredsFile, err := ioutil.TempFile(path.Dir(credsFile), "credentials-*.json")
+	if err != nil {
+		return err
+	}
+	_, err = tempCredsFile.Write(raw)
+	if err != nil {
+		return err
+	}
+	err = tempCredsFile.Close()
+	if err != nil {
+		return err
+	}
+	err = os.Rename(tempCredsFile.Name(), credsFile)
+	if err != nil {
+		contract.IgnoreError(os.Remove(tempCredsFile.Name()))
+		return err
+	}
+
+	return nil
 }
