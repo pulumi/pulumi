af68874338409e11a029bda3c5563151a5ff7291
Dan Friedman <dan@thefriedmans.org>
2020-12-22 20:05:38 -0600

    Limit concurrency

--- sdk/dotnet/Pulumi/X/Automation/Server.cs
+++ sdk/dotnet/Pulumi/X/Automation/Server.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
@@ -8,12 +9,17 @@ using Pulumirpc;
 
 namespace Pulumi.X.Automation
 {
-    internal class Server : LanguageRuntime.LanguageRuntimeBase
+    internal class Server : LanguageRuntime.LanguageRuntimeBase, IDisposable
     {
         // MaxRpcMesageSize raises the gRPC Max Message size from `4194304` (4mb) to `419430400` (400mb)
         public const int MaxRpcMesageSize = 1024 * 1024 * 400;
 
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private CancellationToken Token => _cts.Token;
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
         private readonly PulumiFn _program;
+        private bool _isDisposed;
 
         public Server(PulumiFn program)
         {
@@ -37,29 +43,68 @@ namespace Pulumi.X.Automation
 
         public override async Task<RunResponse> Run(RunRequest request, ServerCallContext context)
         {
-            var args = request.Args;
-            var engineAddr = args != null && args.Any() ? args[0] : "";
+            var contextId = Guid.NewGuid();
+            Console.WriteLine($"Waiting for lock with id {contextId}");
+            await _semaphore.WaitAsync();
 
-            var runInfo = new RuntimeSettings(
-                engineAddr,
-                request.MonitorAddress,
-                request.Config,
-                request.Project,
-                request.Stack,
-                request.Parallel,
-                request.DryRun);
+            Console.WriteLine($"Obtained lock with id {contextId}");
+            if (Token.IsCancellationRequested)
+            {
+                Console.WriteLine($"Releasing lock with id {contextId}");
+                _semaphore.Release();
+                return new RunResponse();
+            }
 
             try
             {
-                var deployment = new Deployment(runInfo);
-                await deployment.RunInstanceAsync(() => _program());
+                var args = request.Args;
+                var engineAddr = args != null && args.Any() ? args[0] : "";
+
+                var runInfo = new RuntimeSettings(
+                    engineAddr,
+                    request.MonitorAddress,
+                    request.Config,
+                    request.Project,
+                    request.Stack,
+                    request.Parallel,
+                    request.DryRun);
+
+                    var deployment = new Deployment(runInfo);
+                    await deployment.RunInstanceAsync(() => _program());
             }
-            catch (Exception e)
+            catch (Exception e) // Use more specific exceptions
             {
-                return new RunResponse { Error = e.ToString() };
+                var error = "Inline source runtime error:" + e.ToString();
+                return new RunResponse { Error = error };
+            }
+            finally
+            {
+                Console.WriteLine($"Releasing lock with id {contextId}");
+                _semaphore.Release();
             }
 
             return new RunResponse();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _cts.Dispose();
+                    //_semaphore.Dispose(); // This can deadlock if there are calls still waiting for the semaphore.
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
