e3613f98d453aeb8f4a991e38ee86f79decf1299
Levi Blackstone <levi@pulumi.com>
2020-12-23 17:34:27 -0700

    Doc backend package

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package backend encapsulates all extensibility points required to fully implement a new cloud provider.
 package backend
 
 import (
