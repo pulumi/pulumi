c047979b168ee1f84310907d5bfee166391dd74a
Paul Stack <public@paulstack.co.uk>
2021-01-04 21:26:59 +0000

    Export UpdateID as part of the UpdateInfo event that comes from the SaaS (#6049)
    Co-authored-by: Chris Smith <chrsmith@users.noreply.github.com>

--- sdk/go/common/apitype/history.go
+++ sdk/go/common/apitype/history.go
@@ -84,6 +84,7 @@ const (
 // Should generally mirror backend.UpdateInfo, but we clone it in this package to add
 // flexibility in case there is a breaking change in the backend-type.
 type UpdateInfo struct {
+	Version int `json:"version"`
 	// Information known before an update is started.
 	Kind        UpdateKind             `json:"kind"`
 	StartTime   int64                  `json:"startTime"`
