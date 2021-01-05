5e998f68ed1456cf13a28a2e4f447b3695d89c9d
Levi Blackstone <levi@pulumi.com>
2021-01-05 15:26:14 -0700

    Doc secrets packages

--- pkg/secrets/cloud/manager.go
+++ pkg/secrets/cloud/manager.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package cloud implements support for a generic cloud secret manager.
 package cloud
 
 import (
