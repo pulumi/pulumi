82ef700931293a0ce567baaf7ab451d52da23f06
Levi Blackstone <levi@pulumi.com>
2021-01-05 16:01:18 -0700

    Apply suggestions from code review
    Co-authored-by: Pat Gavlin <pat@pulumi.com>

--- pkg/resource/deploy/providers/doc.go
+++ pkg/resource/deploy/providers/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package providers contains the logic for first-class providers (versioning, loading, resource operations).
+// Package providers contains the logic for managing provider resources (versioning, loading, resource operations).
 package providers
