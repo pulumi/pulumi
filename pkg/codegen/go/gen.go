15f9fb395f82ce2866030b2132e4e635b3d921b8
Vivek Lakshmanan <vivek@pulumi.com>
2021-01-11 10:00:13 -0800

    Address review comments

--- pkg/codegen/go/gen.go
+++ pkg/codegen/go/gen.go
@@ -276,12 +276,12 @@ func (pkg *pkgContext) inputType(t schema.Type, optional bool) string {
 	var typ string
 	switch t := t.(type) {
 	case *schema.EnumType:
-		var enumType string
+		var prefix string
 		if optional {
-			enumType = "*"
+			prefix = "*"
 		}
 		// Since enum type is itself an input
-		return enumType + pkg.tokenToEnum(t.Token)
+		return prefix + pkg.tokenToEnum(t.Token)
 	case *schema.ArrayType:
 		en := pkg.inputType(t.ElementType, false)
 		return strings.TrimSuffix(en, "Input") + "ArrayInput"
