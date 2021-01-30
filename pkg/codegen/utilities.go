e86a69beddea08ae4c8bc5457e7666a6e45b02a7
Justin Van Patten <jvp@justinvp.com>
2021-01-29 16:52:00 -0800

    [codegen/nodejs] Emit externally referenced resources/types (#6225)

--- pkg/codegen/utilities.go
+++ pkg/codegen/utilities.go
@@ -38,6 +38,10 @@ func (ss StringSet) Add(s string) {
 	ss[s] = struct{}{}
 }
 
+func (ss StringSet) Any() bool {
+	return len(ss) > 0
+}
+
 func (ss StringSet) Delete(s string) {
 	delete(ss, s)
 }
