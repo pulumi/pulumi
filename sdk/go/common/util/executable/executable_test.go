be04da66b283a7075bad97d8668a4100de5c5b33
Youn Den <dendaneys@gmail.com>
2021-02-06 19:03:00 -0500

    Add tests for executable package

---
+++ sdk/go/common/util/executable/executable_test.go
@@ -0,0 +1,38 @@
+package executable
+
+import (
+	"testing"
+)
+
+type tt struct {
+	path     string
+	os       string
+	expected int
+}
+
+func TestGetPotentialPathsShouldReturnExpected(t *testing.T) {
+	tests := []tt{
+		{
+			path:     "/home/user/go:/usr/local/go",
+			os:       "linux",
+			expected: 2,
+		},
+		{
+			path:     "C:/Users/User/Documents/go;C:/Workspace/go",
+			os:       "windows",
+			expected: 2,
+		},
+		{
+			path:     "/home/user/go",
+			os:       "linux",
+			expected: 1,
+		},
+	}
+
+	for _, test := range tests {
+		paths := getPotentialPaths(test.path, test.os)
+		if len(paths) != test.expected {
+			t.Errorf("expected path length to be %d, got %d", test.expected, len(paths))
+		}
+	}
+}
