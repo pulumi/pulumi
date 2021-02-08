348ade8871a4e69b999240c9676a1f29c1400836
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-02-08 15:58:45 -0800

    Fix anchor link to the resource inputs section

--- pkg/codegen/docs/gen.go
+++ pkg/codegen/docs/gen.go
@@ -588,7 +588,7 @@ func (mod *modContext) genConstructorTS(r *schema.Resource, argsOptional bool) [
 			OptionalFlag: argsFlag,
 			Type: propertyType{
 				Name: argsType,
-				Link: "#input",
+				Link: "#inputs",
 			},
 			Comment: ctorArgsArgComment,
 		},
@@ -636,7 +636,7 @@ func (mod *modContext) genConstructorGo(r *schema.Resource, argsOptional bool) [
 			OptionalFlag: argsFlag,
 			Type: propertyType{
 				Name: argsType,
-				Link: "#input",
+				Link: "#inputs",
 			},
 			Comment: ctorArgsArgComment,
 		},
@@ -684,7 +684,7 @@ func (mod *modContext) genConstructorCS(r *schema.Resource, argsOptional bool) [
 			DefaultValue: argsDefault,
 			Type: propertyType{
 				Name: name + "Args",
-				Link: "#input",
+				Link: "#inputs",
 			},
 			Comment: ctorArgsArgComment,
 		},
