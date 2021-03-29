f02929f33e065f40ee98275ec4764246baeb0be8
Anton Tayanovskyy <anton@pulumi.com>
2021-03-29 13:37:38 -0400

    More permissinve regex to parse alpha versions

--- sdk/dotnet/Pulumi.Automation.Tests/LocalPulumiCmdTests.cs
+++ sdk/dotnet/Pulumi.Automation.Tests/LocalPulumiCmdTests.cs
@@ -15,15 +15,15 @@ namespace Pulumi.Automation.Tests
             var localCmd = new LocalPulumiCmd();
             IDictionary<string,string> extraEnv = new Dictionary<string,string>();
             IEnumerable<string> args = new string[]{ "version" };
-            var result = await localCmd.RunAsync(args, ".", extraEnv        
+            var result = await localCmd.RunAsync(args, ".", extraEnv
                 // Uncommenting this currently fails the test on v2.23.1:
                 //
                 // Which versions of Pulumi CLI support --event-log param?
                 //
-                // , onEngineEvent: _ev => {} 
+                // , onEngineEvent: _ev => {}
             );
             Assert.Equal(0, result.Code);
-            Assert.Matches(@"^v\d+\.\d+\.\d+\n$", result.StandardOutput);
+            Assert.Matches(@"^v?\d+\.\d+\.\d+", result.StandardOutput);
             Assert.Matches(@"^(warning: A new version of Pulumi[^\n]+\n)?$", result.StandardError);
         }
     }
