60e8b75b4c95b9c97fdbffa66c07ea98a5c30af4
Anton Tayanovskyy <anton@pulumi.com>
2021-03-30 10:33:38 -0400

    Clean up tests

--- sdk/dotnet/Pulumi.Automation.Tests/LocalPulumiCmdTests.cs
+++ sdk/dotnet/Pulumi.Automation.Tests/LocalPulumiCmdTests.cs
@@ -1,12 +1,13 @@
+// Copyright 2016-2021, Pulumi Corporation
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Pulumi.Automation.Commands;
+
 namespace Pulumi.Automation.Tests
 {
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-
-    using Xunit;
-
-    using Pulumi.Automation.Commands;
-
     public class LocalPulumiCmdTests
     {
         [Fact]
@@ -15,16 +16,34 @@ namespace Pulumi.Automation.Tests
             var localCmd = new LocalPulumiCmd();
             IDictionary<string,string> extraEnv = new Dictionary<string,string>();
             IEnumerable<string> args = new string[]{ "version" };
-            var result = await localCmd.RunAsync(args, ".", extraEnv
-                // Uncommenting this currently fails the test on v2.23.1:
-                //
-                // Which versions of Pulumi CLI support --event-log param?
-                //
-                // , onEngineEvent: _ev => {}
-            );
+
+            var stdoutLines = new List<string>();
+            var stderrLines = new List<string>();
+
+            // NOTE: not testing onEngineEvent arg as that is not
+            // supported for "version"; to test it one needs
+            // workspace-aware commands such as up or preview;
+            // currently this is covered by
+            // LocalWorkspaceTests.HandlesEvents.
+
+            var result = await localCmd.RunAsync(
+                args, ".", extraEnv,
+                onStandardOutput: line => stdoutLines.Add(line),
+                onStandardError: line => stderrLines.Add(line));
+
             Assert.Equal(0, result.Code);
+
             Assert.Matches(@"^v?\d+\.\d+\.\d+", result.StandardOutput);
-            Assert.Matches(@"^(warning: A new version of Pulumi[^\n]+\n)?$", result.StandardError);
+            Assert.Matches(@"^(warning: A new version of Pulumi[^\n]+\n)?$",
+                           result.StandardError);
+
+            Assert.Equal(Lines(result.StandardOutput), stdoutLines);
+            Assert.Equal(Lines(result.StandardError), stderrLines);
+        }
+
+        private IEnumerable<string> Lines(string s) {
+            return s.Split(Environment.NewLine,
+                           StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
