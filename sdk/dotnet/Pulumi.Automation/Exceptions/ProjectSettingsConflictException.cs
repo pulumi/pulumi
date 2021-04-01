94cfb0b9ece80a2d5381dab9d9e994e18ff41d55
Anton Tayanovskyy <anton@pulumi.com>
2021-04-01 10:50:39 -0400

    Make the new exception public

--- sdk/dotnet/Pulumi.Automation/Exceptions/ProjectSettingsConflictException.cs
+++ sdk/dotnet/Pulumi.Automation/Exceptions/ProjectSettingsConflictException.cs
@@ -4,7 +4,22 @@ using System;
 
 namespace Pulumi.Automation.Exceptions
 {
-    internal class ProjectSettingsConflictException : Exception
+    /// <summary>
+    ///
+    /// Thrown when creating a Workspace detects a conflict between
+    /// project settings found on disk (such as Pulumi.yaml) and a
+    /// ProjectSettings object passed to the Create API.
+    ///
+    /// There are two resolutions:
+    ///
+    /// (A) to use the ProjectSettings, delete the Pulumi.yaml file
+    ///     from WorkDir or use a different WorkDir
+    ///
+    /// (B) to use the exiting Pulumi.yaml from WorkDir, avoid
+    ///     customizing the ProjectSettings
+    ///
+    /// </summary>
+    public class ProjectSettingsConflictException : Exception
     {
         internal string SettingsFileLocation { get; }
 
