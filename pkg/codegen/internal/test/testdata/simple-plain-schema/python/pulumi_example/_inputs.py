ff59dc6d3ad0c2837bd2236ae4cefaf0b43af640
Justin Van Patten <jvp@justinvp.com>
2021-04-07 12:35:19 -0700

    [codegen/python] Adopt improved key translation (#6696)
    This change updates the Python SDK codegen to opt-in to the new casing
    translation behavior, which will use the passed-in props type's property
    name metadata for translations, rather than calling the resource's
    `translate_input_property` and `translate_output_property` methods.
    
    - FIX: Keys in user-defined dicts will no longer be unintentionally
      translated/modified.
    
    - BREAKING: Dictionary keys in nested output classes are now
      consistently snake_case. If accessing camelCase keys from such output
      classes, move to accessing the values via the snake_case property
      getters (or snake_case keys). A warning will be logged when accessing
      camelCase keys.
    
    When serializing inputs:
    
    - If a value is a dict and the associated type is an input type, the
    dict's keys will be translated based on the input type's property
    name metadata.
    
    - If a value is a dict and the associated type is a dict (or Mapping),
    the dict's keys will _not_ be translated.
    
    When resolving outputs:
    
    - If a value is a dict and the associated type is an output type, the
    dict's keys will be translated based on the output type's property
    name metadata.
    
    - If a value is a dict and the associated type is a dict (or Mapping),
    the dict's keys will _not_ be translated.

--- pkg/codegen/internal/test/testdata/simple-plain-schema/python/pulumi_example/_inputs.py
+++ pkg/codegen/internal/test/testdata/simple-plain-schema/python/pulumi_example/_inputs.py
@@ -6,7 +6,7 @@ import warnings
 import pulumi
 import pulumi.runtime
 from typing import Any, Mapping, Optional, Sequence, Union, overload
-from . import _utilities, _tables
+from . import _utilities
 
 __all__ = [
     'FooArgs',
