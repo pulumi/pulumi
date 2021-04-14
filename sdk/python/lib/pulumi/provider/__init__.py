f66a427f4f2aab89079a81eefd0ee4372e99fb56
Anton Tayanovskyy <anton@pulumi.com>
2021-04-14 16:55:06 -0400

    Conventinal __init__

--- sdk/python/lib/pulumi/provider/__init__.py
+++ sdk/python/lib/pulumi/provider/__init__.py
@@ -15,8 +15,8 @@
 from pulumi.provider.provider import Provider, ConstructResult
 from pulumi.provider.server import main
 
-__all__ = [getattr(x, '__name__') for x in [
-    Provider,
-    ConstructResult,
-    main
-]]
+__all__ = [
+    'Provider',
+    'ConstructResult',
+    'main'
+]
