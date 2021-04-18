e4746a7e65e90d6c7d13e770fc02bb06f2a377e3
stack72 <public@paulstack.co.uk>
2021-04-18 21:11:18 +0100

    Setting the minimum version of the CLI to be 3.0.0-rc.1 for the automation API

--- sdk/go/auto/minimum_version.go
+++ sdk/go/auto/minimum_version.go
@@ -19,6 +19,6 @@ import "github.com/blang/semver"
 var minimumVersion = calculateMinVersion()
 
 func calculateMinVersion() semver.Version {
-	v, _ := semver.ParseTolerant("3.0.0-alpha.0")
+	v, _ := semver.ParseTolerant("3.0.0-rc.1")
 	return v
 }
