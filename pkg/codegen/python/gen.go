159064891fee89e16ee9a5fcc3ada75c09e2b661
Anton Tayanovskyy <anton@pulumi.com>
2021-04-20 23:15:21 -0400

    Tighten up lazy_import impl

--- pkg/codegen/python/gen.go
+++ pkg/codegen/python/gen.go
@@ -2638,13 +2638,18 @@ def get_resource_args_opts(resource_args_type, resource_options_type, *args, **k
     return resource_args, opts
 
 
+# https://github.com/python/cpython/blob/master/Doc/library/importlib.rst#implementing-lazy-imports
 def lazy_import(fullname):
-    try:
-        return sys.modules[fullname]
-    except KeyError:
-        spec = importlib.util.find_spec(fullname)
-        module = importlib.util.module_from_spec(spec)
-        loader = importlib.util.LazyLoader(spec.loader)
-        loader.exec_module(module)
+    module = sys.modules.get(fullname, None)
+
+    if module is not None:
         return module
+
+    spec = importlib.util.find_spec(fullname)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[fullname] = module
+    loader.exec_module(module)
+    return module
 `
