a517340ddd006bcaeb0afcb971d02161c3034f1e
James Nugent <jen20@apple.com>
2021-04-24 11:41:48 -0500

    [codegen/go] Respect baseImportPath in Go SDKs
    If `rootPackageName` is set, we can look for the version in the
    baseImportPath rather than at a location based on the package name -
    which currently fails if every component is not named `pulumi-*`. To err
    on the side of caution, this method is only used for packages where
    `rootPackageName` is set, meaning existing SDKs retain their current
    behavior.
    
    The new behavior is confirmed via the test added in #6862.

--- pkg/codegen/internal/test/testdata/simple-plain-schema-with-root-package/go/pulumiUtilities.go
+++ pkg/codegen/internal/test/testdata/simple-plain-schema-with-root-package/go/pulumiUtilities.go
@@ -65,7 +65,7 @@ func getEnvOrDefault(def interface{}, parser envParser, vars ...string) interfac
 func PkgVersion() (semver.Version, error) {
 	type sentinal struct{}
 	pkgPath := reflect.TypeOf(sentinal{}).PkgPath()
-	re := regexp.MustCompile("^.*/pulumi-example/sdk(/v\\d+)?")
+	re := regexp.MustCompile("^github.com/pulumi/pulumi/pkg/v3/codegen/internal/test/testdata/simple-plain-schema-with-root-package/go/example(/v\\d+)?")
 	if match := re.FindStringSubmatch(pkgPath); match != nil {
 		vStr := match[1]
 		if len(vStr) == 0 { // If the version capture group was empty, default to v1.
