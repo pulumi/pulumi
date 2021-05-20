50ec927d84a72ee63b8a0d96bd34fb87d19a397e
James Nugent <jen20@apple.com>
2021-05-20 18:50:48 -0500

    Use variable over placeholder for Python version
    This commit modifies the generation of `setup.py` to use Python
    variables as the source for the package version and plugin version
    instead of placeholder strings. This has the effect of making the
    packages installable via the `-e` flag directly from their source
    directory rather than requiring a build step, which is useful while
    developing a plugin and examples in tandem.

--- pkg/codegen/internal/test/testdata/simple-resource-schema-custom-pypackage-name/python/setup.py
+++ pkg/codegen/internal/test/testdata/simple-resource-schema-custom-pypackage-name/python/setup.py
@@ -8,19 +8,22 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
+VERSION = "0.0.0"
+PLUGIN_VERSION = "0.0.0"
+
 class InstallPluginCommand(install):
     def run(self):
         install.run(self)
         try:
-            check_call(['pulumi', 'plugin', 'install', 'resource', 'example', '${PLUGIN_VERSION}'])
+            check_call(['pulumi', 'plugin', 'install', 'resource', 'example', PLUGIN_VERSION])
         except OSError as error:
             if error.errno == errno.ENOENT:
-                print("""
+                print(f"""
                 There was an error installing the example resource provider plugin.
                 It looks like `pulumi` is not installed on your system.
                 Please visit https://pulumi.com/ to install the Pulumi CLI.
                 You may try manually installing the plugin by running
-                `pulumi plugin install resource example ${PLUGIN_VERSION}`
+                `pulumi plugin install resource example {PLUGIN_VERSION}`
                 """)
             else:
                 raise
@@ -35,7 +38,7 @@ def readme():
 
 
 setup(name='custom_py_package',
-      version='${VERSION}',
+      version=VERSION,
       long_description=readme(),
       long_description_content_type='text/markdown',
       cmdclass={
