3f73230ec71028074d5fc5e016e755c897d9a81a
Paul Stack <public@paulstack.co.uk>
2021-06-04 15:08:40 +0300

    [cli] Protect against panic when using incorrect token type in pulumi import (#7202)
    Fixes: #6990

--- pkg/engine/deployment.go
+++ pkg/engine/deployment.go
@@ -16,6 +16,7 @@ package engine
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/opentracing/opentracing-go"
@@ -25,6 +26,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/fsutil"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/result"
@@ -173,6 +175,12 @@ func newDeployment(ctx *Context, info *deploymentContext, opts deploymentOptions
 		}
 		for i := range opts.imports {
 			imp := &opts.imports[i]
+			_, err := tokens.ParseTypeToken(imp.Type.String())
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("import type %q is not a valid resource type token. "+
+					"Type tokens must be of the format <package>:<module>:<type> - "+
+					"refer to the import section of the provider resource documentation.", imp.Type.String()))
+			}
 			if imp.Provider == "" && imp.Version == nil {
 				imp.Version = defaultProviderVersions[imp.Type.Package()]
 			}
