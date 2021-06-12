57ad7a9baebe99eb16c1d30effb6c0efd0713dc8
Adrian Smijulj <adrian1358@gmail.com>
2021-06-12 08:02:43 +0200

    Update descriptions

--- pkg/cmd/pulumi/watch.go
+++ pkg/cmd/pulumi/watch.go
@@ -54,7 +54,7 @@ func newWatchCmd() *cobra.Command {
 		Short:      "[PREVIEW] Continuously update the resources in a stack",
 		Long: "Continuously update the resources in a stack.\n" +
 			"\n" +
-			"This command watches the working directory for the current project and updates the active stack whenever\n" +
+			"This command watches the working directory or specified paths for the current project and updates the active stack whenever\n" +
 			"the project changes.  In parallel, logs are collected for all resources in the stack and displayed along\n" +
 			"with update progress.\n" +
 			"\n" +
@@ -147,7 +147,7 @@ func newWatchCmd() *cobra.Command {
 
 	cmd.PersistentFlags().StringArrayVarP(
 		&pathArray, "path", "", []string{""},
-		"Specify one or more paths that need to be watched. Defaults to working directory")
+		"Specify one or more relative or absolute paths that need to be watched. A path can point to a folder or a file. Defaults to working directory")
 	cmd.PersistentFlags().BoolVarP(
 		&debug, "debug", "d", false,
 		"Print detailed debugging output during resource operations")
