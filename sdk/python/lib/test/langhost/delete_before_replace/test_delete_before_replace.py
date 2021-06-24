281ac535d283ae3703a03259887b4dc0a9aab363
Levi Blackstone <levi@pulumi.com>
2021-06-24 09:17:05 -0600

    Python SDK

--- sdk/python/lib/test/langhost/delete_before_replace/test_delete_before_replace.py
+++ sdk/python/lib/test/langhost/delete_before_replace/test_delete_before_replace.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018, Pulumi Corporation.
+# Copyright 2016-2021, Pulumi Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +19,16 @@ class DeleteBeforeReplaceTest(LanghostTest):
     """
     Tests that DBRed resources correctly pass the "DBR" boolean to the engine.
     """
-    def test_protect(self):
+    def test_delete_before_replace(self):
         self.run_test(
             program=path.join(self.base_path(), "delete_before_replace"),
             expected_resource_count=1)
 
-    def register_resource(self, _ctx, _dry_run, ty, name, _resource,
-                          _dependencies, _parent, _custom, _protect, _provider, _property_deps, delete_before_replace,
-                          _ignore_changes, _version):
+    def register_resource(self, _ctx, _dry_run, ty, name, _resource, _dependencies, _parent, _custom, protect,
+                          _provider, _property_deps, _delete_before_replace, _ignore_changes, _version, _import,
+                          _replace_on_changes):
         self.assertEqual("foo", name)
-        self.assertTrue(delete_before_replace)
+        self.assertTrue(_delete_before_replace)
         return {
             "urn": self.make_urn(ty, name)
         }
