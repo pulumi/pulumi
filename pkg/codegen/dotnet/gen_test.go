6e09054114abf866c6cfe646b539bf723120660f
Pat Gavlin <pat@pulumi.com>
2021-07-06 11:31:14 -0700

    [codegen] Unify SDK codegen testing
    Rather than duplicating the list of tests and codegen driver across each
    SDK, move its definition into `pkg/codegen/internal/test`. This has a
    few notable benefits:
    
    - All SDK code generators will be tested against each test. Though some
      tests may exercise a particular code generator more than others, the
      extra coverage will be generally beneficial.
    - Adding a new test is simpler, as only a single file needs to be
      changed.
    - All SDKs now honor the `PULUMI_ACCEPT` environment variable for
      updating baselines.
    - Codegen tests now validate all generated files instead of only a
      particular subset.

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -1,85 +1,11 @@
 package dotnet
 
 import (
-	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/codegen/internal/test"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
-		name          string
-		schemaDir     string
-		expectedFiles []string
-	}{
-		{
-			"Simple schema with local resource properties",
-			"simple-resource-schema",
-			[]string{
-				"Resource.cs",
-				"OtherResource.cs",
-				"ArgFunction.cs",
-			},
-		},
-		{
-			"Simple schema with enum types",
-			"simple-enum-schema",
-			[]string{
-				"Tree/V1/RubberTree.cs",
-				"Tree/V1/Nursery.cs",
-				"Tree/V1/Enums.cs",
-				"Enums.cs",
-				"Inputs/ContainerArgs.cs",
-				"Outputs/Container.cs",
-			},
-		},
-		{
-			"External resource schema",
-			"external-resource-schema",
-			[]string{
-				"Inputs/PetArgs.cs",
-				"ArgFunction.cs",
-				"Cat.cs",
-				"Component.cs",
-				"Workload.cs",
-			},
-		},
-		{
-			"Simple schema with plain properties",
-			"simple-plain-schema",
-			[]string{
-				"Inputs/FooArgs.cs",
-				"Component.cs",
-				"DoFoo.cs",
-			},
-		},
-		{
-			"Repro for #6957",
-			"plain-schema-gh6957",
-			[]string{
-				"StaticPage.cs",
-				"Inputs/FooArgs.cs",
-			},
-		},
-	}
-	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
-			assert.NoError(t, err)
-
-			dir := filepath.Join(testDir, tt.schemaDir)
-			lang := "dotnet"
-
-			test.RewriteFilesWhenPulumiAccept(t, dir, lang, files)
-
-			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), lang, tt.expectedFiles)
-			assert.NoError(t, err)
-
-			test.ValidateFileEquality(t, files, expectedFiles)
-		})
-	}
+	test.TestSDKCodegen(t, "dotnet", GeneratePackage)
 }
--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -1,10 +1,7 @@
 package python
 
 import (
-	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v3/codegen/internal/test"
 )
@@ -35,122 +32,5 @@ func TestRelPathToRelImport(t *testing.T) {
 }
 
 func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
-		name          string
-		schemaDir     string
-		expectedFiles []string
-	}{
-		{
-			"Simple schema with local resource properties",
-			"simple-resource-schema",
-			[]string{
-				filepath.Join("pulumi_example", "resource.py"),
-				filepath.Join("pulumi_example", "other_resource.py"),
-				filepath.Join("pulumi_example", "arg_function.py"),
-			},
-		},
-		{
-			"Simple schema with local resource properties and custom Python package name",
-			"simple-resource-schema-custom-pypackage-name",
-			[]string{
-				filepath.Join("custom_py_package", "resource.py"),
-				filepath.Join("custom_py_package", "other_resource.py"),
-				filepath.Join("custom_py_package", "arg_function.py"),
-			},
-		},
-		{
-			"External resource schema",
-			"external-resource-schema",
-			[]string{
-				filepath.Join("pulumi_example", "_inputs.py"),
-				filepath.Join("pulumi_example", "arg_function.py"),
-				filepath.Join("pulumi_example", "cat.py"),
-				filepath.Join("pulumi_example", "component.py"),
-				filepath.Join("pulumi_example", "workload.py"),
-			},
-		},
-		{
-			"Simple schema with enum types",
-			"simple-enum-schema",
-			[]string{
-				filepath.Join("pulumi_plant", "_enums.py"),
-				filepath.Join("pulumi_plant", "_inputs.py"),
-				filepath.Join("pulumi_plant", "outputs.py"),
-				filepath.Join("pulumi_plant", "__init__.py"),
-				filepath.Join("pulumi_plant", "tree", "__init__.py"),
-				filepath.Join("pulumi_plant", "tree", "v1", "_enums.py"),
-				filepath.Join("pulumi_plant", "tree", "v1", "__init__.py"),
-				filepath.Join("pulumi_plant", "tree", "v1", "rubber_tree.py"),
-				filepath.Join("pulumi_plant", "tree", "v1", "nursery.py"),
-			},
-		},
-		{
-			"Simple schema with plain properties",
-			"simple-plain-schema",
-			[]string{
-				filepath.Join("pulumi_example", "_inputs.py"),
-				filepath.Join("pulumi_example", "component.py"),
-				filepath.Join("pulumi_example", "do_foo.py"),
-				filepath.Join("pulumi_example", "outputs.py"),
-			},
-		},
-		{
-			"Resource args with same named resource and type",
-			"resource-args-python",
-			[]string{
-				filepath.Join("pulumi_example", "_inputs.py"),
-				filepath.Join("pulumi_example", "outputs.py"),
-				filepath.Join("pulumi_example", "person.py"),
-				filepath.Join("pulumi_example", "pet.py"),
-			},
-		},
-		{
-			"Nested module",
-			"nested-module",
-			[]string{
-				filepath.Join("pulumi_foo", "nested", "__init__.py"),
-				filepath.Join("pulumi_foo", "nested", "module", "__init__.py"),
-				filepath.Join("pulumi_foo", "nested", "module", "resource.py"),
-			},
-		},
-		{
-			"Repro for #6957",
-			"plain-schema-gh6957",
-			[]string{
-				filepath.Join("pulumi_xyz", "_inputs.py"),
-				filepath.Join("pulumi_xyz", "static_page.py"),
-			},
-		},
-		{
-			"Third-party nested module",
-			"nested-module-thirdparty",
-			[]string{
-				filepath.Join("setup.py"),
-				filepath.Join("foo_bar", "deeply", "__init__.py"),
-				filepath.Join("foo_bar", "deeply", "nested", "__init__.py"),
-				filepath.Join("foo_bar", "deeply", "nested", "module", "__init__.py"),
-				filepath.Join("foo_bar", "deeply", "nested", "module", "resource.py"),
-			},
-		},
-	}
-
-	testDir := filepath.Join("..", "internal", "test", "testdata")
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
-			assert.NoError(t, err)
-
-			dir := filepath.Join(testDir, tt.schemaDir)
-			lang := "python"
-
-			test.RewriteFilesWhenPulumiAccept(t, dir, lang, files)
-
-			expectedFiles, err := test.LoadFiles(dir, lang, tt.expectedFiles)
-			assert.NoError(t, err)
-
-			test.ValidateFileEquality(t, files, expectedFiles)
-		})
-	}
+	test.TestSDKCodegen(t, "python", GeneratePackage)
 }
