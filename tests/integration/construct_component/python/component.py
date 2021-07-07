fa046ccc1f6bc83b75f675b18762b7a75778b7fc
Luke Hoban <luke@pulumi.com>
2021-07-07 18:37:20 +1000

    Remove test changes for now

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -5,19 +5,12 @@ from typing import Any, Optional
 import pulumi
 
 class Component(pulumi.ComponentResource):
+    echo: pulumi.Output[Any]
+    childId: pulumi.Output[str]
+
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
         props = dict()
         props["echo"] = echo
-        props["child_id"] = None
+        props["childId"] = None
         props["secret"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
-
-    @property
-    @pulumi.getter(name="echo")
-    def echo(self) -> pulumi.Output[str]:
-        return pulumi.get(self, "echo")
-
-    @property
-    @pulumi.getter(name="childId")
-    def child_id(self) -> pulumi.Output[str]:
-        return pulumi.get(self, "child_id")
