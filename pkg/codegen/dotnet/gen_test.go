c7422228a5fb0d3db2af9a4c3b953001c8451d99
Pat Gavlin <pat@pulumi.com>
2021-07-07 16:36:27 -0700

    [codegen/dotnet] Deeply unwrap collection element types. (#7451)
    `Input{List,Map}` handle some of this themselves. This fixes a breaking
    change in the .NET SDKs.
    
    Fixes #7448.

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -4,8 +4,55 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGeneratePackage(t *testing.T) {
 	test.TestSDKCodegen(t, "dotnet", GeneratePackage)
+}
+
+func TestGenerateType(t *testing.T) {
+	cases := []struct {
+		typ      schema.Type
+		expected string
+	}{
+		{
+			&schema.InputType{
+				ElementType: &schema.ArrayType{
+					ElementType: &schema.InputType{
+						ElementType: &schema.ArrayType{
+							ElementType: &schema.InputType{
+								ElementType: schema.NumberType,
+							},
+						},
+					},
+				},
+			},
+			"InputList<ImmutableArray<double>>",
+		},
+		{
+			&schema.InputType{
+				ElementType: &schema.MapType{
+					ElementType: &schema.InputType{
+						ElementType: &schema.ArrayType{
+							ElementType: &schema.InputType{
+								ElementType: schema.NumberType,
+							},
+						},
+					},
+				},
+			},
+			"InputMap<ImmutableArray<double>>",
+		},
+	}
+
+	mod := &modContext{mod: "main"}
+	for _, c := range cases {
+		t.Run(c.typ.String(), func(t *testing.T) {
+			typeString := mod.typeString(c.typ, "", true, false, false)
+			assert.Equal(t, c.expected, typeString)
+		})
+	}
 }
