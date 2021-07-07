04e2c817bc17944fcaf1bdd5304cd0e319882a8b
Pat Gavlin <pat@pulumi.com>
2021-07-06 22:49:51 -0700

    attempt a test
    Python isn't very happy with gRPC on my machine. Hopefully CI has better
    luck.

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -5,12 +5,19 @@ from typing import Any, Optional
 import pulumi
 
 class Component(pulumi.ComponentResource):
-    echo: pulumi.Output[Any]
-    childId: pulumi.Output[str]
-
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
         props = dict()
         props["echo"] = echo
-        props["childId"] = None
+        props["child_id"] = None
         props["secret"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
+
+    @property
+    @pulumi.getter(name="echo")
+    def echo(self) -> pulumi.Output[str]:
+        return pulumi.get(self, "echo")
+
+    @property
+    @pulumi.getter(name="childId")
+    def child_id(self) -> pulumi.Output[str]:
+        return pulumi.get(self, "child_id")
