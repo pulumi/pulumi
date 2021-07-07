7317d586d27473bd7bc909d7b167da0ccd1e8aab
Komal Ali <komal@pulumi.com>
2021-07-07 16:24:01 -0700

    Simplify

--- pkg/codegen/go/doc.go
+++ pkg/codegen/go/doc.go
@@ -88,9 +88,6 @@ func (d DocLanguageHelper) GetLanguageTypeString(pkg *schema.Package, moduleName
 		glog.Errorf("cannot calculate type string for type %q. could not find a package for module %q", t.String(), moduleName)
 		os.Exit(1)
 	}
-	if typ, ok := t.(*schema.EnumType); ok {
-		return modPkg.tokenToEnum(typ.Token)
-	}
 	return modPkg.typeString(t)
 }
 
