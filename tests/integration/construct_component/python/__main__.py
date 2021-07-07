617935e5056a7ef582cfd0bf79c7c4a2608311c5
Pat Gavlin <pat@pulumi.com>
2021-07-06 23:29:55 -0700

    update a reference

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -4,4 +4,4 @@ from component import Component
 
 component_a = Component("a", echo=42)
 component_b = Component("b", echo=component_a.echo)
-component_c = Component("c", echo=component_a.childId)
+component_c = Component("c", echo=component_a.child_id)
