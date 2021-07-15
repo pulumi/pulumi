bd53af552963419ead6115a9a83ffd4500d5fe9d
Justin Van Patten <jvp@justinvp.com>
2021-07-14 20:52:52 -0700

    Fix codegen tests
    The PR for these codegen changes was opened before these tests were added.

--- pkg/codegen/internal/test/testdata/provider-config-schema/python/setup.py
+++ pkg/codegen/internal/test/testdata/provider-config-schema/python/setup.py
@@ -8,19 +8,22 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
+VERSION = "0.0.0"
+PLUGIN_VERSION = "0.0.0"
+
 class InstallPluginCommand(install):
     def run(self):
         install.run(self)
         try:
-            check_call(['pulumi', 'plugin', 'install', 'resource', 'configstation', '${PLUGIN_VERSION}'])
+            check_call(['pulumi', 'plugin', 'install', 'resource', 'configstation', PLUGIN_VERSION])
         except OSError as error:
             if error.errno == errno.ENOENT:
-                print("""
+                print(f"""
                 There was an error installing the configstation resource provider plugin.
                 It looks like `pulumi` is not installed on your system.
                 Please visit https://pulumi.com/ to install the Pulumi CLI.
                 You may try manually installing the plugin by running
-                `pulumi plugin install resource configstation ${PLUGIN_VERSION}`
+                `pulumi plugin install resource configstation {PLUGIN_VERSION}`
                 """)
             else:
                 raise
@@ -35,7 +38,7 @@ def readme():
 
 
 setup(name='pulumi_configstation',
-      version='${VERSION}',
+      version=VERSION,
       long_description=readme(),
       long_description_content_type='text/markdown',
       cmdclass={
