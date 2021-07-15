e53927abea129ec42f122557e70dc79afef5a1c2
Anton Tayanovskyy <anton@pulumi.com>
2021-07-15 17:33:26 -0400

    Test early exception termination

--- sdk/dotnet/Pulumi.Tests/Deployment/DeploymentRunnerTests.cs
+++ sdk/dotnet/Pulumi.Tests/Deployment/DeploymentRunnerTests.cs
@@ -36,6 +36,50 @@ namespace Pulumi.Tests
             }
         }
 
+        [Fact]
+        public async Task TerminatesEarlyOnException()
+        {
+            var resources = await Deployment.TestAsync<TerminatesEarlyOnExceptionStack>(new EmptyMocks());
+            var stack = (TerminatesEarlyOnExceptionStack)resources[0];
+            var result = await stack.RunnerResult;
+            Assert.Equal(0, result);
+        }
+
+        class TerminatesEarlyOnExceptionStack : Stack
+        {
+            public Task<int> RunnerResult { get; private set; }
+
+            public TerminatesEarlyOnExceptionStack()
+            {
+                var runner = Pulumi.Deployment.Instance.Internal.Runner;
+                var error = new Exception("Deliberate test error");
+
+                Task task1 = Task.FromException(error);
+                Task task2 = Task.Delay(100);
+
+                runner.RegisterTask("task1", task1);
+                runner.RegisterTask("task2", task2);
+
+                var jointTask = runner.RunAsync<EmptyStack>();
+
+                // The joinTask will await register tasks. We check here that it
+                // finishes early because task1 failed, and does not wait for task2.
+                this.RunnerResult = jointTask.ContinueWith(t => {
+                    Assert.True(task1.IsFaulted, "task1 should be IsFaulted");
+                    Assert.False(task2.IsCompleted, "task2 should not be IsCompleted yet");
+                    Assert.Contains(error, runner.SwallowedExceptions);
+                    return 0;
+                });
+
+                // Need to undo the recording of the error in the engine, otherwise
+                // `Deployment.TestAsync` fails and does not let our test inspect
+                // the RunnerResult promise.
+                var deployment = (Deployment)Pulumi.Deployment.Instance.Internal;
+                var engine = (MockEngine)deployment.Engine;
+                engine.Errors.RemoveAll(err => err.Contains("Deliberate test error"));
+            }
+        }
+
         class EmptyStack : Stack
         {
         }
--- sdk/dotnet/Pulumi/Deployment/TaskMonitoringHelper.cs
+++ sdk/dotnet/Pulumi/Deployment/TaskMonitoringHelper.cs
@@ -1,6 +1,8 @@
 // Copyright 2016-2021, Pulumi Corporation
 
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,56 +10,76 @@ namespace Pulumi
 {
     /// Monitors dynamically added tasks for completion. Enters IDLE
     /// state when all monitored tasks finish. Allows awaiting next
-    /// IDLE state or the first exception, whichever comes first.
+    /// IDLE state or an exception, whichever comes first.
     /// Thread-safe.
     internal sealed class TaskMonitoringHelper
     {
-        private readonly object _lockObject = new object();
-        private int _activeTasks;
-        private TaskCompletionSource<Exception?>? _next;
+        private readonly TaskExceptionTracker errTracker = new TaskExceptionTracker();
+        private readonly TaskIdleTracker idleTracker = new TaskIdleTracker();
 
-        // Cache the delegate instance to avoid repeated allocations.
-        private readonly Action<Task> _onTaskCompleted;
-
-        public TaskMonitoringHelper()
+        /// Starts monitoring the given task.
+        public void AddTask(Task task)
         {
-            _onTaskCompleted = OnTaskCompleted;
-            CheckInvariants();
+            errTracker.AddTask(task);
+            idleTracker.AddTask(task);
         }
 
-        /// Adds the task to the task-set being monitored.
+        /// Awaits next IDLE state or an exception, whichever comes first.
+        /// IDLE state is represented as `null` in the result.
+        public async Task<Exception?> AwaitIdleOrFirstExceptionAsync()
+        {
+            var error = errTracker.AwaitExceptionAsync();
+            var idle = idleTracker.AwaitIdleAsync();
+            var first = await Task.WhenAny((Task)error, idle);
+            if (first == idle)
+            {
+                return null;
+            }
+            var err = await error;
+            return err;
+        }
+    }
+
+    /// Monitors dynamically added tasks for completion, allows awaiting IDLE state.
+    internal sealed class TaskIdleTracker
+    {
+        private readonly object _lockObject = new object();
+        private int _activeTasks;
+        private TaskCompletionSource<int>? _promise;
+
+        // Caches the delegate instance to avoid repeated allocations.
+        private readonly Action<Task> _onTaskCompleted;
+
+        public TaskIdleTracker()
+        {
+            _onTaskCompleted = OnTaskCompleted;
+        }
+
+        /// Awaits next IDLE state when no monitored tasks are running.
+        public Task AwaitIdleAsync()
+        {
+            lock (_lockObject)
+            {
+                if (_activeTasks == 0)
+                {
+                    return Task.FromResult(0);
+                }
+                if (_promise == null)
+                {
+                    _promise = new TaskCompletionSource<int>();
+                }
+                return _promise.Task;
+            }
+        }
+
+        /// Monitors the given task.
         public void AddTask(Task task)
         {
             lock (_lockObject)
             {
-                // Check if we are moving out of IDLE state.
-                if (_activeTasks == 0)
-                {
-                    // Fresh promise is needed.
-                    _next = new TaskCompletionSource<Exception?>();
-                }
                 _activeTasks++;
-                CheckInvariants();
-            };
-            task.ContinueWith(_onTaskCompleted);
-        }
-
-        /// Awaits the next IDLE period (reprsented by null) or the first exception
-        /// encountered in the monitored tasks.
-        public Task<Exception?> AwaitIdleOrFirstExceptionAsync()
-        {
-            lock (_lockObject)
-            {
-                if (_next != null)
-                {
-                    return _next.Task;
-                }
-                else
-                {
-                    // In IDLE state already.
-                    return Task.FromResult<Exception?>(null);
-                }
             }
+            task.ContinueWith(_onTaskCompleted);
         }
 
         private void OnTaskCompleted(Task task)
@@ -65,34 +87,93 @@ namespace Pulumi
             lock (_lockObject)
             {
                 _activeTasks--;
-
-                // Notify waiters and reset on IDLE or exception.
-                if (_activeTasks == 0 || task.Exception != null)
+                if (_activeTasks == 0 && _promise != null)
                 {
-                    if (_next != null)
-                    {
-                        _next.SetResult(task.Exception);
-                    }
-                    _next = (_activeTasks == 0) ? null : new TaskCompletionSource<Exception?>();
+                    _promise.SetResult(0);
+                    _promise = null;
                 }
+            }
+        }
+    }
 
-                CheckInvariants();
+    /// Monitors dynamically added tasks for exceptions, allows awaiting exceptions.
+    internal sealed class TaskExceptionTracker
+    {
+        private readonly object _lockObject = new object();
+        private readonly List<Exception> _exceptions = new List<Exception>();
+        private TaskCompletionSource<Exception>? _promise;
+
+        // Caches the delegate instance to avoid repeated allocations.
+        private readonly Action<Task> _onTaskCompleted;
+
+        public TaskExceptionTracker()
+        {
+            _onTaskCompleted = OnTaskCompleted;
+        }
+
+        /// Monitors the given task.
+        public void AddTask(Task task)
+        {
+            task.ContinueWith(_onTaskCompleted);
+        }
+
+        /// Awaits the next `Exception` in the monitored tasks. May never complete. May return
+        /// `AggregateException` if more than one monitored task fails.
+        public Task<Exception> AwaitExceptionAsync()
+        {
+            lock (_lockObject)
+            {
+                if (_exceptions.Count > 0)
+                {
+                    return Task.FromResult(Flush());
+                }
+                if (_promise == null)
+                {
+                    _promise = new TaskCompletionSource<Exception>();
+                }
+                return _promise.Task;
             }
         }
 
-        private void CheckInvariants()
+        private Exception Flush()
         {
-            if (_activeTasks == 0 && _next == null)
+            var err = CombineExceptions(_exceptions[0], _exceptions.Skip(1));
+            _exceptions.Clear();
+            return err;
+        }
+
+        private void OnTaskCompleted(Task task)
+        {
+            if (!task.IsFaulted)
             {
-                // idle state
                 return;
             }
-            if (_activeTasks > 0 && _next != null)
+            AggregateException? errs = task.Exception;
+            if (errs != null)
             {
-                // active state
-                return;
+                lock (_lockObject)
+                {
+                    _exceptions.AddRange(errs.InnerExceptions);
+                    if (_promise != null)
+                    {
+                        _promise.SetResult(Flush());
+                        _promise = null;
+                    }
+                }
             }
-            throw new Exception("TaskMonitoringHelper: instance state invariants violated");
+        }
+
+        /// Packs an AggregateException if necesssary.
+        private static Exception CombineExceptions(Exception exception, IEnumerable<Exception> exceptions)
+        {
+            if (!exceptions.Any())
+            {
+                return exception;
+            }
+            else
+            {
+                return new AggregateException((new []{exception}).Concat(exceptions));
+            }
         }
     }
 }
