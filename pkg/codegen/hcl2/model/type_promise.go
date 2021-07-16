9b23badedd8f991a7a54c7a494d4ac110b39107e
Pat Gavlin <pat@pulumi.com>
2021-07-16 09:56:26 -0700

    [codegen/hcl2] Improve ConversionFrom perf. (#7545)
    - Lazily produce conversion failure diagnostics. This lowers the
      allocation volume and cuts down on execution time by avoiding the
      conversion of source and dest types to strings.
    - Add a fast path for union conversions that checks if the source type
      is identical to any of the union's element types. Type equality
      checks are generally much faster than type conversion checks.
    
    These changes lead to a significant speedup in codegen time in
    azure-native.

--- pkg/codegen/hcl2/model/type_promise.go
+++ pkg/codegen/hcl2/model/type_promise.go
@@ -79,8 +79,8 @@ func (t *PromiseType) ConversionFrom(src Type) ConversionKind {
 }
 
 func (t *PromiseType) conversionFrom(
-	src Type, unifying bool, seen map[Type]struct{}) (ConversionKind, hcl.Diagnostics) {
-	return conversionFrom(t, src, unifying, seen, func() (ConversionKind, hcl.Diagnostics) {
+	src Type, unifying bool, seen map[Type]struct{}) (ConversionKind, lazyDiagnostics) {
+	return conversionFrom(t, src, unifying, seen, func() (ConversionKind, lazyDiagnostics) {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.conversionFrom(src.ElementType, unifying, seen)
 		}
