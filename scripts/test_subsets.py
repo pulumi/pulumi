9d34d7cd7f319d0fa5f409c010665606138a38f1
Anton Tayanovskyy <anton@pulumi.com>
2021-07-26 21:29:23 -0400

    Squash underutilized test configurations

--- scripts/test_subsets.py
+++ scripts/test_subsets.py
@@ -1,7 +1,24 @@
+"""Defines test subsets.
+
+When removing or introducing new test subsets, make sure the
+`test-subset` build matrix in `run-build-and-acceptance-tests.yml`
+matches. An implied subset `etc` will catch tests not matched by any
+explicit subset listed here.
+
+A note on the format of TEST_SUBSETS. The keys are test configuration
+names, and the values are lists of either Go packages containing the
+tests, or test suites names as passed to `run-testsuite.py`.
+
+"""
+
 TEST_SUBSETS = {
-    'integration': ['github.com/pulumi/pulumi/tests/integration'],
-    'lifecycletest': ['github.com/pulumi/pulumi/pkg/v3/engine/lifeycletest'],
-    'auto': ['github.com/pulumi/pulumi/sdk/v3/go/auto'],
+    'integration': [
+        'github.com/pulumi/pulumi/tests/integration'
+    ],
+    'auto-and-lifecycletest': [
+        'github.com/pulumi/pulumi/sdk/v3/go/auto',
+        'github.com/pulumi/pulumi/pkg/v3/engine/lifeycletest'
+    ],
     'native': [
         'dotnet-test',
         'istanbul',
