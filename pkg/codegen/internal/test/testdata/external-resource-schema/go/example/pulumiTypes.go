a7722248c1192b8fd4a0039d54fe7944059a056b
Anton Tayanovskyy <anton@pulumi.com>
2021-08-18 09:31:44 -0400

    Regenerate with PULUMI_IGNORE_AMBIENT_PLUGINS=1

--- pkg/codegen/internal/test/testdata/external-resource-schema/go/example/pulumiTypes.go
+++ pkg/codegen/internal/test/testdata/external-resource-schema/go/example/pulumiTypes.go
@@ -7,7 +7,7 @@ import (
 	"context"
 	"reflect"
 
-	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
+	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
