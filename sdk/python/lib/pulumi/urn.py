5e32c4548acc43c1a42ecb9b656df50496b8b968
Anton Tayanovskyy <anton@pulumi.com>
2021-08-23 16:36:57 -0400

    Fix parsing URNs without urn_name; test; exception wrapper

--- sdk/python/lib/pulumi/urn.py
+++ sdk/python/lib/pulumi/urn.py
@@ -23,16 +23,19 @@ _UrnParts = namedtuple('_UrnParts', ['urn_name',
 
 
 def _parse_urn(urn: str) -> _UrnParts:
-    urn_parts = urn.split('::')
-    urn_name = urn_parts[3]
-    qualified_type = urn_parts[2]
-    typ = qualified_type.split("$")[-1]
-    typ_parts = typ.split(":")
-    pkg_name = typ_parts[0]
-    mod_name = typ_parts[1] if len(typ_parts) > 1 else ""
-    typ_name = typ_parts[2] if len(typ_parts) > 2 else ""
-    return _UrnParts(urn_name=urn_name,
-                     typ=typ,
-                     pkg_name=pkg_name,
-                     mod_name=mod_name,
-                     typ_name=typ_name)
+    try:
+        urn_parts = urn.split('::')
+        urn_name = urn_parts[3] if len(urn_parts) >= 4 else ""
+        qualified_type = urn_parts[2]
+        typ = qualified_type.split("$")[-1]
+        typ_parts = typ.split(":")
+        pkg_name = typ_parts[0]
+        mod_name = typ_parts[1] if len(typ_parts) > 1 else ""
+        typ_name = typ_parts[2] if len(typ_parts) > 2 else ""
+        return _UrnParts(urn_name=urn_name,
+                         typ=typ,
+                         pkg_name=pkg_name,
+                         mod_name=mod_name,
+                         typ_name=typ_name)
+    except Exception as e:
+        raise ValueError(f'Cannot parse URN: {urn}') from e
