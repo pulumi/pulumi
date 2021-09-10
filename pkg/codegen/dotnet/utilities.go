da455a4be989ba036be0c6ae9cfc92a2f018eec1
Vova Ivanov <jetvova@gmail.com>
2021-09-10 11:57:07 -0700

    Fixed helper method list

--- pkg/codegen/dotnet/utilities.go
+++ pkg/codegen/dotnet/utilities.go
@@ -126,10 +126,9 @@ func makeSafeEnumName(name, typeName string) (string, error) {
 func getHelperMethodIfNeeded(functionName string) (string, bool) {
 	switch functionName {
 	case "filebase64":
-		methodBody =
-			`private static string ReadFileBase64(string path) {
+		return `private static string ReadFileBase64(string path) {
 		return Convert.ToBase64String(System.Text.UTF8.GetBytes(File.ReadAllText(path)))
-	}`
+	}`, true
 	case "sha1":
 		return `private static string ComputeSHA1(string input) {
 		return BitConverter.ToString(
