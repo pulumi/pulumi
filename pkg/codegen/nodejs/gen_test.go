71f2330ae0f8249da1a8b6e92e767b26707f162e
Anton Tayanovskyy <anton@pulumi.com>
2021-09-15 18:28:59 -0400

    Upgrade Node

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -3,9 +3,7 @@ package nodejs
 
 import (
 	"bytes"
-	"io/ioutil"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,15 +15,17 @@ import (
 )
 
 func TestGeneratePackage(t *testing.T) {
-	test.TestSDKCodegen(t, "nodejs", GeneratePackage, typeCheckGeneratedPackage)
+	test.TestSDKCodegen(t, &test.TestSDKCodegenOptions{
+		Language:   "nodejs",
+		GenPackage: GeneratePackage,
+		Checks: map[string]test.CodegenCheck{
+			"nodejs/compile": typeCheckGeneratedPackage,
+		},
+	})
 }
 
 func typeCheckGeneratedPackage(t *testing.T, pwd string) {
 	var err error
-	var npm string
-	npm, err = executable.FindExecutable("npm")
-	require.NoError(t, err)
-
 	var stdout, stderr bytes.Buffer
 	cmdOptions := integration.ProgramTestOptions{
 		Verbose: true,
@@ -33,18 +33,39 @@ func typeCheckGeneratedPackage(t *testing.T, pwd string) {
 		Stdout:  &stdout,
 	}
 
-	// TODO remove when https://github.com/pulumi/pulumi/pull/7938 lands
-	file := filepath.Join(pwd, "package.json")
-	oldFile, err := ioutil.ReadFile(file)
-	require.NoError(t, err)
-	newFile := strings.ReplaceAll(string(oldFile), "${VERSION}", "0.0.1")
-	err = ioutil.WriteFile(file, []byte(newFile), 0600)
+	// TODO: previous attempt used npm. It may be more popular and
+	// better target than yarn, however our build uses yarn in
+	// other places at the moment, and yarn does not run into the
+	// ${VERSION} problem; use yarn for now.
+	//
+	// var npm string
+	// npm, err = executable.FindExecutable("npm")
+	// require.NoError(t, err)
+	// // TODO remove when https://github.com/pulumi/pulumi/pull/7938 lands
+	// file := filepath.Join(pwd, "package.json")
+	// oldFile, err := ioutil.ReadFile(file)
+	// require.NoError(t, err)
+	// newFile := strings.ReplaceAll(string(oldFile), "${VERSION}", "0.0.1")
+	// err = ioutil.WriteFile(file, []byte(newFile), 0600)
+	// require.NoError(t, err)
+	// err = integration.RunCommand(t, "npm install", []string{npm, "i"}, pwd, &cmdOptions)
+	// require.NoError(t, err)
+
+	var yarn string
+	yarn, err = executable.FindExecutable("yarn")
 	require.NoError(t, err)
 
-	err = integration.RunCommand(t, "npm install", []string{npm, "i"}, pwd, &cmdOptions)
+	err = integration.RunCommand(t, "yarn link @pulumi/pulumi",
+		[]string{yarn, "link", "@pulumi/pulumi"}, pwd, &cmdOptions)
 	require.NoError(t, err)
-	err = integration.RunCommand(t, "typecheck ts",
+
+	err = integration.RunCommand(t, "yarn install",
+		[]string{yarn, "install"}, pwd, &cmdOptions)
+	require.NoError(t, err)
+
+	err = integration.RunCommand(t, "tsc --noEmit",
 		[]string{filepath.Join(".", "node_modules", ".bin", "tsc"), "--noEmit"}, pwd, &cmdOptions)
+
 	if err != nil {
 		stderr := stderr.String()
 		if len(stderr) > 0 {
