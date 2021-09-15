8b1b66aea20020c965f3f9968084a100d2d65063
Anton Tayanovskyy <anton@pulumi.com>
2021-09-15 16:07:34 -0400

    Finish up unit tests

--- pkg/codegen/internal/test/testdata/output-funcs/nodejs/tests/codegen.spec.ts
+++ pkg/codegen/internal/test/testdata/output-funcs/nodejs/tests/codegen.spec.ts
@@ -1,3 +1,17 @@
+// Copyright 2016-2021, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'mocha';
 import * as assert from 'assert';
 
@@ -8,6 +22,7 @@ import { funcWithAllOptionalInputsOutput } from '../funcWithAllOptionalInputs';
 import { funcWithDefaultValueOutput } from '../funcWithDefaultValue';
 import { funcWithListParamOutput } from '../funcWithListParam';
 import { funcWithDictParamOutput } from '../funcWithDictParam';
+import * as giro from '../getIntegrationRuntimeObjectMetadatum';
 
 pulumi.runtime.setMocks({
     newResource: function(_: pulumi.runtime.MockResourceArgs): {id: string, state: any} {
@@ -24,55 +39,78 @@ pulumi.runtime.setMocks({
                 }]
             };
         }
+        if (args.token == 'mypkg::getIntegrationRuntimeObjectMetadatum') {
+            return {nextLink: JSON.stringify(args.inputs)};
+        }
         if (args.token == 'mypkg::funcWithAllOptionalInputs' ||
             args.token == 'mypkg::funcWithDefaultValue' ||
             args.token == 'mypkg::funcWithListParam' ||
             args.token == 'mypkg::funcWithDictParam')
         {
-            return {
-                'r': JSON.stringify(args.inputs)
-            };
+            return {r: JSON.stringify(args.inputs)};
         }
         throw new Error('call not implemented for ' + args.token);
     },
 });
 
-describe('output-funcs', () => {
-    it('funcWithAllOptionalInputsOutput', (done) => {
-        const output = funcWithAllOptionalInputsOutput({a: pulumi.output('my-a')});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":"my-a"}');
+function checkTable(done: any, transform: (res: any) => any, table: {given: pulumi.Output<any>, expect: any}[]) {
+    checkOutput(done, pulumi.all(table.map(x => x.given)), res => {
+        res.map((actual, i) => {
+            assert.deepStrictEqual(transform(actual), table[i].expect);
         });
     });
+}
 
-    // TODO it seems that Node codegen does not respect default values at
-    // the moment. TODO format this comment properly.
+describe('output-funcs', () => {
+    it('funcWithAllOptionalInputsOutput', (done) => {
+        checkTable(done, res => JSON.parse(res.r), [
+            {given: funcWithAllOptionalInputsOutput({}),
+             expect: {}},
+            {given: funcWithAllOptionalInputsOutput({a: pulumi.output('my-a')}),
+             expect: {'a': 'my-a'}},
+            {given: funcWithAllOptionalInputsOutput({a: pulumi.output('my-a'),
+                                                     b: pulumi.output('my-b')}),
+             expect: {'a': 'my-a', 'b': 'my-b'}}
+        ]);
+    });
+
+    // TODO[pulumi/pulumi#7973] Node codegen does not respect default
+    // values at the moment, otherwise 'b' parameter would receive the
+    // default value from the schema.
     it('funcWithDefaultValueOutput', (done) => {
-        const output = funcWithDefaultValueOutput({a: pulumi.output('my-a')});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":"my-a"}');
-        })
+        checkTable(done, res => JSON.parse(res.r), [
+            {given: funcWithDefaultValueOutput({'a': pulumi.output('my-a')}),
+             expect: {'a': 'my-a'}},
+            {given: funcWithDefaultValueOutput({'a': pulumi.output('my-a'),
+                                                'b': pulumi.output('my-b')}),
+             expect: {'a': 'my-a', 'b': 'my-b'}}
+        ]);
     });
 
     it('funcWithListParamOutput', (done) => {
-        const output = funcWithListParamOutput({a: [
-            pulumi.output('my-a1'),
-            pulumi.output('my-a2'),
-            pulumi.output('my-a3'),
-        ]});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":["my-a1","my-a2","my-a3"]}');
-        })
+        const l = ['a', 'b', 'c'];
+        checkTable(done, res => JSON.parse(res.r), [
+            {given: funcWithListParamOutput({}),
+             expect: {}},
+            {given: funcWithListParamOutput({'a': pulumi.output(l)}),
+             expect: {'a': l}},
+            {given: funcWithListParamOutput({'a': pulumi.output(l),
+                                             'b': pulumi.output('my-b')}),
+             expect: {'a': l, 'b': 'my-b'}},
+        ]);
     });
 
     it('funcWithDictParamOutput', (done) => {
-        const output = funcWithDictParamOutput({a: {
-            'one': pulumi.output('1'),
-            'two': pulumi.output('2')
-        }});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":{"one":"1","two":"2"}}');
-        })
+        const d = {'key-a': 'value-a', 'key-b': 'value-b'};
+        checkTable(done, res => JSON.parse(res.r), [
+            {given: funcWithDictParamOutput({}),
+             expect: {}},
+            {given: funcWithDictParamOutput({'a': pulumi.output(d)}),
+             expect: {'a': d}},
+            {given: funcWithDictParamOutput({'a': pulumi.output(d),
+                                             'b': pulumi.output('my-b')}),
+             expect: {'a': d, 'b': 'my-b'}},
+        ]);
     });
 
     it('listStorageAccountKeysOutput', (done) => {
@@ -111,6 +149,23 @@ describe('output-funcs', () => {
                 'expand': 'my-expand'
             });
         });
+    });
+
+    it('getIntegrationRuntimeObjectMetadatumOutput', (done) => {
+        checkTable(
+            done,
+            (res: giro.GetIntegrationRuntimeObjectMetadatumResult) =>
+                JSON.parse(res.nextLink || "{}"),
+            [{given: giro.getIntegrationRuntimeObjectMetadatumOutput({
+                factoryName: pulumi.output('my-factory-name'),
+                integrationRuntimeName: pulumi.output('my-integration-runtime-name'),
+                metadataPath: pulumi.output('my-metadata-path'),
+                resourceGroupName: pulumi.output('my-resource-group-name')}),
+              expect: {'factoryName': 'my-factory-name',
+                       'integrationRuntimeName': 'my-integration-runtime-name',
+                       'metadataPath': 'my-metadata-path',
+                       'resourceGroupName': 'my-resource-group-name'}}]
+        );
     });
  });
 
