65fc505f502cbe3f1c8268068a563b0156846072
Anton Tayanovskyy <anton@pulumi.com>
2021-09-15 15:31:53 -0400

    Restore most of the existing unit tests

--- pkg/codegen/internal/test/testdata/output-funcs/nodejs/tests/codegen.spec.ts
+++ pkg/codegen/internal/test/testdata/output-funcs/nodejs/tests/codegen.spec.ts
@@ -1,39 +1,33 @@
 import 'mocha';
 import * as assert from 'assert';
 
-describe('anton 1', () => {
-
-    it('should work', (done) => {
-        assert.equal(2, 1 + 1);
-        done()
-    });
-});
-
-/*
-
-
-import * as assert from 'assert';
 import * as pulumi from '@pulumi/pulumi';
-import { listStorageAccountKeysOutput } from './listStorageAccountKeys';
-import { funcWithAllOptionalInputsOutput } from './funcWithAllOptionalInputs';
-import { funcWithDefaultValueOutput } from './funcWithDefaultValue';
-import { funcWithListParamOutput } from './funcWithListParam';
-import { funcWithDictParamOutput } from './funcWithDictParam';
+
+import { listStorageAccountKeysOutput, ListStorageAccountKeysResult } from '../listStorageAccountKeys';
+import { funcWithAllOptionalInputsOutput } from '../funcWithAllOptionalInputs';
+import { funcWithDefaultValueOutput } from '../funcWithDefaultValue';
+import { funcWithListParamOutput } from '../funcWithListParam';
+import { funcWithDictParamOutput } from '../funcWithDictParam';
 
 pulumi.runtime.setMocks({
     newResource: function(_: pulumi.runtime.MockResourceArgs): {id: string, state: any} {
         throw new Error('newResource not implemented');
     },
     call: function(args: pulumi.runtime.MockCallArgs) {
-        if (args.token == 'madeup-package:codegentest:listStorageAccountKeys') {
+        if (args.token == 'mypkg::listStorageAccountKeys') {
             return {
-                'keys': [args.inputs]
+                'keys': [{
+                    'creationTime': 'my-creation-time',
+                    'keyName': 'my-key-name',
+                    'permissions': 'my-permissions',
+                    'value': JSON.stringify(args.inputs),
+                }]
             };
         }
-        if (args.token == 'madeup-package:codegentest:funcWithAllOptionalInputs' ||
-            args.token == 'madeup-package:codegentest:funcWithDefaultValue' ||
-            args.token == 'madeup-package:codegentest:funcWithListParam' ||
-            args.token == 'madeup-package:codegentest:funcWithDictParam')
+        if (args.token == 'mypkg::funcWithAllOptionalInputs' ||
+            args.token == 'mypkg::funcWithDefaultValue' ||
+            args.token == 'mypkg::funcWithListParam' ||
+            args.token == 'mypkg::funcWithDictParam')
         {
             return {
                 'r': JSON.stringify(args.inputs)
@@ -42,6 +36,84 @@ pulumi.runtime.setMocks({
         throw new Error('call not implemented for ' + args.token);
     },
 });
+
+describe('output-funcs', () => {
+    it('funcWithAllOptionalInputsOutput', (done) => {
+        const output = funcWithAllOptionalInputsOutput({a: pulumi.output('my-a')});
+        checkOutput(done, output, res => {
+            assert.equal(res.r, '{"a":"my-a"}');
+        });
+    });
+
+    // TODO it seems that Node codegen does not respect default values at
+    // the moment. TODO format this comment properly.
+    it('funcWithDefaultValueOutput', (done) => {
+        const output = funcWithDefaultValueOutput({a: pulumi.output('my-a')});
+        checkOutput(done, output, res => {
+            assert.equal(res.r, '{"a":"my-a"}');
+        })
+    });
+
+    it('funcWithListParamOutput', (done) => {
+        const output = funcWithListParamOutput({a: [
+            pulumi.output('my-a1'),
+            pulumi.output('my-a2'),
+            pulumi.output('my-a3'),
+        ]});
+        checkOutput(done, output, res => {
+            assert.equal(res.r, '{"a":["my-a1","my-a2","my-a3"]}');
+        })
+    });
+
+    it('funcWithDictParamOutput', (done) => {
+        const output = funcWithDictParamOutput({a: {
+            'one': pulumi.output('1'),
+            'two': pulumi.output('2')
+        }});
+        checkOutput(done, output, res => {
+            assert.equal(res.r, '{"a":{"one":"1","two":"2"}}');
+        })
+    });
+
+    it('listStorageAccountKeysOutput', (done) => {
+        const output = listStorageAccountKeysOutput({
+            accountName: pulumi.output('my-account-name'),
+            resourceGroupName: pulumi.output('my-resource-group-name'),
+        });
+        checkOutput(done, output, (res: ListStorageAccountKeysResult) => {
+            assert.equal(res.keys.length, 1);
+            const k = res.keys[0];
+            assert.equal(k.creationTime, 'my-creation-time');
+            assert.equal(k.keyName, 'my-key-name');
+            assert.equal(k.permissions, 'my-permissions');
+            assert.deepStrictEqual(JSON.parse(k.value), {
+                'accountName': 'my-account-name',
+                'resourceGroupName': 'my-resource-group-name'
+            });
+        });
+    });
+
+    it('listStorageAccountKeysOutput with optional arg set', (done) => {
+        const output = listStorageAccountKeysOutput({
+            accountName: pulumi.output('my-account-name'),
+            resourceGroupName: pulumi.output('my-resource-group-name'),
+            expand: pulumi.output('my-expand'),
+        });
+        checkOutput(done, output, (res: ListStorageAccountKeysResult) => {
+            assert.equal(res.keys.length, 1);
+            const k = res.keys[0];
+            assert.equal(k.creationTime, 'my-creation-time');
+            assert.equal(k.keyName, 'my-key-name');
+            assert.equal(k.permissions, 'my-permissions');
+            assert.deepStrictEqual(JSON.parse(k.value), {
+                'accountName': 'my-account-name',
+                'resourceGroupName': 'my-resource-group-name',
+                'expand': 'my-expand'
+            });
+        });
+    });
+ });
+
 
 function checkOutput<T>(done: any, output: pulumi.Output<T>, check: (value: T) => void) {
     output.apply(value => {
@@ -53,80 +125,3 @@ function checkOutput<T>(done: any, output: pulumi.Output<T>, check: (value: T) =
         }
     });
 }
-
-describe('listStorageAccountKeysOutput', () => {
-    it('should work', (done) => {
-        const output = listStorageAccountKeysOutput({
-            accountName: pulumi.output('my-account-name'),
-            resourceGroupName: pulumi.output('my-resource-group-name'),
-        });
-        checkOutput(done, output, res => {
-            assert.equal(res.keys.length, 1);
-            assert.equal(Object.keys(res.keys[0]).length, 2);
-            assert.equal(res.keys[0].accountName, 'my-account-name');
-            assert.equal(res.keys[0].resourceGroupName, 'my-resource-group-name');
-        });
-    });
-
-    it('should work when the optional arg is set', (done) => {
-        const output = listStorageAccountKeysOutput({
-            accountName: pulumi.output('my-account-name'),
-            resourceGroupName: pulumi.output('my-resource-group-name'),
-            expand: pulumi.output('my-expand')
-        });
-        checkOutput(done, output, res => {
-            assert.equal(res.keys.length, 1);
-            assert.equal(Object.keys(res.keys[0]).length, 3);
-            assert.equal(res.keys[0].accountName, 'my-account-name');
-            assert.equal(res.keys[0].resourceGroupName, 'my-resource-group-name');
-            assert.equal(res.keys[0].expand, 'my-expand');
-        });
-    });
-});
-
-describe('funcWithAllOptionalInputsOutput', () => {
-    it('should work', (done) => {
-        const output = funcWithAllOptionalInputsOutput({a: pulumi.output('my-a')});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":"my-a"}');
-        });
-    });
-});
-
-// TODO it seems that Node codegen does not respect default values at
-// the moment.
-describe('funcWithDefaultValueOutput', () => {
-    it('should work', (done) => {
-        const output = funcWithDefaultValueOutput({a: pulumi.output('my-a')});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":"my-a"}');
-        })
-    });
-});
-
-describe('funcWithListParamOutput', () => {
-    it('should work', (done) => {
-        const output = funcWithListParamOutput({a: [
-            pulumi.output('my-a1'),
-            pulumi.output('my-a2'),
-            pulumi.output('my-a3'),
-        ]});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":["my-a1","my-a2","my-a3"]}');
-        })
-    });
-});
-
-describe('funcWithDictParamOutput', () => {
-    it('should work', (done) => {
-        const output = funcWithDictParamOutput({a: {
-            'one': pulumi.output('1'),
-            'two': pulumi.output('2')
-        }});
-        checkOutput(done, output, res => {
-            assert.equal(res.r, '{"a":{"one":"1","two":"2"}}');
-        })
-    });
-});
-
-*/
