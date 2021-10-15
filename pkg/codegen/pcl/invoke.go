4ebbacf6a14d3beb75348b4bd553423d314f37cb
Anton Tayanovskyy <anton@pulumi.com>
2021-10-15 10:55:14 -0400

    Experiment with changing function signature

--- pkg/codegen/pcl/invoke.go
+++ pkg/codegen/pcl/invoke.go
@@ -15,9 +15,12 @@
 package pcl
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/hcl2/model"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -42,46 +45,28 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 }
 
 func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {
-	signature := model.StaticFunctionSignature{
-		Parameters: []model.Parameter{
-			{
-				Name: "token",
-				Type: model.StringType,
-			},
-			{
-				Name: "args",
-				Type: model.NewOptionalType(model.DynamicType),
-			},
-			{
-				Name: "provider",
-				Type: model.NewOptionalType(model.StringType),
-			},
-		},
-		ReturnType: model.DynamicType,
-	}
-
 	if len(args) < 1 {
-		return signature, nil
+		return b.zeroSignature(), nil
 	}
 
 	template, ok := args[0].(*model.TemplateExpression)
 	if !ok || len(template.Parts) != 1 {
-		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
+		return b.zeroSignature(), hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
 	}
 	lit, ok := template.Parts[0].(*model.LiteralValueExpression)
 	if !ok || model.StringType.ConversionFrom(lit.Type()) == model.NoConversion {
-		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
+		return b.zeroSignature(), hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
 	}
 
 	token, tokenRange := lit.Value.AsString(), args[0].SyntaxNode().Range()
 	pkg, _, _, diagnostics := DecomposeToken(token, tokenRange)
 	if diagnostics.HasErrors() {
-		return signature, diagnostics
+		return b.zeroSignature(), diagnostics
 	}
 
 	pkgSchema, ok := b.options.packageCache.entries[pkg]
 	if !ok {
-		return signature, hcl.Diagnostics{unknownPackage(pkg, tokenRange)}
+		return b.zeroSignature(), hcl.Diagnostics{unknownPackage(pkg, tokenRange)}
 	}
 
 	fn, ok := pkgSchema.functions[token]
@@ -92,22 +77,85 @@ func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunct
 		}
 	}
 	if !ok {
-		return signature, hcl.Diagnostics{unknownFunction(token, tokenRange)}
+		return b.zeroSignature(), hcl.Diagnostics{unknownFunction(token, tokenRange)}
 	}
 
-	// Create args and result types for the schema.
+	sig, err := b.signatureForArgs(fn, args[1])
+	if err != nil {
+		diag := hcl.Diagnostics{errorf(tokenRange, "Invoke binding error: %v", err)}
+		return b.zeroSignature(), diag
+	}
+
+	return sig, nil
+}
+
+func (b *binder) makeSignature(argsType, returnType model.Type) model.StaticFunctionSignature {
+	return model.StaticFunctionSignature{
+		Parameters: []model.Parameter{
+			{
+				Name: "token",
+				Type: model.StringType,
+			},
+			{
+				Name: "args",
+				Type: argsType,
+			},
+			{
+				Name: "provider",
+				Type: model.NewOptionalType(model.StringType),
+			},
+		},
+		ReturnType: returnType,
+	}
+}
+
+func (b *binder) zeroSignature() model.StaticFunctionSignature {
+	return b.makeSignature(model.NewOptionalType(model.DynamicType), model.DynamicType)
+}
+
+func (b *binder) signatureForArgs(fn *schema.Function, args model.Expression) (model.StaticFunctionSignature, error) {
+	useOutputVersion := false
+	if fn.NeedsOutputVersion() {
+		outputVersionType := b.schemaTypeToType(fn.Inputs.InputShape)
+		regularVersionType := b.schemaTypeToType(fn.Inputs)
+		callsiteType := args.Type()
+		if regularVersionType.ConversionFrom(callsiteType) == model.NoConversion &&
+			outputVersionType.ConversionFrom(callsiteType) == model.SafeConversion {
+			useOutputVersion = true
+		}
+	}
+	if useOutputVersion {
+		return b.outputVersionSignature(fn)
+	} else {
+		return b.regularSignature(fn), nil
+	}
+}
+
+func (b *binder) regularSignature(fn *schema.Function) model.StaticFunctionSignature {
+	var argsType model.Type
 	if fn.Inputs == nil {
-		signature.Parameters[1].Type = model.NewOptionalType(model.NewObjectType(map[string]model.Type{}))
+		argsType = model.NewOptionalType(model.NewObjectType(map[string]model.Type{}))
 	} else {
-		signature.Parameters[1].Type = b.schemaTypeToType(fn.Inputs)
+		argsType = b.schemaTypeToType(fn.Inputs)
 	}
 
+	var returnType model.Type
 	if fn.Outputs == nil {
-		signature.ReturnType = model.NewObjectType(map[string]model.Type{})
+		returnType = model.NewObjectType(map[string]model.Type{})
 	} else {
-		signature.ReturnType = b.schemaTypeToType(fn.Outputs)
+		returnType = b.schemaTypeToType(fn.Outputs)
 	}
-	signature.ReturnType = model.NewPromiseType(signature.ReturnType)
 
-	return signature, nil
+	return b.makeSignature(argsType, model.NewPromiseType(returnType))
+}
+
+func (b *binder) outputVersionSignature(fn *schema.Function) (model.StaticFunctionSignature, error) {
+	if !fn.NeedsOutputVersion() {
+		return model.StaticFunctionSignature{}, fmt.Errorf("Function %s does not have an Output version", fn.Token)
+	}
+
+	// Given `fn.NeedsOutputVersion()==true`, can assume `fn.Inputs != nil`, `fn.Outputs != nil`.
+	argsType := b.schemaTypeToType(fn.Inputs.InputShape)
+	returnType := b.schemaTypeToType(fn.Outputs)
+	return b.makeSignature(argsType, model.NewOutputType(returnType)), nil
 }
