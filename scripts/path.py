c1760df37ebce78832a1be800b62ec8a31d191d1
Anton Tayanovskyy <anton@pulumi.com>
2021-11-23 12:49:19 -0500

    Deep dive on the path issue

--- scripts/path.py
+++ scripts/path.py
@@ -1,4 +1,10 @@
 import os
+import shutil
 
+print('== Begin PATH fragments ==')
 for sub_path in os.environ['PATH'].split(os.pathsep):
     print(sub_path)
+print('== End PATH fragments ==')
+
+print('Locating pulumi executable:')
+print(shutil.which('pulumi'))
