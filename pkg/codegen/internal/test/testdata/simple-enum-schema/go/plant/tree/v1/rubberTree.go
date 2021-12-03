74a02bbc1260d3da67ffe37ebd019b049375193c
Pat Gavlin <pat@pulumi.com>
2021-12-02 16:25:38 -0800

    [codegen/go] Simplify object default applies.
    The overall goal of this change is to avoid unnecessary conversions
    between pointer and non-pointer output types. The use of these
    conversions requires that we always generate the pointer variant of an
    object type, which impedes #7943's ability to omit these types. Instead,
    we can let the return type of the callback match the argument type and
    return the dereferenced result of `foo.Defaults()` if necessary.
    
    These changes also simplify the generated code somewhat in order to
    avoid introducing additional variables.

--- pkg/codegen/internal/test/testdata/simple-enum-schema/go/plant/tree/v1/rubberTree.go
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/go/plant/tree/v1/rubberTree.go
@@ -29,9 +29,8 @@ func NewRubberTree(ctx *pulumi.Context,
 		return nil, errors.New("missing one or more required arguments")
 	}
 
-	containerApplier := func(v plant.Container) *plant.Container { return v.Defaults() }
 	if args.Container != nil {
-		args.Container = args.Container.ToContainerPtrOutput().Elem().ApplyT(containerApplier).(plant.ContainerPtrOutput)
+		args.Container = args.Container.ToContainerPtrOutput().ApplyT(func(v *plant.Container) *plant.Container { return v.Defaults() }).(plant.ContainerPtrOutput)
 	}
 	if isZero(args.Diameter) {
 		args.Diameter = Diameter(6.0)
