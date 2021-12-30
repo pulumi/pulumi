7571775ad2389b96cb5785241b3a6641aaedf1e1
Luke Hoban <lukehoban@gmail.com>
2021-12-29 20:50:03 -0800

    Fix test

--- tests/integration/nodejs/esm-js-main/src/main.js
+++ tests/integration/nodejs/esm-js-main/src/main.js
@@ -1,8 +1,8 @@
 // Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
 
 import * as fs from "fs";
-import * as aws from "@pulumi/aws";
 
-const b = new aws.s3.Bucket("b");
+// Use top-level await
+await new Promise(r => setTimeout(r, 2000));
 
 export const res = fs.readFileSync("Pulumi.yaml").toString();
