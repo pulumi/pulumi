2f04a6931f9c4de12c895e3b80e59735756d0f10
Ian Wahbe <ian@wahbe.com>
2022-01-13 16:10:17 -0800

    [sdk/dotnet] add plugin download url (#8739)
    * [sdk/dotnet] Add PluginDownloadURL
    
    * Update CHANGELOG_PENDING.md
    
    * Add dotnet test
    
    * Set Random resource.remote = false
    
    * Add codegen
    
    * Codegen changes to Utilizes.cs
    
    * Fix ResourceOptions.merge
    
    * Remove WithVersion
    
    * Fix local test environment
    
    * Add WithVersion back (marked as obsolete)
    
    * Fix ResourceOptions.Copy
    
    * Use null coalescence operator
    
    * Set csharp to use projectReferences
    
    * Add PluginDownloadURL to CallOptions
    
    * Populate CallOptions in codegen
    
    * Revert "Populate CallOptions in codegen"
    
    This reverts commit e3397e073396271a1fa2c1a594520c822afeda5e.
    
    * Make backwards compatible with previous sdks
    
    * Run integration tests on MacOS
    
    * Attempt to fix macos build error
    
    * Disable full testing suite (MacOS)

--- CHANGELOG_PENDING.md
+++ CHANGELOG_PENDING.md
@@ -1,5 +1,9 @@
 ### Improvements
 
+- [sdk/dotnet] - Add `PluginDownloadURL` as a resource option. When provided by
+  the schema, `PluginDownloadURL` will be baked into `new Resource` and `Invoke`
+  requests in generated SDKs. 
+  [#8739](https://github.com/pulumi/pulumi/pull/8739)
 
 ### Bug Fixes
 
--- pkg/codegen/internal/test/testdata/simple-resource-schema/dotnet/TypeUses.cs
+++ pkg/codegen/internal/test/testdata/simple-resource-schema/dotnet/TypeUses.cs
@@ -44,6 +44,7 @@ namespace Pulumi.Example
             var defaultOptions = new CustomResourceOptions
             {
                 Version = Utilities.Version,
+                PluginDownloadURL = "example.com/download",
             };
             var merged = CustomResourceOptions.Merge(defaultOptions, options);
             // Override the ID if one was specified for consistency with other language SDKs.
