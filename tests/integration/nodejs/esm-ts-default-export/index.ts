c4c4c104b0f2ec0e28c432fb4dd1a4df3374ba60
Luke Hoban <lukehoban@gmail.com>
2022-01-22 17:35:14 -0800

    PR feedback

--- tests/integration/nodejs/esm-ts-default-export/index.ts
+++ tests/integration/nodejs/esm-ts-default-export/index.ts
@@ -1,3 +1,3 @@
-// Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2022, Pulumi Corporation.  All rights reserved.
 
 export default async function() { return {helloWorld: 123} }
