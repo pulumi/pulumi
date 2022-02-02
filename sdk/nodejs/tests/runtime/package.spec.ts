dde49efabd792d5728136d1fc8c78fda01122d91
Kyle Dixler <kyle@pulumi.com>
2022-02-02 10:09:45 -0800

    saving changes to tests

--- sdk/nodejs/tests/runtime/package.spec.ts
+++ sdk/nodejs/tests/runtime/package.spec.ts
@@ -15,11 +15,124 @@
 import * as assert from "assert";
 import * as pkg from "../../runtime/closure/package";
 
+type Exported = string | {
+    import?: string;
+    require?: string;
+}
+function assertElementIn<T>(element: T, collection: T[]) {
+    return assert.strictEqual(true, collection.includes(element))
+}
+
 describe("module", () => {
     it("remaps exports correctly for mockpackage", () => {
-        assert.equal(pkg.getModuleFromPath("mockpackage/lib/index.js"), "mockpackage")
+        assert.strictEqual(pkg.getModuleFromPath("mockpackage/lib/index.js"), "mockpackage")
     });
     it("should return undefined on unexported members", () => {
-        assert.equal(pkg.getModuleFromPath("mockpackage/lib/external.js"), undefined)
+        assert.strictEqual(pkg.getModuleFromPath("mockpackage/lib/external.js"), undefined)
     });
+})
+describe("To exclude private subfolders from patterns, null targets can be used", () => {
+        // ./node_modules/es-module-package/package.json
+        const packagedef = {
+            "name": "es-module-package",
+            "exports": {
+                "./features/*": "./src/features/*.js",
+                "./features/private-internal/*": null
+            }
+        }
+        it(`hides blacklisted wildcard paths`, () => {
+            //import featureInternal from 'es-module-package/features/private-internal/m';
+            // Throws: ERR_PACKAGE_PATH_NOT_EXPORTED
+            assert.strictEqual(pkg.getModuleFromPath('es-module-package/features/private-internal/m', packagedef), undefined);
+        })
+        it(`handles wildcard paths`, () => {
+            //import featureX from 'es-module-package/features/x';
+            // Loads ./node_modules/es-module-package/src/features/x.js
+            assert.strictEqual(pkg.getModuleFromPath('es-module-package/src/features/x.js', packagedef), 'es-module-package/features/x')
+        })
+});
+describe("basic package exports", () => {
+    // https://nodejs.org/api/packages.html#package-entry-points
+    const packagedef = {
+        "name": "my-mod",
+        "exports": {
+            ".": "./lib/index.js",
+            "./lib": "./lib/index.js",
+            "./lib/index": "./lib/index.js",
+            "./lib/index.js": "./lib/index.js",
+            "./feature": "./feature/index.js",
+            "./feature/index.js": "./feature/index.js",
+            "./package.json": "./package.json"
+        }
+    }
+    it("handles multiple aliases 1", () => {
+        assertElementIn(pkg.getModuleFromPath("my-mod/lib/index.js", packagedef), [
+            // not "my-mod",
+            // not "my-mod/lib",
+            // not "my-mod/lib/index",
+            "my-mod/lib/index.js", // highest specificity
+        ]);
+    })
+    it("handles multiple aliases 2", () => {
+        assertElementIn(pkg.getModuleFromPath("my-mod/feature/index.js", packagedef), [
+            // not "my-mod/feature",
+            "my-mod/feature/index.js", // highest specificity
+        ]);
+    })
+    it("returns with no modification", () => {
+        assert.strictEqual(pkg.getModuleFromPath("my-mod/package.json", packagedef), "my-mod/package.json")
+    })
+});
+describe("wildcard package exports", () => {
+    const packagedef = {
+        "name": "my-mod",
+        "exports": {
+            ".": "./lib/index.js",
+            "./lib": "./lib/index.js",
+            "./lib/*": "./lib/*.js",
+            "./feature": "./feature/index.js",
+            "./feature/*": "./feature/*.js",
+            "./package.json": "./package.json"
+        }
+    }
+    it("wildcard module", () => {
+        assert.strictEqual(pkg.getModuleFromPath("my-mod/lib/foobar.js", packagedef), "my-mod/lib/foobar.js")
+        assert.strictEqual(pkg.getModuleFromPath("my-mod/lib/foo", packagedef), "my-mod/lib/foo")
+        assert.strictEqual(pkg.getModuleFromPath("my-mod/feature/foobar.js", packagedef), "my-mod/feature/foobar.js")
+    })
+});
+describe("conditional import/require package exports", () => {
+    const packagedef = {
+        // package.json
+        "name": "that-mod",
+        "exports": {
+            ".": "./main.js",
+            "./feature": {
+                "node": "./feature-node.js",
+                "default": "./feature.js"
+            }
+        },
+        "type": "module"
+    }
+    it("remaps conditional node/default nested packages", () => {
+        assert.strictEqual(pkg.getModuleFromPath("that-mod/main.js", packagedef), "that-mod")
+        assert.strictEqual(pkg.getModuleFromPath("that-mod/feature/feature-node.js", packagedef), "that-mod/feature")
+        assert.strictEqual(pkg.getModuleFromPath("that-mod/feature/feature.js", packagedef), "that-mod/feature")
+    })
+});
+describe("conditional import/require package exports", () => {
+    const packagedef = {
+        // package.json
+        "name": "this-mod",
+        "main": "./main-require.cjs",
+        "exports": {
+            "import": "./main-module.js",
+            "require": "./main-require.cjs"
+        },
+        "type": "module"
+    }
+    it("remaps to main pkg", () => {
+        assert.strictEqual(pkg.getModuleFromPath("this-mod/main-require.js", packagedef), "this-mod")
+        assert.strictEqual(pkg.getModuleFromPath("this-mod/main-require.cjs", packagedef), "this-mod")
+    })
 });
