8d8a83600ee8dc363f01466d700d04e19cfd3c9b
Ian Wahbe <ian@wahbe.com>
2022-02-03 21:02:32 +0100

    Update lib/pulumi formatting

--- sdk/python/lib/pulumi/runtime/sync_await.py
+++ sdk/python/lib/pulumi/runtime/sync_await.py
@@ -32,10 +32,10 @@ if sys.version_info[0] == 3 and sys.version_info[1] < 7:
     _all_tasks = asyncio.Task.all_tasks
     _get_current_task = asyncio.Task.current_task
 else:
-    _enter_task = asyncio.tasks._enter_task # type: ignore
-    _leave_task = asyncio.tasks._leave_task # type: ignore
-    _all_tasks = asyncio.all_tasks # type: ignore
-    _get_current_task = asyncio.current_task # type: ignore
+    _enter_task = asyncio.tasks._enter_task  # type: ignore
+    _leave_task = asyncio.tasks._leave_task  # type: ignore
+    _all_tasks = asyncio.all_tasks  # type: ignore
+    _get_current_task = asyncio.current_task  # type: ignore
 
 
 def _sync_await(awaitable: Awaitable[Any]) -> Any:
@@ -67,17 +67,17 @@ def _sync_await(awaitable: Awaitable[Any]) -> Any:
     #
     # See https://github.com/python/cpython/blob/3.6/Lib/asyncio/base_events.py#L1428-L1452 for the details of the
     # _run_once kernel with which we need to cooperate.
-    ntodo = len(loop._ready) # type: ignore
+    ntodo = len(loop._ready)  # type: ignore
     while not fut.done() and not fut.cancelled():
-        loop._run_once() # type: ignore
-        if loop._stopping: # type: ignore
+        loop._run_once()  # type: ignore
+        if loop._stopping:  # type: ignore
             break
     # If we drained the ready list past what a calling _run_once would have expected, fix things up by pushing
     # cancelled handles onto the list.
-    while len(loop._ready) < ntodo: # type: ignore
+    while len(loop._ready) < ntodo:  # type: ignore
         handle = asyncio.Handle(lambda: None, [], loop)
         handle._cancelled = True
-        loop._ready.append(handle) # type: ignore
+        loop._ready.append(handle)  # type: ignore
 
     # If we were executing inside a task, restore its context and continue on.
     if task is not None:
