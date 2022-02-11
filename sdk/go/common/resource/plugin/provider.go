5ae3c0e75167ef2951eda687a9c5fe54d8764bfb
Fraser Waters <fraser@pulumi.com>
2022-02-11 12:13:41 +0000

    Main doens't need a port

--- sdk/go/common/resource/plugin/provider.go
+++ sdk/go/common/resource/plugin/provider.go
@@ -103,6 +103,7 @@ type Provider interface {
 	SignalCancellation() error
 
 	// Attach triggers an attach for a currently running provider to the engine
+	// TODO It would be nice if this was a HostClient rather than the string address but due to dependency ordering we don't have access to declare that here.
 	Attach(address string) error
 }
 
