5837c519c1882ee1d62d58aa8ab98482473fb92c
Fraser Waters <fraser@pulumi.com>
2022-02-25 09:59:35 +0000

    lint

--- sdk/nodejs/automation/stack.ts
+++ sdk/nodejs/automation/stack.ts
@@ -825,9 +825,12 @@ const cleanUp = async (logFile?: string, rl?: ReadlineResult) => {
     }
     if (logFile) {
         // remove the logfile
-        // remove with Node JS 15.X+
-        if(fs.rm) fs.rm(path.dirname(logFile), { recursive: true }, () => { return; });
-        // remove with Node JS 14.X
-        else fs.rmdir(path.dirname(logFile), { recursive: true }, () => { return; });
+        if(fs.rm) {
+            // remove with Node JS 15.X+
+            fs.rm(path.dirname(logFile), { recursive: true }, () => { return; });
+        } else {
+            // remove with Node JS 14.X
+            fs.rmdir(path.dirname(logFile), { recursive: true }, () => { return; });
+        }
     }
 };
