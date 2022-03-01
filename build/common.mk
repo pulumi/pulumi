6e51f13c86cf2dfd89f98e035030dfa559e02ed2
Aaron Friel <mayreply@aaronfriel.com>
2022-03-01 15:29:13 -0800

    ci: simplify test skipping

--- build/common.mk
+++ build/common.mk
@@ -109,7 +109,6 @@ PULUMI_BIN          := $(PULUMI_ROOT)/bin
 PULUMI_NODE_MODULES := $(PULUMI_ROOT)/node_modules
 PULUMI_NUGET        := $(PULUMI_ROOT)/nuget
 
-TESTSUITE_SKIPPED = $(PYTHON) ${PROJECT_ROOT}/scripts/skipped.py
 GO_TEST_FAST = $(PYTHON) ${PROJECT_ROOT}/scripts/go-test.py -short -count=1 -cover -tags=all -timeout 1h -parallel ${TESTPARALLELISM}
 GO_TEST = $(PYTHON) $(PROJECT_ROOT)/scripts/go-test.py -count=1 -cover -timeout 1h -tags=all -parallel ${TESTPARALLELISM}
 GOPROXY = 'https://proxy.golang.org'
