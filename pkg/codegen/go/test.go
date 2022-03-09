b53ce35c258b1fa6a6e8a8cec65e095ec923c2ba
Ian Wahbe <ian@wahbe.com>
2022-03-09 15:36:59 -0800

    Move shared testing code out of '*_test.go' files

---
+++ pkg/codegen/go/test.go
@@ -0,0 +1,62 @@
+package gen
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/pulumi/pulumi/pkg/v3/codegen"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/testing/test"
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/executable"
+)
+
+var ProgramCodegenOptions = test.ProgramCodegenOptions{
+	Language:   "go",
+	Extension:  "go",
+	OutputFile: "main.go",
+	Check:      goCheck,
+	GenProgram: GenerateProgram,
+	TestCases:  test.PulumiPulumiProgramTests,
+}
+
+func goCheck(t *testing.T, path string, _ codegen.StringSet) {
+	dir := filepath.Dir(path)
+	ex, err := executable.FindExecutable("go")
+	require.NoError(t, err)
+
+	// We remove go.mod to ensure tests are reproducible.
+	goMod := filepath.Join(dir, "go.mod")
+	if err = os.Remove(goMod); !os.IsNotExist(err) {
+		require.NoError(t, err)
+	}
+	err = integration.RunCommand(t, "generate go.mod",
+		[]string{ex, "mod", "init", "main"},
+		dir, &integration.ProgramTestOptions{})
+	require.NoError(t, err)
+	err = integration.RunCommand(t, "go tidy",
+		[]string{ex, "mod", "tidy"},
+		dir, &integration.ProgramTestOptions{})
+	require.NoError(t, err)
+	err = integration.RunCommand(t, "point towards local Go SDK",
+		[]string{ex, "mod", "edit",
+			fmt.Sprintf("--replace=%s=%s",
+				"github.com/pulumi/pulumi/sdk/v3",
+				"../../../../../../../sdk")},
+		dir, &integration.ProgramTestOptions{})
+	require.NoError(t, err)
+	err = integration.RunCommand(t, "go tidy after replace",
+		[]string{ex, "mod", "tidy"},
+		dir, &integration.ProgramTestOptions{})
+	require.NoError(t, err)
+
+	err = integration.RunCommand(t, "test build", []string{ex, "build", "-v", "all"},
+		dir, &integration.ProgramTestOptions{})
+	require.NoError(t, err)
+	os.Remove(filepath.Join(dir, "main"))
+	assert.NoError(t, err)
+}
--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -1,38 +1,11 @@
 package python
 
 import (
-	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/pulumi/pulumi/pkg/v3/codegen"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/testing/test"
-	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v3/python"
 )
-
-var testdataPath = filepath.Join("..", "testing", "test", "testdata")
-
-var ProgramCodegenOptions = test.ProgramCodegenOptions{
-	Language:   "python",
-	Extension:  "py",
-	OutputFile: "__main__.py",
-	Check:      pythonCheck,
-	GenProgram: GenerateProgram,
-	TestCases:  test.PulumiPulumiProgramTests,
-}
 
 func TestGenerateProgram(t *testing.T) {
 	test.TestProgramCodegen(t, ProgramCodegenOptions)
-}
-
-func pythonCheck(t *testing.T, path string, _ codegen.StringSet) {
-	ex, _, err := python.CommandPath()
-	require.NoError(t, err)
-	name := filepath.Base(path)
-	dir := filepath.Dir(path)
-	err = integration.RunCommand(t, "python syntax check",
-		[]string{ex, "-m", "py_compile", name}, dir, &integration.ProgramTestOptions{})
-	require.NoError(t, err)
 }
