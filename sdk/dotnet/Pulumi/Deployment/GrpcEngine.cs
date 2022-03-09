a0dacd54b7367e5d4db321339add11f2394743bc
Zaid Ajaj <zaid@zaids-mbp.kpn>
2022-03-09 10:45:05 +0100

    Use grpc-dotnet instead of grpc

--- sdk/dotnet/Pulumi/Deployment/GrpcEngine.cs
+++ sdk/dotnet/Pulumi/Deployment/GrpcEngine.cs
@@ -3,6 +3,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Grpc.Net.Client;
 using Pulumirpc;
 
 namespace Pulumi
@@ -15,8 +16,11 @@ namespace Pulumi
         {
             // maxRpcMessageSize raises the gRPC Max Message size from `4194304` (4mb) to `419430400` (400mb)
             var maxRpcMessageSize = 400 * 1024 * 1024;
-            var grpcChannelOptions = new List<ChannelOption> { new ChannelOption(ChannelOptions.MaxReceiveMessageLength, maxRpcMessageSize)};
-            this._engine = new Engine.EngineClient(new Channel(engine, ChannelCredentials.Insecure, grpcChannelOptions));
+            var channel = GrpcChannel.ForAddress($"http://{engine}", new GrpcChannelOptions
+            {
+                MaxReceiveMessageSize = maxRpcMessageSize
+            });
+            this._engine = new Engine.EngineClient(channel);
         }
         
         public async Task LogAsync(LogRequest request)
