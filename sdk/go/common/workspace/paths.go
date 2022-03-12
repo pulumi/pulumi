7d14515cd1ba09a12c377f50f7a485cd1d366e4a
Zaid Ajaj <zaid.naom@gmail.com>
2022-03-12 11:20:30 +0100

    Improve error message when provided stack is not fully qualified

--- sdk/go/common/workspace/paths.go
+++ sdk/go/common/workspace/paths.go
@@ -142,7 +142,8 @@ func DetectProjectAndPath() (*Project, string, error) {
 		return nil, "", err
 	} else if path == "" {
 		return nil, "", errors.Errorf("no Pulumi project found in the current working directory. " +
-			"Move to a directory with a Pulumi project or try creating a project first with `pulumi new`.")
+			"Move to a directory with a Pulumi project or try creating a project first with `pulumi new`.\n" +
+			"If you're using the --stack flag, make sure to pass the fully qualified name (org/project/stack)")
 	}
 
 	proj, err := LoadProject(path)
