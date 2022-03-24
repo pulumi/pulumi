59102a75ba360b57b29a9102f07fe268b28dfb44
Anton Tayanovskyy <anton@pulumi.com>
2022-03-24 11:08:53 -0400

    Alternate fix to safeguard dereferencing nil

--- pkg/codegen/go/gen.go
+++ pkg/codegen/go/gen.go
@@ -2073,13 +2073,11 @@ func ${fn}Output(ctx *pulumi.Context, args ${fn}OutputArgs, opts ...pulumi.Invok
 		ApplyT(func(v interface{}) (${fn}Result, error) {
 			args := v.(${fn}Args)
 			r, err := ${fn}(ctx, &args, opts...)
-			if err != nil {
-				return nil, err
-			}
-			if r == nil {
-				return nil, fmt.Errorf("expected either result or error to be nil, not both")
-			}
-			return *r, err
+                        var s ${fn}Result
+                        if r != nil {
+                                s = *r
+                        }
+			return s, err
 		}).(${outputType})
 }
 
