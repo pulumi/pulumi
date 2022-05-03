2b540980ee190b795aa8a420555c6f2f0b5b0810
Aaron Friel <mayreply@aaronfriel.com>
2022-05-03 12:32:02 -0700

    fixup: artifact dist

--- scripts/get-language-providers.sh
+++ scripts/get-language-providers.sh
@@ -13,14 +13,16 @@ for i in "pascal v0.0.1"; do
   mkdir -p "$LANG_DIST"
   cd "${LANG_DIST}"
 
-  rm -rf ./*.tar.gz
-  gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}" --pattern '*.tar.gz'
+  rm -rf ./*
+  gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}" --pattern '*.tar.gz' || true
+  gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}" --pattern '*.zip' || true
 
   for DIST_OS in darwin linux windows; do
     for DIST_ARCH in amd64 arm64; do
       DIR="$DIST_OS-$DIST_ARCH"
       mkdir -p $DIR
       find '.' -name "*-$DIST_OS-$DIST_ARCH.tar.gz" -print0 -exec tar -xzvf {} -C $DIR \;
+      find '.' -name "*-$DIST_OS-$DIST_ARCH.zip" -print0 -exec unzip {} -d $DIR \;
     done
   done
 
