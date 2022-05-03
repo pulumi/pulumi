ea8ee083a66f2cf5e2a9f6103ab97fd7b7421f5f
Aaron Friel <mayreply@aaronfriel.com>
2022-05-03 13:36:08 -0700

    fixup: rename amd64 dir to x64 for goreleaser

--- scripts/get-language-providers.sh
+++ scripts/get-language-providers.sh
@@ -18,11 +18,16 @@ for i in "pascal v0.0.1"; do
   gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}" --pattern '*.zip' || true
 
   for DIST_OS in darwin linux windows; do
-    for DIST_ARCH in amd64 arm64; do
-      DIR="$DIST_OS-$DIST_ARCH"
-      mkdir -p $DIR
-      find '.' -name "*-$DIST_OS-$DIST_ARCH.tar.gz" -print0 -exec tar -xzvf {} -C $DIR \;
-      find '.' -name "*-$DIST_OS-$DIST_ARCH.zip" -print0 -exec unzip {} -d $DIR \;
+    for i in "amd64 x64" "arm64 arm64"; do
+      set -- $i # treat strings in loop as args
+      DIST_ARCH="$1"
+      RENAMED_ARCH="$2" # goreleaser in pulumi/pulumi renames amd64 to x64
+
+      OUTDIR="$DIST_OS-$RENAMED_ARCH"
+
+      mkdir -p $OUTDIR
+      find '.' -name "*-$DIST_OS-$DIST_ARCH.tar.gz" -print0 -exec tar -xzvf {} -C $OUTDIR \;
+      find '.' -name "*-$DIST_OS-$DIST_ARCH.zip" -print0 -exec unzip {} -d $OUTDIR \;
     done
   done
 
