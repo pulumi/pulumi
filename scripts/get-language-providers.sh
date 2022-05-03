00952d6294a270859f6c26c963db2c6737b254ac
Aaron Friel <mayreply@aaronfriel.com>
2022-05-03 10:58:27 -0700

    ci: actually include ci script

---
+++ scripts/get-language-providers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eo pipefail
+set -x
+
+# shellcheck disable=SC2043
+for i in "pascal v0.0.1"; do
+  set -- $i # treat strings in loop as args
+  PULUMI_LANG="$1"
+  TAG="$2"
+
+  LANG_DIST="goreleaser-lang/${PULUMI_LANG}"
+  mkdir -p "$LANG_DIST"
+  cd "${LANG_DIST}"
+
+  rm -rf ./*.tar.gz
+  gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}" --pattern '*.tar.gz'
+
+  for DIST_OS in darwin linux windows; do
+    for DIST_ARCH in amd64 arm64; do
+      DIR="$DIST_OS-$DIST_ARCH"
+      mkdir -p $DIR
+      find '.' -name "*-$DIST_OS-$DIST_ARCH.tar.gz" -print0 -exec tar -xzvf {} -C $DIR \;
+    done
+  done
+done
