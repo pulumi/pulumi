5e0440306fda6de8431809dff8f814cabf3a1185
Fraser Waters <fraser@pulumi.com>
2022-05-10 12:03:14 +0100

    Testing for https://github.com/pulumi/pulumi/issues/9565

--- pkg/codegen/testing/test/sdk_driver.go
+++ pkg/codegen/testing/test/sdk_driver.go
@@ -265,6 +265,11 @@ var PulumiPulumiSDKTests = []*SDKTest{
 		Skip:        allLanguages.Except("python/any"),
 	},
 	{
+		Directory:   "enum-reference-python",
+		Description: "Ensure referencing external types/resources with referenced enums import correctly",
+		Skip:        allLanguages.Except("python/any"),
+	},
+	{
 		Directory:   "internal-dependencies-go",
 		Description: "Emit Go internal dependencies",
 		Skip:        allLanguages.Except("go/any"),
