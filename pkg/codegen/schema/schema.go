886cfc2cefdfbc2141f1e11bdd6c2764d612116a
Ian Wahbe <ian@wahbe.com>
2022-05-17 10:03:21 -0700

    Add a test + Fix

--- pkg/codegen/schema/schema.go
+++ pkg/codegen/schema/schema.go
@@ -1358,6 +1358,9 @@ type PropertySpec struct {
 	Secret bool `json:"secret,omitempty" yaml:"secret,omitempty"`
 	// ReplaceOnChanges specifies if the property is to be replaced instead of updated (default false).
 	ReplaceOnChanges bool `json:"replaceOnChanges,omitempty" yaml:"replaceOnChanges,omitempty"`
+	// WillReplaceOnChanges indicates that the provider will replace the resource when
+	// this property is changed. This property is used exclusively for docs.
+	WillReplaceOnChanges bool `json:"willReplaceOnChanges,omitempty" yaml:"willReplaceOnChanges,omitempty"`
 }
 
 // ObjectTypeSpec is the serializable form of an object type.
