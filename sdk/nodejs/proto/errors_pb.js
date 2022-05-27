ab47db1c66e5d4cc565bfe7aca6c52da774c0d12
Fraser Waters <fraser@pulumi.com>
2022-05-27 11:54:21 +0100

    Regenerate protobufs

--- sdk/nodejs/proto/errors_pb.js
+++ sdk/nodejs/proto/errors_pb.js
@@ -2,11 +2,14 @@
 /**
  * @fileoverview
  * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
  * @suppress {messageConventions} JS Compiler reports an error if a variable or
  *     field starts with 'MSG_' and isn't a translatable message.
  * @public
  */
 // GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
 
 var jspb = require('google-protobuf');
 var goog = jspb;
