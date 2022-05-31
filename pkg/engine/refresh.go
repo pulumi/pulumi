af72e40a3f8bda94ec02c5d4002827533e041b79
Ian Wahbe <ian@wahbe.com>
2022-05-31 11:42:28 -0700

    Always call deduplicate

--- pkg/engine/refresh.go
+++ pkg/engine/refresh.go
@@ -74,7 +74,7 @@ func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj 
 	}
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
-	if err := ensurePluginsAreInstalled(plugins); err != nil {
+	if err := ensurePluginsAreInstalled(plugins.Deduplicate()); err != nil {
 		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)
 	}
 
