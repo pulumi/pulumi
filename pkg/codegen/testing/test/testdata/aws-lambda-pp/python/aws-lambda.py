61ce4dfe44f3cc4f61dbeb8922b49ac60b60a449
Ian Wahbe <ian@wahbe.com>
2022-07-07 15:44:37 -0700

    Test passes

--- pkg/codegen/testing/test/testdata/aws-lambda-pp/python/aws-lambda.py
+++ pkg/codegen/testing/test/testdata/aws-lambda-pp/python/aws-lambda.py
@@ -7,7 +7,7 @@ test_lambda = aws.lambda_.Function("testLambda",
     role=iam_for_lambda.arn,
     handler="index.test",
     runtime="nodejs12.x",
-    environment=aws.lambda..FunctionEnvironmentArgs(
+    environment=aws.lambda_.FunctionEnvironmentArgs(
         variables={
             "foo": "bar",
         },
