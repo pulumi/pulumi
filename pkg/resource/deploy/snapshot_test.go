70a912ff347b8f8dd177433e13fdae20abc20a73
Anton Tayanovskyy <anton@pulumi.com>
2022-07-15 18:25:49 -0400

    More unit tests

--- pkg/resource/deploy/snapshot_test.go
+++ pkg/resource/deploy/snapshot_test.go
@@ -20,6 +20,11 @@ func createSnapshot() Snapshot {
 	return Snapshot{Resources: resources}
 }
 
+func createSnapshotPtr() *Snapshot {
+	s := createSnapshot()
+	return &s
+}
+
 func TestGlobUrn(t *testing.T) {
 	t.Parallel()
 
@@ -69,4 +74,41 @@ func TestGlobUrn(t *testing.T) {
 			assert.Equal(t, tt.expected, actual)
 		})
 	}
+}
+
+func TestSnapshotNormalizeURNReferences(t *testing.T) {
+	s1 := createSnapshotPtr()
+	s1n, err := s1.NormalizeURNReferences()
+	assert.NoError(t, err)
+	assert.Same(t, s1, s1n)
+
+	s2 := createSnapshotPtr()
+	r0 := s2.Resources[0]
+	r0.Aliases = []resource.URN{r0.URN}
+	s2.Resources[2].Parent = r0.URN
+	r0.URN += "!"
+	s2n, err := s2.NormalizeURNReferences()
+	assert.NoError(t, err)
+	assert.NotSame(t, s2, s2n)
+	// before normalize in s2, Parent link uses outdated URL
+	assert.Equal(t, s2.Resources[2].Parent+"!", s2.Resources[0].URN)
+	// after normalize in s2n, Parent link uses the real URL rewritten via aliases
+	assert.Equal(t, s2n.Resources[2].Parent, s2n.Resources[0].URN)
+}
+
+func TestSnapshotWithUpdatedResources(t *testing.T) {
+	s1 := createSnapshotPtr()
+
+	s := s1.withUpdatedResources(func(r *resource.State) *resource.State {
+		return r
+	})
+	assert.Same(t, s, s1)
+
+	s = s1.withUpdatedResources(func(r *resource.State) *resource.State {
+		copy := *r
+		copy.URN += "!"
+		return &copy
+	})
+	assert.NotSame(t, s, s1)
+	assert.Equal(t, s1.Resources[0].URN+"!", s.Resources[0].URN)
 }
