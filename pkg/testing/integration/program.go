0c598cb9d3d08b1654be28cc91e5c65b7cc9dbc3
Harry <hliuson@hotmail.com>
2022-07-25 12:38:00 -0700

    parse PCL files

--- pkg/testing/integration/program.go
+++ pkg/testing/integration/program.go
@@ -15,6 +15,7 @@
 package integration
 
 import (
+	"bytes"
 	"context"
 	cryptorand "crypto/rand"
 	"encoding/hex"
@@ -39,6 +40,8 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/pulumi/pulumi/pkg/v3/backend/filestate"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/hcl2/syntax"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/pcl"
 	"github.com/pulumi/pulumi/pkg/v3/engine"
 	"github.com/pulumi/pulumi/pkg/v3/operations"
 	"github.com/pulumi/pulumi/pkg/v3/resource/stack"
@@ -49,12 +52,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tools"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/ciutil"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/fsutil"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/retry"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
+
+	javagen "github.com/pulumi/pulumi-java/pkg/codegen/java"
+	yamlgen "github.com/pulumi/pulumi-yaml/pkg/pulumiyaml/codegen"
+	dotnetgen "github.com/pulumi/pulumi/pkg/v3/codegen/dotnet"
+	gogen "github.com/pulumi/pulumi/pkg/v3/codegen/go"
+	jsgen "github.com/pulumi/pulumi/pkg/v3/codegen/nodejs"
+	pygen "github.com/pulumi/pulumi/pkg/v3/codegen/python"
 )
 
 const (
@@ -316,6 +327,8 @@ type BatteryTestOptions struct {
 	Version  *semver.Version
 	Opts     *ProgramTestOptions
 }
+
+type projectGeneratorFunc func(directory string, project workspace.Project, p *pcl.Program) error
 
 func (opts *ProgramTestOptions) GetDebugLogLevel() int {
 	if opts.DebugLogLevel > 0 {
@@ -709,26 +722,71 @@ func BatteryTest(t *testing.T, opts *ProgramTestOptions, langOpts []BatteryTestO
 	//run get projinfo
 	projinfo, err := pt.getProjinfo(pt.projdir)
 	assert.NoError(t, err)
+
 	//Assert runtime is YAML
 	assert.Equal(t, projinfo.Proj.Runtime.Name(), "yaml")
 
+	pwd, _, ctx, err := engine.ProjectInfoContext(projinfo, nil, nil, cmdutil.Diag(), cmdutil.Diag(), false, nil)
+	assert.NoError(t, err)
+
+	proj, pclProgram, err := yamlgen.Eject(pwd, nil)
+	assert.NoError(t, err)
+
+	//check if *.pp file exists in projdir
+	files, err := ioutil.ReadDir(pt.projdir)
+	assert.NoError(t, err)
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".pp") {
+			parser := syntax.NewParser()
+			//get file content
+			ppFilePath := filepath.Join(pt.projdir, file.Name())
+			contents, err := ioutil.ReadFile(ppFilePath)
+			assert.NoError(t, err)
+			err = parser.ParseFile(bytes.NewReader(contents), ppFilePath)
+			assert.NoError(t, err)
+			program, diags, err := pcl.BindProgram(parser.Files, pcl.PluginHost(ctx.Host))
+			assert.NoError(t, err)
+			assert.Empty(t, diags)
+			pclProgram = program
+
+		}
+	}
+
+	name := projinfo.Proj.Name
+
 	//Instantiate new directories and run pulumi convert on each language with new directories as output.
 	for _, langOpt := range langOpts {
+
+		var projectGenerator projectGeneratorFunc
+		switch langOpt.Language {
+		case "csharp":
+			projectGenerator = dotnetgen.GenerateProject
+		case "go":
+			projectGenerator = gogen.GenerateProject
+		case "typescript":
+			projectGenerator = jsgen.GenerateProject
+		case "python":
+			projectGenerator = pygen.GenerateProject
+		case "java":
+			projectGenerator = javagen.GenerateProject
+		case "yaml": // nolint: goconst
+			projectGenerator = yamlgen.GenerateProject
+		default:
+			projectGenerator = nil
+		}
+
+		assert.NotEqual(t, projectGenerator, nil)
+
 		//create subdirectory under pt.tmpdir
+
 		subdir := filepath.Join(pt.tmpdir, langOpt.Language)
-		err := os.Mkdir(subdir, 0755)
+
+		//This feels a little sketchy to me but the alternative is to go in and modify it after it's been written to file.
+		//Either that or just don't mutate the name.
+		//projinfo.Proj.Name = tokens.PackageName(fmt.Sprintf("%s-%s", name, langOpt.Language))
+
+		err = projectGenerator(subdir, *proj, pclProgram)
 		assert.NoError(t, err)
-
-		args := []string{"convert", "--language", langOpt.Language, "--out", subdir}
-
-		//convert to new language
-		err = pt.runPulumiCommand("convert", args, pt.tmpdir, false)
-		assert.NoError(t, err)
-
-		TransformProject(func(proj *workspace.Project) *workspace.Project {
-			proj.Name = tokens.PackageName(fmt.Sprintf("%s-%s", proj.Name, langOpt.Language))
-			return proj
-		}, subdir)
 
 		//Configure opts
 		langPtOpts := ProgramTestOptions{}
@@ -739,8 +797,7 @@ func BatteryTest(t *testing.T, opts *ProgramTestOptions, langOpts []BatteryTestO
 			langPtOpts = langPtOpts.With(*langOpt.Opts)
 		}
 		langPtOpts = langPtOpts.With(ProgramTestOptions{
-			Dir:       subdir,
-			StackName: langPtOpts.GetStackName().String() + "-" + langOpt.Language,
+			Dir: subdir,
 		})
 		t.Run(langOpt.Language, func(t *testing.T) {
 			ProgramTest(t, &langPtOpts)
