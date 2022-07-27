14726e224a090a2becd5e7996b1619175783807c
Aaron Friel <mayreply@aaronfriel.com>
2022-07-27 12:06:17 -0700

    Revert #10149
    This reverts commit f6ee2fd20cbf1892865eadb9b173f1b07f892453.
    This reverts commit 33a84df3cfa3fba3234b15daa8faf7e5b645236c.
    This reverts commit 01c1719c1c503e4f58f91179fef90ce181d8a852.
    This reverts commit e7b96115be0cf3a3bfc868b884144a43d42998d7.
    This reverts commit 60fb2894083363004db2210b3a7f0935d6cef967.
    This reverts commit 7a81b1435f95a0b28dd596767e34ce8a3e1bc0c7.
    This reverts commit b1f2e41b5686c09071a3cb1586799fe228327045.

--- scripts/get-language-providers.sh
+++ scripts/get-language-providers.sh
@@ -3,58 +3,48 @@
 set -eo pipefail
 set -x
 
-get_version() {
-  repo="$1"
-  (
-    cd pkg
-    GOWORK=off go list -m all | grep "${repo}" | cut -d" " -f2
-  )
-}
-
 # shellcheck disable=SC2043
-for i in "github.com/pulumi/pulumi-java java" "github.com/pulumi/pulumi-yaml yaml"; do
+for i in "java v0.5.0" "yaml v0.5.3"; do
   set -- $i # treat strings in loop as args
-  REPO="$1"
-  PULUMI_LANG="$2"
-  TAG=$(get_version "${REPO}")
+  PULUMI_LANG="$1"
+  TAG="$2"
 
   LANG_DIST="goreleaser-lang/${PULUMI_LANG}"
   mkdir -p "$LANG_DIST"
-  (
-    # Run in a subshell to ensure we don't alter current working directory.
-    cd "${LANG_DIST}"
+  cd "${LANG_DIST}"
 
-    rm -rf ./*
+  rm -rf ./*
 
-    # Currently avoiding a dependency on GH CLI in favor of curl, so
-    # that this script works in the context of the Brew formula:
-    #
-    # https://github.com/Homebrew/homebrew-core/blob/master/Formula/pulumi.rb
-    #
-    # Formerly:
-    #
-    # gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}"
+  # Currently avoiding a dependency on GH CLI in favor of curl, so
+  # that this script works in the context of the Brew formula:
+  #
+  # https://github.com/Homebrew/homebrew-core/blob/master/Formula/pulumi.rb
+  #
+  # Formerly:
+  #
+  # gh release download "${TAG}" --repo "pulumi/pulumi-${PULUMI_LANG}"
 
-    for DIST_OS in darwin linux windows; do
-      for i in "amd64 x64" "arm64 arm64"; do
-        set -- $i # treat strings in loop as args
-        DIST_ARCH="$1"
-        RENAMED_ARCH="$2" # goreleaser in pulumi/pulumi renames amd64 to x64
+  for DIST_OS in darwin linux windows; do
+    for i in "amd64 x64" "arm64 arm64"; do
+      set -- $i # treat strings in loop as args
+      DIST_ARCH="$1"
+      RENAMED_ARCH="$2" # goreleaser in pulumi/pulumi renames amd64 to x64
 
-        ARCHIVE="pulumi-language-${PULUMI_LANG}-${TAG}-${DIST_OS}-${DIST_ARCH}"
+      ARCHIVE="pulumi-language-${PULUMI_LANG}-${TAG}-${DIST_OS}-${DIST_ARCH}"
 
-        # No consistency on whether Windows archives use .zip or
-        # .tar.gz, try both.
+      # No consistency on whether Windows archives use .zip or
+      # .tar.gz, try both.
 
-        curl -OL --fail "https://github.com/pulumi/pulumi-${PULUMI_LANG}/releases/download/${TAG}/${ARCHIVE}.tar.gz" \
-          || curl -OL --fail "https://github.com/pulumi/pulumi-${PULUMI_LANG}/releases/download/${TAG}/${ARCHIVE}.zip"
+      curl -OL --fail "https://github.com/pulumi/pulumi-${PULUMI_LANG}/releases/download/${TAG}/${ARCHIVE}.tar.gz" || echo "ignoring download"
+      curl -OL --fail "https://github.com/pulumi/pulumi-${PULUMI_LANG}/releases/download/${TAG}/${ARCHIVE}.zip" || echo "ignoring download"
 
-        OUTDIR="$DIST_OS-$RENAMED_ARCH"
+      OUTDIR="$DIST_OS-$RENAMED_ARCH"
 
-        mkdir -p $OUTDIR
-        find '.' -name "*-$DIST_OS-$DIST_ARCH.tar.gz" -print0 -exec tar -xzvf {} -C $OUTDIR \;
-        find '.' -name "*-$DIST_OS-$DIST_ARCH.zip" -print0 -exec unzip {} -d $OUTDIR \;
-      done
+      mkdir -p $OUTDIR
+      find '.' -name "*-$DIST_OS-$DIST_ARCH.tar.gz" -print0 -exec tar -xzvf {} -C $OUTDIR \;
+      find '.' -name "*-$DIST_OS-$DIST_ARCH.zip" -print0 -exec unzip {} -d $OUTDIR \;
     done
-  )
+  done
+
+  cd ../..
 done
