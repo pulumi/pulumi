399cce7fbe6036fd2894b92c3a0bbac7d3d18ae8
Your Name <you@example.com>
2022-08-01 23:44:01 -0500

    parsing poc

--- pkg/cmd/pulumi/deploy.go
+++ pkg/cmd/pulumi/deploy.go
@@ -68,14 +68,183 @@ func (d *singletonDeployment) ProjectDir() string {
 	return strings.Join([]string{"global-stack", d.resourceName, d.resourceType}, "_")
 }
 
-func parseResource(args []string) (YamlResource, error) {
-	resourceName, resourceType, props := args[1], args[0], args[2:]
+type ObjectArgs struct {
+	children map[string]interface{} // either ObjectArgs | ListArgs
+}
 
+type ListArgs struct {
+	children []interface{} // either ObjectArgs | ListArgs
+}
+
+func parseObj(args []string) interface{} {
+	obj := make(map[string]interface{})
+
+	openObj := 0
+	openList := 0
+
+	start := 0
+
+argLoop:
+	for i, k := range args {
+		s := strings.SplitN(k, "=", 2)
+		key, value := s[0], s[1]
+		switch value {
+		case "[[":
+			if openList > 0 {
+				// disregard as we are parsing a list
+				continue argLoop
+			}
+			if openObj == 0 {
+				start = i
+			}
+			openObj += 1
+			continue argLoop
+		case "]]":
+			if openList > 0 {
+				// disregard as we are parsing a list
+				continue argLoop
+			}
+			openObj += -1
+			if openObj == 0 {
+				obj[key] = parseArgs(args[start : i+1])
+			}
+			continue argLoop
+		case "[":
+			if openObj > 0 {
+				// disregard as we are parsing an object
+				continue argLoop
+			}
+			if openList == 0 {
+				start = i
+			}
+			openList += 1
+			continue argLoop
+		case "]":
+			if openObj > 0 {
+				// disregard as we are parsing an object
+				continue argLoop
+			}
+			openList += -1
+			if openList == 0 {
+				obj[key] = parseArgs(args[start : i+1])
+			}
+			continue argLoop
+		}
+		if openObj > 0 || openList > 0 {
+			continue argLoop
+		}
+		obj[key] = value
+	}
+	return obj
+}
+
+func parseList(args []string) interface{} {
+	lst := make([]interface{}, 0, len(args))
+
+	openObj := 0
+	openList := 0
+
+	start := 0
+
+argLoop:
+	for i, k := range args {
+		s := strings.Split(k, "=")
+		_, value := s[0], s[len(s)-1]
+		switch value {
+		case "[[":
+			if openList > 0 {
+				// disregard as we are parsing a list
+				continue argLoop
+			}
+			if openObj == 0 {
+				start = i
+			}
+			openObj += 1
+			continue argLoop
+		case "]]":
+			if openList > 0 {
+				// disregard as we are parsing a list
+				continue argLoop
+			}
+			openObj += -1
+			if openObj == 0 {
+				lst = append(lst, parseArgs(args[start:i+1]))
+			}
+			continue argLoop
+		case "[":
+			if openObj > 0 {
+				// disregard as we are parsing an lst
+				continue argLoop
+			}
+			if openList == 0 {
+				start = i
+			}
+			openList += 1
+			continue argLoop
+		case "]":
+			if openObj > 0 {
+				// disregard as we are parsing an lst
+				continue argLoop
+			}
+			openList += -1
+			if openList == 0 {
+				lst = append(lst, parseArgs(args[start:i+1]))
+			}
+			continue argLoop
+		}
+		if openObj > 0 || openList > 0 {
+			continue argLoop
+		}
+		lst = append(lst, k)
+	}
+	return lst
+}
+
+func parseArgs(a []string) interface{} {
+	// minimum size of 2
+	args := make([]string, 0, len(a))
+	s := strings.SplitN(a[0], "=", 2)
+	prefix := s[0]
+
+	for i, arg := range a {
+		args = append(args, strings.TrimPrefix(arg, prefix+"="))
+		fmt.Println(args[i])
+	}
+
+	if args[0] == "[[" {
+		return parseObj(args[1 : len(args)-1])
+	} else if args[0] == "[" {
+		return parseList(args[1 : len(args)-1])
+	}
+	panic("should not get here")
+}
+
+func parseResource(args []string) (YamlResource, error) {
+	/*
+	 */
+	resourceName, resourceType, props := args[1], args[0], args[2:]
 	fmt.Println("args", props)
+
+	newProps := make([]string, 0, len(props)+2)
+
+	newProps = append(newProps, "=[[")
+	for _, prop := range props {
+		newProps = append(newProps, "="+prop)
+	}
+	newProps = append(newProps, "=[[")
+
+	p := parseObj(props)
+
+	finalProps, ok := p.(map[string]interface{})
+	if !ok {
+		return YamlResource{}, fmt.Errorf("failed to parse args")
+	}
+	// args are implied to be in an object
 	return YamlResource{
 		Name: resourceName,
 		Definition: YamlResourceDef{
-			Type: resourceType,
+			Type:       resourceType,
+			Properties: finalProps,
 		},
 	}, nil
 }
