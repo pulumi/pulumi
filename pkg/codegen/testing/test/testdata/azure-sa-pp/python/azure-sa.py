8d08a5b2648a28ad7cb3ce8a180fb3f07d1c5a14
Kyle Pitzen <kyle.pitzen@gmail.com>
2022-08-10 15:11:42 -0400

    feat: Use new default arguments in python pulumi.Config.get

--- pkg/codegen/testing/test/testdata/azure-sa-pp/python/azure-sa.py
+++ pkg/codegen/testing/test/testdata/azure-sa-pp/python/azure-sa.py
@@ -5,15 +5,9 @@ config = pulumi.Config()
 storage_account_name_param = config.require("storageAccountNameParam")
 resource_group_name_param = config.require("resourceGroupNameParam")
 resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
-location_param = config.get("locationParam")
-if location_param is None:
-    location_param = resource_group_var.location
-storage_account_tier_param = config.get("storageAccountTierParam")
-if storage_account_tier_param is None:
-    storage_account_tier_param = "Standard"
-storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
-if storage_account_type_replication_param is None:
-    storage_account_type_replication_param = "LRS"
+location_param = config.get("locationParam", resource_group_var.location)
+storage_account_tier_param = config.get("storageAccountTierParam", "Standard")
+storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam", "LRS")
 storage_account_resource = azure.storage.Account("storageAccountResource",
     name=storage_account_name_param,
     account_kind="StorageV2",
