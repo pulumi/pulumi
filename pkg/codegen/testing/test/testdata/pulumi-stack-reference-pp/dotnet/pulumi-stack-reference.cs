7ffa69b8802e9b9e75c9df8cb587fa5f9edf0b71
aq17 <aqiu@pulumi.com>
2022-08-18 16:17:49 -0700

    update c# test

--- pkg/codegen/testing/test/testdata/pulumi-stack-reference-pp/dotnet/pulumi-stack-reference.cs
+++ pkg/codegen/testing/test/testdata/pulumi-stack-reference-pp/dotnet/pulumi-stack-reference.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic
 using Pulumi;
 
 return await Deployment.RunAsync(() =>
@@ -5,3 +6,12 @@ return await Deployment.RunAsync(() =>
 	var stackRef = new Pulumi.StackReference("foo/bar/dev");
 });
 
+
+return await Deployment.RunAsync(() =>
+{
+	var stackRef = new Pulumi.Pulumi.StackReference("stackRef", new()
+	{
+		Name = "foo/bar/dev",
+	});
+
+});
