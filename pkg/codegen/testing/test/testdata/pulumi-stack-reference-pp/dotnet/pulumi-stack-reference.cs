2acee1d1c0f0428773714ec4ae1ecc29262a2007
aq17 <aqiu@pulumi.com>
2022-08-18 17:14:22 -0700

    fix

--- pkg/codegen/testing/test/testdata/pulumi-stack-reference-pp/dotnet/pulumi-stack-reference.cs
+++ pkg/codegen/testing/test/testdata/pulumi-stack-reference-pp/dotnet/pulumi-stack-reference.cs
@@ -1,11 +1,5 @@
-using System.Collections.Generic
+using System.Collections.Generic;
 using Pulumi;
-
-return await Deployment.RunAsync(() =>
-{
-	var stackRef = new Pulumi.StackReference("foo/bar/dev");
-});
-
 
 return await Deployment.RunAsync(() =>
 {
