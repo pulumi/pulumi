56379531f673ffe4b9a0754602606c09538a7865
aq17 <aqiu@pulumi.com>
2022-08-22 10:18:33 -0700

    add typecheck for version, pluginurl

--- pkg/codegen/pcl/binder_resource.go
+++ pkg/codegen/pcl/binder_resource.go
@@ -331,7 +331,11 @@ func (b *binder) bindResourceBody(node *Resource) hcl.Diagnostics {
 					t = model.NewListType(ResourcePropertyType)
 					resourceOptions.IgnoreChanges = item.Value
 				case "version":
-					continue
+					t = model.StringType
+					resourceOptions.Version = item.Value
+				case "pluginDownloadURL":
+					t = model.StringType
+					resourceOptions.PluginDownloadURL = item.Value
 				default:
 					diagnostics = append(diagnostics, unsupportedAttribute(item.Name, item.Syntax.NameRange))
 					continue
