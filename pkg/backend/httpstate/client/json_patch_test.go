ae8d3f644ca1e150e144ed14584b89f366f8b2f6
Anton Tayanovskyy <anton@pulumi.com>
2022-09-08 19:03:32 -0400

    Check restricted models to improve coverage

--- pkg/backend/httpstate/client/json_patch_test.go
+++ pkg/backend/httpstate/client/json_patch_test.go
@@ -29,43 +29,61 @@ func decodePatch(raw []byte) (jpatch.Patch, error) {
 }
 
 func TestPatchTurnaround(t *testing.T) {
-	rapid.Check(t, func(t *rapid.T) {
-		j := rapidJson(rapidJsonOpts{
-			maxHeight:  3,
-			objectOnly: true,
-			allowNull:  false,
+	t.Run("general-3", func(t *testing.T) {
+		rapid.Check(t, func(t *rapid.T) {
+			checkTurnaround(t, rapidJsonOpts{
+				maxHeight:  3,
+				objectOnly: true,
+				allowNull:  false,
+			})
 		})
-		original := j.Draw(t, "original").(json.RawMessage)
-		modified := j.Draw(t, "modified").(json.RawMessage)
-		t.Logf("original=%v", string(original))
-		t.Logf("modified=%v", string(modified))
-
-		mergePatch, err := jpatch.CreateMergePatch(original, modified)
-		if err != nil {
-			t.Fatalf("CreateMergePatch failed: %v", err)
-		}
-		t.Logf("mergePatch=%v", string(mergePatch))
-
-		reconstructed, err := jpatch.MergePatch(original, mergePatch)
-		if err != nil {
-			t.Fatalf("MergePatch failed: %v", err)
-		}
-
-		reconstructedNorm, err := canonicalizeJson(reconstructed)
-		if err != nil {
-			t.Fatalf("canonicalizeJson failed: %v", err)
-		}
-		t.Logf("reconstr=%v", string(reconstructedNorm))
-
-		modifiedNorm, err := canonicalizeJson(modified)
-		if err != nil {
-			t.Fatalf("canonicalizeJson failed: %v", err)
-		}
-
-		if string(reconstructedNorm) != string(modifiedNorm) {
-			t.Fatalf("patch.Apply() did not match")
-		}
 	})
+	t.Run("restricted-3", func(t *testing.T) {
+		rapid.Check(t, func(t *rapid.T) {
+			checkTurnaround(t, rapidJsonOpts{
+				maxHeight:  3,
+				objectOnly: true,
+				allowNull:  false,
+				stringGen:  rapid.StringMatching("a|b"),
+				intGen:     rapid.IntRange(1, 1),
+				float64Gen: rapid.Float64Range(2.0, 2.0),
+			})
+		})
+	})
+}
+
+func checkTurnaround(t *rapid.T, opts rapidJsonOpts) {
+	j := rapidJson(opts)
+	original := j.Draw(t, "original").(json.RawMessage)
+	modified := j.Draw(t, "modified").(json.RawMessage)
+	t.Logf("original=%v", string(original))
+	t.Logf("modified=%v", string(modified))
+
+	mergePatch, err := jpatch.CreateMergePatch(original, modified)
+	if err != nil {
+		t.Fatalf("CreateMergePatch failed: %v", err)
+	}
+	t.Logf("mergePatch=%v", string(mergePatch))
+
+	reconstructed, err := jpatch.MergePatch(original, mergePatch)
+	if err != nil {
+		t.Fatalf("MergePatch failed: %v", err)
+	}
+
+	reconstructedNorm, err := canonicalizeJson(reconstructed)
+	if err != nil {
+		t.Fatalf("canonicalizeJson failed: %v", err)
+	}
+	t.Logf("reconstr=%v", string(reconstructedNorm))
+
+	modifiedNorm, err := canonicalizeJson(modified)
+	if err != nil {
+		t.Fatalf("canonicalizeJson failed: %v", err)
+	}
+
+	if string(reconstructedNorm) != string(modifiedNorm) {
+		t.Fatalf("patch.Apply() did not match")
+	}
 }
 
 // Generates arbitrary JSON trees as json.RawMessage obtained by
@@ -74,14 +92,27 @@ func TestPatchTurnaround(t *testing.T) {
 //
 // Excludes null values from maps.
 func rapidJson(opts rapidJsonOpts) *rapid.Generator {
-	str := rapid.String()
+	strG := opts.stringGen
+	if strG == nil {
+		strG = rapid.String()
+	}
+
+	intG := opts.intGen
+	if intG == nil {
+		intG = rapid.Int()
+	}
+
+	f64G := opts.float64Gen
+	if f64G == nil {
+		f64G = rapid.Float64()
+	}
 
 	options := []*rapid.Generator{
 		rapid.Just(json.RawMessage(`true`)),
 		rapid.Just(json.RawMessage(`false`)),
-		str.Map(func(x string) json.RawMessage { return marshal(x) }),
-		rapid.Int().Map(func(x int) json.RawMessage { return marshal(x) }),
-		rapid.Float64().Map(func(x float64) json.RawMessage { return marshal(x) }),
+		strG.Map(func(x string) json.RawMessage { return marshal(x) }),
+		intG.Map(func(x int) json.RawMessage { return marshal(x) }),
+		f64G.Map(func(x float64) json.RawMessage { return marshal(x) }),
 	}
 
 	if opts.allowNull {
@@ -89,10 +120,13 @@ func rapidJson(opts rapidJsonOpts) *rapid.Generator {
 	}
 
 	if opts.maxHeight > 1 {
-		object := rapid.MapOf(str, rapidJson(rapidJsonOpts{
+		object := rapid.MapOf(strG, rapidJson(rapidJsonOpts{
 			maxHeight:  opts.maxHeight - 1,
 			objectOnly: false,
 			allowNull:  false,
+			stringGen:  opts.stringGen,
+			intGen:     opts.intGen,
+			float64Gen: opts.float64Gen,
 		})).Map(func(x map[string]interface{}) json.RawMessage { return marshal(x) })
 
 		if opts.objectOnly {
@@ -103,6 +137,9 @@ func rapidJson(opts rapidJsonOpts) *rapid.Generator {
 			maxHeight:  opts.maxHeight - 1,
 			objectOnly: false,
 			allowNull:  true,
+			stringGen:  opts.stringGen,
+			intGen:     opts.intGen,
+			float64Gen: opts.float64Gen,
 		})).Map(func(x []interface{}) json.RawMessage { return marshal(x) })
 
 		options = append(options, array, object)
@@ -115,6 +152,9 @@ type rapidJsonOpts struct {
 	maxHeight  int
 	objectOnly bool
 	allowNull  bool
+	stringGen  *rapid.Generator
+	intGen     *rapid.Generator
+	float64Gen *rapid.Generator
 }
 
 func marshal(x interface{}) json.RawMessage {
