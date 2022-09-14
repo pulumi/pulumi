2a987447fa18f66b70513d726600b1854741ade0
Aaron Friel <mayreply@aaronfriel.com>
2022-09-13 19:34:53 -0700

    ci: Remove v0.10.0 compatibility test

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Config } from "@pulumi/pulumi";
-
-let config = new Config("minimal");
-console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);
-
