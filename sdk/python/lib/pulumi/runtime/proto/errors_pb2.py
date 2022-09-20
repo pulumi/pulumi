920e928886163bb37f4c6b3c6d9d4fefe542274d
Fraser Waters <fraser@pulumi.com>
2022-09-20 09:31:01 +0100

    Fix the go_package option on the .proto files
    This doesn't really effect anything with our current usage, but if we
    ever put proto files in another package and try to import the current
    set it wouldn't actually be able to find them.
    
    I noticed this while working on
    https://github.com/pulumi/pulumi/pull/10792 where I added a new
    "pulumirpc.engine" package. The proto file could refer to things like
    "pulumirpc.PluginDependency" but the go code then tried to import it
    like `_go "github.com/pulumi/pulumi/v3/proto/go/pulumirpc"` which isn't
    actually the correct import path (missing the sdk folder part, and there
    isn't acutally a folder called pulumirpc).

--- sdk/python/lib/pulumi/runtime/proto/errors_pb2.py
+++ sdk/python/lib/pulumi/runtime/proto/errors_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13pulumi/errors.proto\x12\tpulumirpc\"1\n\nErrorCause\x12\x0f\n\x07message\x18\x01 \x01(\t\x12\x12\n\nstackTrace\x18\x02 \x01(\tB0Z.github.com/pulumi/pulumi/v3/proto/go/pulumirpcb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13pulumi/errors.proto\x12\tpulumirpc\"1\n\nErrorCause\x12\x0f\n\x07message\x18\x01 \x01(\t\x12\x12\n\nstackTrace\x18\x02 \x01(\tB4Z2github.com/pulumi/pulumi/sdk/v3/proto/go;pulumirpcb\x06proto3')
 
 
 
@@ -29,7 +29,7 @@ _sym_db.RegisterMessage(ErrorCause)
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  DESCRIPTOR._serialized_options = b'Z.github.com/pulumi/pulumi/v3/proto/go/pulumirpc'
+  DESCRIPTOR._serialized_options = b'Z2github.com/pulumi/pulumi/sdk/v3/proto/go;pulumirpc'
   _ERRORCAUSE._serialized_start=34
   _ERRORCAUSE._serialized_end=83
 # @@protoc_insertion_point(module_scope)
--- sdk/python/lib/pulumi/runtime/proto/plugin_pb2.py
+++ sdk/python/lib/pulumi/runtime/proto/plugin_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13pulumi/plugin.proto\x12\tpulumirpc\"\x1d\n\nPluginInfo\x12\x0f\n\x07version\x18\x01 \x01(\t\"O\n\x10PluginDependency\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04kind\x18\x02 \x01(\t\x12\x0f\n\x07version\x18\x03 \x01(\t\x12\x0e\n\x06server\x18\x04 \x01(\t\"\x1f\n\x0cPluginAttach\x12\x0f\n\x07\x61\x64\x64ress\x18\x01 \x01(\tB0Z.github.com/pulumi/pulumi/v3/proto/go/pulumirpcb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13pulumi/plugin.proto\x12\tpulumirpc\"\x1d\n\nPluginInfo\x12\x0f\n\x07version\x18\x01 \x01(\t\"O\n\x10PluginDependency\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04kind\x18\x02 \x01(\t\x12\x0f\n\x07version\x18\x03 \x01(\t\x12\x0e\n\x06server\x18\x04 \x01(\t\"\x1f\n\x0cPluginAttach\x12\x0f\n\x07\x61\x64\x64ress\x18\x01 \x01(\tB4Z2github.com/pulumi/pulumi/sdk/v3/proto/go;pulumirpcb\x06proto3')
 
 
 
@@ -45,7 +45,7 @@ _sym_db.RegisterMessage(PluginAttach)
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  DESCRIPTOR._serialized_options = b'Z.github.com/pulumi/pulumi/v3/proto/go/pulumirpc'
+  DESCRIPTOR._serialized_options = b'Z2github.com/pulumi/pulumi/sdk/v3/proto/go;pulumirpc'
   _PLUGININFO._serialized_start=34
   _PLUGININFO._serialized_end=63
   _PLUGINDEPENDENCY._serialized_start=65
