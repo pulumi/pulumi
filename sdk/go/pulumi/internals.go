8788934bb3c554f4df92e4a46cdff9ef7797fa57
Aaron Friel <mayreply@aaronfriel.com>
2022-10-05 11:07:35 -0700

    Update sdk/go/pulumi/internals.go
    Co-authored-by: Justin Van Patten <jvp@justinvp.com>

--- sdk/go/pulumi/internals.go
+++ sdk/go/pulumi/internals.go
@@ -1,6 +1,6 @@
 package pulumi
 
-import "golang.org/x/net/context"
+import "context"
 
 // Functions in this file are exposed in pulumi/internals via go:linkname
 func awaitWithContext(ctx context.Context, o Output) (interface{}, bool, bool, []Resource, error) {
