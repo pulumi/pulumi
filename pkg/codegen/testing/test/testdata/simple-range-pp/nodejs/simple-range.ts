4c7580471c2841cf756f04873d6dbae16280bb14
Zaid Ajaj <zaid.naom@gmail.com>
2022-10-09 18:28:49 +0200

    Fixes int constant range expressions for go

---
+++ pkg/codegen/testing/test/testdata/simple-range-pp/nodejs/simple-range.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const bucket: aws.s3.Bucket[];
+for (const range = {value: 0}; range.value < 10; range.value++) {
+    bucket.push(new aws.s3.Bucket(`bucket-${range.value}`, {website: {
+        indexDocument: `index-${range.value}.html`,
+    }}));
+}
