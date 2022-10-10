dab81be2624df4f4de031a4884deadfd4ef1db1e
杨成锴 <homeboyc@foxmail.com>
2022-10-10 21:52:06 +0800

    typo: apply suggestions from code review
    Co-authored-by: Fraser Waters <frassle@gmail.com>

--- sdk/go/auto/optdestroy/optdestroy.go
+++ sdk/go/auto/optdestroy/optdestroy.go
@@ -87,7 +87,7 @@ func UserAgent(agent string) Option {
 	})
 }
 
-// ShowSecrets Show config secrets when they appear.
+// ShowSecrets show config secrets when they appear.
 func ShowSecrets(show bool) Option {
 	return optionFunc(func(opts *Options) {
 		opts.ShowSecrets = &show
