0ab75557b63022a1f6df4ac20c5e7ebb186bee0a
Ian Wahbe <ian@wahbe.com>
2022-11-09 13:53:13 -0800

    Add Pulumi as a default dependency for .NET

--- pkg/codegen/dotnet/gen.go
+++ pkg/codegen/dotnet/gen.go
@@ -2198,6 +2198,12 @@ func genProjectFile(pkg *schema.Package,
 	packageReferences map[string]string,
 	projectReferences []string) ([]byte, error) {
 
+	if packageReferences == nil {
+		packageReferences = map[string]string{}
+	}
+	if _, ok := packageReferences["Pulumi"]; !ok {
+		packageReferences["Pulumi"] = "3.*"
+	}
 	w := &bytes.Buffer{}
 	err := csharpProjectFileTemplate.Execute(w, csharpProjectFileTemplateContext{
 		XMLDoc:            fmt.Sprintf(`.\%s.xml`, assemblyName),
