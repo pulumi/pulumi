pulumi/pulumi

commit 32dcaa58d661f2fcaff03474d1a5c5a412a54b09

Monitor.Invoke and Provider.Invoke take different arguments (#9323)

pkg/resource/deploy/source_eval.go
@@ -634,7 +634,7 @@ func (rm *resmon) SupportsFeature(ctx context.Context,
 }
 
 // Invoke performs an invocation of a member located in a resource provider.
-func (rm *resmon) Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+func (rm *resmon) Invoke(ctx context.Context, req *pulumirpc.ResourceInvokeRequest) (*pulumirpc.InvokeResponse, error) {
 	// Fetch the token and load up the resource provider if necessary.
 	tok := tokens.ModuleMember(req.GetTok())
 	providerReq, err := parseProviderRequest(tok.Package(), req.GetVersion(), req.GetPluginDownloadURL())
@@ -668,7 +668,7 @@ func (rm *resmon) Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*pu
 	mret, err := plugin.MarshalProperties(ret, plugin.MarshalOptions{
 		Label:         label,
 		KeepUnknowns:  true,
-		KeepResources: true,
+		KeepResources: req.GetAcceptResources(),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal %v return: %w", tok, err)
@@ -770,7 +770,7 @@ func (rm *resmon) Call(ctx context.Context, req *pulumirpc.CallRequest) (*pulumi
 }
 
 func (rm *resmon) StreamInvoke(
-	req *pulumirpc.InvokeRequest, stream pulumirpc.ResourceMonitor_StreamInvokeServer) error {
+	req *pulumirpc.ResourceInvokeRequest, stream pulumirpc.ResourceMonitor_StreamInvokeServer) error {
 
 	tok := tokens.ModuleMember(req.GetTok())
 	label := fmt.Sprintf("ResourceMonitor.StreamInvoke(%s)", tok)
@@ -1320,7 +1320,7 @@ func decorateResourceSpans(span opentracing.Span, method string, req, resp inter
 
 	switch method {
 	case "/pulumirpc.ResourceMonitor/Invoke":
-		span.SetTag("pulumi-decorator", req.(*pulumirpc.InvokeRequest).Tok)
+		span.SetTag("pulumi-decorator", req.(*pulumirpc.ResourceInvokeRequest).Tok)
 	case "/pulumirpc.ResourceMonitor/ReadResource":
 		span.SetTag("pulumi-decorator", req.(*pulumirpc.ReadResourceRequest).Type)
 	case "/pulumirpc.ResourceMonitor/RegisterResource":

sdk/go/pulumi/context.go
@@ -282,7 +282,7 @@ func (ctx *Context) Invoke(tok string, args interface{}, result interface{}, opt
 
 	// Now, invoke the RPC to the provider synchronously.
 	logging.V(9).Infof("Invoke(%s, #args=%d): RPC call being made synchronously", tok, len(resolvedArgsMap))
-	resp, err := ctx.monitor.Invoke(ctx.ctx, &pulumirpc.InvokeRequest{
+	resp, err := ctx.monitor.Invoke(ctx.ctx, &pulumirpc.ResourceInvokeRequest{
 		Tok:               tok,
 		Args:              rpcArgs,
 		Provider:          providerRef,
@@ -699,7 +699,7 @@ func (ctx *Context) getResource(urn string) (*pulumirpc.RegisterResourceResponse
 
 	tok := "pulumi:pulumi:getResource"
 	logging.V(9).Infof("Invoke(%s, #args=%d): RPC call being made synchronously", tok, len(resolvedArgsMap))
-	resp, err := ctx.monitor.Invoke(ctx.ctx, &pulumirpc.InvokeRequest{
+	resp, err := ctx.monitor.Invoke(ctx.ctx, &pulumirpc.ResourceInvokeRequest{
 		Tok:  "pulumi:pulumi:getResource",
 		Args: rpcArgs,
 	})

sdk/nodejs/runtime/resource.ts
@@ -111,7 +111,7 @@ export function getResource(res: Resource, props: Inputs, custom: boolean, urn: 
     debuggablePromise(resopAsync.then(async (resop) => {
         const inputs = await serializeProperties(label, { urn });
 
-        const req = new providerproto.InvokeRequest();
+        const req = new resproto.ResourceInvokeRequest();
         req.setTok("pulumi:pulumi:getResource");
         req.setArgs(gstruct.Struct.fromJavaScript(inputs));
         req.setProvider("");

sdk/python/lib/pulumi/runtime/resource.py
@@ -243,7 +243,7 @@ def get_resource(
 
             monitor = settings.get_monitor()
             inputs = await rpc.serialize_properties({"urn": urn}, {})
-            req = provider_pb2.InvokeRequest(
+            req = resource_pb2.ResourceInvokeRequest(
                 tok="pulumi:pulumi:getResource", args=inputs, provider="", version=""
             )
 

pkg/resource/deploy/deploytest/resourcemonitor.go
@@ -284,7 +284,7 @@ func (rm *ResourceMonitor) Invoke(tok tokens.ModuleMember, inputs resource.Prope
 	}
 
 	// submit request
-	resp, err := rm.resmon.Invoke(context.Background(), &pulumirpc.InvokeRequest{
+	resp, err := rm.resmon.Invoke(context.Background(), &pulumirpc.ResourceInvokeRequest{
 		Tok:      string(tok),
 		Provider: provider,
 		Args:     ins,

pkg/resource/deploy/source.go
@@ -62,7 +62,7 @@ type SourceResourceMonitor interface {
 
 	Address() string
 	Cancel() error
-	Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error)
+	Invoke(ctx context.Context, req *pulumirpc.ResourceInvokeRequest) (*pulumirpc.InvokeResponse, error)
 	Call(ctx context.Context, req *pulumirpc.CallRequest) (*pulumirpc.CallResponse, error)
 	ReadResource(ctx context.Context,
 		req *pulumirpc.ReadResourceRequest) (*pulumirpc.ReadResourceResponse, error)

pkg/resource/deploy/source_query.go
@@ -321,7 +321,9 @@ func (rm *queryResmon) Cancel() error {
 }
 
 // Invoke performs an invocation of a member located in a resource provider.
-func (rm *queryResmon) Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+func (rm *queryResmon) Invoke(
+	ctx context.Context, req *pulumirpc.ResourceInvokeRequest) (*pulumirpc.InvokeResponse, error) {
+
 	tok := tokens.ModuleMember(req.GetTok())
 	label := fmt.Sprintf("QueryResourceMonitor.Invoke(%s)", tok)
 
@@ -372,7 +374,7 @@ func (rm *queryResmon) Invoke(ctx context.Context, req *pulumirpc.InvokeRequest)
 }
 
 func (rm *queryResmon) StreamInvoke(
-	req *pulumirpc.InvokeRequest, stream pulumirpc.ResourceMonitor_StreamInvokeServer) error {
+	req *pulumirpc.ResourceInvokeRequest, stream pulumirpc.ResourceMonitor_StreamInvokeServer) error {
 
 	tok := tokens.ModuleMember(req.GetTok())
 	label := fmt.Sprintf("QueryResourceMonitor.StreamInvoke(%s)", tok)
@@ -396,7 +398,10 @@ func (rm *queryResmon) StreamInvoke(
 	// streaming operation completes!
 	logging.V(5).Infof("QueryResourceMonitor.StreamInvoke received: tok=%v #args=%v", tok, len(args))
 	failures, err := prov.StreamInvoke(tok, args, func(event resource.PropertyMap) error {
-		mret, err := plugin.MarshalProperties(event, plugin.MarshalOptions{Label: label, KeepUnknowns: true})
+		mret, err := plugin.MarshalProperties(event, plugin.MarshalOptions{Label: label,
+			KeepUnknowns:  true,
+			KeepResources: req.GetAcceptResources(),
+		})
 		if err != nil {
 			return fmt.Errorf("failed to marshal return: %w", err)
 		}

pkg/resource/deploy/source_query_test.go
@@ -184,7 +184,7 @@ func (rm *mockQueryResmon) Cancel() error {
 	return nil
 }
 func (rm *mockQueryResmon) Invoke(ctx context.Context,
-	req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+	req *pulumirpc.ResourceInvokeRequest) (*pulumirpc.InvokeResponse, error) {
 
 	panic("not implemented")
 }

commit 1949b65072e6f2e1317c2ebf705d7a7d0113dc30

Keep resource refs when invoking `pulumi:pulumi:getResource`
We used to always keep resource references when marshaling the results of `pulumi:pulumi:getResource`, but this regressed in #9323 to only keep resource if request's `acceptResources` flag was set. Unfortunately, that flag was not being set when calling `pulumi:pulumi:getResource` in the Go, Node.js, and Python SDKs (although, it was being set for .NET).

Two fixes:
1. Update the monitor to go back to always keeping resources when `pulumi:pulumi:getResource` is being invoked. This way, older SDKs that are not setting `acceptResources` will go back to the original behavior.
2. Update the SDKs to always set `acceptResources`, so that these newer versions of the SDKs will work with older engines that are checking `acceptResources`.

(2) will help us with EKS 1.0. We'll be able to update EKS to use a version of `@pulumi/pulumi` with the fix to set the `acceptResources` flag.

pkg/resource/deploy/source_eval.go
@@ -710,11 +710,20 @@ func (rm *resmon) Invoke(ctx context.Context, req *pulumirpc.ResourceInvokeReque
 	if err != nil {
 		return nil, fmt.Errorf("invocation of %v returned an error: %w", tok, err)
 	}
+
+	// Respect `AcceptResources` unless `tok` is for the built-in `pulumi:pulumi:getResource` function,
+	// in which case always keep resources to maintain the original behavior for older SDKs that are not
+	// setting the `AccceptResources` flag.
+	keepResources := req.GetAcceptResources()
+	if tok == "pulumi:pulumi:getResource" {
+		keepResources = true
+	}
+
 	mret, err := plugin.MarshalProperties(ret, plugin.MarshalOptions{
 		Label:         label,
 		KeepUnknowns:  true,
 		KeepSecrets:   true,
-		KeepResources: req.GetAcceptResources(),
+		KeepResources: keepResources,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal %v return: %w", tok, err)

sdk/go/pulumi/context.go
@@ -723,8 +723,9 @@ func (ctx *Context) getResource(urn string) (*pulumirpc.RegisterResourceResponse
 	tok := "pulumi:pulumi:getResource"
 	logging.V(9).Infof("Invoke(%s, #args=%d): RPC call being made synchronously", tok, len(resolvedArgsMap))
 	resp, err := ctx.monitor.Invoke(ctx.ctx, &pulumirpc.ResourceInvokeRequest{
-		Tok:  "pulumi:pulumi:getResource",
-		Args: rpcArgs,
+		Tok:             "pulumi:pulumi:getResource",
+		Args:            rpcArgs,
+		AcceptResources: !disableResourceReferences,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("invoke(%s, ...): error: %v", tok, err)

sdk/nodejs/runtime/resource.ts
@@ -114,6 +114,7 @@ export function getResource(res: Resource, parent: Resource | undefined, props: 
         req.setArgs(gstruct.Struct.fromJavaScript(inputs));
         req.setProvider("");
         req.setVersion("");
+        req.setAcceptresources(!utils.disableResourceReferences);
 
         // Now run the operation, serializing the invocation if necessary.
         const opLabel = `monitor.getResource(${label})`;

sdk/python/lib/pulumi/runtime/resource.py
@@ -243,8 +243,17 @@ def get_resource(
 
             monitor = settings.get_monitor()
             inputs = await rpc.serialize_properties({"urn": urn}, {})
+
+            accept_resources = not (
+                os.getenv("PULUMI_DISABLE_RESOURCE_REFERENCES", "").upper()
+                in {"TRUE", "1"}
+            )
             req = resource_pb2.ResourceInvokeRequest(
-                tok="pulumi:pulumi:getResource", args=inputs, provider="", version=""
+                tok="pulumi:pulumi:getResource",
+                args=inputs,
+                provider="",
+                version="",
+                acceptResources=accept_resources,
             )
 
             def do_invoke():