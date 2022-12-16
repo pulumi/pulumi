00272973941e84664130264e7960fa74499c60b8
Ian Wahbe <ian@wahbe.com>
2022-12-16 13:37:08 +0100

    Don't return half-nil fat ptrs from `SyntaxNode()`
    Passing a typed `nil` through a interface conversion, such as a
    `*hclsyntax.UnaryOpExpr` through a `hclsyntax.Node` interface will
    result in a half-nil fat pointer with the value part `nil`. Since most
    of programgen assumes that `(model.Expression).SyntaxNode().Range()` is
    valid, a `syntax.None` is passed instead of a `nil`.

--- pkg/codegen/pcl/rewrite_convert.go
+++ pkg/codegen/pcl/rewrite_convert.go
@@ -45,6 +45,9 @@ func sameSchemaTypes(xt, yt model.Type) bool {
 // rewriteConversions implements the core of RewriteConversions. It returns the rewritten expression and true if the
 // type of the expression may have changed.
 func rewriteConversions(x model.Expression, to model.Type, diags *hcl.Diagnostics) (model.Expression, bool) {
+	if x == nil {
+		return nil, false
+	}
 	// If rewriting an operand changed its type and the type of the expression depends on the type of that operand, the
 	// expression must be typechecked in order to update its type.
 	var typecheck bool
