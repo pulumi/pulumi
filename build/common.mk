01775ef8a85b1c0629791a11919ac40e2cd80a96
Abhinav Gupta <abhinav@pulumi.com>
2023-01-18 15:32:45 -0800

    make: Set -race=true for tests
    Run tests in CI with data race detection turned on.
    
    Refs #10092

--- build/common.mk
+++ build/common.mk
@@ -107,11 +107,13 @@ GO_TEST_PARALLELISM     ?= 10   # -parallel, number of parallel tests to run wit
 GO_TEST_PKG_PARALLELISM ?= 2    # -p flag, number of parallel packages to test
 GO_TEST_SHUFFLE         ?= off  # -shuffle flag, randomizes order of tests within a package
 GO_TEST_TAGS            ?= all
+GO_TEST_RACE            ?= true
 
 GO_TEST_FLAGS = -count=1 -cover -tags="${GO_TEST_TAGS}" -timeout 1h \
 	-parallel=${GO_TEST_PARALLELISM} \
 	-shuffle=${GO_TEST_SHUFFLE} \
 	-p=${GO_TEST_PKG_PARALLELISM} \
+	-race=${GO_TEST_RACE} \
 	${GO_TEST_OPTIONS}
 GO_TEST_FAST_FLAGS = -short ${GO_TEST_FLAGS}
 GOPROXY = 'https://proxy.golang.org'
