9b43e56addb136969e02d9b9605a08a7089474a2
Abhinav Gupta <abhinav@pulumi.com>
2023-01-18 15:13:38 -0800

    pkg/hcl2/model: Make String methods thread-safe
    ObjectType.String and a few other types
    do a fair bit of computation to build a string representation.
    They memoize the result in an internal `s` field.
    This causes a data race if two goroutines call `String()`
    on the same object at the same time.
    
    Tests in pkg/codegen/pcl ran into the following
    from ObjectType.String.
    
    ```
    WARNING: DATA RACE
    Write at 0x00c00055c030 by goroutine 36:
      github.com/pulumi/pulumi/pkg/v3/codegen/hcl2/model.(*ObjectType).string()
          [..]/pulumi/pkg/codegen/hcl2/model/type_object.go:300 +0x418
      [.. snip ..]
      github.com/pulumi/pulumi/pkg/v3/codegen/pcl.TestApplyRewriter.func1()
          [..]/pulumi/pkg/codegen/pcl/rewrite_apply_test.go:176 +0x68
      testing.tRunner()
          /opt/homebrew/Cellar/go/1.19.5/libexec/src/testing/testing.go:1446 +0x188
      testing.(*T).Run.func1()
          /opt/homebrew/Cellar/go/1.19.5/libexec/src/testing/testing.go:1493 +0x40
    
    Previous read at 0x00c00055c030 by goroutine 37:
      github.com/pulumi/pulumi/pkg/v3/codegen/hcl2/model.(*ObjectType).string()
          [..]/pulumi/pkg/codegen/hcl2/model/type_object.go:276 +0x40
      github.com/pulumi/pulumi/pkg/v3/codegen/hcl2/model.(*OutputType).string()
          [..]/pulumi/pkg/codegen/hcl2/model/type_output.go:110 +0x4c
      [..]
    ```
    
    To fix this, turn the memoized field into an `atomic.Value`
    in all types that memoize it.
    
    When two goroutines race to calculate this value,
    one of them will do extra work that will be thrown away,
    but it won't cause a data race.
    
    Refs #10092

--- pkg/codegen/hcl2/model/type_object.go
+++ pkg/codegen/hcl2/model/type_object.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"sync/atomic"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
@@ -37,7 +38,7 @@ type ObjectType struct {
 	Annotations []interface{}
 
 	propertyUnion Type
-	s             string
+	s             atomic.Value // Value<string>
 }
 
 // NewObjectType creates a new object type with the given properties and annotations.
@@ -273,8 +274,8 @@ func (t *ObjectType) String() string {
 }
 
 func (t *ObjectType) string(seen map[Type]struct{}) string {
-	if t.s != "" {
-		return t.s
+	if s := t.s.Load(); s != nil {
+		return s.(string)
 	}
 
 	if seen != nil {
@@ -297,8 +298,9 @@ func (t *ObjectType) string(seen map[Type]struct{}) string {
 		annotations = fmt.Sprintf(", annotated(%p)", t)
 	}
 
-	t.s = fmt.Sprintf("object({%s}%v)", strings.Join(properties, ", "), annotations)
-	return t.s
+	s := fmt.Sprintf("object({%s}%v)", strings.Join(properties, ", "), annotations)
+	t.s.Store(s)
+	return s
 }
 
 func (t *ObjectType) unify(other Type) (Type, ConversionKind) {
