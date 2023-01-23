f8dd44bf8c867d9573635a63c333467c721c4b75
Fraser Waters <fraser@pulumi.com>
2023-01-23 19:53:29 +0000

    Require in old test as well

--- sdk/nodejs/cmd/pulumi-language-nodejs/main_test.go
+++ sdk/nodejs/cmd/pulumi-language-nodejs/main_test.go
@@ -148,16 +148,16 @@ func TestGetRequiredPlugins(t *testing.T) {
 	}
 	for _, file := range files {
 		err := os.MkdirAll(filepath.Dir(file.path), 0755)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		err = os.WriteFile(file.path, []byte(file.content), 0600)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 
 	host := &nodeLanguageHost{}
 	resp, err := host.GetRequiredPlugins(context.TODO(), &pulumirpc.GetRequiredPluginsRequest{
 		Program: dir,
 	})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	actual := make(map[string]string)
 	for _, plugin := range resp.GetPlugins() {
