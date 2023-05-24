96db1fd607fed1b5215f917d10361a710ff5e124
Fraser Waters <fraser@pulumi.com>
2023-05-24 16:56:23 +0100

    Hardcode some known provider plugin locations
    Fixes https://github.com/pulumi/pulumi-terraform-bridge/issues/1058

---
+++ pkg/util/plugin.go
@@ -0,0 +1,67 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
+)
+
+// SetKnownPluginDownloadURL sets the PluginDownloadURL for the given PluginSpec if it's a known plugin.
+// Returns true if it filled in the URL.
+func SetKnownPluginDownloadURL(spec *workspace.PluginSpec) bool {
+	// If the download url is already set don't touch it
+	if spec.PluginDownloadURL != "" {
+		return false
+	}
+
+	pulumiversePlugins := []string{
+		"acme",
+		"aquasec",
+		"astra",
+		"aws-eksa",
+		"buildkite",
+		"concourse",
+		"configcat",
+		"doppler",
+		"exoscale",
+		"gandi",
+		"github-credentials",
+		"googleworkspace",
+		"harbor",
+		"hcp",
+		"heroku",
+		"ibm",
+		"matchbox",
+		"mssql",
+		"ngrok",
+		"purrl",
+		"sentry",
+		"statuscake",
+		"time",
+		"unifi",
+		"vra",
+		"zitadel",
+	}
+	if spec.Kind == workspace.ResourcePlugin {
+		for _, plugin := range pulumiversePlugins {
+			if spec.Name == plugin {
+				spec.PluginDownloadURL = "github://api.github.com/pulumiverse"
+				return true
+			}
+		}
+	}
+
+	return false
+}
