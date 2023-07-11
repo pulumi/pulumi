14806926a1ec50f8ceb7eb4f3592c219315bb0b6
David Flanagan <david@rawkode.dev>
2023-07-11 13:55:00 +0100

    feat: support package name override for dotnet

--- pkg/codegen/dotnet/gen_program.go
+++ pkg/codegen/dotnet/gen_program.go
@@ -280,7 +280,12 @@ func GenerateProject(directory string, project workspace.Project, program *pcl.P
 		if langInfo, found := p.Language["csharp"]; found {
 			csharpInfo, ok := langInfo.(CSharpPackageInfo)
 			if ok {
-				namespace := namespaceName(csharpInfo.Namespaces, p.Name)
+				pkgName := p.Name
+				if name := csharpInfo.PackageName; name != "" {
+					pkgName = name
+				}
+
+				namespace := namespaceName(csharpInfo.Namespaces, pkgName)
 				packageName = fmt.Sprintf("%s.%s", csharpInfo.GetRootNamespace(), namespace)
 			}
 		}
