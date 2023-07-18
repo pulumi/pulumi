2fe48ce7116b549ce4c0fab2369efa55465a0adc
Zaid Ajaj <zaid.naom@gmail.com>
2023-07-18 09:25:19 +0200

    regen example

--- pkg/codegen/testing/test/testdata/interpolated-string-keys-pp/nodejs/interpolated-string-keys.ts
+++ pkg/codegen/testing/test/testdata/interpolated-string-keys-pp/nodejs/interpolated-string-keys.ts
@@ -2,6 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 
 const config = new pulumi.Config();
 const value = config.require("value");
-const tags = config.getObject("tags") || {
+const tags = config.getObject<Record<string, string>>("tags") || {
     [`interpolated/${value}`]: "value",
 };
