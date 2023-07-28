fead67f29a40e1fa890eae390e6b7d4521025437
Abhinav Gupta <abhinav@pulumi.com>
2023-07-28 08:57:10 -0700

    cmd/plugin install: Extract implementation into struct
    Extracts the implementation of the `pulumi plugin install` command
    into a separate pluginInstallCmd.
    This mirrors the layout we use for some of the other commands,
    and will allow injecting global dependencies from tests.
    
    This change contains no behavioral changes.

--- pkg/cmd/pulumi/plugin_install.go
+++ pkg/cmd/pulumi/plugin_install.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"context"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -23,28 +24,20 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/pulumi/pulumi/sdk/v3/go/common/encoding"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
-
 	"github.com/blang/semver"
-
-	"github.com/spf13/cobra"
-
-	"github.com/pulumi/pulumi/pkg/v3/backend/display"
 	"github.com/pulumi/pulumi/pkg/v3/util"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/diag/colors"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/encoding"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
+	"github.com/spf13/cobra"
 )
 
 func newPluginInstallCmd() *cobra.Command {
-	var serverURL string
-	var exact bool
-	var file string
-	var reinstall bool
-	var checksum string
-
+	var picmd pluginInstallCmd
 	cmd := &cobra.Command{
 		Use:   "install [KIND NAME [VERSION]]",
 		Args:  cmdutil.MaximumNArgs(3),
@@ -62,161 +55,179 @@ func newPluginInstallCmd() *cobra.Command {
 			"the plugin, though the result is not guaranteed.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			ctx := commandContext()
-			displayOpts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),
-			}
-
-			// Parse the kind, name, and version, if specified.
-			var installs []workspace.PluginSpec
-			if len(args) > 0 {
-				if !workspace.IsPluginKind(args[0]) {
-					return fmt.Errorf("unrecognized plugin kind: %s", args[0])
-				} else if len(args) < 2 {
-					return errors.New("missing plugin name argument")
-				}
-
-				var version *semver.Version
-				if len(args) == 3 {
-					parsedVersion, err := semver.ParseTolerant(args[2])
-					version = &parsedVersion
-					if err != nil {
-						return fmt.Errorf("invalid plugin semver: %w", err)
-					}
-				}
-				if len(args) < 3 && file != "" {
-					return errors.New("missing plugin version argument, this is required if installing from a file")
-				}
-
-				var checksums map[string][]byte
-				if checksum != "" {
-					checksumBytes, err := hex.DecodeString(checksum)
-					if err != nil {
-						return fmt.Errorf("--checksum was not a valid hex string: %w", err)
-					}
-					checksums = map[string][]byte{
-						runtime.GOOS + "-" + runtime.GOARCH: checksumBytes,
-					}
-				}
-
-				pluginSpec := workspace.PluginSpec{
-					Kind:              workspace.PluginKind(args[0]),
-					Name:              args[1],
-					Version:           version,
-					PluginDownloadURL: serverURL, // If empty, will use default plugin source.
-					Checksums:         checksums,
-				}
-
-				// Try and set known plugin download URLs
-				if urlSet := util.SetKnownPluginDownloadURL(&pluginSpec); urlSet {
-					cmdutil.Diag().Infof(
-						diag.Message("", "Plugin download URL set to %s"), pluginSpec.PluginDownloadURL)
-				}
-
-				// If we don't have a version try to look one up
-				if version == nil {
-					latestVersion, err := pluginSpec.GetLatestVersion()
-					if err != nil {
-						return err
-					}
-					pluginSpec.Version = latestVersion
-				}
-
-				installs = append(installs, pluginSpec)
-			} else {
-				if file != "" {
-					return errors.New("--file (-f) is only valid if a specific package is being installed")
-				}
-				if checksum != "" {
-					return errors.New("--checksum is only valid if a specific package is being installed")
-				}
-
-				// If a specific plugin wasn't given, compute the set of plugins the current project needs.
-				plugins, err := getProjectPlugins()
-				if err != nil {
-					return err
-				}
-				for _, plugin := range plugins {
-					// Skip language plugins; by definition, we already have one installed.
-					// TODO[pulumi/pulumi#956]: eventually we will want to honor and install these in the usual way.
-					if plugin.Kind != workspace.LanguagePlugin {
-						installs = append(installs, plugin)
-					}
-				}
-			}
-
-			// Now for each kind, name, version pair, download it from the release website, and install it.
-			for _, install := range installs {
-				label := fmt.Sprintf("[%s plugin %s]", install.Kind, install)
-
-				// If the plugin already exists, don't download it unless --reinstall was passed.  Note that
-				// by default we accept plugins with >= constraints, unless --exact was passed which requires ==.
-				if !reinstall {
-					if exact {
-						if workspace.HasPlugin(install) {
-							logging.V(1).Infof("%s skipping install (existing == match)", label)
-							continue
-						}
-					} else {
-						if has, _ := workspace.HasPluginGTE(install); has {
-							logging.V(1).Infof("%s skipping install (existing >= match)", label)
-							continue
-						}
-					}
-				}
-
-				cmdutil.Diag().Infoerrf(
-					diag.Message("", "%s installing"), label)
-
-				// If we got here, actually try to do the download.
-				var source string
-				var payload workspace.PluginContent
-				var err error
-				if file == "" {
-					withProgress := func(stream io.ReadCloser, size int64) io.ReadCloser {
-						return workspace.ReadCloserProgressBar(stream, size, "Downloading plugin", displayOpts.Color)
-					}
-					retry := func(err error, attempt int, limit int, delay time.Duration) {
-						cmdutil.Diag().Warningf(
-							diag.Message("", "Error downloading plugin: %s\nWill retry in %v [%d/%d]"), err, delay, attempt, limit)
-					}
-
-					r, err := workspace.DownloadToFile(install, withProgress, retry)
-					if err != nil {
-						return fmt.Errorf("%s downloading from %s: %w", label, install.PluginDownloadURL, err)
-					}
-					defer func() { contract.IgnoreError(os.Remove(r.Name())) }()
-
-					payload = workspace.TarPlugin(r)
-				} else {
-					source = file
-					logging.V(1).Infof("%s opening tarball from %s", label, file)
-					payload, err = getFilePayload(file, install)
-					if err != nil {
-						return err
-					}
-				}
-				logging.V(1).Infof("%s installing tarball ...", label)
-				if err = install.InstallWithContext(ctx, payload, reinstall); err != nil {
-					return fmt.Errorf("installing %s from %s: %w", label, source, err)
-				}
-			}
-
-			return nil
+			return picmd.Run(ctx, args)
 		}),
 	}
 
-	cmd.PersistentFlags().StringVar(&serverURL,
+	cmd.PersistentFlags().StringVar(&picmd.serverURL,
 		"server", "", "A URL to download plugins from")
-	cmd.PersistentFlags().BoolVar(&exact,
+	cmd.PersistentFlags().BoolVar(&picmd.exact,
 		"exact", false, "Force installation of an exact version match (usually >= is accepted)")
-	cmd.PersistentFlags().StringVarP(&file,
+	cmd.PersistentFlags().StringVarP(&picmd.file,
 		"file", "f", "", "Install a plugin from a binary, folder or tarball, instead of downloading it")
-	cmd.PersistentFlags().BoolVar(&reinstall,
+	cmd.PersistentFlags().BoolVar(&picmd.reinstall,
 		"reinstall", false, "Reinstall a plugin even if it already exists")
-	cmd.PersistentFlags().StringVar(&checksum,
+	cmd.PersistentFlags().StringVar(&picmd.checksum,
 		"checksum", "", "The expected SHA256 checksum for the plugin archive")
 
 	return cmd
+}
+
+type pluginInstallCmd struct {
+	serverURL string
+	exact     bool
+	file      string
+	reinstall bool
+	checksum  string
+
+	diag  diag.Sink
+	color colors.Colorization
+}
+
+func (cmd *pluginInstallCmd) Run(ctx context.Context, args []string) error {
+	if cmd.diag == nil {
+		cmd.diag = cmdutil.Diag()
+	}
+	if cmd.color == "" {
+		cmd.color = cmdutil.GetGlobalColorization()
+	}
+
+	// Parse the kind, name, and version, if specified.
+	var installs []workspace.PluginSpec
+	if len(args) > 0 {
+		if !workspace.IsPluginKind(args[0]) {
+			return fmt.Errorf("unrecognized plugin kind: %s", args[0])
+		} else if len(args) < 2 {
+			return errors.New("missing plugin name argument")
+		}
+
+		var version *semver.Version
+		if len(args) == 3 {
+			parsedVersion, err := semver.ParseTolerant(args[2])
+			version = &parsedVersion
+			if err != nil {
+				return fmt.Errorf("invalid plugin semver: %w", err)
+			}
+		}
+		if len(args) < 3 && cmd.file != "" {
+			return errors.New("missing plugin version argument, this is required if installing from a file")
+		}
+
+		var checksums map[string][]byte
+		if cmd.checksum != "" {
+			checksumBytes, err := hex.DecodeString(cmd.checksum)
+			if err != nil {
+				return fmt.Errorf("--checksum was not a valid hex string: %w", err)
+			}
+			checksums = map[string][]byte{
+				runtime.GOOS + "-" + runtime.GOARCH: checksumBytes,
+			}
+		}
+
+		pluginSpec := workspace.PluginSpec{
+			Kind:              workspace.PluginKind(args[0]),
+			Name:              args[1],
+			Version:           version,
+			PluginDownloadURL: cmd.serverURL, // If empty, will use default plugin source.
+			Checksums:         checksums,
+		}
+
+		// Try and set known plugin download URLs
+		if urlSet := util.SetKnownPluginDownloadURL(&pluginSpec); urlSet {
+			cmd.diag.Infof(
+				diag.Message("", "Plugin download URL set to %s"), pluginSpec.PluginDownloadURL)
+		}
+
+		// If we don't have a version try to look one up
+		if version == nil {
+			latestVersion, err := pluginSpec.GetLatestVersion()
+			if err != nil {
+				return err
+			}
+			pluginSpec.Version = latestVersion
+		}
+
+		installs = append(installs, pluginSpec)
+	} else {
+		if cmd.file != "" {
+			return errors.New("--file (-f) is only valid if a specific package is being installed")
+		}
+		if cmd.checksum != "" {
+			return errors.New("--checksum is only valid if a specific package is being installed")
+		}
+
+		// If a specific plugin wasn't given, compute the set of plugins the current project needs.
+		plugins, err := getProjectPlugins()
+		if err != nil {
+			return err
+		}
+		for _, plugin := range plugins {
+			// Skip language plugins; by definition, we already have one installed.
+			// TODO[pulumi/pulumi#956]: eventually we will want to honor and install these in the usual way.
+			if plugin.Kind != workspace.LanguagePlugin {
+				installs = append(installs, plugin)
+			}
+		}
+	}
+
+	// Now for each kind, name, version pair, download it from the release website, and install it.
+	for _, install := range installs {
+		label := fmt.Sprintf("[%s plugin %s]", install.Kind, install)
+
+		// If the plugin already exists, don't download it unless --reinstall was passed.  Note that
+		// by default we accept plugins with >= constraints, unless --exact was passed which requires ==.
+		if !cmd.reinstall {
+			if cmd.exact {
+				if workspace.HasPlugin(install) {
+					logging.V(1).Infof("%s skipping install (existing == match)", label)
+					continue
+				}
+			} else {
+				if has, _ := workspace.HasPluginGTE(install); has {
+					logging.V(1).Infof("%s skipping install (existing >= match)", label)
+					continue
+				}
+			}
+		}
+
+		cmdutil.Diag().Infoerrf(
+			diag.Message("", "%s installing"), label)
+
+		// If we got here, actually try to do the download.
+		var source string
+		var payload workspace.PluginContent
+		var err error
+		if cmd.file == "" {
+			withProgress := func(stream io.ReadCloser, size int64) io.ReadCloser {
+				return workspace.ReadCloserProgressBar(stream, size, "Downloading plugin", cmd.color)
+			}
+			retry := func(err error, attempt int, limit int, delay time.Duration) {
+				cmd.diag.Warningf(
+					diag.Message("", "Error downloading plugin: %s\nWill retry in %v [%d/%d]"), err, delay, attempt, limit)
+			}
+
+			r, err := workspace.DownloadToFile(install, withProgress, retry)
+			if err != nil {
+				return fmt.Errorf("%s downloading from %s: %w", label, install.PluginDownloadURL, err)
+			}
+			defer func() { contract.IgnoreError(os.Remove(r.Name())) }()
+
+			payload = workspace.TarPlugin(r)
+		} else {
+			source = cmd.file
+			logging.V(1).Infof("%s opening tarball from %s", label, cmd.file)
+			payload, err = getFilePayload(cmd.file, install)
+			if err != nil {
+				return err
+			}
+		}
+		logging.V(1).Infof("%s installing tarball ...", label)
+		if err = install.InstallWithContext(ctx, payload, cmd.reinstall); err != nil {
+			return fmt.Errorf("installing %s from %s: %w", label, source, err)
+		}
+	}
+
+	return nil
 }
 
 func getFilePayload(file string, spec workspace.PluginSpec) (workspace.PluginContent, error) {
