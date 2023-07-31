ba2992615183a194afedf7f1757e163f8c8df6d8
Kyle Pitzen <kpitzen@pulumi.com>
2023-07-31 09:37:44 -0500

    Separates search and AI search commands

--- pkg/cmd/pulumi/search_ai.go
+++ pkg/cmd/pulumi/search_ai.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,21 +22,17 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/backend"
 	"github.com/pulumi/pulumi/pkg/v3/backend/display"
 	"github.com/pulumi/pulumi/pkg/v3/backend/httpstate"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/env"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
-	envutil "github.com/pulumi/pulumi/sdk/v3/go/common/util/env"
 	"github.com/spf13/cobra"
-	auto_table "go.pennock.tech/tabular/auto"
 )
 
-func newSearchCmd() *cobra.Command {
+func newAISearchCmd() *cobra.Command {
+	var queryString *string
 	var orgName string
-	var queryParams *[]string
 	cmd := &cobra.Command{
-		Use:   "search",
-		Short: "Search for resources in Pulumi Cloud",
-		Long:  "Search for resources in Pulumi Cloud.",
+		Use:   "ai",
+		Short: "Search for resources in Pulumi Cloud using Pulumi AI",
+		Long:  "Search for resources in Pulumi Cloud using Pulumi AI",
 		Args:  cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			ctx := commandContext()
@@ -76,8 +72,7 @@ func newSearchCmd() *cobra.Command {
 				return fmt.Errorf("user %s is not a member of org %s", userName, orgName)
 			}
 
-			parsedQueryParams := apitype.ParseQueryParams(queryParams)
-			res, err := cloudBackend.Search(ctx, filterName, parsedQueryParams)
+			res, err := cloudBackend.NaturalLanguageSearch(ctx, filterName, *queryString)
 			if err != nil {
 				return err
 			}
@@ -89,43 +84,14 @@ func newSearchCmd() *cobra.Command {
 		},
 		),
 	}
-
-	// TODO: Remove this branch once we release this feature fully.
-	if envutil.BoolValue(env.Dev).Value() {
-		cmd.AddCommand(newAISearchCmd())
-	}
-
 	cmd.PersistentFlags().StringVarP(
 		&orgName, "org", "o", "",
-		"Allow P resource operations to run in parallel at once (1 for no parallelism). Defaults to unbounded.",
+		"Organization name to search within",
 	)
-	queryParams = cmd.PersistentFlags().StringArrayP(
-		"query", "q", []string{},
-		"Key-value pairs to use as query parameters. Must be formatted like: -q key1=value1 -q key2=value2",
+	queryString = cmd.PersistentFlags().StringP(
+		"query", "q", "",
+		"Plaintext natural language query",
 	)
 
 	return cmd
-}
-
-func sliceContains(slice []string, search string) bool {
-	for _, s := range slice {
-		if s == search {
-			return true
-		}
-	}
-	return false
-}
-
-func renderTable(results []apitype.ResourceResult) error {
-	table := auto_table.New("utf8-heavy")
-	table.AddHeaders("Project", "Stack", "Name", "Type", "Package", "Module", "Modified")
-	for _, r := range results {
-		table.AddRowItems(*r.Program, *r.Stack, *r.Name, *r.Type, *r.Package, *r.Module, *r.Modified)
-	}
-	if errs := table.Errors(); errs != nil {
-		for _, err := range errs {
-			fmt.Fprintf(os.Stderr, "table error: %s\n", err)
-		}
-	}
-	return table.RenderTo(os.Stdout)
 }
