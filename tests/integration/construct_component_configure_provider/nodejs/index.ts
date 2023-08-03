2c4a25659c70fb54452c9e32891c65bb8b80fcc3
Anton Tayanovskyy <anton@pulumi.com>
2023-08-03 15:52:17 -0400

    Emualte unknowns in the node test

--- tests/integration/construct_component_configure_provider/nodejs/index.ts
+++ tests/integration/construct_component_configure_provider/nodejs/index.ts
@@ -7,8 +7,16 @@ import * as tls from "@pulumi/tls";
 export = async () => {
   const config = new pulumi.Config();
   const proxy = config.require("proxy");
+
+  // This resource is just here to generate unknown outputs to test propagating unknowns; there seems to be no way to
+  // construct an unknown output in Node.
+  const helperPrivateKey = new tls.PrivateKey("helper-private-key", {
+    algorithm: "ECDSA",
+    ecdsaCurve: "P384",
+  });
+
   const configurer = new metaprovider.Configurer("configurer", {
-    tlsProxy: proxy
+    tlsProxy: helperPrivateKey.publicKeyPem.apply(_ => proxy) // apply trick makes it unknown at preview
   });
   const key = new tls.PrivateKey("my-private-key", {
     algorithm: "ECDSA",
@@ -16,7 +24,6 @@ export = async () => {
   }, {
     provider: await configurer.tlsProvider()
   });
-  const keyAlgo = key.algorithm;
   return {
     keyAlgo: key.algorithm
   }
