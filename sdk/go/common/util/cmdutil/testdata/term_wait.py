c8cacf034becaefaaf1f463c0153e13df41bce6a
Abhinav Gupta <abhinav@pulumi.com>
2023-08-23 15:38:19 -0700

    test(py): SIGBREAK handles CTRL_BREAK_EVENT

--- sdk/go/common/util/cmdutil/testdata/term_wait.py
+++ sdk/go/common/util/cmdutil/testdata/term_wait.py
@@ -7,6 +7,9 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_handler)
+if hasattr(signal, 'SIGBREAK'):
+    # SIGBREAK is present on Windows only.
+    signal.signal(signal.SIGBREAK, signal_handler)
 print("ready", flush=True)
 
 time.sleep(3)
