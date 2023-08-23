34f93c0bf7ee8f981e0c888869611bffc7a1b7ea
Abhinav Gupta <abhinav@pulumi.com>
2023-08-23 15:33:27 -0700

    test(py/win): Can't register CTRL_BREAK_EVENT handler

--- sdk/go/common/util/cmdutil/testdata/term_wait.py
+++ sdk/go/common/util/cmdutil/testdata/term_wait.py
@@ -7,8 +7,6 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_handler)
-if hasattr(signal, 'CTRL_BREAK_EVENT'):
-    signal.signal(signal.CTRL_BREAK_EVENT, signal_handler)
 print("ready", flush=True)
 
 time.sleep(3)
