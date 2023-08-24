aa5468f163e099bfc7615975050d0a622ec164ea
Abhinav Gupta <abhinav@pulumi.com>
2023-08-24 08:59:29 -0700

    win: attempt to send CTRL_C_EVENT a different way

--- sdk/go/common/util/cmdutil/term_windows.go
+++ sdk/go/common/util/cmdutil/term_windows.go
@@ -18,11 +18,50 @@
 package cmdutil
 
 import (
+	"fmt"
 	"os"
 
 	"golang.org/x/sys/windows"
 )
 
 func shutdownProcess(proc *os.Process) error {
-	return windows.GenerateConsoleCtrlEvent(windows.CTRL_C_EVENT, uint32(proc.Pid))
+	kernel32, err := windows.LoadDLL("kernel32.dll")
+	if err != nil {
+		return fmt.Errorf("load kernel32.dll: %w", err)
+	}
+	defer func() {
+		_ = kernel32.Release()
+	}()
+
+	// attachConsole, err := kernel32.FindProc("AttachConsole")
+	// if err != nil {
+	// 	return fmt.Errorf("find AttachConsole: %w", err)
+	// }
+	//
+	// setConsoleCtrlHandler, err := kernel32.FindProc("SetConsoleCtrlHandler")
+	// if err != nil {
+	// 	return fmt.Errorf("find SetConsoleCtrlHandler: %w", err)
+	// }
+
+	generateConsoleCtrlEvent, err := kernel32.FindProc("GenerateConsoleCtrlEvent")
+	if err != nil {
+		return fmt.Errorf("find GenerateConsoleCtrlEvent: %w", err)
+	}
+
+	pid := proc.Pid
+	// if r, _, err := attachConsole.Call(uintptr(pid)); r == 0 && !errors.Is(err, windows.ERROR_ACCESS_DENIED) {
+	// 	return fmt.Errorf("attach console: %w", err)
+	// }
+	//
+	// // Disable Ctrl-C handling for our program.
+	// if r, _, err := setConsoleCtrlHandler.Call(0, 0); r == 0 {
+	// 	return fmt.Errorf("set console ctrl handler: %w", err)
+	// }
+
+	// Send Ctrl-C event to the process group.
+	if r, _, err := generateConsoleCtrlEvent.Call(windows.CTRL_C_EVENT, uintptr(pid)); r == 0 {
+		return fmt.Errorf("generate console ctrl event: %w", err)
+	}
+
+	return nil
 }
