1ae6388386093287f92af42dbc9d5640c0dfb155
Zaid Ajaj <Zaid-Ajaj@users.noreply.github.com>
2023-09-22 22:32:33 +0200

    [python/sdk-gen] Generate output-versioned invokes for functions without inputs (#13685)
    # Description 
    
    Partially addressing https://github.com/pulumi/pulumi/issues/12449
    implements output-versioned invokes for functions without inputs for
    python SDKs
    
    ## Checklist
    
    - [ ] I have run `make tidy` to update any new dependencies
    - [x] I have run `make lint` to verify my code passes the lint check
      - [ ] I have formatted my code using `gofumpt`
    
    <!--- Please provide details if the checkbox below is to be left
    unchecked. -->
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    <!--- 
    User-facing changes require a CHANGELOG entry.
    -->
    - [x] I have run `make changelog` and committed the
    `changelog/pending/<file>` documenting my change
    <!--
    If the change(s) in this PR is a modification of an existing call to the
    Pulumi Cloud,
    then the service should honor older versions of the CLI where this
    change would not exist.
    You must then bump the API version in
    /pkg/backend/httpstate/client/api.go, as well as add
    it to the service.
    -->
    - [ ] Yes, there are changes in this PR that warrants bumping the Pulumi
    Cloud API version
    <!-- @Pulumi employees: If yes, you must submit corresponding changes in
    the service repo. -->

--- pkg/codegen/testing/test/testdata/output-funcs/python/pulumi_mypkg/get_client_config.py
+++ pkg/codegen/testing/test/testdata/output-funcs/python/pulumi_mypkg/get_client_config.py
@@ -13,6 +13,7 @@ __all__ = [
     'GetClientConfigResult',
     'AwaitableGetClientConfigResult',
     'get_client_config',
+    'get_client_config_output',
 ]
 
 @pulumi.output_type
@@ -92,3 +93,11 @@ def get_client_config(opts: Optional[pulumi.InvokeOptions] = None) -> AwaitableG
         object_id=pulumi.get(__ret__, 'object_id'),
         subscription_id=pulumi.get(__ret__, 'subscription_id'),
         tenant_id=pulumi.get(__ret__, 'tenant_id'))
+
+
+@_utilities.lift_output_func(get_client_config)
+def get_client_config_output(opts: Optional[pulumi.InvokeOptions] = None) -> pulumi.Output[GetClientConfigResult]:
+    """
+    Failing example taken from azure-native. Original doc: Use this function to access the current configuration of the native Azure provider.
+    """
+    ...
