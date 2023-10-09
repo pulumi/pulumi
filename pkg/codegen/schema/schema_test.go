6166bd28861454628491701e8bbd0eefcf727824
Fraser Waters <fraser@pulumi.com>
2023-10-09 14:36:20 +0100

    Fix TestImportResourceRef

--- pkg/codegen/schema/schema_test.go
+++ pkg/codegen/schema/schema_test.go
@@ -397,10 +397,11 @@ func TestImportResourceRef(t *testing.T) {
 			},
 		},
 	}
+	//nolint:paralleltest // needs to set plugin acquisition env var
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
+			t.Setenv("PULUMI_DISABLE_AUTOMATIC_PLUGIN_ACQUISITION", "false")
 
 			// Read in, decode, and import the schema.
 			schemaBytes, err := os.ReadFile(
