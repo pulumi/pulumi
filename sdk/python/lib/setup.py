981589dc28960d35b730b999b43d18669adc7d67
Alexander Hungenberg <defreng@users.noreply.github.com>
2023-10-16 18:29:27 +0200

    updated once more to allow for minor updates

--- sdk/python/lib/setup.py
+++ sdk/python/lib/setup.py
@@ -44,7 +44,7 @@ setup(name='pulumi',
       # Keep this list in sync with Pipfile
       install_requires=[
           'protobuf~=4.21',
-          'grpcio~=1.59.0',
+          'grpcio~=1.59',
           'dill~=0.3',
           'six~=1.12',
           'semver~=2.13',
