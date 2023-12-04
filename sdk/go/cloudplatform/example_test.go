6f8b609506c5ea863eee483f715a5a428675f896
Evan Boyle <evan@pulumi.com>
2023-12-03 19:43:12 -0800

    add more examples

--- sdk/go/cloudplatform/example_test.go
+++ sdk/go/cloudplatform/example_test.go
@@ -1,5 +1,7 @@
 package cloudplatform
 
+import "fmt"
+
 func ExampleAutomationJob() {
 	org := "acme"
 	p, err := NewCloudPlatform(org)
@@ -7,6 +9,7 @@ func ExampleAutomationJob() {
 		panic(err)
 	}
 
+	// run any automation API program in Pulumi Cloud
 	p.CreateAutomationJob("multi-region-deployer", AutomationJobArgs{
 		Repo: "github.com/pulumi/automation-api-examples",
 		Dir:  "nodejs/multi-region-deployer",
@@ -14,13 +17,94 @@ func ExampleAutomationJob() {
 			"npm install",
 		},
 		Entrypoint: "npm start",
-		// automaticaly provide credentials for stripe and AWS in the dev environment
+		// automaticaly provide credentials for stripe and AWS via ESC
 		Environemnt: []string{
 			"aws-dev",
 			"stripe-dev",
 		},
 		Mode: "cron",
-		// deploy every day at 8am
+		// run every day at 8am
 		Schedule: "0 8 * * *",
+	})
+}
+
+func ExampleCostManagement() {
+	org := "acme"
+	p, err := NewCloudPlatform(org)
+	if err != nil {
+		panic(err)
+	}
+
+	// find all G5 GPU EC2 instances
+	res, err := p.Search(`type:"aws:ec2/instance:instance" .instanceType:G5`)
+	if err != nil {
+		panic(err)
+	}
+
+	// create a list of all matching resource IDs grouped by stack
+	stacksToIds := map[Stack][]string{}
+	for _, resource := range res {
+		if stacksToIds[resource.Stack()] == nil {
+			stacksToIds[resource.Stack()] = []string{resource.ID()}
+		} else {
+			stacksToIds[resource.Stack()] = append(stacksToIds[resource.Stack()], resource.ID())
+		}
+	}
+
+	// iterate through all stacks in the results set
+	for stack, ids := range stacksToIds {
+		// check to ensure deployments is configured
+		if stack.SupportsDeployments() {
+			// run a destroy targeting only the GPUs
+			stack.RunDeployment(DeploymentArgs{
+				Targets:   ids,
+				Operation: "destroy",
+			})
+		}
+	}
+}
+
+func ExampleSearchPlusPolicyViolations() {
+	org := "acme"
+	p, err := NewCloudPlatform(org)
+	if err != nil {
+		panic(err)
+	}
+
+	// find all lambdas running nodejs v12 (deprecated and in need of security upgrade)
+	res, err := p.Search(`.runtime:nodejs12`)
+	if err != nil {
+		panic(err)
+	}
+
+	// for each resource report a violation to Pulumi Cloud that will show up on our dashboard
+	// along with check to re-validate the resource upon next update
+	for _, resource := range res {
+		securityLevel := "HIGH"
+		note := "NodeJS LTS now at v20, resource at v12 and needs security updates"
+		revalidationQuery := fmt.Sprintf(".runtime:nodejs12 ID:%s", resource.ID)
+		resource.ReportPolicyViolation(securityLevel, note, revalidationQuery)
+	}
+}
+
+func ExampleServiceCreationFromTemplate() {
+	org := "acme"
+	p, err := NewCloudPlatform(org)
+	if err != nil {
+		panic(err)
+	}
+
+	t := p.GetServiceTemplate("kubernetes-cluster")
+	t.TemplateAndDeploy(TemplateArgs{
+		DestinationRepo: "github.com/acme/new-kubernetes-app",
+		Dir:             "infra",
+		PushToDeploy:    true,
+		PRPreview:       true,
+		ReviewStacks:    true,
+		Drift:           true,
+		TTLSeconds:      42,
+		Environment: []string{
+			"aws-dev",
+		},
 	})
 }
--- sdk/go/cloudplatform/platform.go
+++ sdk/go/cloudplatform/platform.go
@@ -12,7 +12,9 @@ type AutomationJobArgs struct {
 
 type Platform interface {
 	CreateAutomationJob(name string, args AutomationJobArgs)
-	Search() ([]Resource, error)
+	Search(query string) ([]Resource, error)
+	RunDeployment(args DeploymentArgs)
+	GetServiceTemplate(name string) ServiceTemplate
 }
 
 type CloudPlatform struct{}
@@ -26,12 +28,20 @@ func (c *CloudPlatform) CreateAutomationJob(name string, args AutomationJobArgs)
 	// TODO
 }
 
-func (c *CloudPlatform) Search() ([]Resource, error) {
+func (c *CloudPlatform) RunDeployment(args DeploymentArgs)
+
+func (c *CloudPlatform) Search(query string) ([]Resource, error) {
 	// hit the pulumi cloud resource search API
 	return nil, nil
 }
 
+func (c *CloudPlatform) GetServiceTemplate(name string) ServiceTemplate {
+	return &CloudServiceTemplate{}
+}
+
 type Stack interface {
+	SupportsDeployments() bool
+	RunDeployment(args DeploymentArgs)
 }
 
 type CloudStack struct{}
@@ -40,10 +50,72 @@ func NewCloudStack() (Stack, error) {
 	return &CloudStack{}, nil
 }
 
-type Resource interface{}
+type DeploymentArgs struct {
+	Repo      string
+	Operation string
+	Dir       string
+	EnvVars   []string
+	Targets   []string
+}
+
+func (cs *CloudStack) SupportsDeployments() bool {
+	return true
+}
+
+func (cs *CloudStack) RunDeployment(args DeploymentArgs) {
+}
+
+type Resource interface {
+	Stack() Stack
+	ID() string
+	ReportPolicyViolation(Level string, Note string, RevalidationQuery string)
+}
 
 type CloudResource struct{}
 
 func NewCloudResource() (Resource, error) {
 	return &CloudResource{}, nil
 }
+
+func (cr *CloudResource) Stack() Stack {
+	c, _ := NewCloudStack()
+	return c
+}
+
+func (cr *CloudResource) ID() string {
+	return ""
+}
+
+func (cr *CloudResource) ReportPolicyViolation(Level string, Note string, RevalidationQuery string) {
+	// these APIs don't yet exist in pulumi cloud but you could imagine
+	// a database of:
+	// 	1. resource ID
+	//  2. violation level
+	// 	3. note
+	//  4. revalition query (search query to check that resource is back in compliance)
+	// would very very useful for rolling out broad new policy over existing infra
+	// as well as policy on resources not managed by IaC but imported into Pulumi
+}
+
+type ServiceTemplate interface {
+	TemplateAndDeploy(args TemplateArgs)
+}
+
+type CloudServiceTemplate struct{}
+
+type TemplateArgs struct {
+	DestinationRepo string
+	Dir             string
+	PushToDeploy    bool
+	PRPreview       bool
+	ReviewStacks    bool
+	Drift           bool
+	TTLSeconds      int64
+	Environment     []string
+}
+
+func NewCloudServiceTemplate() ServiceTemplate {
+	return &CloudServiceTemplate{}
+}
+
+func (cst *CloudServiceTemplate) TemplateAndDeploy(args TemplateArgs) {}
