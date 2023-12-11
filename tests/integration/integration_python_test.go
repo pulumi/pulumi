2dc70fda900560825ecdc42e51b01cddeb97c34f
Kyle Dixler <kyle@pulumi.com>
2023-12-11 11:45:09 -0800

    Re-enable TestPythonTranslation

--- tests/integration/integration_python_test.go
+++ tests/integration/integration_python_test.go
@@ -1038,7 +1038,6 @@ func TestPythonAwaitOutputs(t *testing.T) {
 //
 //nolint:paralleltest // ProgramTest calls t.Parallel()
 func TestPythonTranslation(t *testing.T) {
-	t.Skip("Temporarily skipping test - pulumi/pulumi#14765")
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir: filepath.Join("python", "translation"),
 		Dependencies: []string{
