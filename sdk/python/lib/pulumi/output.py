1b63aa772113f96a910f42f7248bd2dd98db7f56
Mark Nevill <mark.nevill@gmail.com>
2023-12-20 19:43:07 +0800

    Add option to throw on output toString and toJSON calls (#14811)
    <!--- 
    Thanks so much for your contribution! If this is your first time
    contributing, please ensure that you have read the
    [CONTRIBUTING](https://github.com/pulumi/pulumi/blob/master/CONTRIBUTING.md)
    documentation.
    -->
    
    # Description
    
    This adds an environment variable that changes output's `.toString` and
    `.toJSON` methods to throw an exception instead of returning a string
    containing a warning.
    
    This feature mitigates the issue described in #4261 by allowing users to
    opt in to having accidental toString calls (e.g. in a JS backtick string
    that is missing `pulumi.interpolate`) throw an exception. The behaviour
    ends up similar to the `get()` method of outputs, which throws an
    exception.
    
    I don't know how to write tests for this change, or even test it
    manually.
    
    ## Checklist
    
    - [x] I have run `make tidy` to update any new dependencies
    - [x] I have run `make lint` to verify my code passes the lint check
      - [ ] I have formatted my code using `gofumpt`
    
    <!--- Please provide details if the checkbox below is to be left
    unchecked. -->
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    <!--- 
    User-facing changes require a CHANGELOG entry.
    -->
    - [x] I have run `make changelog` and committed the
    `changelog/pending/<file>` documenting my change
    <!--
    If the change(s) in this PR is a modification of an existing call to the
    Pulumi Cloud,
    then the service should honor older versions of the CLI where this
    change would not exist.
    You must then bump the API version in
    /pkg/backend/httpstate/client/api.go, as well as add
    it to the service.
    -->
    - [ ] Yes, there are changes in this PR that warrants bumping the Pulumi
    Cloud API version
    <!-- @Pulumi employees: If yes, you must submit corresponding changes in
    the service repo. -->
    
    ---------
    
    Co-authored-by: Fraser Waters <frassle@gmail.com>

--- sdk/python/lib/pulumi/output.py
+++ sdk/python/lib/pulumi/output.py
@@ -14,6 +14,7 @@
 import asyncio
 import contextlib
 import json
+import os
 from functools import reduce
 from inspect import isawaitable
 from typing import (
@@ -718,17 +719,20 @@ class Output(Generic[T_co]):
 
         # You'd think this could all be on one line but mypy seems to think `s` is a `Sequence[object]` if you
         # do.
-        os: Output[Union[str, bytes, bytearray]] = Output.from_input(s)
-        return os.apply(loads)
+        s_output: Output[Union[str, bytes, bytearray]] = Output.from_input(s)
+        return s_output.apply(loads)
 
     def __str__(self) -> str:
-        return """Calling __str__ on an Output[T] is not supported.
+        msg = """Calling __str__ on an Output[T] is not supported.
 
 To get the value of an Output[T] as an Output[str] consider:
 1. o.apply(lambda v: f"prefix{v}suffix")
 
-See https://www.pulumi.com/docs/concepts/inputs-outputs for more details.
-This function may throw in a future version of Pulumi."""
+See https://www.pulumi.com/docs/concepts/inputs-outputs for more details."""
+        if os.getenv("PULUMI_ERROR_OUTPUT_STRING", "").lower() in ["1", "true"]:
+            raise TypeError(msg)
+        msg += "\nThis function may throw in a future version of Pulumi."
+        return msg
 
 
 class Unknown:
