b96f19b2fabae8406cefde91b199c659958282a8
Zaid Ajaj <Zaid-Ajaj@users.noreply.github.com>
2023-12-27 13:42:52 +0100

    [program-gen/pcl] Fixes type-annotating nested resource properties when these have quoted keys (#15001)
    # Description
    
    When binding resource properties and annotating these with types from
    their schemas, we seem to skip this annotation process when resource
    properties and their nested objects use _quoted_ keys `{ "key" = <value>
    }` rather than using _literal_ keys `{ key = <value> }`.
    
    This results in issues such as
    https://github.com/pulumi/kube2pulumi/issues/60 where a csharp property
    name override was not correctly applied because
    1) kube2pulumi generated properties for resources that are quoted (this
    should be fine)
    2) binding the resource properties skipped annotating the nested object
    with its corresponding schema type
    3) program-gen in dotnet didn't have access to the schema type of the
    nested object to correctly apply the property override
    
    This PR fixes this issue by extending PCL resource binding to also check
    for properties which have quoted keys.
    
    Fixes https://github.com/pulumi/kube2pulumi/issues/60 and potentially
    other issues that arise from converters generating PCL with quoted keys.
    
    ## Checklist
    
    - [ ] I have run `make tidy` to update any new dependencies
    - [ ] I have run `make lint` to verify my code passes the lint check
      - [ ] I have formatted my code using `gofumpt`
    
    <!--- Please provide details if the checkbox below is to be left
    unchecked. -->
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    <!--- 
    User-facing changes require a CHANGELOG entry.
    -->
    - [x] I have run `make changelog` and committed the
    `changelog/pending/<file>` documenting my change
    <!--
    If the change(s) in this PR is a modification of an existing call to the
    Pulumi Cloud,
    then the service should honor older versions of the CLI where this
    change would not exist.
    You must then bump the API version in
    /pkg/backend/httpstate/client/api.go, as well as add
    it to the service.
    -->
    - [ ] Yes, there are changes in this PR that warrants bumping the Pulumi
    Cloud API version
    <!-- @Pulumi employees: If yes, you must submit corresponding changes in
    the service repo. -->

---
+++ pkg/codegen/testing/test/testdata/kubernetes-template-quoted-pp/dotnet/kubernetes-template-quoted.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using Pulumi;
+using Kubernetes = Pulumi.Kubernetes;
+
+return await Deployment.RunAsync(() => 
+{
+    var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new()
+    {
+        ApiVersion = "apps/v1",
+        Kind = "Deployment",
+        Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
+        {
+            Name = "argocd-server",
+        },
+        Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
+        {
+            Selector = new Kubernetes.Types.Inputs.Meta.V1.LabelSelectorArgs
+            {
+                MatchLabels = 
+                {
+                    { "app", "server" },
+                },
+            },
+            Replicas = 1,
+            Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
+            {
+                Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
+                {
+                    Labels = 
+                    {
+                        { "app", "server" },
+                    },
+                },
+                Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
+                {
+                    Containers = new[]
+                    {
+                        new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
+                        {
+                            Name = "nginx",
+                            Image = "nginx",
+                            Ports = new[]
+                            {
+                                new Kubernetes.Types.Inputs.Core.V1.ContainerPortArgs
+                                {
+                                    ContainerPortValue = 80,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+});
+
