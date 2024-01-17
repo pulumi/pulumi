1cb1a94978cbad943179ea53ef0fbbeb4d4f4fb8
Thomas Gummerer <t.gummerer@gmail.com>
2024-01-17 10:31:45 +0100

    add test for log in with SAS token
    This test succeeds with gocloud.dev v0.27.0, and fails with v0.28.0.

---
+++ tests/integration/backend/filestate/backend_azure_test.go
@@ -0,0 +1,88 @@
+package filestate
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func loginAndCreateStack(t *testing.T, cloudURL string) {
+	t.Helper()
+	out, err := exec.Command("pulumi", "login", cloudURL).CombinedOutput()
+	require.NoError(t, err, string(out))
+
+	t.Setenv("PULUMI_CONFIG_PASSPHRASE", "test")
+	out, err = exec.Command("pulumi", "stack", "init", "testing").CombinedOutput()
+	require.NoError(t, err, string(out))
+	defer func() {
+		out, err := exec.Command("pulumi", "stack", "rm", "--yes", "-s", "testing").CombinedOutput()
+		assert.NoError(t, err, string(out))
+	}()
+
+	out, err = exec.Command("pulumi", "stack", "select", "testing").CombinedOutput()
+	require.NoError(t, err, string(out))
+
+	out, err = exec.Command("pulumi", "stack", "ls").CombinedOutput()
+	assert.NoError(t, err, string(out))
+	assert.Contains(t, string(out), "testing*")
+}
+
+//nolint:paralleltest // this test sets the global login state
+func TestAzureLoginSasToken(t *testing.T) {
+	err := os.Chdir("project")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err := os.Chdir("..")
+		require.NoError(t, err)
+	})
+	cloudURL := "azblob://pulumitesting?storage_account=pulumitesting"
+	assert.NotEmpty(t, os.Getenv("AZURE_STORAGE_SAS_TOKEN"),
+		"an azure storage SAS token needs to be set in the environment")
+
+	t.Cleanup(func() {
+		err := exec.Command("pulumi", "logout").Run()
+		assert.NoError(t, err)
+		fmt.Println("cleaned up 1")
+	})
+	loginAndCreateStack(t, cloudURL)
+}
+
+//nolint:paralleltest // this test uses the global azure login state
+func TestAzureLoginAzLogin(t *testing.T) {
+	err := os.Chdir("project")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err := os.Chdir("..")
+		require.NoError(t, err)
+	})
+	cloudURL := "azblob://pulumitesting?storage_account=pulumitesting"
+	assert.NotEmpty(t, os.Getenv("AZURE_CLIENT_ID"),
+		"an azure client id needs to be set in the environment")
+	assert.NotEmpty(t, os.Getenv("AZURE_CLIENT_SECRET"),
+		"an azure client secret needs to be set in the environment")
+	assert.NotEmpty(t, os.Getenv("AZURE_TENANT_ID"),
+		"an azure tenant id needs to be set in the environment")
+
+	t.Setenv("AZURE_STORAGE_SAS_TOKEN", "")
+
+	//nolint:gosec // this is a test
+	err = exec.Command("az", "login", "--service-principal",
+		"--username", os.Getenv("AZURE_CLIENT_ID"),
+		"--password", os.Getenv("AZURE_CLIENT_SECRET"),
+		"--tenant", os.Getenv("AZURE_TENANT_ID")).Run()
+	assert.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := exec.Command("az", "logout").Run()
+		assert.NoError(t, err)
+		err = exec.Command("pulumi", "logout").Run()
+		assert.NoError(t, err)
+		fmt.Println("cleaned up 2")
+	})
+
+	loginAndCreateStack(t, cloudURL)
+}
