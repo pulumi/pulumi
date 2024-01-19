58d41a45aa77c1a32f45e3d850300aea1572b9d2
Ryan Doyle <ryan@rdoyle.net>
2024-01-19 11:25:37 -0700

    Logging

--- pkg/authhelpers/s3auth.go
+++ pkg/authhelpers/s3auth.go
@@ -7,23 +7,41 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 	"gocloud.dev/blob"
 	"gocloud.dev/blob/s3blob"
 )
 
+const (
+	logPrefix                      string = "S3 Backend: "
+	profileNameEnvironmentVariable string = "PULUMI_BACKEND_AWS_PROFILE_NAME"
+)
+
 func S3BuildSessionOptions(ctx context.Context, backend *workspace.ProjectBackend) (*session.Options, error) {
 	// Select the session options based on the backend setting, superceded by the PULUMI_ env var if set.
 	// If neither set, fall back to default session builder (Which interprets AWS_ environment vars first)
+	log := logging.V(5)
 
 	definitiveProfileName := ""
 	if backend != nil && backend.AwsProfileName != "" {
 		definitiveProfileName = backend.AwsProfileName
+		log.Infof(
+			"%sUsing aws profile \"%s\" from project configuration",
+			logPrefix,
+			definitiveProfileName,
+		)
 	}
 
-	profileNameEnv := os.Getenv("PULUMI_BACKEND_AWS_PROFILE_NAME")
+	profileNameEnv := os.Getenv(profileNameEnvironmentVariable)
 	if profileNameEnv != "" {
 		definitiveProfileName = profileNameEnv
+		log.Infof(
+			"%sOverriding aws profile \"%s\" from environment configuration (%s)",
+			logPrefix,
+			definitiveProfileName,
+			profileNameEnvironmentVariable,
+		)
 	}
 
 	opts := session.Options{}
@@ -37,6 +55,17 @@ func S3BuildSessionOptions(ctx context.Context, backend *workspace.ProjectBacken
 		opts.Config = aws.Config{
 			Region: &profileConfig.Region,
 		}
+		log.Infof(
+			"%sSelected profile \"%s\" and region \"%s\" from profile config for backend auth",
+			logPrefix,
+			definitiveProfileName,
+			profileConfig.Region,
+		)
+	} else {
+		log.Infof(
+			"%sNo profile configuration was provided for S3 auth.  Defaulting to environment then default aws profile",
+			logPrefix,
+		)
 	}
 	return &opts, nil
 }
