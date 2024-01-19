2a52c93ddb45d2e4da59d2d3b1cda2faa3a91ae0
Ryan Doyle <ryan@rdoyle.net>
2024-01-18 17:17:17 -0700

    Pull region from config same as profile

--- pkg/authhelpers/s3auth.go
+++ pkg/authhelpers/s3auth.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 	"gocloud.dev/blob"
 	"gocloud.dev/blob/s3blob"
 )
 
-func S3BuildSessionOptions(ctx context.Context, backend *workspace.ProjectBackend) session.Options {
+func S3BuildSessionOptions(ctx context.Context, backend *workspace.ProjectBackend) (*session.Options, error) {
 	// Select the session options based on the backend setting, superceded by the PULUMI_ env var if set.
 	// If neither set, fall back to default session builder (Which interprets AWS_ environment vars first)
 
@@ -24,18 +26,30 @@ func S3BuildSessionOptions(ctx context.Context, backend *workspace.ProjectBacken
 		definitiveProfileName = profileNameEnv
 	}
 
-	return session.Options{
-		// It's okay for the value to be "" here.  That will induce the fallback
-		// behavior when used in session.NewSessionWithOptions().  Which is checking
-		// for a non-zero-length string
-		Profile: definitiveProfileName,
+	opts := session.Options{}
+	if definitiveProfileName != "" {
+		// Get config for profile
+		profileConfig, err := config.LoadSharedConfigProfile(ctx, definitiveProfileName)
+		if err != nil {
+			return nil, err
+		}
+		opts.Profile = definitiveProfileName
+		opts.Config = aws.Config{
+			Region: &profileConfig.Region,
+		}
 	}
+	return &opts, nil
 
 }
 
 func S3CredentialsMux(ctx context.Context, backend *workspace.ProjectBackend) (*blob.URLMux, error) {
 	// Returns a blobmux only registered to handle s3, and do so in our specially defined way
-	sess, err := session.NewSessionWithOptions(S3BuildSessionOptions(ctx, backend))
+	opts, err := S3BuildSessionOptions(ctx, backend)
+	if err != nil {
+		return nil, err
+	}
+
+	sess, err := session.NewSessionWithOptions(*opts)
 	if err != nil {
 		return nil, err
 	}
