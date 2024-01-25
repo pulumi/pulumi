d446736e5935a58a9a37bd66380cd4f35596931b
Justin Van Patten <jvp@justinvp.com>
2024-01-25 11:24:22 -0800

    Unskip tests (#15257)
    - Unskip Python tests that needed to be skipped until v3.103.0 of the
    `pulumi` PyPi package was released with support for Python 3.12.
    - Unskip Node.js test that needed to be skipped until v3.103.1 of the
    `@pulumi/pulumi` NPM package was published with a fix for the semver
    type issue.
    
    Fixes #15195

--- tests/smoke_test.go
+++ tests/smoke_test.go
@@ -39,10 +39,6 @@ func TestLanguageNewSmoke(t *testing.T) {
 		t.Run(runtime, func(t *testing.T) {
 			//nolint:paralleltest
 
-			if runtime == "python" {
-				t.Skip("Temporarily skipping test - pulumi/pulumi#15195")
-			}
-
 			e := ptesting.NewEnvironment(t)
 			defer deleteIfNotFailed(e)
 
@@ -102,10 +98,6 @@ func TestLanguageConvertSmoke(t *testing.T) {
 		runtime := runtime
 		t.Run(runtime, func(t *testing.T) {
 			t.Parallel()
-
-			if runtime == "python" {
-				t.Skip("Temporarily skipping test - pulumi/pulumi#15195")
-			}
 
 			e := ptesting.NewEnvironment(t)
 			defer deleteIfNotFailed(e)
@@ -169,10 +161,6 @@ func TestLanguageConvertComponentSmoke(t *testing.T) {
 			}
 			if runtime == "java" {
 				t.Skip("java doesn't support components")
-			}
-
-			if runtime == "python" {
-				t.Skip("Temporarily skipping test - pulumi/pulumi#15195")
 			}
 
 			e := ptesting.NewEnvironment(t)
@@ -314,10 +302,6 @@ func TestLanguageImportSmoke(t *testing.T) {
 	for _, runtime := range Runtimes {
 		t.Run(runtime, func(t *testing.T) {
 			//nolint:paralleltest
-
-			if runtime == "python" {
-				t.Skip("Temporarily skipping test - pulumi/pulumi#15195")
-			}
 
 			e := ptesting.NewEnvironment(t)
 			defer deleteIfNotFailed(e)
