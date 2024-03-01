c6128232d0db5a54654d178de3d77f42f1b1bdeb
Thomas Gummerer <t.gummerer@gmail.com>
2024-03-01 19:48:10 +0100

    do as integration test

--- tests/creds_test.go
+++ tests/creds_test.go
@@ -1,9 +1,24 @@
-package workspace
+// Copyright 2024-2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
 
 import (
 	"sync"
 	"testing"
 
+	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,16 +26,16 @@ import (
 func TestConcurrentCredentialsWrites(t *testing.T) {
 	// save and remember to restore creds in ~/.pulumi/credentials
 	// as the test will be modifying them
-	oldCreds, err := GetStoredCredentials()
+	oldCreds, err := workspace.GetStoredCredentials()
 	assert.NoError(t, err)
 	defer func() {
-		err := StoreCredentials(oldCreds)
+		err := workspace.StoreCredentials(oldCreds)
 		assert.NoError(t, err)
 	}()
 
 	// use test creds that have at least 1 AccessToken to force a
 	// disk write and contention
-	testCreds := Credentials{
+	testCreds := workspace.Credentials{
 		AccessTokens: map[string]string{
 			"token-name": "token-value",
 		},
@@ -34,18 +49,18 @@ func TestConcurrentCredentialsWrites(t *testing.T) {
 
 	// Store testCreds initially so asserts in
 	// GetStoredCredentials goroutines find the expected data
-	err = StoreCredentials(testCreds)
+	err = workspace.StoreCredentials(testCreds)
 	assert.NoError(t, err)
 
 	for i := 0; i < n; i++ {
 		go func() {
 			defer wg.Done()
-			err := StoreCredentials(testCreds)
+			err := workspace.StoreCredentials(testCreds)
 			assert.NoError(t, err)
 		}()
 		go func() {
 			defer wg.Done()
-			creds, err := GetStoredCredentials()
+			creds, err := workspace.GetStoredCredentials()
 			assert.NoError(t, err)
 			assert.Equal(t, "token-value", creds.AccessTokens["token-name"])
 		}()
