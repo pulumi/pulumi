060ad21652853699cccf9731a65c511320f63c9a
Thomas Gummerer <t.gummerer@gmail.com>
2024-03-01 20:26:59 +0100

    add changelog

--- pkg/testing/integration/command.go
+++ pkg/testing/integration/command.go
@@ -31,8 +31,16 @@ func RunCommand(t *testing.T, name string, args []string, wd string, opts *Progr
 }
 
 // RunCommandPulumiHome executes the specified command and additional arguments, wrapping any output in the
-// specialized test output streams that list the location the test is running in, and sets the PULUMI_HOME environment variable.
-func RunCommandPulumiHome(t *testing.T, name string, args []string, wd string, opts *ProgramTestOptions, pulumiHome string) error {
+// specialized test output streams that list the location the test is running in, and sets the PULUMI_HOME
+// environment variable.
+func RunCommandPulumiHome(
+	t *testing.T,
+	name string,
+	args []string,
+	wd string,
+	opts *ProgramTestOptions,
+	pulumiHome string,
+) error {
 	path := args[0]
 	command := strings.Join(args, " ")
 	t.Logf("**** Invoke '%v' in '%v'", command, wd)
