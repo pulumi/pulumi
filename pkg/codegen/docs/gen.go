e463b8952172152baa9cd2d2e5bee927dd8d6217
Sean Holung <sean.holung@centurylink.com>
2024-03-12 15:06:09 -0700

    append to comment
    fix spelling
    
    unlink
    
    fic

--- pkg/codegen/docs/gen.go
+++ pkg/codegen/docs/gen.go
@@ -1134,12 +1134,13 @@ func (mod *modContext) getPropertiesWithIDPrefixAndExclude(properties []*schema.
 		comment := prop.Comment
 		link := "#" + propID
 
-		// Check if type is defined in a package external to the current pacakge. If
+		// Check if type is defined in a package external to the current package. If
 		// it is external, update comment to indicate to user that type is defined
 		// in another package and link there.
 		if isExt, _ := isExternalType(codegen.UnwrapType(prop.Type), mod.pkg); isExt {
 			packageName := tokenToPackageName(fmt.Sprintf("%v", codegen.UnwrapType(prop.Type)))
-			comment = fmt.Sprintf("This type is defined in the [%s](/registry/packages/%s) package.", getPackageDisplayName(packageName), packageName)
+			extPkgLink := fmt.Sprintf("/registry/packages/%s", packageName)
+			comment += fmt.Sprintf("\nThis type is defined in the [%s](%s) package.", getPackageDisplayName(packageName), extPkgLink)
 			link = ""
 		}
 
