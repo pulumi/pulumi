27b9be60ae8f9322d3e9f903d791a90d1b42b937
Ian Wahbe <ian@wahbe.com>
2024-03-14 20:58:59 +0100

    A property.Value implementation to replace resource.PropertyValue (#15145)
    <!--- 
    Thanks so much for your contribution! If this is your first time
    contributing, please ensure that you have read the
    [CONTRIBUTING](https://github.com/pulumi/pulumi/blob/master/CONTRIBUTING.md)
    documentation.
    -->
    
    # Description
    
    <!--- Please include a summary of the change and which issue is fixed.
    Please also include relevant motivation and context. -->
    
    The plan is to gradually replace resource.PropertyValue both internally
    (in `pulumi/pulumi`) and in providers with this implementation.
    
    This representation eliminates certain classes of bugs inherent with the
    old representation:
    
    - Modifiers (computed, secret, dependencies) live in a separate space
    from values, so there is no longer a problem with different nestings.
    
    - Equality is well defined and encapsulated.
    
    - The distinction between `Output`, `Secret` and `Computed`.
    
    - Names have been normalized (Input/Computed, Map/Object).
    
    This is our chance to have a property value representation that we like,
    so please leave comments if you think we can improve the API here.
    
    ## Checklist
    
    - [X] I have run `make tidy` to update any new dependencies
    - [X] I have run `make lint` to verify my code passes the lint check
      - [ ] I have formatted my code using `gofumpt`
    
    <!--- Please provide details if the checkbox below is to be left
    unchecked. -->
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    <!--- 
    User-facing changes require a CHANGELOG entry.
    -->
    - [ ] I have run `make changelog` and committed the
    `changelog/pending/<file>` documenting my change
    <!--
    If the change(s) in this PR is a modification of an existing call to the
    Pulumi Cloud,
    then the service should honor older versions of the CLI where this
    change would not exist.
    You must then bump the API version in
    /pkg/backend/httpstate/client/api.go, as well as add
    it to the service.
    -->
    - [ ] Yes, there are changes in this PR that warrants bumping the Pulumi
    Cloud API version
    <!-- @Pulumi employees: If yes, you must submit corresponding changes in
    the service repo. -->

---
+++ sdk/go/property/values_test.go
@@ -0,0 +1,102 @@
+// Copyright 2016-2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package property
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/asset"
+)
+
+// Calling == does not implement desirable behavior, so we ensure that it is invalid.
+func TestCannotCompareValues(t *testing.T) {
+	t.Parallel()
+	assert.False(t, reflect.TypeOf(Value{}).Comparable())
+}
+
+func TestNullEquivalence(t *testing.T) {
+	t.Parallel()
+	assert.Nil(t, New(Null).v)
+
+	assert.True(t, New(Null).Equals(Value{}))
+
+	assert.True(t, New(Null).IsNull())
+}
+
+// This test locks in the nil vs null behavior.
+//
+// In go, nil is a value but not a type. For example:
+//
+//	(any)([]A(nil)) != nil
+//
+// This is because when comparing values typed as `any` still carry their underlying type
+// information. This is true even when:
+//
+//	[]A(nil) == nil
+//
+// Unlike the Go language, Value's Null is not typed. This means that:
+//
+//	New[Array](nil).Equals(New(Null))
+//
+// Further, since Null is it's own type, it is the case that:
+//
+//	New[Array](nil).IsArray() == false
+//
+//	New[Map](nil).IsNull() == true
+func TestNil(t *testing.T) {
+	t.Parallel()
+
+	var nullValue Value
+
+	// []T based type, zero value is Array(nil)
+	t.Run("array", func(t *testing.T) {
+		t.Parallel()
+		nilArray := New[Array](nil)
+
+		assert.False(t, nilArray.IsArray())
+		assert.True(t, nilArray.IsNull())
+		assert.True(t, nilArray.Equals(nullValue))
+
+		assert.True(t, New(Array{}).IsArray())
+	})
+
+	// *T based type, zero value is *resource.Asset(nil)
+	t.Run("asset", func(t *testing.T) {
+		t.Parallel()
+		nilAsset := New[Asset](nil)
+
+		assert.False(t, nilAsset.IsAsset())
+		assert.True(t, nilAsset.IsNull())
+		assert.True(t, nilAsset.Equals(nullValue))
+
+		a, err := asset.FromText("")
+		require.NoError(t, err)
+		assert.True(t, New(a).IsAsset())
+	})
+
+	// string based type, zero value is ""
+	t.Run("string", func(t *testing.T) {
+		t.Parallel()
+		emptyString := New[string]("")
+
+		assert.True(t, emptyString.IsString())
+		assert.False(t, emptyString.IsNull())
+		assert.False(t, emptyString.Equals(nullValue))
+	})
+}
