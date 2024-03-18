e55f7711c3dae43ff2ce659fff41a8d001510561
Justin Van Patten <jvp@justinvp.com>
2024-03-18 14:54:24 -0700

    test: Update deps
    Update test deps, particularly to upgrade go-git to a version without any known security vulnerabilities.

--- tests/testdata/codegen/methods-return-plain-resource/go/metaprovider/init.go
+++ tests/testdata/codegen/methods-return-plain-resource/go/metaprovider/init.go
@@ -7,8 +7,8 @@ import (
 	"fmt"
 
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/pkg/codegen/testing/test/testdata/methods-return-plain-resource/go/metaprovider/internal"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/tests/testdata/codegen/methods-return-plain-resource/go/metaprovider/internal"
 )
 
 type module struct {
