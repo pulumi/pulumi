2a89fd5cb9128d418ee12187d6e38aa99d71d5b5
Ian Wahbe <ian@wahbe.com>
2024-03-26 11:52:37 +0100

    Add test for `Coming soon!`

--- pkg/codegen/docs/description_test.go
+++ pkg/codegen/docs/description_test.go
@@ -16,6 +16,7 @@ func TestProcessDescription(t *testing.T) {
 		prefix string
 	}{
 		{"lambda-description"},
+		{"scaleway-k8s-cluster-description"}, // Repro: https://github.com/pulumi/registry/issues/4202
 	}
 
 	for _, tt := range tests {
