f58a699b12f8f1b297a1ff0ce7318d1b6e6d513c
Thomas Gummerer <t.gummerer@gmail.com>
2024-03-26 13:41:13 +0100

    fix python test

--- sdk/python/lib/test/automation/test_stack.py
+++ sdk/python/lib/test/automation/test_stack.py
@@ -12,23 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
+import os
 import tempfile
+import time
+import unittest
 
-class TestStack(unittest.TestCase):
-    def test_always_read_complete_lines(self):
-        tempfile = tempfile.NamedTemporaryFile()
-        async def write_lines():
-            with tempfile as f:
-                parts = ['{"stdoutEvent": ', '{"message": "hello", "color": "blue"}', '}\n', '{"stdoutEvent": ', '{"message": "world"', ', "color": "red"}}\n']
+from pulumi.automation._stack import _watch_logs
+from pulumi.automation import EngineEvent, StdoutEngineEvent
+
+class TestStack(unittest.IsolatedAsyncioTestCase):
+    async def test_always_read_complete_lines(self):
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+
+        async def write_lines(tmp):
+            with open(tmp, 'w') as f:
+                parts = ['{"stdoutEvent": ', '{"message": "hello", "color": "blue"}', '}\n', '{"stdoutEvent": ', '{"message": "world"', ', "color": "red"}}\n', '{"cancelEvent": {}}\n']
                 for part in parts:
                     f.write(part)
-                    await asyncio.sleep(0.2)
-        write_task = asyncio.create_task(write_lines())
-        event_num = 0
-        def callback(event):
-            if event_num == 0:
-                self.assertEqual(event, {"message": "hello", "color": "blue"})
-            elif event_num == 1:
-                self.assertEqual(event, {"message": "world", "color": "red"})
+                    f.flush()
+                    time.sleep(0.2)
 
-        _watch_logs(tempfile.name, callback)
+        write_task = asyncio.create_task(write_lines(tmp.name))
+        event_num = 0
+
+        def callback(event):
+            nonlocal event_num
+            if event_num == 0:
+                self.assertTrue(event.stdout_event)
+                self.assertEqual(event.stdout_event.message, "hello")
+                self.assertEqual(event.stdout_event.color, "blue")
+            elif event_num == 1:
+                self.assertTrue(event.stdout_event)
+                self.assertEqual(event.stdout_event.message, "world")
+                self.assertEqual(event.stdout_event.color, "red")
+            event_num += 1
+
+        async def watch_async():
+            _watch_logs(tmp.name, callback)
+        watch_task = asyncio.create_task(watch_async())
+        await asyncio.gather(write_task, watch_task)
