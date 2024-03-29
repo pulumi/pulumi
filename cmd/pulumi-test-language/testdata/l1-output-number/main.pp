553f23cf5f51fc58d2fd7e997f44a989b96b7f15
Fraser Waters <fraser@pulumi.com>
2024-03-29 12:12:31 +0000

    Add a conformance test for number outputs (#15824)
    This adds a conformance test that we can generate programs that return
    various numbers as stack output values.

---
+++ cmd/pulumi-test-language/testdata/l1-output-number/main.pp
@@ -0,0 +1,19 @@
+output "zero" {
+    value = 0
+}
+
+output "one" {
+    value = 1
+}
+
+output "e" {
+    value = 2.718
+}
+
+output "max" {
+    value = 1.7976931348623157E+308
+}
+
+output "min" {
+    value = 4.94065645841247E-324
+}
