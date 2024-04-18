b04b7ccbf1620068f054ee37f472e65f23487367
Justin Van Patten <jvp@justinvp.com>
2024-04-18 14:44:19 -0700

    Freeze v3.113.2

--- sdk/python/lib/pulumi/_version.py
+++ sdk/python/lib/pulumi/_version.py
@@ -14,7 +14,7 @@
 
 from semver import VersionInfo
 
-_VERSION = "3.113.2"
+_VERSION = "3.113.3"
 
 version = VersionInfo.parse(_VERSION)
 """Version is the Pulumi SDK's release version."""
--- sdk/python/lib/setup.py
+++ sdk/python/lib/setup.py
@@ -17,7 +17,7 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = "3.113.2"
+VERSION = "3.113.3"
 
 def readme():
     try:
