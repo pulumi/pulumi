a519bc2f7794c39b3326a722c46a79aa327eb7aa
Thomas Gummerer <t.gummerer@gmail.com>
2024-04-19 14:05:37 +0200

    lint fix

--- cmd/pulumi-test-language/l2continue_on_error_test.go
+++ cmd/pulumi-test-language/l2continue_on_error_test.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -78,7 +79,7 @@ func (h *L2ContinueOnErrorHost) GenerateProject(
 		return nil, fmt.Errorf("unexpected fail_on_create sdk %s", req.LocalDependencies["fail_on_create"])
 	}
 	if !req.Strict {
-		return nil, fmt.Errorf("expected strict to be true")
+		return nil, errors.New("expected strict to be true")
 	}
 	if req.TargetDirectory != filepath.Join(h.tempDir, "projects", "l2-failed-create-continue-on-error") {
 		return nil, fmt.Errorf("unexpected target directory %s", req.TargetDirectory)
