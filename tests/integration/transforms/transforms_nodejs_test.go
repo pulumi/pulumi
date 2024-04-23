f9860c4ff3a00a4fad3ad0eec645f9ff56efe421
Fraser Waters <fraser@pulumi.com>
2024-04-23 13:57:04 +0100

    Ensure NodeJS SDK awaits full logical operations (#16005)
    <!--- 
    Thanks so much for your contribution! If this is your first time
    contributing, please ensure that you have read the
    [CONTRIBUTING](https://github.com/pulumi/pulumi/blob/master/CONTRIBUTING.md)
    documentation.
    -->
    
    # Description
    
    <!--- Please include a summary of the change and which issue is fixed.
    Please also include relevant motivation and context. -->
    
    Fixes https://github.com/pulumi/pulumi/issues/15973.
    
    Ensures that the `rpcDone` promise covers the whole async operation
    atomically.
    
    ## Checklist
    
    - [x] I have run `make tidy` to update any new dependencies
    - [x] I have run `make lint` to verify my code passes the lint check
      - [ ] I have formatted my code using `gofumpt`
    
    <!--- Please provide details if the checkbox below is to be left
    unchecked. -->
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    <!--- 
    User-facing changes require a CHANGELOG entry.
    -->
    - [x] I have run `make changelog` and committed the
    `changelog/pending/<file>` documenting my change
    <!--
    If the change(s) in this PR is a modification of an existing call to the
    Pulumi Cloud,
    then the service should honor older versions of the CLI where this
    change would not exist.
    You must then bump the API version in
    /pkg/backend/httpstate/client/api.go, as well as add
    it to the service.
    -->
    - [ ] Yes, there are changes in this PR that warrants bumping the Pulumi
    Cloud API version
    <!-- @Pulumi employees: If yes, you must submit corresponding changes in
    the service repo. -->

--- tests/integration/transforms/transforms_nodejs_test.go
+++ tests/integration/transforms/transforms_nodejs_test.go
@@ -10,22 +10,29 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 )
 
-func TestNodejsTransforms(t *testing.T) {
-	t.Parallel()
+//nolint:paralleltest // ProgramTest calls t.Parallel()
+func TestNodejsSimpleTransforms(t *testing.T) {
+	d := filepath.Join("nodejs", "simple")
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          d,
+		Dependencies: []string{"@pulumi/pulumi"},
+		LocalProviders: []integration.LocalDependency{
+			{Package: "testprovider", Path: filepath.Join("..", "..", "testprovider")},
+		},
+		Quick:                  true,
+		ExtraRuntimeValidation: Validator,
+	})
+}
 
-	//nolint:paralleltest // ProgramTest calls t.Parallel()
-	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:          d,
-				Dependencies: []string{"@pulumi/pulumi"},
-				LocalProviders: []integration.LocalDependency{
-					{Package: "testprovider", Path: filepath.Join("..", "..", "testprovider")},
-				},
-				Quick:                  true,
-				ExtraRuntimeValidation: Validator,
-			})
-		})
-	}
+//nolint:paralleltest // ProgramTest calls t.Parallel()
+func TestNodejsSingleTransforms(t *testing.T) {
+	d := filepath.Join("nodejs", "single")
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          d,
+		Dependencies: []string{"@pulumi/pulumi"},
+		LocalProviders: []integration.LocalDependency{
+			{Package: "testprovider", Path: filepath.Join("..", "..", "testprovider")},
+		},
+		Quick: true,
+	})
 }
