c2d1c8c1c0db648c4e2aa4ed8342c7ff5284b307
Eron Wright <eron@pulumi.com>
2024-05-06 14:51:57 -0700

    tests

---
+++ tests/integration/construct_component_failures/nodejs/index.ts
@@ -0,0 +1,6 @@
+// Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+
+import { Component } from "./component"
+
+const component = new Component("component");
+const result = component.getMessage({ echo: "hello" });
