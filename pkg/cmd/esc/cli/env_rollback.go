eb6d4ded3cc5c2a59e9a16cc08fdb8d16b35ffbe
Pat Gavlin <pat@pulumi.com>
2024-05-20 23:51:31 -0700

    cli: add a rollback command (#305)
    `esc env rollback` rolls the environment definition back to a specific
    version.
    
    Note that this can fail if the target of the rollback imports
    environments that have changed in incompatible ways. Imports should be
    versioned in order to prevent this.

---
+++ pkg/cmd/esc/cli/env_rollback.go
@@ -0,0 +1,60 @@
+// Copyright 2023, Pulumi Corporation.
+
+package cli
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
+	"github.com/spf13/cobra"
+)
+
+func newEnvRollbackCmd(env *envCommand) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "rollback [<org-name>/]<environment-name>@<version>",
+		Args:  cobra.ExactArgs(1),
+		Short: "Roll back to a specific version",
+		Long: "Roll back to a specific version\n" +
+			"\n" +
+			"This command rolls an environment's definition back to the specified\n" +
+			"version. The environment's definition will be replaced with the\n" +
+			"definition at that version, creating a new revision.\n",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+
+			if err := env.esc.getCachedClient(ctx); err != nil {
+				return err
+			}
+
+			ref, args, err := env.getEnvRef(args)
+			if err != nil {
+				return err
+			}
+			if ref.version == "" {
+				return errors.New("please specify a version")
+			}
+			_ = args
+
+			yaml, _, err := env.esc.client.GetEnvironment(ctx, ref.orgName, ref.envName, ref.version, false)
+			if err != nil {
+				return err
+			}
+			diags, err := env.esc.client.UpdateEnvironment(ctx, ref.orgName, ref.envName, yaml, "")
+			if err != nil {
+				return err
+			}
+			if len(diags) != 0 {
+				err = env.writeYAMLEnvironmentDiagnostics(env.esc.stderr, ref.envName, yaml, diags)
+				contract.IgnoreError(err)
+				return errors.New("could not roll back: too many errors")
+			}
+			fmt.Fprintln(env.esc.stdout, "Environment updated.")
+			return nil
+		},
+	}
+
+	return cmd
+}
