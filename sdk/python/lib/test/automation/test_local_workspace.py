8186bcd540c2ca2ee02a6a1359abd509ca04d914
zbuchheit <zbuchheit@pulumi.com>
2024-05-30 18:04:23 -0700

    update list_stacks test to use new optional include_all args and ensure correct command arguments are used in python auto api

--- sdk/python/lib/test/automation/test_local_workspace.py
+++ sdk/python/lib/test/automation/test_local_workspace.py
@@ -562,6 +562,34 @@ class TestLocalWorkspace(unittest.TestCase):
             stacks[1].url, "https://app.pulumi.com/testorg1/testproj1/teststack2"
         )
 
+    def test_list_stacks_with_correct_params(self):
+        captured_args = []
+        mock_with_returned_stacks = PulumiCommand()
+        mock_with_returned_stacks.run = lambda *args, **kwargs: (
+            captured_args.append(args[0]),
+            CommandResult(
+                stdout=json.dumps(
+                    [
+                        {
+                            "name": "testorg1/testproj1/teststack1",
+                            "current": False,
+                            "url": "https://app.pulumi.com/testorg1/testproj1/teststack1",
+                        },
+                        {
+                            "name": "testorg1/testproj2/teststack2",
+                            "current": False,
+                            "url": "https://app.pulumi.com/testorg1/testproj2/teststack2",
+                        },
+                    ]
+                ),
+                stderr="",
+                code=0,
+            ),
+        )[1]
+        ws = LocalWorkspace(pulumi_command=mock_with_returned_stacks)
+        ws.list_stacks()
+        self.assertEqual(captured_args[0], ["stack", "ls", "--json"])
+
     def test_list_all_stacks(self):
         mock_with_returned_stacks = PulumiCommand()
         mock_with_returned_stacks.run = lambda *args, **kwargs: CommandResult(
@@ -583,7 +611,7 @@ class TestLocalWorkspace(unittest.TestCase):
             code=0,
         )
         ws = LocalWorkspace(pulumi_command=mock_with_returned_stacks)
-        stacks = ws.list_all_stacks()
+        stacks = ws.list_stacks(include_all=True)
         self.assertEqual(len(stacks), 2)
         self.assertEqual(stacks[0].name, "testorg1/testproj1/teststack1")
         self.assertEqual(stacks[0].current, False)
@@ -595,6 +623,34 @@ class TestLocalWorkspace(unittest.TestCase):
         self.assertEqual(
             stacks[1].url, "https://app.pulumi.com/testorg1/testproj2/teststack2"
         )
+
+    def test_list_all_stacks_with_correct_params(self):
+        captured_args = []
+        mock_with_returned_stacks = PulumiCommand()
+        mock_with_returned_stacks.run = lambda *args, **kwargs: (
+            captured_args.append(args[0]),
+            CommandResult(
+                stdout=json.dumps(
+                    [
+                        {
+                            "name": "testorg1/testproj1/teststack1",
+                            "current": False,
+                            "url": "https://app.pulumi.com/testorg1/testproj1/teststack1",
+                        },
+                        {
+                            "name": "testorg1/testproj2/teststack2",
+                            "current": False,
+                            "url": "https://app.pulumi.com/testorg1/testproj2/teststack2",
+                        },
+                    ]
+                ),
+                stderr="",
+                code=0,
+            ),
+        )[1]
+        ws = LocalWorkspace(pulumi_command=mock_with_returned_stacks)
+        ws.list_stacks(include_all=True)
+        self.assertEqual(captured_args[0], ["stack", "ls", "--json", "--all"])
 
     def test_stack_status_methods(self):
         project_name = "python_test"
