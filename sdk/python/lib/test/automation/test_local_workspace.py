75dc9b9b8e4744c4c4ed8e48f0fefa8f862aeb6b
Mike Seese <seesemichaelj@gmail.com>
2024-06-08 09:42:03 -0700

    remove redundant remove_stack call in test

--- sdk/python/lib/test/automation/test_local_workspace.py
+++ sdk/python/lib/test/automation/test_local_workspace.py
@@ -763,52 +763,49 @@ class TestLocalWorkspace(unittest.TestCase):
             "buzz": ConfigValue(value="secret", secret=True),
         }
 
+        stack.set_all_config(stack_config)
+
+        # pulumi up
+        up_res = stack.up()
+        self.assertEqual(len(up_res.outputs), 3)
+        self.assertEqual(up_res.outputs["exp_static"].value, "foo")
+        self.assertFalse(up_res.outputs["exp_static"].secret)
+        self.assertEqual(up_res.outputs["exp_cfg"].value, "abc")
+        self.assertFalse(up_res.outputs["exp_cfg"].secret)
+        self.assertEqual(up_res.outputs["exp_secret"].value, "secret")
+        self.assertTrue(up_res.outputs["exp_secret"].secret)
+        self.assertEqual(up_res.summary.kind, "update")
+        self.assertEqual(up_res.summary.result, "succeeded")
+
+        # pulumi preview
+        preview_result = stack.preview()
+        self.assertEqual(preview_result.change_summary.get(OpType.SAME), 1)
+
+        # pulumi refresh
+        refresh_res = stack.refresh()
+        self.assertEqual(refresh_res.summary.kind, "refresh")
+        self.assertEqual(refresh_res.summary.result, "succeeded")
+
+        # pulumi stack rm
+        removed_stack: bool
         try:
-            stack.set_all_config(stack_config)
-
-            # pulumi up
-            up_res = stack.up()
-            self.assertEqual(len(up_res.outputs), 3)
-            self.assertEqual(up_res.outputs["exp_static"].value, "foo")
-            self.assertFalse(up_res.outputs["exp_static"].secret)
-            self.assertEqual(up_res.outputs["exp_cfg"].value, "abc")
-            self.assertFalse(up_res.outputs["exp_cfg"].secret)
-            self.assertEqual(up_res.outputs["exp_secret"].value, "secret")
-            self.assertTrue(up_res.outputs["exp_secret"].secret)
-            self.assertEqual(up_res.summary.kind, "update")
-            self.assertEqual(up_res.summary.result, "succeeded")
-
-            # pulumi preview
-            preview_result = stack.preview()
-            self.assertEqual(preview_result.change_summary.get(OpType.SAME), 1)
-
-            # pulumi refresh
-            refresh_res = stack.refresh()
-            self.assertEqual(refresh_res.summary.kind, "refresh")
-            self.assertEqual(refresh_res.summary.result, "succeeded")
-
-            # pulumi stack rm
-            removed_stack: bool
-            try:
-                stack.workspace.remove_stack(stack_name)
-                removed_stack = True
-            except:
-                removed_stack = False
-
-            self.assertFalse(removed_stack, "Stack was able to remove without the force flag and without destroying it first")
-
-            stack.workspace.remove_stack(stack_name, opts=RemoveStackOptions(force=True))
-
-            stack_exists: bool
-            try:
-                stack.workspace.select_stack(stack_name)
-                stack_exists = True
-            except:
-                stack_exists = False
-
-            self.assertFalse(stack_exists, "Stack should have been removed, but we could still select it")
-        finally:
             stack.workspace.remove_stack(stack_name)
+            removed_stack = True
+        except:
+            removed_stack = False
+
+        self.assertFalse(removed_stack, "Stack was able to remove without the force flag and without destroying it first")
+
+        stack.workspace.remove_stack(stack_name, opts=RemoveStackOptions(force=True))
+
+        stack_exists: bool
+        try:
+            stack.workspace.select_stack(stack_name)
+            stack_exists = True
+        except:
+            stack_exists = False
+
+        self.assertFalse(stack_exists, "Stack should have been removed, but we could still select it")
 
     def test_stack_lifecycle_async_inline_program(self):
         project_name = "async_inline_python"
