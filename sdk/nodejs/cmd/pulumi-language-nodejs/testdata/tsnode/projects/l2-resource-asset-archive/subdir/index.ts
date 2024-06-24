b795fae9b7b47ca2be240b39c1cd30dbd0282e7d
Fraser Waters <fraser@pulumi.com>
2024-06-24 13:21:16 +0100

    Run conformance tests

--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/tsnode/projects/l2-resource-asset-archive/subdir/index.ts
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/tsnode/projects/l2-resource-asset-archive/subdir/index.ts
@@ -4,3 +4,9 @@ import * as asset_archive from "@pulumi/asset-archive";
 const ass = new asset_archive.AssetResource("ass", {value: new pulumi.asset.FileAsset("../test.txt")});
 const arc = new asset_archive.ArchiveResource("arc", {value: new pulumi.asset.FileArchive("../archive.tar")});
 const dir = new asset_archive.ArchiveResource("dir", {value: new pulumi.asset.FileArchive("../folder")});
+const assarc = new asset_archive.ArchiveResource("assarc", {value: new pulumi.asset.AssetArchive({
+    string: new pulumi.asset.StringAsset("file contents"),
+    file: new pulumi.asset.FileAsset("../test.txt"),
+    folder: new pulumi.asset.FileArchive("../folder"),
+    archive: new pulumi.asset.FileArchive("../archive.tar"),
+})});
