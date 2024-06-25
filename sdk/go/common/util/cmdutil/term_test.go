3afe3db12984603995d98d655219d39c3d20c10c
Julien Poissonnier <julien@caffeine.lu>
2024-06-25 21:22:34 +0200

    look for correct python executable depending on OS

--- sdk/go/common/util/cmdutil/term_test.go
+++ sdk/go/common/util/cmdutil/term_test.go
@@ -393,7 +393,22 @@ func (p testProgram) Build(t *testing.T) (cmd *exec.Cmd) {
 		return exec.Command(nodeBin, append([]string{src}, p.args...)...)
 
 	case pythonTestProgram:
-		pythonBin := lookPathOrSkip(t, "python3")
+		pythonCmds := []string{"python3", "python"}
+		if runtime.GOOS == "windows" {
+			pythonCmds = []string{"python", "python3"}
+		}
+		pythonBin := ""
+		for _, bin := range pythonCmds {
+			bin, err := exec.LookPath(bin)
+			if err == nil {
+				pythonBin = bin
+				break
+			}
+		}
+		if pythonBin == "" {
+			t.Skipf("Skipping test: could not find python or python executable")
+			return nil
+		}
 		return exec.Command(pythonBin, append([]string{src}, p.args...)...)
 
 	default:
