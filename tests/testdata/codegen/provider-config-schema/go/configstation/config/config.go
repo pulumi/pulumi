4ffb11376ad14b6a527c7d9157fddea666c0b754
Fraser Waters <fraser@pulumi.com>
2024-07-15 18:34:02 +0100

    Default the import base path to something sensible (#16664)
    This fixes go package generation so users don't _have_ to set the import
    base path explicitly in the go info part of the schema. This only really
    helps our packages (because we assume the path is "github.com/pulumi")
    but it fixes some of the generated test data and should also work for
    local SDKs, where they aren't _really_ at "github.com/pulumi" but also
    that it doesn't matter what URL is used as long as it's a valid one.
    
    Fixes https://github.com/pulumi/pulumi/issues/13756
    
    ---------
    
    Co-authored-by: Thomas Gummerer <t.gummerer@gmail.com>

--- tests/testdata/codegen/provider-config-schema/go/configstation/config/config.go
+++ tests/testdata/codegen/provider-config-schema/go/configstation/config/config.go
@@ -4,9 +4,9 @@
 package config
 
 import (
+	"github.com/pulumi/pulumi-configstation/sdk/go/configstation/internal"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
-	"internal"
 )
 
 var _ = internal.GetEnvOrDefault
