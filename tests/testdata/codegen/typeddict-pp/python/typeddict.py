e763f1a5fd0be3cf0a32b4228a62062fa15b26b1
Julien <julien@caffeine.lu>
2024-07-17 10:11:49 +0200

    Use python names for dictionary literals when inside a typed dictionary input (#16658)
    When generating literals for inputs that have TypedDict types, we want
    to use the pythonic names (snake_case) for keys.
    
    We also have to take care of tracking the fact that we’re inisde a
    TypedDict for nested dicts.
    
    Fixes https://github.com/pulumi/pulumi/issues/16646

--- tests/testdata/codegen/typeddict-pp/python/typeddict.py
+++ tests/testdata/codegen/typeddict-pp/python/typeddict.py
@@ -3,10 +3,10 @@ import pulumi_typeddict as typeddict
 
 main = typeddict.ExampleComponent("main",
     my_type={
-        "stringProp": "hello",
-        "nestedProp": {
-            "nestedStringProp": "world",
-            "nestedNumberProp": "123",
+        "string_prop": "hello",
+        "nested_prop": {
+            "nested_string_prop": "world",
+            "nested_number_prop": "123",
         },
     },
     external_input=aws.s3.BucketWebsiteArgs(
