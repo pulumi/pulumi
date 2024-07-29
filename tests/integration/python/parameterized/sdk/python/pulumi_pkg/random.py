0c6ec02902554ecbc6c5a380bbb0393ddded7ef9
Will Jones <will@sacharissa.co.uk>
2024-07-29 12:23:03 +0100

    Use `snake_case` for package refences in the Python SDK (#16830)
    In adding support for parameterized providers to the Python SDK, some
    variables erroneously used `camelCase` instead of `snake_case`. This
    commit cleans that up ahead of general release.

--- tests/integration/python/parameterized/sdk/python/pulumi_pkg/random.py
+++ tests/integration/python/parameterized/sdk/python/pulumi_pkg/random.py
@@ -110,7 +110,7 @@ class Random(pulumi.CustomResource):
             resource_name,
             __props__,
             opts,
-            packageRef=_utilities.get_package())
+            package_ref=_utilities.get_package())
 
     @staticmethod
     def get(resource_name: str,
