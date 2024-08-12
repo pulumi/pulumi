f6235f88c298b11fef9ce39696ed8bee4826ed8c
Thomas Gummerer <t.gummerer@gmail.com>
2024-08-12 17:17:58 +0200

    recurse into structs + support more types + dashify names

--- pkg/cmd/pulumi/util_rc.go
+++ pkg/cmd/pulumi/util_rc.go
@@ -3,57 +3,39 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"github.com/spf13/viper"
 )
 
-func boolFromRc(v *viper.Viper, name, iniSection string) bool {
+func valueFromRc(v *viper.Viper, name, iniSection string) any {
 	if v.IsSet(name) {
-		return v.GetBool(name)
+		return v.Get(name)
 	}
 	sectionName := fmt.Sprintf("%s.%s", iniSection, name)
 	if v.IsSet(sectionName) {
-		return v.GetBool(sectionName)
+		return v.Get(sectionName)
 	}
 	defaultName := fmt.Sprintf("default.%s", name)
 	if v.IsSet(defaultName) {
-		return v.GetBool(defaultName)
+		return v.Get(defaultName)
 	}
-	val := v.GetBool(name)
+	val := v.Get(name)
 	return val
 }
 
-func stringFromRc(v *viper.Viper, name, iniSection string) string {
-	if v.IsSet(name) {
-		return v.GetString(name)
+func dashedFieldName(name string) string {
+	var result string
+	for i, c := range name {
+		if i > 0 && 'A' <= c && c <= 'Z' {
+			result += "-"
+			result += strings.ToLower(string(c))
+		} else {
+			result += string(c)
+		}
 	}
-	sectionName := fmt.Sprintf("%s.%s", iniSection, name)
-	if v.IsSet(sectionName) {
-		return v.GetString(sectionName)
-	}
-	defaultName := fmt.Sprintf("default.%s", name)
-	if v.IsSet(defaultName) {
-		return v.GetString(defaultName)
-	}
-	val := v.GetString(name)
-	return val
-}
-
-func intFromRc(v *viper.Viper, name, iniSection string) int {
-	if v.IsSet(name) {
-		return v.GetInt(name)
-	}
-	sectionName := fmt.Sprintf("%s.%s", iniSection, name)
-	if v.IsSet(sectionName) {
-		return v.GetInt(sectionName)
-	}
-	defaultName := fmt.Sprintf("default.%s", name)
-	if v.IsSet(defaultName) {
-		return v.GetInt(defaultName)
-	}
-	val := v.GetInt(name)
-	return val
+	return result
 }
 
 func UnmashalOpts(v *viper.Viper, opts any, iniSection string) any {
@@ -65,14 +47,14 @@ func UnmashalOpts(v *viper.Viper, opts any, iniSection string) any {
 			if !rv.Field(i).CanSet() {
 				panic(fmt.Sprintf("can't set field %s", ref.Type().Field(i).Name))
 			}
-			fieldName := ref.Type().Field(i).Name
+			fieldName := dashedFieldName(ref.Type().Field(i).Name)
 			switch rv.Field(i).Kind() {
-			case reflect.String:
-				rv.Field(i).Set(reflect.ValueOf(stringFromRc(v, fieldName, iniSection)))
-			case reflect.Bool:
-				rv.Field(i).Set(reflect.ValueOf(boolFromRc(v, fieldName, iniSection)))
-			case reflect.Int:
-				rv.Field(i).Set(reflect.ValueOf(intFromRc(v, fieldName, iniSection)))
+			case reflect.Struct:
+				rv.Field(i).Set(reflect.ValueOf(UnmashalOpts(v, rv.Field(i).Interface(), iniSection)))
+			case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+				reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32,
+				reflect.Uint64, reflect.Uintptr:
+				rv.Field(i).Set(reflect.ValueOf(valueFromRc(v, fieldName, iniSection)))
 			}
 		}
 		return rv.Interface()
