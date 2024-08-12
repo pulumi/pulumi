87ad394ad80dc8d9911b919a19256c5e3c31dc97
Germán Lena <german.lena@gmail.com>
2024-08-12 16:28:13 -0300

    Fix deployment settings serialization and keys consistency (#16904)
    This PR fixes deployment settings serialization/deserialization:
    - `repoUrl` was being incorrectly serialized as `repoURL`, which for
    JSON was not a problem as it is case insensitive ([To unmarshal JSON
    into a struct, Unmarshal matches incoming object keys to the keys used
    by Marshal ..., preferring an exact match but also accepting a
    case-insensitive match.](https://pkg.go.dev/encoding/json#Unmarshal)),
    but YAML is case sensitive.
    - `time.Duration` unmarshalling failed as it was not consistent with the
    way the service "stringifies" the durations. I am bringing the custom
    marshallers implemented under `DurationMarshaller` (it is called
    `WorkflowTimeout` in the service but since there is no notion of
    Workflows here, I renamed to something more explicit).

--- pkg/cmd/pulumi/deployment_settings_utils_test.go
+++ pkg/cmd/pulumi/deployment_settings_utils_test.go
@@ -16,11 +16,15 @@ package main
 
 import (
 	"context"
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -159,4 +163,69 @@ func TestValidateShortInputNonEmpty(t *testing.T) {
 
 	err = ValidateShortInputNonEmpty(strings.Repeat("a", 257))
 	require.Error(t, err, "must be 256 characters or less")
+}
+
+func TestDSFileParsing(t *testing.T) {
+	t.Parallel()
+
+	stackDeploymentConfigFile := "Pulumi.stack.deploy.yaml"
+	projectDir := t.TempDir()
+	pyaml := filepath.Join(projectDir, stackDeploymentConfigFile)
+
+	err := os.WriteFile(pyaml, []byte(`settings:
+  sourceContext:
+    git:
+      repoUrl: git@github.com:pulumi/test-repo.git
+      branch: main
+      repoDir: .
+      gitAuth:
+        basicAuth:
+          userName: jdoe
+          password:
+            ciphertext: AAABAMcGtHDraogfM3Qk4WyaNp3F/syk2cjHPQTb6Hu6ps8=
+  operationContext:
+    oidc:
+      aws:
+        duration: 1h0m0s
+        policyArns:
+          - policy:arn
+        roleArn: the_role
+        sessionName: the_session_name
+    options:
+      skipInstallDependencies: false
+      skipIntermediateDeployments: true
+  agentPoolID: 51035bee-a4d6-4b63-9ff6-418775c5da8d`), 0o600)
+	require.NoError(t, err)
+
+	deploymentFile, err := workspace.LoadProjectStackDeployment(pyaml)
+	require.NoError(t, err)
+	require.NotNil(t, deploymentFile)
+	require.NotNil(t, deploymentFile.DeploymentSettings.SourceContext)
+	require.NotNil(t, deploymentFile.DeploymentSettings.SourceContext.Git)
+	require.Equal(t, "git@github.com:pulumi/test-repo.git", deploymentFile.DeploymentSettings.SourceContext.Git.RepoURL)
+	require.Equal(t, "main", deploymentFile.DeploymentSettings.SourceContext.Git.Branch)
+	require.Equal(t, ".", deploymentFile.DeploymentSettings.SourceContext.Git.RepoDir)
+	assert.NotNil(t, deploymentFile.DeploymentSettings.SourceContext.Git.GitAuth)
+	assert.NotNil(t, deploymentFile.DeploymentSettings.SourceContext.Git.GitAuth.BasicAuth)
+	assert.NotNil(t, deploymentFile.DeploymentSettings.SourceContext.Git.GitAuth.BasicAuth.UserName)
+	assert.Equal(t, "jdoe", deploymentFile.DeploymentSettings.SourceContext.Git.GitAuth.BasicAuth.UserName.Value)
+	assert.NotNil(t, deploymentFile.DeploymentSettings.SourceContext.Git.GitAuth.BasicAuth.Password)
+	assert.Equal(t, "AAABAMcGtHDraogfM3Qk4WyaNp3F/syk2cjHPQTb6Hu6ps8=",
+		deploymentFile.DeploymentSettings.SourceContext.Git.GitAuth.BasicAuth.Password.Ciphertext)
+	require.NotNil(t, deploymentFile.DeploymentSettings.Operation)
+	require.NotNil(t, deploymentFile.DeploymentSettings.Operation.Options)
+	require.False(t, deploymentFile.DeploymentSettings.Operation.Options.SkipInstallDependencies)
+	require.True(t, deploymentFile.DeploymentSettings.Operation.Options.SkipIntermediateDeployments)
+	require.False(t, deploymentFile.DeploymentSettings.Operation.Options.DeleteAfterDestroy)
+	require.False(t, deploymentFile.DeploymentSettings.Operation.Options.RemediateIfDriftDetected)
+	assert.NotNil(t, deploymentFile.DeploymentSettings.Operation.OIDC)
+	assert.Nil(t, deploymentFile.DeploymentSettings.Operation.OIDC.Azure)
+	assert.Nil(t, deploymentFile.DeploymentSettings.Operation.OIDC.GCP)
+	assert.NotNil(t, deploymentFile.DeploymentSettings.Operation.OIDC.AWS)
+	assert.Equal(t, "the_session_name", deploymentFile.DeploymentSettings.Operation.OIDC.AWS.SessionName)
+	assert.Equal(t, "the_role", deploymentFile.DeploymentSettings.Operation.OIDC.AWS.RoleARN)
+	duration, _ := time.ParseDuration("1h0m0s")
+	assert.Equal(t, apitype.DeploymentDuration(duration), deploymentFile.DeploymentSettings.Operation.OIDC.AWS.Duration)
+	assert.Equal(t, []string{"policy:arn"}, deploymentFile.DeploymentSettings.Operation.OIDC.AWS.PolicyARNs)
+	assert.Equal(t, "51035bee-a4d6-4b63-9ff6-418775c5da8d", *deploymentFile.DeploymentSettings.AgentPoolID)
 }
