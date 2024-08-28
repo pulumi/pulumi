419b5150f27690124df35a421b7140b2601e83be
Artur Laksberg <1875734+arturl@users.noreply.github.com>
2024-08-28 09:17:25 -0700

    Update comment per PR feedback

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -45,7 +45,7 @@ type Options struct {
 	ShowReads              bool                // true to show resources that are being read in
 	TruncateOutput         bool                // true if we should truncate long outputs
 	SuppressOutputs        bool                // true to suppress output summarization, e.g. if contains sensitive info.
-	SuppressPermalink      bool                // true to suppress state permalink
+	SuppressPermalink      bool                // true to suppress state permalink (including in DIY backends)
 	SummaryDiff            bool                // true if diff display should be summarized.
 	IsInteractive          bool                // true if we should display things interactively.
 	Type                   Type                // type of display (rich diff, progress, or query).
