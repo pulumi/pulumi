237ec24d17718e6d751416650032d3b1cb802502
Mikhail Shilkov <github@mikhail.io>
2024-08-29 14:31:15 +0200

    Switch to UUIDs

--- sdk/python/scripts/test_auto.sh
+++ sdk/python/scripts/test_auto.sh
@@ -8,7 +8,7 @@ coverage run -m pytest lib/test/automation
 
 if [[ "$PULUMI_TEST_COVERAGE_PATH" ]]; then
     if [ -e .coverage ]; then
-        TS=$(python -c 'import time; print(int(time.time() * 1000))')
-        coverage xml -o $PULUMI_TEST_COVERAGE_PATH/python-auto-$TS.xml
+        UUID=$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
+        coverage xml -o $PULUMI_TEST_COVERAGE_PATH/python-auto-$UUID.xml
     fi
 fi
