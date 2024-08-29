ff349fba5fe449175fa05f3c59972fd6b4fd9c66
Mikhail Shilkov <github@mikhail.io>
2024-08-29 16:44:05 +0200

    Switch from uuidgen to python to support Windows

--- sdk/python/scripts/test_auto.sh
+++ sdk/python/scripts/test_auto.sh
@@ -8,7 +8,7 @@ coverage run -m pytest lib/test/automation
 
 if [[ "$PULUMI_TEST_COVERAGE_PATH" ]]; then
     if [ -e .coverage ]; then
-        UUID=$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
+        UUID=$(python -c "import uuid; print(str(uuid.uuid4()).replace('-', '').lower())")
         coverage xml -o $PULUMI_TEST_COVERAGE_PATH/python-auto-$UUID.xml
     fi
 fi
