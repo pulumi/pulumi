4d62f365c0bef2eeb6be56a58d93244dce5b15fb
Julien Poissonnier <julien@caffeine.lu>
2024-09-03 10:31:59 +0200

    Update tests to run on Go 1.22 and 1.23
    Go 1.23 was released a couple weeks ago https://go.dev/blog/go1.23, update CI to use the 2 latest versions.

--- scripts/get-job-matrix.py
+++ scripts/get-job-matrix.py
@@ -120,7 +120,7 @@ ALL_PLATFORMS = ["ubuntu-latest", "windows-latest", "macos-latest"]
 MINIMUM_SUPPORTED_VERSION_SET = {
     "name": "minimum",
     "dotnet": "6",
-    "go": "1.21.x",
+    "go": "1.22.x",
     "nodejs": "18.x",
     "python": "3.8.x",
 }
@@ -128,7 +128,7 @@ MINIMUM_SUPPORTED_VERSION_SET = {
 CURRENT_VERSION_SET = {
     "name": "current",
     "dotnet": "8",
-    "go": "1.22.x",
+    "go": "1.23.x",
     "nodejs": "22.x",
     "python": "3.12.x",
 }
