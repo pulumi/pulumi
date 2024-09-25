2da151181558d1ab1f18239d42f80049b837bde4
Julien Poissonnier <julien@caffeine.lu>
2024-09-25 14:40:06 +0200

    Add simple performance gate to integration tests
    Fixes https://github.com/pulumi/pulumi/issues/15347

---
+++ tests/performance/performance_test.go
@@ -0,0 +1,91 @@
+// Copyright 2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build all
+
+package perf
+
+import (
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+)
+
+//nolint:paralleltest // Do not run in parallel to avoid resource contention
+func TestPerfEmptyUpdate(t *testing.T) {
+	benchmarkEnforcer := &integration.AssertPerfBenchmark{
+		T:                  t,
+		MaxPreviewDuration: 2 * time.Second,
+		MaxUpdateDuration:  2 * time.Second,
+	}
+
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		NoParallel: true,
+		Dir:        "empty",
+		Dependencies: []string{
+			filepath.Join("..", "..", "sdk", "python", "env", "src"),
+		},
+		Quick:       true,
+		ReportStats: benchmarkEnforcer,
+		CloudURL:    integration.MakeTempBackend(t),
+	})
+}
+
+//nolint:paralleltest // Do not run in parallel to avoid resource contention
+func TestPerfManyComponentUpdate(t *testing.T) {
+	benchmarkEnforcer := &integration.AssertPerfBenchmark{
+		T:                  t,
+		MaxPreviewDuration: 10 * time.Second,
+		MaxUpdateDuration:  10 * time.Second,
+	}
+
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		NoParallel: true,
+		Dir:        "component",
+		Dependencies: []string{
+			filepath.Join("..", "..", "sdk", "python", "env", "src"),
+		},
+		Quick:       true,
+		ReportStats: benchmarkEnforcer,
+		CloudURL:    integration.MakeTempBackend(t),
+		LocalProviders: []integration.LocalDependency{
+			{Package: "testprovider", Path: filepath.Join("..", "testprovider")},
+		},
+	})
+}
+
+//nolint:paralleltest // Do not run in parallel to avoid resource contention
+func TestPerfParentChainUpdate(t *testing.T) {
+	benchmarkEnforcer := &integration.AssertPerfBenchmark{
+		T:                  t,
+		MaxPreviewDuration: 10 * time.Second,
+		MaxUpdateDuration:  10 * time.Second,
+	}
+
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		NoParallel: true,
+		Dir:        "parents",
+		Dependencies: []string{
+			filepath.Join("..", "..", "sdk", "python", "env", "src"),
+		},
+		Quick:       true,
+		ReportStats: benchmarkEnforcer,
+		CloudURL:    integration.MakeTempBackend(t),
+		LocalProviders: []integration.LocalDependency{
+			{Package: "testprovider", Path: filepath.Join("..", "testprovider")},
+		},
+	})
+}
