81a36e5c07ed7761947215d8c01fdc5c77a11d05
Ian Wahbe <me@iwahbe.com>
2024-09-30 16:13:09 +0100

    make error types private

--- sdk/go/property/path.go
+++ sdk/go/property/path.go
@@ -39,7 +39,7 @@ func (p Path) Get(v Value) (Value, PathApplyFailure) {
 
 // Set a value.
 //
-// Set does not src in place, rather producing a copy.
+// Set does not change src in place, rather producing a copy.
 func (p Path) Set(src, to Value) (Value, PathApplyFailure) {
 	if len(p) == 0 {
 		return to, nil
@@ -53,10 +53,10 @@ func (p Path) Set(src, to Value) (Value, PathApplyFailure) {
 	case v.IsArray():
 		i, ok := last.(IndexSegment)
 		if !ok {
-			return Value{}, pathErrorf(v, "expected an index step, found %T", last)
+			return Value{}, pathErrorf(v, "expected an IndexSegment, found %T", last)
 		}
 		if i.int < 0 || i.int > len(v.AsArray()) {
-			return Value{}, PathApplyIndexOutOfBounds{found: v.AsArray(), idx: i.int}
+			return Value{}, pathApplyIndexOutOfBoundsError{found: v.AsArray(), idx: i.int}
 		}
 		// We now want to set set v.AsArray()[i]=to, but we don't want to mutate v. We make
 		// a shallow copy of v and then alter that.
@@ -67,7 +67,7 @@ func (p Path) Set(src, to Value) (Value, PathApplyFailure) {
 	case v.IsMap():
 		k, ok := last.(KeySegment)
 		if !ok {
-			return Value{}, pathErrorf(v, "expected a key step, found %T", last)
+			return Value{}, pathErrorf(v, "expected a KeySegment, found %T", last)
 		}
 		// We now want to set set v.AsMap()[k]=to, but we don't want to mutate v. We make
 		// a shallow copy of v and then alter that.
@@ -133,9 +133,9 @@ func (k KeySegment) apply(v Value) (Value, PathApplyFailure) {
 		if ok {
 			return r, nil
 		}
-		return Value{}, PathApplyKeyMissing{found: m, needle: k.string}
+		return Value{}, pathApplyKeyMissingError{found: m, needle: k.string}
 	}
-	return Value{}, PathApplyKeyExpectedMap{found: v}
+	return Value{}, pathApplyKeyExpectedMapError{found: v}
 }
 
 type IndexSegment struct{ int }
@@ -144,72 +144,72 @@ func (k IndexSegment) apply(v Value) (Value, PathApplyFailure) {
 	if v.IsArray() {
 		a := v.AsArray()
 		if k.int < 0 || k.int >= len(a) {
-			return Value{}, PathApplyIndexOutOfBounds{found: a, idx: k.int}
+			return Value{}, pathApplyIndexOutOfBoundsError{found: a, idx: k.int}
 		}
 		return a[k.int], nil
 	}
-	return Value{}, PathApplyIndexExpectedArray{found: v}
+	return Value{}, pathApplyIndexExpectedArrayError{found: v}
 }
 
-type PathApplyKeyExpectedMap struct {
+type pathApplyKeyExpectedMapError struct {
 	found Value
 }
 
-func (err PathApplyKeyExpectedMap) Error() string {
+func (err pathApplyKeyExpectedMapError) Error() string {
 	return "expected a map, found a " + typeString(err.found)
 }
 
-func (err PathApplyKeyExpectedMap) Found() Value {
+func (err pathApplyKeyExpectedMapError) Found() Value {
 	return err.found
 }
 
-type PathApplyKeyMissing struct {
+type pathApplyKeyMissingError struct {
 	found  Map
 	needle string
 }
 
-func (err PathApplyKeyMissing) Error() string {
+func (err pathApplyKeyMissingError) Error() string {
 	return fmt.Sprintf("missing key %q in map", err.needle)
 }
 
-func (err PathApplyKeyMissing) Found() Value {
+func (err pathApplyKeyMissingError) Found() Value {
 	return New(err.found)
 }
 
-type PathApplyIndexExpectedArray struct {
+type pathApplyIndexExpectedArrayError struct {
 	found Value
 }
 
-func (err PathApplyIndexExpectedArray) Error() string {
+func (err pathApplyIndexExpectedArrayError) Error() string {
 	return "expected an array, found a " + typeString(err.found)
 }
 
-func (err PathApplyIndexExpectedArray) Found() Value {
+func (err pathApplyIndexExpectedArrayError) Found() Value {
 	return err.found
 }
 
-type PathApplyIndexOutOfBounds struct {
+type pathApplyIndexOutOfBoundsError struct {
 	found Array
 	idx   int
 }
 
-func (err PathApplyIndexOutOfBounds) Found() Value {
+func (err pathApplyIndexOutOfBoundsError) Found() Value {
 	return New(err.found)
 }
 
-func (err PathApplyIndexOutOfBounds) Error() string {
+func (err pathApplyIndexOutOfBoundsError) Error() string {
 	return fmt.Sprintf("index %d out of bounds of an array of length %d",
 		err.idx, len(err.found))
 }
 
 func pathErrorf(v Value, msg string, a ...any) PathApplyFailure {
-	return pathApplyFailure{found: v, msg: fmt.Sprintf(msg, a...)}
+	return pathApplyError{found: v, msg: fmt.Sprintf(msg, a...)}
 }
 
-type pathApplyFailure struct {
+type pathApplyError struct {
 	found Value
 	msg   string
 }
 
-func (err pathApplyFailure) Error() string { return err.msg }
-func (err pathApplyFailure) Found() Value  { return err.found }
+func (err pathApplyError) Error() string { return err.msg }
+func (err pathApplyError) Found() Value  { return err.found }
