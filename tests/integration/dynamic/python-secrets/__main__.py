104dd3eec7176b0aa8696b6e09b6cd50a8312b47
Julien Poissonnier <julien@caffeine.lu>
2024-10-14 16:43:26 +0200

    pass config argument to dynamic provider methods

--- tests/integration/dynamic/python-secrets/__main__.py
+++ tests/integration/dynamic/python-secrets/__main__.py
@@ -1,26 +1,41 @@
 # Copyright 2016-2024, Pulumi Corporation.  All rights reserved.
 
 import pulumi
-from pulumi.dynamic import CreateResult, Resource, ResourceProvider
+from pulumi.dynamic import CreateResult, Resource, ResourceProvider, Config
 
 
-config = pulumi.Config()
-password = config.require_secret("password")
+class SimpleProviderWithConfig(ResourceProvider):
+    def create(self, props, config: Config = None):
+        password = config.require("password")
+        # This simulates using this as a credential to talk to an external system.
+        return CreateResult(
+            "0", {"authenticated": "200" if password == "s3cret" else "401"}
+        )
+
+
+class SimpleResourceWithConfig(Resource):
+    authenticated: pulumi.Output[str]
+
+    def __init__(self, name):
+        super().__init__(SimpleProviderWithConfig(), name, {"authenticated": None})
 
 
 class SimpleProvider(ResourceProvider):
     def create(self, props):
-        # Need to use `password.get()` to get the underlying value of the secret from within the serialized code.
-        # This simulates using this as a credential to talk to an external system.
-        return CreateResult("0", { "authenticated": "200" if password.get() == "s3cret" else "401" })
+        return CreateResult("0", {"authenticated": "304"})
 
 
 class SimpleResource(Resource):
     authenticated: pulumi.Output[str]
 
     def __init__(self, name):
-        super().__init__(SimpleProvider(), name, { "authenticated": None })
+        super().__init__(SimpleProvider(), name, {"authenticated": None})
 
 
-r = SimpleResource("foo")
-pulumi.export("out", r.authenticated)
+# password_outside.get() # should raise an exception
+
+r_with_config = SimpleResourceWithConfig("with-config")
+pulumi.export("authenticated_with_config", r_with_config.authenticated)
+
+r_without_config = SimpleResource("without-config")
+pulumi.export("authenticated_without_config", r_without_config.authenticated)
