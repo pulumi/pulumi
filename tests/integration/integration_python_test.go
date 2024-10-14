409241bc0e259235fdfc60c5dbccdc603209826c
Julien Poissonnier <julien@caffeine.lu>
2024-10-14 09:23:28 +0200

    unskip python debugger tests

--- tests/integration/integration_python_test.go
+++ tests/integration/integration_python_test.go
@@ -1474,9 +1474,6 @@ func TestConfigGetterOverloads(t *testing.T) {
 func TestDebuggerAttachPython(t *testing.T) {
 	t.Parallel()
 
-	t.Skip("XXX")
-	// TODO: only skip on 3.13, keep running on older version
-
 	e := ptesting.NewEnvironment(t)
 	defer e.DeleteIfNotFailed()
 	e.ImportDirectory(filepath.Join("python", "venv"))
@@ -1611,9 +1608,6 @@ func TestConstructFailuresPython(t *testing.T) {
 //
 //nolint:paralleltest // ProgramTest calls t.Parallel()
 func TestLogDebugPython(t *testing.T) {
-	t.Skip("XXX")
-	// TODO: only skip on 3.13, keep running on older version
-
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir: filepath.Join("log_debug", "python"),
 		Dependencies: []string{
