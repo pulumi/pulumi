4314ab90bece3460360167d4a547ec0fc574cdc3
Anton Tayanovskyy <anton@pulumi.com>
2024-10-29 14:53:59 -0400

    Automatic code generation

--- sdk/go/pulumi-language-go/testdata/projects/l2-provider-grpc-config-schema-secret/main.go
+++ sdk/go/pulumi-language-go/testdata/projects/l2-provider-grpc-config-schema-secret/main.go
@@ -10,6 +10,20 @@ func main() {
 		// This provider covers scenarios where configuration properties are marked as secret in the schema.
 		config_grpc_provider, err := configgrpc.NewProvider(ctx, "config_grpc_provider", &configgrpc.ProviderArgs{
 			SecretString1: pulumi.String("SECRET"),
+			SecretInt1:    pulumi.Int(16),
+			SecretNum1:    pulumi.Float64(123456.789),
+			SecretBool1:   pulumi.Bool(true),
+			ListSecretString1: pulumi.StringArray{
+				pulumi.String("SECRET"),
+				pulumi.String("SECRET2"),
+			},
+			MapSecretString1: pulumi.StringMap{
+				"key1": pulumi.String("SECRET"),
+				"key2": pulumi.String("SECRET2"),
+			},
+			ObjSecretString1: &configgrpc.TsecretString1Args{
+				SecretX: pulumi.String("SECRET"),
+			},
 		})
 		if err != nil {
 			return err
--- sdk/python/cmd/pulumi-language-python/testdata/toml/projects/l2-provider-grpc-config-schema-secret/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/toml/projects/l2-provider-grpc-config-schema-secret/__main__.py
@@ -2,8 +2,20 @@ import pulumi
 import pulumi_config_grpc as config_grpc
 
 # This provider covers scenarios where configuration properties are marked as secret in the schema.
-config_grpc_provider = config_grpc.Provider("config_grpc_provider", secret_string1="SECRET")
-# listSecretString1 = ["SECRET", "SECRET2"]
-# mapSecretString1 = { key1 = "SECRET", key2 = "SECRET2" }
-# objSecretString1 = { x = "SECRET" }
+config_grpc_provider = config_grpc.Provider("config_grpc_provider",
+    secret_string1="SECRET",
+    secret_int1=16,
+    secret_num1=123456.789,
+    secret_bool1=True,
+    list_secret_string1=[
+        "SECRET",
+        "SECRET2",
+    ],
+    map_secret_string1={
+        "key1": "SECRET",
+        "key2": "SECRET2",
+    },
+    obj_secret_string1={
+        "secret_x": "SECRET",
+    })
 config = config_grpc.ConfigFetcher("config", opts = pulumi.ResourceOptions(provider=config_grpc_provider))
