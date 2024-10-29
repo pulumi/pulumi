2701835151f26e9aa7a13b68cbb4b149f463ea28
Anton Tayanovskyy <anton@pulumi.com>
2024-10-29 14:53:30 -0400

    Add more variety

--- cmd/pulumi-test-language/testdata/l2-provider-grpc-config-schema-secret/main.pp
+++ cmd/pulumi-test-language/testdata/l2-provider-grpc-config-schema-secret/main.pp
@@ -1,13 +1,13 @@
 # This provider covers scenarios where configuration properties are marked as secret in the schema.
 resource "config_grpc_provider" "pulumi:providers:config-grpc" {
     secretString1 = "SECRET"
-    # secretInt1 = 1234567890
-    # secretNum1 = 123456.7890
-    # secretBool1 = true
+    secretInt1 = 16
+    secretNum1 = 123456.7890
+    secretBool1 = true
 
-    # listSecretString1 = ["SECRET", "SECRET2"]
-    # mapSecretString1 = { key1 = "SECRET", key2 = "SECRET2" }
-    # objSecretString1 = { x = "SECRET" }
+    listSecretString1 = ["SECRET", "SECRET2"]
+    mapSecretString1 = { key1 = "SECRET", key2 = "SECRET2" }
+    objSecretString1 = { secretX = "SECRET" }
 }
 
 resource "config" "config-grpc:index:ConfigFetcher" {
