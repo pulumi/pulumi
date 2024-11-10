f73ed5d7c0b263610d13b8d547a6104ec5500353
Joe Duffy <joe@pulumi.com>
2024-11-09 16:41:57 -0800

    Get rid of a leftover Context() function

--- sdk/go/common/resource/plugin/mock.go
+++ sdk/go/common/resource/plugin/mock.go
@@ -46,10 +46,6 @@ type MockHost struct {
 
 var _ Host = (*MockHost)(nil)
 
-func (m *MockHost) Context() context.Context {
-	return context.Background()
-}
-
 func (m *MockHost) ServerAddr() string {
 	if m.ServerAddrF != nil {
 		return m.ServerAddrF()
