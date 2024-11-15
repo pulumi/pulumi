7f3feaff1f66dfa33da01dd40e08aaf75b5e17a9
Julien Poissonnier <julien@caffeine.lu>
2024-11-15 13:58:26 +0100

    Workaround for escape codes in output from .NET 9
    .NET 9.0 introduced a bug where the output from `dotnet run` can include a progress indicator, even though the output is redirected: https://github.com/dotnet/sdk/issues/44610
    
    This causes .NET plugins to fail when running under .NET 9.0 because we can't parse the port number.
    
    As a workaround, we strip these control characters.

--- sdk/go/common/resource/plugin/plugin_test.go
+++ sdk/go/common/resource/plugin/plugin_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLogFlowArgumentPropagation(t *testing.T) {
@@ -54,4 +55,35 @@ func TestLogFlowArgumentPropagation(t *testing.T) {
 		verbose:         9,
 		tracingEndpoint: "127.0.0.1:6007",
 	}), []string{"--logtostderr", "-v=9", "--tracing", "127.0.0.1:6007", "127.0.0.1:12345"})
+}
+
+func TestParsePort(t *testing.T) {
+	for _, port := range []string{
+		"1234",
+		" 1234",
+		"     1234",
+		"1234 ",
+		"1234     ",
+		"1234\r\n",
+		"1234\n",
+		"\x1b]9;4;3;\x1b\\\x1b]9;4;0;\x1b\\1234",
+		"\x1b]9;4;3;\x1b\\\x1b]9;4;0;\x1b\\ 1234",
+		"\x1b]9;4;3;\x1b\\\x1b]9;4;0;\x1b\\ 1234 ",
+		"\x1b]9;4;3;\x1b\\\x1b]9;4;0;\x1b\\1234\n",
+	} {
+		parsedPort, err := parsePort(port)
+		require.NoError(t, err)
+		require.Equal(t, 1234, parsedPort)
+	}
+
+	for _, port := range []string{
+		"",
+		"banana",
+		"0",
+		"-1234",
+		"100000",
+	} {
+		_, err := parsePort(port)
+		require.Error(t, err)
+	}
 }
