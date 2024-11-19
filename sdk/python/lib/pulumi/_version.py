e6f10f38505174725afea9d4f81d2338cc1a1113
Fraser Waters <fraser@pulumi.com>
2024-11-19 12:52:28 +0000

    Prepare v3.140.0 release (#17802)
    ### Features
    
    - [auto/go] Add --config-file functionality to Go Automation API
    preview, update, refresh, and destroy
    [#17774](https://github.com/pulumi/pulumi/pull/17774)
    
     ### Bug Fixes
    
    - [cli/plugin] Workaround for escape codes in output from .NET 9
    [#17783](https://github.com/pulumi/pulumi/pull/17783)
    
    - [sdk/python] Fix merging InvokeOptions.version
    [#17750](https://github.com/pulumi/pulumi/pull/17750)

--- sdk/python/lib/pulumi/_version.py
+++ sdk/python/lib/pulumi/_version.py
@@ -14,7 +14,7 @@
 
 from semver import VersionInfo
 
-_VERSION = "3.139.1"
+_VERSION = "3.140.0"
 
 version = VersionInfo.parse(_VERSION)
 """Version is the Pulumi SDK's release version."""
--- sdk/python/lib/setup.py
+++ sdk/python/lib/setup.py
@@ -17,7 +17,7 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = "3.139.1"
+VERSION = "3.140.0"
 
 def readme():
     try:
