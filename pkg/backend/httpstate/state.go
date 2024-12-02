48fb7b7bdbd931f795a1b213880925a1d2ab847a
Will Jones <will@sacharissa.co.uk>
2024-12-02 16:25:50 +0000

    Move `sdk/go/common/channel` to `pkg` (#17895)
    `sdk/go/common/channel` is home to a single helper function,
    `FilterRead`, that is [only used
    
    internally](https://github.com/search?q=%28org%3Apulumiverse+OR+org%3Apulumi%29+%22FilterRead%22&type=code)
    and doesn't need to be part of the public API that `sdk/go/common` in
    theory comprises. This commit moves it into `pkg/channel` to try and
    reduce unnecessary surface in `sdk/go/common`.

--- pkg/backend/httpstate/state.go
+++ pkg/backend/httpstate/state.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, Pulumi Corporation.
+// Copyright 2016-2024, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import (
 	"time"
 
 	"github.com/jonboulle/clockwork"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/channel"
+	"github.com/pulumi/pulumi/pkg/v3/channel"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
