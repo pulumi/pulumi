f665a3854a75da2b45f8eeefbed47e01827725d0
Fraser Waters <fraser@pulumi.com>
2024-12-05 09:50:59 +0000

    Use PrePrepareProject in TestParameterizedGo (#17924)
    Rather than checking the generated code into the repo.

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Excluding tests that have their dependencies code-generated but under .gitignore.
-EXCLUDE="-e tests/integration/construct_component_configure_provider/go/go.mod -e sdk/go/pulumi-language-go/testdata -e tests/integration/go/parameterized/sdk/go/pkg/go.mod"
+EXCLUDE="-e tests/integration/construct_component_configure_provider/go/go.mod -e sdk/go/pulumi-language-go/testdata -e tests/integration/go/parameterized/go.mod"
 
 for f in $(git ls-files '**go.mod' | grep -v $EXCLUDE)
 do
