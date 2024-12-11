df869f912e26a62fad859ab5223e4fa132675a00
Will Jones <will@sacharissa.co.uk>
2024-12-11 14:13:19 +0000

    Break out the `plugin ...` command hierarchy (#17970)
    Continuing with the clean-up of `pkg/cmd/pulumi`, this factors out the
    `plugin ...` hierarchy.

--- pkg/cmd/pulumi/plugin/plugin.go
+++ pkg/cmd/pulumi/plugin/plugin.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2024, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package plugin
 
 import (
 	"github.com/spf13/cobra"
@@ -25,7 +25,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 )
 
-func newPluginCmd() *cobra.Command {
+func NewPluginCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "plugin",
 		Short: "Manage language and resource provider plugins",
