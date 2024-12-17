8bce8605c1e7f131098b8c4fd6812e8e0a027644
Will Jones <will@sacharissa.co.uk>
2024-12-17 11:01:32 +0000

    Break out the `install` command (#17984)
    Continuing with the clean-up of `pkg/cmd/pulumi`, this factors out the
    `install` command.

--- pkg/cmd/pulumi/install/install.go
+++ pkg/cmd/pulumi/install/install.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, Pulumi Corporation.
+// Copyright 2016-2024, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package install
 
 import (
 	"errors"
@@ -34,7 +34,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
 )
 
-func newInstallCmd() *cobra.Command {
+func NewInstallCmd() *cobra.Command {
 	var reinstall bool
 	var noPlugins, noDependencies bool
 	var useLanguageVersionTools bool
