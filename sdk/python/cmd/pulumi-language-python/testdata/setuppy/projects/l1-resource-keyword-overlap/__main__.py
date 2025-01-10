33b6b9b7dbad8fd6c32cfa193e04de30ed746e52
Brandon Pollack <brandonpollack23@gmail.com>
2025-01-10 12:52:21 +0900

    add generated python conformance

--- sdk/python/cmd/pulumi-language-python/testdata/setuppy/projects/l1-resource-keyword-overlap/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/setuppy/projects/l1-resource-keyword-overlap/__main__.py
@@ -1,6 +1,19 @@
 import pulumi
 
-# Since the name is "this" it will fail in typescript and other languages with
-# this reservered keyword if it is not renamed.
-this = "somestring"
-pulumi.export("output", this)
+# Keywords in various languages should be renamed and work.
+class_ = "class_output_string"
+export = "export_output_string"
+import_ = "import_output_string"
+mod = "mod_output_string"
+object = {
+    "object": "object_output_string",
+}
+self = "self_output_string"
+this = "this_output_string"
+pulumi.export("output_class", class_)
+pulumi.export("output_export", export)
+pulumi.export("output_import", import_)
+pulumi.export("output_mod", mod)
+pulumi.export("output_object", object)
+pulumi.export("output_self", self)
+pulumi.export("output_this", this)
