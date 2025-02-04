0c1654ecf70e24237dcd116e8f2d0650f5c08038
Thomas Gummerer <t.gummerer@gmail.com>
2025-02-04 11:02:34 +0100

    get rid of xplatform_acceptance tag

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build (go || all) && !xplatform_acceptance
+//go:build (go || all)
 
 package ints
 
--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build (python || all) && !xplatform_acceptance
+//go:build (python || all)
 
 package ints
 
