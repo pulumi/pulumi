bdde9cbd541a8e4c5014757b0ea72623215910bc
Will Jones <will@sacharissa.co.uk>
2025-02-06 12:00:58 +0000

    Prepare for 3.149.0

--- sdk/nodejs/version.ts
+++ sdk/nodejs/version.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2025, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export const version = "3.148.1";
+export const version = "3.149.0";
