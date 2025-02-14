2bd3e7f0e5ee66f110acc75373fd82bc441b04e7
Julien Poissonnier <julien@caffeine.lu>
2025-02-14 09:55:28 +0100

    [Experimental/Components] Hook up inputs & outputs in the provider
    This passes outputs back from the provider's Construct call, making the
    components work end-to-end.
    
    Fixes https://github.com/pulumi/pulumi/issues/18367
    Ref https://github.com/pulumi/pulumi/issues/15939

--- tests/integration/component_provider/nodejs/component-provider-host/provider/component.ts
+++ tests/integration/component_provider/nodejs/component-provider-host/provider/component.ts
@@ -11,8 +11,17 @@ export interface MyComponentArgs {
 export class MyComponent extends pulumi.ComponentResource {
     aNumberOutput: pulumi.Output<number>;
     anOptionalStringOutput?: pulumi.Output<string>;
+    aBooleanOutput: pulumi.Output<boolean>;
 
     constructor(name: string, args: MyComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         super("nodejs-component-provider:index:MyComponent", name, args, opts);
+        this.aNumberOutput = pulumi.output(args.aNumber * 2);
+        this.anOptionalStringOutput = pulumi.output("Hello, " + (args.anOptionalString ?? "World") + "!");
+        this.aBooleanOutput = pulumi.output(args.aBooleanInput).apply(b => !b);
+        this.registerOutputs({
+            aNumberOutput: this.aNumberOutput,
+            anOptionalStringOutput: this.anOptionalStringOutput,
+            aBooleanOutput: this.aBooleanOutput,
+        });
     }
 }
--- tests/integration/component_provider/nodejs/component-provider-host/python/__main__.py
+++ tests/integration/component_provider/nodejs/component-provider-host/python/__main__.py
@@ -4,8 +4,11 @@ import pulumi_nodejs_component_provider as provider
 comp = provider.MyComponent(
     "comp",
     a_number=123,
-    an_optional_string="hello",
+    an_optional_string="Bonnie",
     a_boolean_input=pulumi.Output.from_input(True),
 )
 
 pulumi.export("urn", comp.urn)
+pulumi.export("aNumberOutput", comp.a_number_output)
+pulumi.export("anOptionalStringOutput", comp.an_optional_string_output)
+pulumi.export("aBooleanOutput", comp.a_boolean_output)
