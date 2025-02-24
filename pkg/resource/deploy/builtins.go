c92b37089fb3d8a0a2bb52cf6cdccd6148ade822
Fraser Waters <fraser@pulumi.com>
2025-02-24 17:41:23 +0000

    Error on stack reference import
    Fixes https://github.com/pulumi/pulumi/issues/18594

--- pkg/resource/deploy/builtins.go
+++ pkg/resource/deploy/builtins.go
@@ -278,7 +278,12 @@ func (p *builtinProvider) SignalCancellation(context.Context) error {
 
 func (p *builtinProvider) readStackReference(inputs resource.PropertyMap) (resource.PropertyMap, error) {
 	name, ok := inputs["name"]
-	contract.Assertf(ok, "missing required property 'name'")
+	// If the name is not provided, we should return an error. This is probably due to a user trying to import
+	// this stack reference.
+	if !ok {
+		return nil, errors.New("stack reference can not be imported")
+	}
+
 	contract.Assertf(name.IsString(), "expected 'name' to be a string")
 
 	if p.backendClient == nil {
