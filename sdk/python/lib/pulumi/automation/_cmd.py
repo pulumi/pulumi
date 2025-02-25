2709101b10134b04c97d5aada057cebd5f04d823
Tom Harding <i.am.tom.harding@gmail.com>
2025-02-25 13:30:35 +0100

    Dump args to help with debugging

--- sdk/python/lib/pulumi/automation/_cmd.py
+++ sdk/python/lib/pulumi/automation/_cmd.py
@@ -17,6 +17,7 @@ import os
 import subprocess
 import tempfile
 import urllib.request
+import sys
 from typing import Any, Callable, Dict, List, Mapping, Optional
 
 from semver import VersionInfo
@@ -206,6 +207,7 @@ class PulumiCommand:
             env = _fixup_path(env, os.path.dirname(self.command))
         cmd = [self.command]
         cmd.extend(args)
+        sys.stderr.write(str(args))
 
         stdout_chunks: List[str] = []
 
