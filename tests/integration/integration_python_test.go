pulumi/pulumi

commit e880feb955929400f63f0e8be7cfdf65618543ee

Add path.root (rootDirectory) equivalent to pcl and generate for go/python/node (#18595)
Reference:
https://developer.hashicorp.com/terraform/language/expressions/references#filesystem-and-workspace-info

pcl.root as per the docs returns the project root, which we define as
the
location of the Pulumi.yaml (for example if we pass in the `-C`/`cwd`
parameter to execute
in another directory).

path.cwd is the directory where terraform is run from, this means even
if
`-C`/`cwd` are set, it will return the current directory. This is not
included in this PR and will require a bit more (but similar) work.
(sending CWD through rpcs to language, and all the piping from this CL.
etc).

To do after this submits:
Make similar changes in pulumi/pulumi-dotnet and pulumi/pulumi-java

Part of #18565

sdk/python/cmd/pulumi-language-python-exec
@@ -66,6 +66,7 @@ if __name__ == "__main__":
     # Parse the arguments, program name, and optional arguments.
     ap = argparse.ArgumentParser(description='Execute a Pulumi Python program')
     ap.add_argument('--project', help='Set the project name')
+    ap.add_argument('--root_directory', help='Set the project root directory, location of Pulumi.yaml')
     ap.add_argument('--stack', help='Set the stack name')
     ap.add_argument('--parallel', help='Run P resource operations in parallel (default=none)')
     ap.add_argument('--dry_run', help='Simulate resource changes, but without making them')
@@ -97,6 +98,7 @@ if __name__ == "__main__":
             monitor=args.monitor,
             engine=args.engine,
             project=args.project,
+            root_directory=args.root_directory,
             stack=args.stack,
             parallel=int(args.parallel),
             dry_run=args.dry_run == "true",

sdk/go/pulumi-language-go/testdata/projects/l1-builtin-project-root/Pulumi.yaml
@@ -1,0 +1,2 @@
+name: l1-builtin-project-root
+runtime: go

sdk/nodejs/cmd/pulumi-language-nodejs/testdata/tsc/projects/l1-builtin-project-root/.gitignore
@@ -1,0 +1,2 @@
+/bin/
+/node_modules/

sdk/nodejs/cmd/pulumi-language-nodejs/testdata/tsc/projects/l1-builtin-project-root/Pulumi.yaml
@@ -1,0 +1,5 @@
+name: l1-builtin-project-root
+runtime:
+  name: nodejs
+  options:
+    typescript: false

sdk/nodejs/cmd/pulumi-language-nodejs/testdata/tsnode/projects/l1-builtin-project-root/.gitignore
@@ -1,0 +1,2 @@
+/bin/
+/node_modules/

sdk/nodejs/cmd/pulumi-language-nodejs/testdata/tsnode/projects/l1-builtin-project-root/Pulumi.yaml
@@ -1,0 +1,2 @@
+name: l1-builtin-project-root
+runtime: nodejs

sdk/python/cmd/pulumi-language-python/testdata/classes/projects/l1-builtin-project-root/.gitignore
@@ -1,0 +1,2 @@
+*.pyc
+venv/

sdk/python/cmd/pulumi-language-python/testdata/classes/projects/l1-builtin-project-root/Pulumi.yaml
@@ -1,0 +1,6 @@
+name: l1-builtin-project-root
+runtime:
+  name: python
+  options:
+    typechecker: pyright
+    virtualenv: venv

commit ed5287a3faf63039791ca0faa8ad434bf0ea4f65

[sdk/python] Fix `get_organization` returning `None`
When versions v3.152.0 and earlier of the `pulumi` python SDK are used with v3.153.0 of the CLI, the `pulumi.get_organization()` function returns `None` rather than the expected version. This is a regression that was introduced by #18595.

This is what's happening: `pulumi-language-python-exec` ships with the CLI. In v3.153.0, it tries to create an instance of `pulumi.runtime.Settings` passing a new parameter, `root_directory`. However, older versions of the `pulumi` python SDK do not support this parameter. An error is raised, which is caught and we fallback to default values, which excludes the `organization`, so it ends up being `None`.

This change fixes the problem in `pulumi-language-python-exec`. It first tries to create `Settings` with all possible parameters. If it runs into an error, it falls back to a slower path that inspects the signature of `Settings.__init__` to determine the supported parameters, and then tries to create `Settings` again with only the parameters it supports.

sdk/python/cmd/pulumi-language-python-exec
@@ -4,6 +4,7 @@
 import argparse
 import asyncio
 from typing import Optional
+import inspect
 import logging
 import os
 import sys
@@ -92,27 +93,30 @@ if __name__ == "__main__":
             pulumi.runtime.set_config(k, v)
 
     # Configure the runtime so that the user program hooks up to Pulumi as appropriate.
-    # New versions of pulumi python support setting organization, old versions do not
+    # Note that older versions of the `pulumi` python SDK do not support newer
+    # parameters like `organization` and `root_directory`. We automatically fall back
+    # to excluding unsupported parameters, if needed.
+    settings_kwargs = {
+        "monitor": args.monitor,
+        "engine": args.engine,
+        "project": args.project,
+        "stack": args.stack,
+        "parallel": int(args.parallel),
+        "dry_run": args.dry_run == "true",
+        "organization": args.organization,
+        "root_directory": args.root_directory,
+    }
     try:
-        settings = pulumi.runtime.Settings(
-            monitor=args.monitor,
-            engine=args.engine,
-            project=args.project,
-            root_directory=args.root_directory,
-            stack=args.stack,
-            parallel=int(args.parallel),
-            dry_run=args.dry_run == "true",
-            organization=args.organization,
-        )
+        # Try to create the `Settings` object with all possible parameters.
+        settings = pulumi.runtime.Settings(**settings_kwargs)
     except TypeError:
-        settings = pulumi.runtime.Settings(
-            monitor=args.monitor,
-            engine=args.engine,
-            project=args.project,
-            stack=args.stack,
-            parallel=int(args.parallel),
-            dry_run=args.dry_run == "true"
-        )
+        # The `Settings` class didn't support some of the parameters we passed in.
+        # Fall back to a slower path that inspects the valid parameters from the
+        # signature, filters out unsupported parameters, and tries to create the
+        # `Settings` object again with only supported parameters.
+        settings_params = inspect.signature(pulumi.runtime.Settings.__init__).parameters
+        filtered_kwargs = {k: v for k, v in settings_kwargs.items() if k in settings_params}
+        settings = pulumi.runtime.Settings(**filtered_kwargs)
 
     pulumi.runtime.configure(settings)
 

tests/integration/integration_python_test.go
@@ -2121,3 +2121,17 @@ func installPythonProviderDependencies(t *testing.T, dir string) {
 	out, err := cmd.CombinedOutput()
 	require.NoError(t, err, "output: %s", out)
 }
+
+// Regression test for https://github.com/pulumi/pulumi/issues/18768
+//
+//nolint:paralleltest // ProgramTest calls t.Parallel()
+func TestOrganization(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          filepath.Join("python", "organization"),
+		Dependencies: []string{
+			// We explicitly do not depend on the local Python SDK here.
+			// Instead, the version specified in requirements.txt is used.
+		},
+		Quick: true,
+	})
+}

tests/integration/python/organization/.gitignore
@@ -1,0 +1,5 @@
+*.pyc
+/.pulumi/
+/dist/
+/*.egg-info
+venv/

tests/integration/python/organization/Pulumi.yaml
@@ -1,0 +1,3 @@
+name: pulumi-python-organization
+description: A simple Python Pulumi program that expects the organization to be set
+runtime: python

tests/integration/python/organization/__main__.py
@@ -1,0 +1,3 @@
+import pulumi
+
+assert pulumi.get_organization() is not None, "Organization expected but not found"