a1d06c6cf8d67d3de261e6d816e44371190fb3fe
Fraser Waters <fraser@pulumi.com>
2025-03-10 08:55:28 +0000

    Add resourceType/Name to TS

--- sdk/nodejs/resource.ts
+++ sdk/nodejs/resource.ts
@@ -1511,3 +1511,24 @@ export function pkgFromType(type: string): string | undefined {
     }
     return undefined;
 }
+
+
+/**
+ * The type assigned to the resource at construction.
+ */
+export function resourceType(res: Resource): string {
+    return res.__pulumiType;
+}
+
+/**
+ * The name assigned to the resource at construction.
+ */
+export function resourceName(res: Resource): string {
+    if (res.__name === undefined) {
+        throw new ResourceError(
+            "Resource name is not available, this resource instance must have been constructed by an old SDK",
+            res,
+        );
+    }
+    return res.__name;
+}
