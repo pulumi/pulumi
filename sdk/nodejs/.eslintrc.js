e5fbd303a5cc23d27f0a67aec044d67125320224
Tom Harding <i.am.tom.harding@gmail.com>
2025-03-12 10:18:39 +0100

    Or

--- sdk/nodejs/.eslintrc.js
+++ sdk/nodejs/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "tsconfig.json",
+        project: true,
         tsconfigRootDir: __dirname,
         sourceType: "module",
     },
