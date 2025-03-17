98b13484b36061d3175312c9d2b1c6dccaeb6e1a
Brandon Pollack <brandonpollack23@gmail.com>
2025-03-17 18:53:32 +0900

    Add canOutput generation to python
    This is the same as #18897 but for python
    
    Part of #18860 and #18522

--- sdk/python/cmd/pulumi-language-python/testdata/classes/projects/l1-builtin-can/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/classes/projects/l1-builtin-can/__main__.py
@@ -1,12 +1,20 @@
 import pulumi
 
-def can_(fn):
-    try:
-        _result = fn()
-        return True
-    except:
-        return False
+def canOutput_(fn) -> pulumi.Output[bool]:
+	try:
+		result = pulumi.Output.from_input(fn())
+		return result.apply(lambda x: x != None)
+	except:
+		return pulumi.Output.from_input(False)
 
+
+def can_(fn) -> bool:
+		try:
+			result = fn()
+			return True
+		except:
+			return False
+	
 
 str = "str"
 a_list = [
@@ -23,3 +31,8 @@ another_object = {
 pulumi.export("canFalse", canOutput_(lambda: object["a"]))
 pulumi.export("canFalseDoubleNested", canOutput_(lambda: object["a"]["b"]))
 pulumi.export("canTrue", can_(lambda: another_object["nested"]))
+# canOutput should also generate, secrets are l1 functions which return outputs.
+some_secret = pulumi.Output.secret({
+    "a": "a",
+})
+pulumi.export("canOutput", canOutput_(lambda: some_secret).apply(lambda can: "true" if can else "false"))
