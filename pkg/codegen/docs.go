f2dd5483ab1b92d391973531c4947920fce0fa57
Daniel Bradley <daniel@pulumi.com>
2025-03-18 17:50:34 +0000

    Parse schema docs refs
    Add a utility function to parse `<pulumi ref="" />` tags from description fields. Parse the ref itself into a struct which the specific codegen implementations can easy work from.
    
    Implement with goldmark + html token parser for robustness.
    
    - Include additional docs renderer to match behaviour of the examples filtering rendering, with extra override for HTML.
    - Add DocRef constructor & IsWithin helper
    - Avoid reformatting if no refs found
    - Trim trailing newlines if they weren't present before

--- pkg/codegen/docs.go
+++ pkg/codegen/docs.go
@@ -15,9 +15,20 @@
 package codegen
 
 import (
+	"bytes"
+	"fmt"
+	"net/url"
+	"strings"
+
 	"github.com/pgavlin/goldmark/ast"
+	"github.com/pgavlin/goldmark/renderer"
+	"github.com/pgavlin/goldmark/renderer/markdown"
+	"github.com/pgavlin/goldmark/util"
+	"golang.org/x/net/html"
 
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
 // DocLanguageHelper is an interface for extracting language-specific information from a Pulumi schema.
@@ -99,4 +110,277 @@ func FilterExamples(description string, lang string) string {
 	parsed := schema.ParseDocs(source)
 	filterExamples(source, parsed, lang)
 	return schema.RenderDocsToString(source, parsed)
+}
+
+type PulumiRefResolver func(ref DocRef) (string, bool)
+
+func InterpretPulumiRefs(description string, resolveRefToName PulumiRefResolver) string {
+	if description == "" {
+		return ""
+	}
+
+	source := []byte(description)
+	parsed := schema.ParseDocs(source)
+
+	md := &markdown.Renderer{}
+
+	refRenderer := &pulumiRefNodeRenderer{resolveRefToName, 0}
+	r := renderer.NewRenderer(renderer.WithNodeRenderers(
+		util.Prioritized(refRenderer, 100),
+		util.Prioritized(schema.NewRenderer(), 150),
+		util.Prioritized(md, 200),
+	))
+	var buf bytes.Buffer
+	err := r.Render(&buf, source, parsed)
+	contract.AssertNoErrorf(err, "error rendering docs")
+	// Avoid reformatting if no refs found.
+	if refRenderer.renderedCount == 0 {
+		return description
+	}
+	newDescription := buf.String()
+	// Cut trailing newline if the original description didn't have one.
+	if !strings.HasSuffix(description, "\n") {
+		newDescription, _ = strings.CutSuffix(newDescription, "\n")
+	}
+	return newDescription
+}
+
+type pulumiRefNodeRenderer struct {
+	resolveRefToName PulumiRefResolver
+	renderedCount    int
+}
+
+func (r *pulumiRefNodeRenderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
+	reg.Register(ast.KindRawHTML,
+		func(writer util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
+			if !entering {
+				return ast.WalkContinue, nil
+			}
+
+			raw := n.(*ast.RawHTML)
+			var htmlSource bytes.Buffer
+			for i := range raw.Segments.Len() {
+				segment := raw.Segments.At(i)
+				htmlSource.Write(segment.Value(source))
+			}
+
+			ref, ok := parsePulumiRef(htmlSource.String())
+			var err error
+			if !ok {
+				_, err = writer.Write(htmlSource.Bytes())
+			} else {
+				name, ok := r.resolveRefToName(ref)
+				if !ok {
+					name = defaultRefRender(ref)
+				}
+				_, err = writer.Write([]byte(name))
+				r.renderedCount++
+			}
+			if err != nil {
+				return ast.WalkStop, fmt.Errorf("error writing pulumi ref: %w", err)
+			}
+
+			return ast.WalkSkipChildren, nil
+		},
+	)
+}
+
+func defaultRefRender(docRef DocRef) string {
+	if docRef.Property != "" {
+		return docRef.Property
+	}
+	if docRef.Token != "" {
+		return docRef.Token.DisplayName()
+	}
+	return docRef.Ref
+}
+
+func parsePulumiRef(htmlFragment string) (DocRef, bool) {
+	nodes, err := html.ParseFragment(strings.NewReader(htmlFragment), &html.Node{Type: html.ElementNode})
+	if err != nil || len(nodes) != 1 {
+		return DocRef{}, false
+	}
+	node := nodes[0]
+	if node.Type != html.ElementNode || node.Data != "pulumi" {
+		return DocRef{}, false
+	}
+
+	for _, attr := range node.Attr {
+		if attr.Key == "ref" {
+			return parseDocRef(attr.Val), true
+		}
+	}
+	return DocRef{}, false
+}
+
+type DocRefType string
+
+const (
+	DocRefTypeUnknown                DocRefType = ""
+	DocRefTypeResource               DocRefType = "resource"
+	DocRefTypeFunction               DocRefType = "function"
+	DocRefTypeType                   DocRefType = "type"
+	DocRefTypeResourceProperty       DocRefType = "resourceProperty"
+	DocRefTypeResourceInputProperty  DocRefType = "resourceInputProperty"
+	DocRefTypeFunctionInputProperty  DocRefType = "functionInputProperty"
+	DocRefTypeFunctionOutputProperty DocRefType = "functionOutputProperty"
+	DocRefTypeTypeProperty           DocRefType = "typeProperty"
+)
+
+type DocRef struct {
+	// Original parsed ref
+	Ref string
+	// The type of the parsed ref
+	Type DocRefType
+	// The token of the resource, function, or type, or empty if not applicable.
+	Token tokens.Type
+	// The referenced property name, or empty if not applicable.
+	Property string
+}
+
+// Returns true is the current reference is a property within the other reference.
+func (r DocRef) IsWithin(other DocRef) bool {
+	switch r.Type {
+	case DocRefTypeResourceProperty, DocRefTypeResourceInputProperty:
+		return other.Type == DocRefTypeResource && r.Token == other.Token
+	case DocRefTypeFunctionInputProperty, DocRefTypeFunctionOutputProperty:
+		return other.Type == DocRefTypeFunction && r.Token == other.Token
+	case DocRefTypeTypeProperty:
+		return other.Type == DocRefTypeType && r.Token == other.Token
+	case DocRefTypeResource, DocRefTypeFunction, DocRefTypeType, DocRefTypeUnknown:
+		return false
+	}
+	return false
+}
+
+// NewDocRef constructs a validated DocRef from the given type, token, and property.
+// `token` is required for all types except `DocRefTypeUnknown`.
+// `property` is required for property types.
+func NewDocRef(docRefType DocRefType, token string, property string) DocRef {
+	var ref string
+	switch docRefType {
+	case DocRefTypeResource:
+		contract.Assertf(token != "", "resource token must be provided")
+		contract.Assertf(property == "", "property name must not be set for resource")
+		ref = "#/resources/" + url.PathEscape(token)
+	case DocRefTypeFunction:
+		contract.Assertf(token != "", "function token must be provided")
+		contract.Assertf(property == "", "property name must not be set for function")
+		ref = "#/functions/" + url.PathEscape(token)
+	case DocRefTypeType:
+		contract.Assertf(token != "", "type token must be provided")
+		contract.Assertf(property == "", "property name must not be set for type")
+		ref = "#/types/" + url.PathEscape(token)
+	case DocRefTypeResourceProperty:
+		contract.Assertf(token != "", "resource token must be provided")
+		contract.Assertf(property != "", "property name must be provided")
+		ref = fmt.Sprintf("#/resources/%s/properties/%s", url.PathEscape(token), url.PathEscape(property))
+	case DocRefTypeResourceInputProperty:
+		contract.Assertf(token != "", "resource token must be provided")
+		contract.Assertf(property != "", "property name must be provided")
+		ref = fmt.Sprintf("#/resources/%s/inputProperties/%s", url.PathEscape(token), url.PathEscape(property))
+	case DocRefTypeFunctionInputProperty:
+		contract.Assertf(token != "", "function token must be provided")
+		contract.Assertf(property != "", "property name must be provided")
+		ref = fmt.Sprintf("#/functions/%s/inputProperties/%s", url.PathEscape(token), url.PathEscape(property))
+	case DocRefTypeFunctionOutputProperty:
+		contract.Assertf(token != "", "function token must be provided")
+		contract.Assertf(property != "", "property name must be provided")
+		ref = fmt.Sprintf("#/functions/%s/outputProperties/%s", url.PathEscape(token), url.PathEscape(property))
+	case DocRefTypeTypeProperty:
+		contract.Assertf(token != "", "type token must be provided")
+		contract.Assertf(property != "", "property name must be provided")
+		ref = fmt.Sprintf("#/types/%s/properties/%s", url.PathEscape(token), url.PathEscape(property))
+	case DocRefTypeUnknown:
+		return DocRef{Type: DocRefTypeUnknown}
+	default:
+		contract.Failf("unsupported doc ref type %s", docRefType)
+	}
+	return DocRef{
+		Ref:      ref,
+		Type:     docRefType,
+		Token:    tokens.Type(token),
+		Property: property,
+	}
+}
+
+// Parses a doc reference string into a DocRef struct.
+// The supported formats of the ref is:
+//
+//	#/resources/{token}
+//	#/functions/{token}
+//	#/types/{token}
+//	#/resources/{token}/properties/{property}
+//	#/resources/{token}/inputProperties/{property}
+//	#/functions/{token}/inputProperties/{property}
+//	#/functions/{token}/outputProperties/{property}
+//	#/types/{token}/properties/{property}
+//
+// Note: Tokens containing a slash ("/") must be encoded as "%2F".
+func parseDocRef(ref string) DocRef {
+	docRefUnknown := DocRef{Ref: ref, Type: DocRefTypeUnknown}
+	parts := strings.Split(ref, "/")
+	if len(parts) < 3 || parts[0] != "#" {
+		return docRefUnknown
+	}
+	// Extract which top-level type the ref is referring to.
+	var topLevelType string
+	switch parts[1] {
+	case "resources", "functions", "types":
+		// Strip "s" from the end of the type.
+		topLevelType = parts[1][:len(parts[1])-1]
+	default:
+		return docRefUnknown
+	}
+	tokenString, err := url.PathUnescape(parts[2])
+	if err != nil || tokenString == "" {
+		return docRefUnknown
+	}
+	token, err := tokens.ParseTypeToken(tokenString)
+	if err != nil {
+		return docRefUnknown
+	}
+
+	if len(parts) == 3 {
+		return DocRef{Ref: ref, Type: DocRefType(topLevelType), Token: token}
+	}
+
+	if len(parts) != 5 {
+		return docRefUnknown
+	}
+
+	property, err := url.PathUnescape(parts[4])
+	if err != nil || property == "" {
+		return docRefUnknown
+	}
+
+	// Extract the property kind and name.
+	switch parts[3] {
+	case "properties":
+		switch topLevelType {
+		case "resource", "type":
+			return DocRef{Ref: ref, Type: DocRefType(topLevelType + "Property"), Token: token, Property: property}
+		default:
+			// Properties isn't a valid ref
+			return docRefUnknown
+		}
+	case "inputProperties":
+		switch topLevelType {
+		case "resource", "function":
+			return DocRef{Ref: ref, Type: DocRefType(topLevelType + "InputProperty"), Token: token, Property: property}
+		default:
+			// Properties isn't a valid ref
+			return docRefUnknown
+		}
+	case "outputProperties":
+		switch topLevelType {
+		case "function":
+			return DocRef{Ref: ref, Type: DocRefType(topLevelType + "OutputProperty"), Token: token, Property: property}
+		default:
+			// Properties isn't a valid ref
+			return docRefUnknown
+		}
+	default:
+		return docRefUnknown
+	}
 }
