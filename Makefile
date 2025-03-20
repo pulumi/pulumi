pulumi/pulumi

commit 2d6fe46cc224dba68ff02ae96f5450360e17adc3

De-duplicate the build for `make build` and `make test` (#18728)
Both `make build` and `make install` install the `pulumi` binary to a
global location (using `go install` under the hood). This PR adds a
`bin/pulumi` target that will build a local copy of Pulumi. It also
changes the implementation of `make build` and `make install` to use the
copy generated with `make bin/pulumi`.

Since I often run `make build install`, this cuts build time for me in
half.

Makefile
@@ -70,8 +70,16 @@ generate::
 	$(call STEP_MESSAGE)
 	echo "This command does not do anything anymore. It will be removed in a future version."
 
-build:: build_proto build_display_wasm .make/ensure/go
-	cd pkg && go install -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${PROJECT}
+.PHONY: bin/pulumi
+bin/pulumi: build_proto .make/ensure/go .make/ensure/phony
+	go build -C pkg -o ../$@ -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${PROJECT}
+
+build:: bin/pulumi build_display_wasm
+ifneq (${GOBIN},)
+	cp $< ${GOBIN}/pulumi
+else
+	cp $< $(shell go env GOPATH)/pulumi
+endif
 
 build_display_wasm:: .make/ensure/go
 	cd pkg && GOOS=js GOARCH=wasm go build -o ../bin/pulumi-display.wasm -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ./backend/display/wasm
@@ -79,8 +87,8 @@ build_display_wasm:: .make/ensure/go
 build_local:: build_proto .make/ensure/go
 	export GOBIN=$(shell realpath ./bin) && make dist
 
-install:: .make/ensure/phony .make/ensure/go
-	cd pkg && GOBIN=$(PULUMI_BIN) go install -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${PROJECT}
+install:: bin/pulumi
+	cp $< $(PULUMI_BIN)/pulumi
 
 build_debug::
 	cd pkg && go install -gcflags="all=-N -l" -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${PROJECT}

commit 347f62d4fc736c81ad589be30982153414749149

make dist target not depend on build and fix PULUMI_INTEGRATION_REBUILD_BINARIES (#18958)
`make dist` currently depends on `build`, even though it doens't use any
of its artifacts. At the same time `dist` is also used in `make
build_local`, which is used for rebuilding binaries for integration
tests. However since we're setting GOBIN to `$repo_root/bin`, `build`
now tries to copy the pulumi binary to the same directory as the `dist`
command does, and the same directory the binary is being built in.

We still want to use `make dist` for `build_local`, because it also
builds the language plugins, which are used in the integration tests.
Just remove the `build` target from the dependency list of `dist`. `go
install` already does that job, and we don't need to build the wasm
bits, as `dist` is expected to install binaries not just build them
anyway.

(This was accidentally broken in
https://github.com/pulumi/pulumi/pull/18728)

Makefile
@@ -106,7 +106,7 @@ developer_docs::
 
 install_all:: install
 
-dist:: build
+dist::
 	cd pkg && go install -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${PROJECT}
 
 .PHONY: brew