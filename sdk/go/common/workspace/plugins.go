e89317b4fe7a264281501d09f1eb6de0543cb687
Thomas Gummerer <t.gummerer@gmail.com>
2025-03-20 07:55:41 +0100

    Revert "correctly marshal pluginspec in GetRequiredPackages (#18786)"
    This reverts commit 6d26ecf10b3c1e39e1b6b16fa83207945aecc190.

--- sdk/go/common/workspace/plugins.go
+++ sdk/go/common/workspace/plugins.go
@@ -1040,7 +1040,7 @@ func NewPluginSpec(
 			// a consistent experience once the plugin is installed, and won't have any problems when the repo
 			// is updated.  The version will then be added to the plugins SDK, and will be reused when the NewPluginSpec
 			// is used, so the user gets a consistent experience.
-			if versionStr == "" && version == nil {
+			if versionStr == "" {
 				var err error
 				version, err = gitutil.GetLatestTagOrHash(context.Background(), url)
 				if err != nil {
