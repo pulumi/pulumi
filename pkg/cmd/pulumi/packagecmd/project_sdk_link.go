2b0a46a510f8cd619ff6a717954f9da1e0f35b3e
Thomas Gummerer <t.gummerer@gmail.com>
2025-03-25 14:58:41 +0100

    debug

--- pkg/cmd/pulumi/packagecmd/project_sdk_link.go
+++ pkg/cmd/pulumi/packagecmd/project_sdk_link.go
@@ -242,6 +242,7 @@ func linkNodeJsPackage(ws pkgWorkspace.Context, root string, pkg *schema.Package
 		// Assume npm if no packagemanager is specified
 		addCmd = exec.Command("npm", "add", packageSpecifier)
 	}
+	fmt.Println(addCmd.String())
 
 	addCmd.Stdout = os.Stdout
 	addCmd.Stderr = os.Stderr
