f707abe4e9606a836ee620851b072ef35da40e93
Brandon Pollack <brandonpollack23@gmail.com>
2025-03-25 09:40:35 +0100

    Add canOutput generation to nodejs (#18897)
    This is the same as #18852 but for the can implementation.
    
    Part of https://github.com/pulumi/pulumi/issues/18860 and
    https://github.com/pulumi/pulumi/issues/18522
    
    ---------
    
    Co-authored-by: Fraser Waters <fraser@pulumi.com>

--- cmd/pulumi-test-language/tests/testdata/l1-builtin-can/main.pp
+++ cmd/pulumi-test-language/tests/testdata/l1-builtin-can/main.pp
@@ -1,23 +1,50 @@
-str = "str"
+config "aMap" "map(string)" {}
+
+output "plainTrySuccess" {
+  value = can(aMap["a"])
+}
+
+output "plainTryFailure" {
+  value = can(aMap["b"])
+}
+
+aSecretMap = secret(aMap)
+
+output "outputTrySuccess" {
+  value = can(aSecretMap["a"])
+}
+
+output "outputTryFailure" {
+  value = can(aSecretMap["b"])
+}
 
 # A dynamically typed value, whose field accesses will not be type errors (since the type is not known to the type
 # checker), but may fail dynamically, and can thus be used as test inputs to can.
-config "object" {}
+config "anObject" {}
 
-anotherObject = {
-  nested = "nestedValue"
+output "dynamicTrySuccess" {
+  value = can(anObject.a)
 }
 
-# This should return false, since object.a is undefined.
-output "canFalse" {
-  value = can(object.a)
+output "dynamicTryFailure" {
+  value = can(anObject.b)
 }
 
-output "canFalseDoubleNested" {
-  value = can(object.a.b)
+aSecretObject = secret(anObject)
+
+output "outputDynamicTrySuccess" {
+  value = can(aSecretObject.a)
 }
 
-# This should return true, since anotherObject.nested is defined.
-output "canTrue" {
-  value = can(anotherObject.nested)
+output "outputDynamicTryFailure" {
+  value = can(aSecretObject.b)
+}
+
+# Check that explicit null values can be returned
+output "plainTryNull" {
+  value = can(anObject.opt)
+}
+
+output "outputTryNull" {
+  value = can(aSecretObject.opt)
 }
