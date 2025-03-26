97a3bb7f440ecbc6467b67601b313ba67dd66270
Mikhail Shilkov <github@mikhail.io>
2025-03-26 10:49:45 +0100

    [components/node] Run componentProviderHost automatically in RunPlugin if any Components are exported (#18984)
    This is what we have been tracking as "Automatic component inference
    from code without an explicit provider implementation". This change
    hides `componentProviderHost` from users, or at least makes it optional.
    Instead of relying on user code to bootstrap the provider, we inspect
    the exports of the loaded module. If any of the exports is a class that
    extends ComponentResource, we assume that users want a component host
    and call `componentProviderHost({dirname: programDir, components})`.
    
    I updated one of the existing integration tests to illustrate the UX.

--- sdk/nodejs/cmd/run-plugin/run.ts
+++ sdk/nodejs/cmd/run-plugin/run.ts
@@ -22,7 +22,7 @@ import * as tsutils from "../../tsutils";
 import { ResourceError, RunError } from "../../errors";
 import * as log from "../../log";
 import * as settings from "../../runtime/settings";
-import * as stack from "../../runtime/stack";
+import { componentProviderHost, getPulumiComponents } from "../../provider/experimental/provider";
 
 // Keep track if we already logged the information about an unhandled error to the user..  If
 // so, we end with a different exit code.  The language host recognizes this and will not print
@@ -275,14 +275,22 @@ ${errMsg}`,
         // loop empties.
         log.debug(`Running program '${program}' in pwd '${process.cwd()}' w/ args: ${programArgs}`);
         try {
-            // Execute the module and capture any module outputs it exported. If the exported value
-            // was itself a Function, then just execute it.  This allows for exported top level
-            // async functions that pulumi programs can live in.  Finally, await the value we get
-            // back.  That way, if it is async and throws an exception, we properly capture it here
-            // and handle it.
+            // Execute the module and capture any module outputs it exported.
             const reqResult = require(program);
+
+            // Get any Pulumi Components exported by the module
+            const components = getPulumiComponents(reqResult);
+            if (components.length > 0) {
+                // If we found any components, boot up a component provider host
+                return await componentProviderHost({ dirname: program, components });
+            }
+
+            // If the exported value was itself a Function, then just execute it. This allows for
+            // exported top level async functions that pulumi programs can live in.
             const invokeResult = reqResult instanceof Function ? reqResult() : reqResult;
 
+            // Finally, await the value we get back. That way, if it is async and throws an exception,
+            // we properly capture it here and handle it.
             return await invokeResult;
         } catch (e) {
             // User JavaScript can throw anything, so if it's not an Error it's definitely
