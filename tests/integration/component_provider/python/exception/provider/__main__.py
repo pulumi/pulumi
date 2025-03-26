646f2283276792887ead64ad84ef2f5c59376f62
Julien Poissonnier <julien@caffeine.lu>
2025-03-26 15:26:15 +0100

    test fixes

--- tests/integration/component_provider/python/exception/provider/__main__.py
+++ tests/integration/component_provider/python/exception/provider/__main__.py
@@ -17,4 +17,4 @@ from pulumi.provider.experimental import component_provider_host
 from component import MyComponent
 
 if __name__ == "__main__":
-    component_provider_host([MyComponent], "exception")
+    component_provider_host([MyComponent], "provider")
