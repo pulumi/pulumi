52f98231aec9d92f2779245d4cc1548f83bd1d0f
Thomas Gummerer <t.gummerer@gmail.com>
2025-04-03 15:48:23 +0200

    python: clean up after `Pack`
    `Pack` packs the SDK, and then copies it to a destination directory,
    from where it will actually be used.  However we forget to clean the
    original temp directory up, leaving it behind.  Fix that.

--- sdk/python/cmd/pulumi-language-python/main.go
+++ sdk/python/cmd/pulumi-language-python/main.go
@@ -336,6 +336,7 @@ func (host *pythonLanguageHost) Pack(ctx context.Context, req *pulumirpc.PackReq
 	if err != nil {
 		return nil, fmt.Errorf("create temporary directory: %w", err)
 	}
+	defer os.RemoveAll(tmp)
 	// We use [build](https://build.pypa.io/en/stable/) as the build frontend to
 	// pack the Python SDK. We install this in an isolated virtual environment
 	// to avoid conflicts with the user's environment.
