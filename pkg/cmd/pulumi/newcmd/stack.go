950a0c34784185418a10fb1c6bc36d98c6645456
caseyyh <casey@pulumi.com>
2025-04-17 15:13:16 -0700

    Build Stack Name

--- pkg/cmd/pulumi/newcmd/stack.go
+++ pkg/cmd/pulumi/newcmd/stack.go
@@ -22,6 +22,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/backend"
 	"github.com/pulumi/pulumi/pkg/v3/backend/display"
 	cmdStack "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/stack"
+	"github.com/pulumi/pulumi/pkg/v3/placeholder"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 
@@ -65,7 +66,7 @@ func PromptAndCreateStack(ctx context.Context, ws pkgWorkspace.Context, b backen
 	contract.Requiref(root != "", "root", "must not be empty")
 
 	if stack != "" {
-		stackName, err := buildStackName(stack)
+		stackName, err := buildStackName(ctx, b, stack)
 		if err != nil {
 			return nil, err
 		}
@@ -87,7 +88,7 @@ func PromptAndCreateStack(ctx context.Context, ws pkgWorkspace.Context, b backen
 		if err != nil {
 			return nil, err
 		}
-		formattedStackName, err := buildStackName(stackName)
+		formattedStackName, err := buildStackName(ctx, b, stackName)
 		if err != nil {
 			return nil, err
 		}
@@ -104,7 +105,7 @@ func PromptAndCreateStack(ctx context.Context, ws pkgWorkspace.Context, b backen
 	}
 }
 
-func buildStackName(stackName string) (string, error) {
+func buildStackName(ctx context.Context, b backend.Backend, stackName string) (string, error) {
 	// If we already have a slash (e.g. org/stack, or org/proj/stack) don't add the default org.
 	if strings.Contains(stackName, "/") {
 		return stackName, nil
@@ -112,7 +113,7 @@ func buildStackName(stackName string) (string, error) {
 
 	// We never have a project at the point of calling buildStackName (only called from new), so we just pass
 	// nil for the project and only check the global settings.
-	defaultOrg, err := pkgWorkspace.GetBackendConfigDefaultOrg(nil)
+	defaultOrg, err := placeholder.GetDefaultOrg(ctx, b, nil)
 	if err != nil {
 		return "", err
 	}
