pulumi/pulumi

commit 3ce33689867d5f981ec94c3366216e03f9d609d4

Add Handshake to analyzer plugins (#19328)
This adds the handshake method to analyzer plugins. This has a number of
advantages for passing data back and forth.

Firstly it expose a channel for analysers to report things back to the
engine, we don't currently make use of that but we _could_ similar to
provider plugins where they report if they understand new property types
(e.g. if we added sets or integers).

Secondly it allows the engine to attach to a running analyzer, again
we're not currnetly making use of that, but if the issue comes up it's
now easy to solve in the same way we do for providers.

Thirdly it's a well typed and documented list of things that are sent to
analyzer plugins rather than dealing with undocumented environment
variables set in engine code.

Fourthly it passes config as typed property maps rather than
`map[string]string` allowing us to cleanly pass things like `true` vs
`"true"`.

Fifthly it allows us to pass down directory information. Current
analyzer plugins always have to run in the analyzer directory rather
than the pulumi programs directory, which _every_ other plugin runs in.
Passing the directory information explicitly like this means new
analyzer plugins will be able to start in the pulumi program directory
but still find their directory if needed.

Sixthly this unblocks binary policy packs and using RunPlugin for policy
packs. We're not concerned about doing the former yet, but the latter is
a priority before doing dotnet/java/go support.

proto/pulumi/analyzer.proto
@@ -43,6 +43,45 @@ service Analyzer {
     rpc GetPluginInfo(google.protobuf.Empty) returns (PluginInfo) {}
     // Configure configures the analyzer, passing configuration properties for each policy.
     rpc Configure(ConfigureAnalyzerRequest) returns (google.protobuf.Empty) {}
+
+    // `Handshake` is the first call made by the engine to an analyzer. It is used to pass the engine's address to the
+    // analyzer so that it may establish its own connections back, and to establish protocol configuration that will be
+    // used to communicate between the two parties.
+    rpc Handshake(AnalyzerHandshakeRequest) returns (AnalyzerHandshakeResponse) {}
+}
+
+
+// `AnalyzerHandshakeRequest` is the type of requests sent as part of a [](pulumirpc.Analyzer.Handshake) call.
+message AnalyzerHandshakeRequest {
+    // The gRPC address of the engine handshaking with the analyzer. At a minimum, this address will expose an instance
+    // of the [](pulumirpc.Engine) service.
+    string engine_address = 1;
+
+    // A *root directory* where the analyzer's binary, `PulumiPolicy.yaml`, or other identifying source code is located.
+    // In the event that the analyzer is *not* being booted by the engine (e.g. in the case that the engine has been
+    // asked to attach to an existing running analyzer instance via a host/port number), this field will be empty.
+    optional string root_directory = 2;
+
+    // A *program directory* in which the analyzer should execute. This is generally a subdirectory of the root
+    // directory, though this is not required. In the event that the analyzer is *not* being booted by the engine (e.g.
+    // in the case that the engine has been asked to attach to an existing running analyzer instance via a host/port
+    // number), this field will be empty.
+    optional string program_directory = 3;
+
+    // The stack name being analyzed.
+    string stack = 4; 
+    // The project name of the stack being analyzed.
+    string project = 5;
+    // The organization name of the stack being analyzed.
+    string organization = 6;
+    // True if this is a preview/dry run.
+    bool dry_run = 7;
+    // The configuration of the stack being analyzed as a property map.
+    google.protobuf.Struct config = 8;
+}
+
+// `AnalyzerHandshakeResponse` is the type of responses sent by a [](pulumirpc.Analyzer.Handshake) call.
+message AnalyzerHandshakeResponse {
 }
 
 message AnalyzeRequest {

sdk/go/common/resource/plugin/analyzer_plugin.go
@@ -15,6 +15,7 @@
 package plugin
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -29,6 +30,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/structpb"
 
@@ -140,8 +142,47 @@ func NewPolicyAnalyzer(
 		}
 	}
 
+	handshake := func(
+		ctx context.Context, bin string, prefix string, conn *grpc.ClientConn,
+	) (*pulumirpc.AnalyzerHandshakeResponse, error) {
+		// For analyzers the root directory and program directory are the location of the PulumiPolicy.yaml _not_ the
+		// location of the shim plugin.
+		dir := policyPackPath
+		client := pulumirpc.NewAnalyzerClient(conn)
+
+		mconfig, err := MarshalProperties(resource.ToResourcePropertyMap(opts.Config),
+			MarshalOptions{KeepSecrets: true})
+		if err != nil {
+			return nil, fmt.Errorf("marshalling config: %w", err)
+		}
+
+		res, err := client.Handshake(ctx, &pulumirpc.AnalyzerHandshakeRequest{
+			EngineAddress:    host.ServerAddr(),
+			RootDirectory:    &dir,
+			ProgramDirectory: &dir,
+
+			Stack:        opts.Stack,
+			Project:      opts.Project,
+			Organization: opts.Organization,
+
+			DryRun: opts.DryRun,
+			Config: mconfig,
+		})
+		if err != nil {
+			status, ok := status.FromError(err)
+			if ok && status.Code() == codes.Unimplemented {
+				// If the provider doesn't implement Handshake, that's fine -- we'll fall back to existing behavior.
+				logging.V(7).Infof("Handshake: not supported by '%v'", bin)
+				return nil, nil
+			}
+		}
+
+		logging.V(7).Infof("Handshake: success [%v]", bin)
+		return res, nil
+	}
+
 	plug, _, err := newPlugin(ctx, pwd, pluginPath, fmt.Sprintf("%v (analyzer)", name),
-		apitype.AnalyzerPlugin, args, env, testConnection,
+		apitype.AnalyzerPlugin, args, env, handshake,
 		analyzerPluginDialOptions(ctx, fmt.Sprintf("%v", name)))
 	if err != nil {
 		// The original error might have been wrapped before being returned from newPlugin. So we look for

sdk/go/common/resource/plugin/testdata/analyzer/main.go
@@ -1,14 +1,59 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"reflect"
 
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/rpcutil"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 	"google.golang.org/grpc"
 )
+
+type analyzer struct {
+	pulumirpc.UnimplementedAnalyzerServer
+}
+
+func (a *analyzer) Handshake(ctx context.Context, req *pulumirpc.AnalyzerHandshakeRequest) (*pulumirpc.AnalyzerHandshakeResponse, error) {
+	if req.Stack != "test-stack" {
+		return nil, fmt.Errorf("expected stack to be test-stack, got %s", req.Stack)
+	}
+	if req.Project != "test-project" {
+		return nil, fmt.Errorf("expected project to be test-project, got %s", req.Project)
+	}
+	if req.Organization != "test-org" {
+		return nil, fmt.Errorf("expected organization to be test-org, got %s", req.Organization)
+	}
+	if !req.DryRun {
+		return nil, fmt.Errorf("expected dry run to be true, got false")
+	}
+
+	actualConfig, err := plugin.UnmarshalProperties(req.Config, plugin.MarshalOptions{
+		KeepSecrets: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	expectedConfig := resource.PropertyMap{
+		"test-project:bool":   resource.NewBoolProperty(true),
+		"test-project:float":  resource.NewNumberProperty(1.5),
+		"test-project:string": resource.NewStringProperty("hello"),
+		"test-project:obj": resource.NewObjectProperty(resource.PropertyMap{
+			"key": resource.NewStringProperty("value"),
+		}),
+	}
+
+	if !actualConfig.DeepEquals(expectedConfig) {
+		return nil, fmt.Errorf("expected config to be %v, got %v", expectedConfig, actualConfig)
+	}
+
+	return &pulumirpc.AnalyzerHandshakeResponse{}, nil
+}
 
 func main() {
 	// Bootup a policy plugin but first assert that the config is what we expect
@@ -34,7 +79,7 @@ func main() {
 	handle, err := rpcutil.ServeWithOptions(rpcutil.ServeOptions{
 		Cancel: cancelChannel,
 		Init: func(srv *grpc.Server) error {
-			// pulumirpc.RegisterResourceProviderServer(srv, prov)
+			pulumirpc.RegisterAnalyzerServer(srv, &analyzer{})
 			return nil
 		},
 		Options: rpcutil.OpenTracingServerInterceptorOptions(nil),

sdk/nodejs/proto/analyzer_grpc_pb.d.ts
@@ -17,6 +17,7 @@ interface IAnalyzerService extends grpc.ServiceDefinition<grpc.UntypedServiceImp
     getAnalyzerInfo: IAnalyzerService_IGetAnalyzerInfo;
     getPluginInfo: IAnalyzerService_IGetPluginInfo;
     configure: IAnalyzerService_IConfigure;
+    handshake: IAnalyzerService_IHandshake;
 }
 
 interface IAnalyzerService_IAnalyze extends grpc.MethodDefinition<pulumi_analyzer_pb.AnalyzeRequest, pulumi_analyzer_pb.AnalyzeResponse> {
@@ -73,6 +74,15 @@ interface IAnalyzerService_IConfigure extends grpc.MethodDefinition<pulumi_analy
     responseSerialize: grpc.serialize<google_protobuf_empty_pb.Empty>;
     responseDeserialize: grpc.deserialize<google_protobuf_empty_pb.Empty>;
 }
+interface IAnalyzerService_IHandshake extends grpc.MethodDefinition<pulumi_analyzer_pb.AnalyzerHandshakeRequest, pulumi_analyzer_pb.AnalyzerHandshakeResponse> {
+    path: "/pulumirpc.Analyzer/Handshake";
+    requestStream: false;
+    responseStream: false;
+    requestSerialize: grpc.serialize<pulumi_analyzer_pb.AnalyzerHandshakeRequest>;
+    requestDeserialize: grpc.deserialize<pulumi_analyzer_pb.AnalyzerHandshakeRequest>;
+    responseSerialize: grpc.serialize<pulumi_analyzer_pb.AnalyzerHandshakeResponse>;
+    responseDeserialize: grpc.deserialize<pulumi_analyzer_pb.AnalyzerHandshakeResponse>;
+}
 
 export const AnalyzerService: IAnalyzerService;
 
@@ -83,6 +93,7 @@ export interface IAnalyzerServer extends grpc.UntypedServiceImplementation {
     getAnalyzerInfo: grpc.handleUnaryCall<google_protobuf_empty_pb.Empty, pulumi_analyzer_pb.AnalyzerInfo>;
     getPluginInfo: grpc.handleUnaryCall<google_protobuf_empty_pb.Empty, pulumi_plugin_pb.PluginInfo>;
     configure: grpc.handleUnaryCall<pulumi_analyzer_pb.ConfigureAnalyzerRequest, google_protobuf_empty_pb.Empty>;
+    handshake: grpc.handleUnaryCall<pulumi_analyzer_pb.AnalyzerHandshakeRequest, pulumi_analyzer_pb.AnalyzerHandshakeResponse>;
 }
 
 export interface IAnalyzerClient {
@@ -104,6 +115,9 @@ export interface IAnalyzerClient {
     configure(request: pulumi_analyzer_pb.ConfigureAnalyzerRequest, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
     configure(request: pulumi_analyzer_pb.ConfigureAnalyzerRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
     configure(request: pulumi_analyzer_pb.ConfigureAnalyzerRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+    handshake(request: pulumi_analyzer_pb.AnalyzerHandshakeRequest, callback: (error: grpc.ServiceError | null, response: pulumi_analyzer_pb.AnalyzerHandshakeResponse) => void): grpc.ClientUnaryCall;
+    handshake(request: pulumi_analyzer_pb.AnalyzerHandshakeRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: pulumi_analyzer_pb.AnalyzerHandshakeResponse) => void): grpc.ClientUnaryCall;
+    handshake(request: pulumi_analyzer_pb.AnalyzerHandshakeRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: pulumi_analyzer_pb.AnalyzerHandshakeResponse) => void): grpc.ClientUnaryCall;
 }
 
 export class AnalyzerClient extends grpc.Client implements IAnalyzerClient {
@@ -126,4 +140,7 @@ export class AnalyzerClient extends grpc.Client implements IAnalyzerClient {
     public configure(request: pulumi_analyzer_pb.ConfigureAnalyzerRequest, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
     public configure(request: pulumi_analyzer_pb.ConfigureAnalyzerRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
     public configure(request: pulumi_analyzer_pb.ConfigureAnalyzerRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+    public handshake(request: pulumi_analyzer_pb.AnalyzerHandshakeRequest, callback: (error: grpc.ServiceError | null, response: pulumi_analyzer_pb.AnalyzerHandshakeResponse) => void): grpc.ClientUnaryCall;
+    public handshake(request: pulumi_analyzer_pb.AnalyzerHandshakeRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: pulumi_analyzer_pb.AnalyzerHandshakeResponse) => void): grpc.ClientUnaryCall;
+    public handshake(request: pulumi_analyzer_pb.AnalyzerHandshakeRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: pulumi_analyzer_pb.AnalyzerHandshakeResponse) => void): grpc.ClientUnaryCall;
 }

sdk/nodejs/proto/analyzer_grpc_pb.js
@@ -66,6 +66,28 @@ function deserialize_pulumirpc_AnalyzeStackRequest(buffer_arg) {
   return pulumi_analyzer_pb.AnalyzeStackRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_pulumirpc_AnalyzerHandshakeRequest(arg) {
+  if (!(arg instanceof pulumi_analyzer_pb.AnalyzerHandshakeRequest)) {
+    throw new Error('Expected argument of type pulumirpc.AnalyzerHandshakeRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_pulumirpc_AnalyzerHandshakeRequest(buffer_arg) {
+  return pulumi_analyzer_pb.AnalyzerHandshakeRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_pulumirpc_AnalyzerHandshakeResponse(arg) {
+  if (!(arg instanceof pulumi_analyzer_pb.AnalyzerHandshakeResponse)) {
+    throw new Error('Expected argument of type pulumirpc.AnalyzerHandshakeResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_pulumirpc_AnalyzerHandshakeResponse(buffer_arg) {
+  return pulumi_analyzer_pb.AnalyzerHandshakeResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 function serialize_pulumirpc_AnalyzerInfo(arg) {
   if (!(arg instanceof pulumi_analyzer_pb.AnalyzerInfo)) {
     throw new Error('Expected argument of type pulumirpc.AnalyzerInfo');
@@ -191,6 +213,20 @@ configure: {
     requestDeserialize: deserialize_pulumirpc_ConfigureAnalyzerRequest,
     responseSerialize: serialize_google_protobuf_Empty,
     responseDeserialize: deserialize_google_protobuf_Empty,
+  },
+  // `Handshake` is the first call made by the engine to an analyzer. It is used to pass the engine's address to the
+// analyzer so that it may establish its own connections back, and to establish protocol configuration that will be
+// used to communicate between the two parties.
+handshake: {
+    path: '/pulumirpc.Analyzer/Handshake',
+    requestStream: false,
+    responseStream: false,
+    requestType: pulumi_analyzer_pb.AnalyzerHandshakeRequest,
+    responseType: pulumi_analyzer_pb.AnalyzerHandshakeResponse,
+    requestSerialize: serialize_pulumirpc_AnalyzerHandshakeRequest,
+    requestDeserialize: deserialize_pulumirpc_AnalyzerHandshakeRequest,
+    responseSerialize: serialize_pulumirpc_AnalyzerHandshakeResponse,
+    responseDeserialize: deserialize_pulumirpc_AnalyzerHandshakeResponse,
   },
 };
 

sdk/nodejs/proto/analyzer_pb.d.ts
@@ -9,6 +9,73 @@ import * as pulumi_plugin_pb from "./plugin_pb";
 import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
 import * as google_protobuf_struct_pb from "google-protobuf/google/protobuf/struct_pb";
 
+export class AnalyzerHandshakeRequest extends jspb.Message { 
+    getEngineAddress(): string;
+    setEngineAddress(value: string): AnalyzerHandshakeRequest;
+
+    hasRootDirectory(): boolean;
+    clearRootDirectory(): void;
+    getRootDirectory(): string | undefined;
+    setRootDirectory(value: string): AnalyzerHandshakeRequest;
+
+    hasProgramDirectory(): boolean;
+    clearProgramDirectory(): void;
+    getProgramDirectory(): string | undefined;
+    setProgramDirectory(value: string): AnalyzerHandshakeRequest;
+    getStack(): string;
+    setStack(value: string): AnalyzerHandshakeRequest;
+    getProject(): string;
+    setProject(value: string): AnalyzerHandshakeRequest;
+    getOrganization(): string;
+    setOrganization(value: string): AnalyzerHandshakeRequest;
+    getDryRun(): boolean;
+    setDryRun(value: boolean): AnalyzerHandshakeRequest;
+
+    hasConfig(): boolean;
+    clearConfig(): void;
+    getConfig(): google_protobuf_struct_pb.Struct | undefined;
+    setConfig(value?: google_protobuf_struct_pb.Struct): AnalyzerHandshakeRequest;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): AnalyzerHandshakeRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: AnalyzerHandshakeRequest): AnalyzerHandshakeRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: AnalyzerHandshakeRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): AnalyzerHandshakeRequest;
+    static deserializeBinaryFromReader(message: AnalyzerHandshakeRequest, reader: jspb.BinaryReader): AnalyzerHandshakeRequest;
+}
+
+export namespace AnalyzerHandshakeRequest {
+    export type AsObject = {
+        engineAddress: string,
+        rootDirectory?: string,
+        programDirectory?: string,
+        stack: string,
+        project: string,
+        organization: string,
+        dryRun: boolean,
+        config?: google_protobuf_struct_pb.Struct.AsObject,
+    }
+}
+
+export class AnalyzerHandshakeResponse extends jspb.Message { 
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): AnalyzerHandshakeResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: AnalyzerHandshakeResponse): AnalyzerHandshakeResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: AnalyzerHandshakeResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): AnalyzerHandshakeResponse;
+    static deserializeBinaryFromReader(message: AnalyzerHandshakeResponse, reader: jspb.BinaryReader): AnalyzerHandshakeResponse;
+}
+
+export namespace AnalyzerHandshakeResponse {
+    export type AsObject = {
+    }
+}
+
 export class AnalyzeRequest extends jspb.Message { 
     getType(): string;
     setType(value: string): AnalyzeRequest;

commit edbfac09ce6da13104f5b7648a95d7b2d1ab4ec7

Add an integration test for policy publishing
We broke this in https://github.com/pulumi/pulumi/pull/19328 and fixed it in https://github.com/pulumi/pulumi/issues/19383.

The fix has a unit test. Go one step further and add an integration test that ensures we can publish (and remove) a policy from the live backend.

tests/integration/integration_test.go
@@ -25,6 +25,7 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -1372,6 +1373,37 @@ func TestPolicyPackNew(t *testing.T) {
 	require.Contains(t, stdout, "Finished creating virtual environment")
 	require.Contains(t, stdout, "Finished installing dependencies")
 	require.True(t, e.PathExists("venv"))
+}
+
+func TestPolicyPackPublish(t *testing.T) {
+	t.Parallel()
+	if os.Getenv("PULUMI_ACCESS_TOKEN") == "" {
+		t.Skipf("Skipping: PULUMI_ACCESS_TOKEN is not set")
+	}
+	testOrg := os.Getenv("PULUMI_TEST_ORG")
+	if testOrg == "" {
+		t.Skipf("Skipping: PULUMI_TEST_ORG is not set")
+	}
+
+	e := ptesting.NewEnvironment(t)
+	defer e.DeleteIfNotFailed()
+	e.RunCommand("pulumi", "policy", "new", "aws-typescript", "--force")
+	// Change the name of the policy in case `aws-typescript` is already published
+	// and so that we have a clear indication that this is coming from a test.
+	// We do our best to clean up the policy after the test completes;
+	name := fmt.Sprintf("test-policy-pack-publish-%d", time.Now().UnixNano())
+	policyIndexPath := filepath.Join(e.RootPath, "index.ts")
+	policyContent, err := os.ReadFile(policyIndexPath)
+	require.NoError(t, err)
+	newContent := strings.Replace(string(policyContent),
+		`new PolicyPack("aws-typescript"`,
+		fmt.Sprintf(`new PolicyPack("%s"`, name), 1)
+	err = os.WriteFile(policyIndexPath, []byte(newContent), 0644)
+	require.NoError(t, err)
+
+	e.RunCommand("pulumi", "policy", "publish")
+
+	e.RunCommand("pulumi", "policy", "rm", testOrg+"/"+name, "all", "--yes")
 }
 
 func TestPolicyPackInstallDependencies(t *testing.T) {