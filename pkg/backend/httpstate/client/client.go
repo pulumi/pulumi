4065be3d21e00c2e155a911c9acc5df8251b6e5f
Simon Howe <footless@gmail.com>
2025-05-05 20:06:52 +0200

    Remove debug helpers again

--- pkg/backend/httpstate/client/client.go
+++ pkg/backend/httpstate/client/client.go
@@ -25,7 +25,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os"
 	"path"
 	"regexp"
 	"strconv"
@@ -1443,13 +1442,6 @@ func (pc *Client) callCopilot(ctx context.Context, requestBody interface{}) (str
 	url := pc.apiURL + "/api/ai/chat/preview"
 	apiToken := string(pc.apiToken)
 
-	// TODO: Debug helpers. Remove before merging.
-	copilotURL := os.Getenv("DEBUG_PULUMI_COPILOT_URL")
-	if copilotURL != "" {
-		url = copilotURL + "/api/ai/chat/preview"
-		apiToken = os.Getenv("DEBUG_PULUMI_COPILOT_TOKEN")
-	}
-
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(jsonData))
 	if err != nil {
 		return "", fmt.Errorf("creating request: %w", err)
@@ -1458,9 +1450,6 @@ func (pc *Client) callCopilot(ctx context.Context, requestBody interface{}) (str
 	req.Header.Set("X-Pulumi-Source", "Pulumi CLI")
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "token "+apiToken)
-
-	// TODO: Debug helpers. Remove before merging.
-	req.Header.Set("X-Pulumi-Origin", "api.pulumi.com")
 
 	resp, err := pc.do(ctx, req)
 	if err != nil {
