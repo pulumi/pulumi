f4cd8c2a378ee90a7afa9836b947d0cdb1c74e2e
Fraser Waters <fraser@pulumi.com>
2025-05-22 10:37:20 +0100

    Make the language tester a host itself

--- cmd/pulumi-test-language/interface.go
+++ cmd/pulumi-test-language/interface.go
@@ -58,10 +58,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	pbempty "google.golang.org/protobuf/types/known/emptypb"
 )
 
 type LanguageTestServer interface {
 	testingrpc.LanguageTestServer
+	pulumirpc.EngineServer
 
 	// Address returns the address at which the test RPC server may be reached.
 	Address() string
@@ -93,6 +95,7 @@ func Start(ctx context.Context) (LanguageTestServer, error) {
 	port, done, err := rpcutil.Serve(0, server.cancel, []func(*grpc.Server) error{
 		func(srv *grpc.Server) error {
 			testingrpc.RegisterLanguageTestServer(srv, server)
+			pulumirpc.RegisterEngineServer(srv, server)
 			return nil
 		},
 	}, nil)
@@ -109,6 +112,7 @@ func Start(ctx context.Context) (LanguageTestServer, error) {
 // languageTestServer is the server side of the language testing RPC machinery.
 type languageTestServer struct {
 	testingrpc.UnsafeLanguageTestServer
+	pulumirpc.UnimplementedEngineServer
 
 	ctx    context.Context
 	cancel chan bool
@@ -122,6 +126,11 @@ type languageTestServer struct {
 
 	// Used by _bad snapshot_ tests to disable snapshot writing.
 	DisableSnapshotWriting bool
+
+	logLock   sync.Mutex
+	logRepeat int
+	// Used by the Log method to track the last message logged, this is so we can elide duplicate messages.
+	previousMessage string
 }
 
 func (eng *languageTestServer) Address() string {
@@ -134,6 +143,59 @@ func (eng *languageTestServer) Cancel() {
 
 func (eng *languageTestServer) Done() error {
 	return <-eng.done
+}
+
+func (eng *languageTestServer) Log(_ context.Context, req *pulumirpc.LogRequest) (*pbempty.Empty, error) {
+	eng.logLock.Lock()
+	defer eng.logLock.Unlock()
+
+	var sev diag.Severity
+	switch req.Severity {
+	case pulumirpc.LogSeverity_DEBUG:
+		sev = diag.Debug
+	case pulumirpc.LogSeverity_INFO:
+		sev = diag.Info
+	case pulumirpc.LogSeverity_WARNING:
+		sev = diag.Warning
+	case pulumirpc.LogSeverity_ERROR:
+		sev = diag.Error
+	default:
+		return nil, fmt.Errorf("Unrecognized logging severity: %v", req.Severity)
+	}
+
+	message := req.Message
+	if os.Getenv("PULUMI_LANGUAGE_TEST_SHOW_FULL_OUTPUT") != "true" {
+		// Cut down logs so they don't overwhelm the test output
+		if len(message) > 2048 {
+			message = message[:2048] + "... (truncated, run with PULUMI_LANGUAGE_TEST_SHOW_FULL_OUTPUT=true to see full logs))"
+		}
+	}
+
+	if eng.previousMessage == message {
+		eng.logRepeat++
+		return &pbempty.Empty{}, nil
+	}
+
+	if eng.logRepeat > 1 {
+		_, err := fmt.Fprintf(os.Stderr, "Last message repeated %d times\n", eng.logRepeat)
+		if err != nil {
+			return nil, err
+		}
+	}
+	eng.logRepeat = 1
+	eng.previousMessage = message
+
+	var err error
+	if req.StreamId != 0 {
+		_, err = fmt.Fprintf(os.Stderr, "(%d) %s[%s]: %s", req.StreamId, sev, req.Urn, message)
+	} else {
+		_, err = fmt.Fprintf(os.Stderr, "%s[%s]: %s", sev, req.Urn, message)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &pbempty.Empty{}, nil
 }
 
 // A providerLoader is a schema loader that loads schemas from a given set of providers.
