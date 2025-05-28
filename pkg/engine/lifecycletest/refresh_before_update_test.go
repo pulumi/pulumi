f169289b42a46036a529a23d698df7e57134cba9
Justin Van Patten <jvp@justinvp.com>
2025-05-28 11:33:03 -0700

    Add same and refresh tests

--- pkg/engine/lifecycletest/refresh_before_update_test.go
+++ pkg/engine/lifecycletest/refresh_before_update_test.go
@@ -16,6 +16,7 @@ package lifecycletest
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/blang/semver"
@@ -31,6 +32,8 @@ import (
 
 func TestRefreshBeforeUpdate(t *testing.T) {
 	t.Parallel()
+
+	var readToken int
 
 	loaders := []*deploytest.ProviderLoader{
 		deploytest.NewProviderLoader("pkgA", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
@@ -61,8 +64,8 @@ func TestRefreshBeforeUpdate(t *testing.T) {
 					return plugin.DiffResponse{Changes: plugin.DiffSome}, nil
 				},
 				UpdateF: func(_ context.Context, req plugin.UpdateRequest) (plugin.UpdateResponse, error) {
-					assert.Equal(t, "<FRESH-INPUT>", req.OldInputs["input"].StringValue())
-					assert.Equal(t, "<FRESH-RESULT>", req.OldOutputs["result"].StringValue())
+					assert.Equal(t, fmt.Sprintf("<FRESH-INPUT-%d>", readToken), req.OldInputs["input"].StringValue())
+					assert.Equal(t, fmt.Sprintf("<FRESH-RESULT-%d>", readToken), req.OldOutputs["result"].StringValue())
 
 					props := req.NewInputs.Copy()
 					props["result"] = props["input"]
@@ -73,9 +76,10 @@ func TestRefreshBeforeUpdate(t *testing.T) {
 				},
 				ReadF: func(_ context.Context, req plugin.ReadRequest) (plugin.ReadResponse, error) {
 					inputs := req.Inputs.Copy()
-					inputs["input"] = resource.NewStringProperty("<FRESH-INPUT>")
+					inputs["input"] = resource.NewProperty(fmt.Sprintf("<FRESH-INPUT-%d>", readToken))
 					props := req.State.Copy()
-					props["result"] = resource.NewStringProperty("<FRESH-RESULT>")
+					props["input"] = inputs["input"]
+					props["result"] = resource.NewProperty(fmt.Sprintf("<FRESH-RESULT-%d>", readToken))
 					return plugin.ReadResponse{
 						Status: resource.StatusOK,
 						ReadResult: plugin.ReadResult{
@@ -95,7 +99,10 @@ func TestRefreshBeforeUpdate(t *testing.T) {
 	}
 
 	programF := deploytest.NewLanguageRuntimeF(func(_ plugin.RunInfo, monitor *deploytest.ResourceMonitor) error {
-		_, err := monitor.RegisterResource("pkgA:m:typA", "resA", true, deploytest.ResourceOptions{
+		_, err := monitor.RegisterResource("pulumi:pulumi:Stack", "test", false)
+		assert.NoError(t, err)
+
+		_, err = monitor.RegisterResource("pkgA:m:typA", "resA", true, deploytest.ResourceOptions{
 			Inputs: inputs,
 		})
 		assert.NoError(t, err)
@@ -114,12 +121,18 @@ func TestRefreshBeforeUpdate(t *testing.T) {
 	// First update.
 	p.Steps = []lt.TestStep{{Op: Update}}
 	snap := p.Run(t, nil)
-	assert.Len(t, snap.Resources, 2)
-	assert.Equal(t, "default", snap.Resources[0].URN.Name())
-	assert.Equal(t, "resA", snap.Resources[1].URN.Name())
-	assert.Equal(t, "value-1", snap.Resources[1].Inputs["input"].StringValue())
-	assert.Equal(t, "value-1", snap.Resources[1].Outputs["result"].StringValue())
-	assert.True(t, snap.Resources[1].RefreshBeforeUpdate)
+	assert.Len(t, snap.Resources, 3)
+	assert.Equal(t, tokens.Type("pulumi:pulumi:Stack"), snap.Resources[0].URN.Type())
+	assert.Equal(t, "default", snap.Resources[1].URN.Name())
+	assert.Equal(t, "resA", snap.Resources[2].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("value-1"),
+	}, snap.Resources[2].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("value-1"),
+		"result": resource.NewProperty("value-1"),
+	}, snap.Resources[2].Outputs)
+	assert.True(t, snap.Resources[2].RefreshBeforeUpdate)
 
 	// Second update.
 	inputs = resource.PropertyMap{
@@ -127,25 +140,99 @@ func TestRefreshBeforeUpdate(t *testing.T) {
 	}
 	p.Steps = []lt.TestStep{{Op: Update}}
 	snap = p.Run(t, snap)
-	assert.Len(t, snap.Resources, 2)
-	assert.Equal(t, "default", snap.Resources[0].URN.Name())
-	assert.Equal(t, "resA", snap.Resources[1].URN.Name())
-	assert.Equal(t, "value-2", snap.Resources[1].Inputs["input"].StringValue())
-	assert.Equal(t, "value-2", snap.Resources[1].Outputs["result"].StringValue())
-	assert.True(t, snap.Resources[1].RefreshBeforeUpdate)
+	assert.Len(t, snap.Resources, 3)
+	assert.Equal(t, tokens.Type("pulumi:pulumi:Stack"), snap.Resources[0].URN.Type())
+	assert.Equal(t, "default", snap.Resources[1].URN.Name())
+	assert.Equal(t, "resA", snap.Resources[2].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("value-2"),
+	}, snap.Resources[2].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("value-2"),
+		"result": resource.NewProperty("value-2"),
+	}, snap.Resources[2].Outputs)
+	assert.True(t, snap.Resources[2].RefreshBeforeUpdate)
 
-	// Fresh import with new snapshot.
+	// Third update (same).
+	readToken++
+	p.Steps = []lt.TestStep{{Op: Update}}
+	snap = p.Run(t, snap)
+	assert.Len(t, snap.Resources, 3)
+	assert.Equal(t, tokens.Type("pulumi:pulumi:Stack"), snap.Resources[0].URN.Type())
+	assert.Equal(t, "default", snap.Resources[1].URN.Name())
+	assert.Equal(t, "resA", snap.Resources[2].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("value-2"),
+	}, snap.Resources[2].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("value-2"),
+		"result": resource.NewProperty("value-2"),
+	}, snap.Resources[2].Outputs)
+	assert.True(t, snap.Resources[2].RefreshBeforeUpdate)
+
+	// Fourth update with refresh (same).
+	readToken++
+	p.Steps = []lt.TestStep{{Op: Update}}
+	p.Options.Refresh = true
+	snap = p.Run(t, snap)
+	assert.Len(t, snap.Resources, 3)
+	assert.Equal(t, tokens.Type("pulumi:pulumi:Stack"), snap.Resources[0].URN.Type())
+	assert.Equal(t, "default", snap.Resources[1].URN.Name())
+	assert.Equal(t, "resA", snap.Resources[2].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("value-2"),
+	}, snap.Resources[2].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("value-2"),
+		"result": resource.NewProperty("value-2"),
+	}, snap.Resources[2].Outputs)
+	assert.True(t, snap.Resources[2].RefreshBeforeUpdate)
+
+	// Refresh.
+	readToken++
+	p.Steps = []lt.TestStep{{Op: Refresh}}
+	p.Options.Refresh = false
+	snap = p.Run(t, snap)
+	assert.Len(t, snap.Resources, 3)
+	assert.Equal(t, tokens.Type("pulumi:pulumi:Stack"), snap.Resources[0].URN.Type())
+	assert.Equal(t, "default", snap.Resources[1].URN.Name())
+	assert.Equal(t, "resA", snap.Resources[2].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("<FRESH-INPUT-3>"),
+	}, snap.Resources[2].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("<FRESH-INPUT-3>"),
+		"result": resource.NewProperty("<FRESH-RESULT-3>"),
+	}, snap.Resources[2].Outputs)
+	assert.True(t, snap.Resources[2].RefreshBeforeUpdate)
+
+	// Import.
+	readToken = 42
 	p.Steps = []lt.TestStep{{Op: lt.ImportOp([]deploy.Import{{
 		Type: "pkgA:m:typA",
 		Name: "resB",
 		ID:   "imported-id",
 	}})}}
-	snap = p.Run(t, nil)
-	assert.Len(t, snap.Resources, 3)
+	snap = p.Run(t, snap)
+	assert.Len(t, snap.Resources, 4)
 	assert.Equal(t, tokens.Type("pulumi:pulumi:Stack"), snap.Resources[0].URN.Type())
 	assert.Equal(t, "default", snap.Resources[1].URN.Name())
-	assert.Equal(t, "resB", snap.Resources[2].URN.Name())
-	assert.Equal(t, "<FRESH-INPUT>", snap.Resources[2].Inputs["input"].StringValue())
-	assert.Equal(t, "<FRESH-RESULT>", snap.Resources[2].Outputs["result"].StringValue())
+	assert.Equal(t, "resA", snap.Resources[2].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("<FRESH-INPUT-3>"),
+	}, snap.Resources[2].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("<FRESH-INPUT-3>"),
+		"result": resource.NewProperty("<FRESH-RESULT-3>"),
+	}, snap.Resources[2].Outputs)
 	assert.True(t, snap.Resources[2].RefreshBeforeUpdate)
+	assert.Equal(t, "resB", snap.Resources[3].URN.Name())
+	assert.Equal(t, resource.PropertyMap{
+		"input": resource.NewProperty("<FRESH-INPUT-42>"),
+	}, snap.Resources[3].Inputs)
+	assert.Equal(t, resource.PropertyMap{
+		"input":  resource.NewProperty("<FRESH-INPUT-42>"),
+		"result": resource.NewProperty("<FRESH-RESULT-42>"),
+	}, snap.Resources[3].Outputs)
+	assert.True(t, snap.Resources[3].RefreshBeforeUpdate)
 }
