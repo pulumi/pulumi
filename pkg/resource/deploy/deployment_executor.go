b7623152cd6bc17a78a66c5a2aea3ee3c7133b0e
Thomas Gummerer <t.gummerer@gmail.com>
2025-05-29 17:02:47 +0200

    implement journaling in the engine

--- pkg/resource/deploy/deployment_executor.go
+++ pkg/resource/deploy/deployment_executor.go
@@ -150,6 +150,12 @@ func (ex *deploymentExecutor) Execute(callerCtx context.Context) (_ *Plan, err e
 
 	// Close the deployment when we're finished.
 	defer contract.IgnoreClose(ex.deployment)
+	if ex.deployment.rebase && ex.deployment.events != nil {
+		err := ex.deployment.events.OnRebase(ex.deployment.prev)
+		if err != nil {
+			return nil, result.BailErrorf("failed to rebase deployment: %v", err)
+		}
+	}
 
 	// If this deployment is an import, run the imports and exit.
 	if ex.deployment.isImport {
