b8dbfe695478e72bc40e04386db708438118544f
Brian Nutt <brian.nutt@snowflake.com>
2025-06-24 17:17:39 -0400

    Support runProgram for preview/up in auto sdk (#19729)
    https://github.com/pulumi/pulumi/pull/19218 introduces runProgram to the
    automation api, but only for destroy/preview.
    https://github.com/pulumi/pulumi/pull/19667 introduces support to feed
    runProgram into pulumi up in the auto sdk.
    https://github.com/pulumi/pulumi/pull/19449/files already added support
    for preview/up.
    
    The following change adds the similar support for preview/up in the sdk
    for go/nodejs/python.

--- sdk/nodejs/automation/stack.ts
+++ sdk/nodejs/automation/stack.ts
@@ -229,6 +229,13 @@ Event: ${line}\n${e.toString()}`);
             if (opts.attachDebugger) {
                 args.push("--attach-debugger");
             }
+            if (opts.runProgram !== undefined) {
+                if (opts.runProgram) {
+                    args.push("--run-program=true");
+                } else {
+                    args.push("--run-program=false");
+                }
+            }
             applyGlobalOpts(opts, args);
         }
 
@@ -375,6 +382,13 @@ Event: ${line}\n${e.toString()}`);
             }
             if (opts.attachDebugger) {
                 args.push("--attach-debugger");
+            }
+            if (opts.runProgram !== undefined) {
+                if (opts.runProgram) {
+                    args.push("--run-program=true");
+                } else {
+                    args.push("--run-program=false");
+                }
             }
             applyGlobalOpts(opts, args);
         }
@@ -1485,6 +1499,11 @@ export interface UpOptions extends GlobalOpts {
      * A signal to abort an ongoing operation.
      */
     signal?: AbortSignal;
+
+    /**
+     * Run the program in the workspace to perform the refresh.
+     */
+    runProgram?: boolean;
 }
 
 /**
@@ -1590,6 +1609,11 @@ export interface PreviewOptions extends GlobalOpts {
      * A signal to abort an ongoing operation.
      */
     signal?: AbortSignal;
+
+    /**
+     * Run the program in the workspace to perform the refresh.
+     */
+    runProgram?: boolean;
 }
 
 /**
