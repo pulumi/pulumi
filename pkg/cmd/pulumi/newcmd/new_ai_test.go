pulumi/pulumi

commit 1eadd23e8f1614fab681115d34c73bc870eff46f

Fetch Org-wide Project Templates from `app.pulumi.com` (#18627)
This PR allows `pulumi new` to fetch org-wide templates from
`app.pulumi.com` when they
are enabled. Part of
https://github.com/pulumi/pulumi-service/issues/25732.

Conceptually, there are 2 parts to this PR:

- Wraps `workspace.RetrieveTemplates` in a new API specific for `cmd`:

    ```go
    // Create a new [Template] [Source] associated with a given [Scope].
    func New(
    	ctx context.Context, templateNamePathOrURL string, scope Scope,
    	templateKind workspace.TemplateKind, interactive bool,
    ) *Source { ... }
    ```

This is necessary because `workspace.RetrieveTemplates` is in `sdk`:
`"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"`. The new
template download functionality needs to depend on various backend
features such as `cmdBackend
"github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/backend"`, which are
obviously in `pkg`.

The new `cmdTemplate.Template` abstraction allows for templates that are
not present in the file system, such as those sourced by `(*Client)
ListOrgTemplates`.

- Adjusts the template chooser and the rest of `cmd` to use the new
`cmdTemplates.{Source,Template}` wrapper instead of
`workspace.Template`. This is the mechanism that realizes the next two
changes.

pkg/cmd/pulumi/newcmd/new.go
@@ -18,9 +18,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
-	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -38,6 +37,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/backend/state"
 	cmdBackend "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/backend"
 	cmdStack "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/stack"
+	cmdTemplates "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/templates"
 	"github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/ui"
 	"github.com/pulumi/pulumi/pkg/v3/engine"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
@@ -56,7 +56,7 @@ import (
 type promptForValueFunc func(yes bool, valueType string, defaultValue string, secret bool,
 	isValidFn func(value string) error, opts display.Options) (string, error)
 
-type chooseTemplateFunc func(templates []workspace.Template, opts display.Options) (workspace.Template, error)
+type chooseTemplateFunc func(templates []cmdTemplates.Template, opts display.Options) (cmdTemplates.Template, error)
 
 type runtimeOptionsFunc func(ctx *plugin.Context, info *workspace.ProjectRuntimeInfo, main string,
 	opts display.Options, yes, interactive bool, prompt promptForValueFunc) (map[string]interface{}, error)
@@ -157,6 +157,15 @@ func runNew(ctx context.Context, args newArgs) error {
 		}
 	}
 
+	// Retrieve the template repo.
+	scope := cmdTemplates.ScopeAll
+	if args.offline {
+		scope = cmdTemplates.ScopeLocal
+	}
+	templateSource := cmdTemplates.New(ctx,
+		args.templateNameOrURL, scope, workspace.TemplateKindPulumiProject, args.interactive)
+	defer func() { contract.IgnoreError(templateSource.Close()) }()
+
 	if args.templateNameOrURL == "" {
 		// Try to read the current project
 		project, _, err := ws.ReadProject()
@@ -197,44 +206,29 @@ func runNew(ctx context.Context, args newArgs) error {
 			args.templateNameOrURL = conversationURL
 		}
 	}
-	// Retrieve the template repo.
-	var repo workspace.TemplateRepository
-	repo, err = workspace.RetrieveTemplates(
-		ctx, args.templateNameOrURL, args.offline, workspace.TemplateKindPulumiProject)
-	if err != nil {
-		// Bail on all errors unless its a 401 from a Pulumi Cloud backend...
-		if !errors.Is(err, workspace.ErrPulumiCloudUnauthorized) {
-			return err
-		}
-
-		// ...If the request has 401'd AND we've identified the backend as being a Pulumi Cloud instance, we can
-		// attempt to retrieve the template using the user's Pulumi Cloud credentials.
-		repo, err = retrievePrivatePulumiCloudTemplate(args.templateNameOrURL)
-		if err != nil {
-			return fmt.Errorf("retrieving private pulumi cloud template: %w", err)
-		}
-	}
-
-	defer func() {
-		contract.IgnoreError(repo.Delete())
-	}()
 
 	// List the templates from the repo.
-	templates, err := repo.Templates()
+	templates, err := templateSource.Templates()
 	if err != nil {
 		return err
 	}
 
-	var template workspace.Template
+	var cmdTemplate cmdTemplates.Template
 	if len(templates) == 0 {
 		return errors.New("no templates")
 	} else if len(templates) == 1 {
-		template = templates[0]
+		cmdTemplate = templates[0]
 	} else {
-		if template, err = args.chooseTemplate(templates, opts); err != nil {
+		if cmdTemplate, err = args.chooseTemplate(templates, opts); err != nil {
 			return err
 		}
 	}
+
+	template, err := cmdTemplate.Download(ctx)
+	if err != nil {
+		return err
+	}
+
 	if template.Errored() {
 		return fmt.Errorf("template '%s' is currently broken: %w", template.Name, template.Error)
 	}
@@ -491,49 +485,6 @@ func runNew(ctx context.Context, args newArgs) error {
 	return nil
 }
 
-// Retrieve a Private template from the given Pulumi Cloud URL **including an auth token for Pulumi Cloud**.
-//
-// workspace.GetAccount ensures the user has a valid session with the Pulumi Cloud backend.
-//   - If the user is not logged in, the login flow will be initiated.
-//   - If the user is not logged in and pulumi does not recognize the backend as a known workspace then
-//     the user will see an authentication error.
-func retrievePrivatePulumiCloudTemplate(templateURL string) (workspace.TemplateRepository, error) {
-	u, err := url.Parse(templateURL)
-	if err != nil {
-		return workspace.TemplateRepository{}, fmt.Errorf("parsing template URL: %w", err)
-	}
-	// Docs convention is to store the cloud URL with the protocol.
-	// e.g. `pulumi login https://api.pulumi.com` or `pulumi login https://api.acme.org`
-	templatePulumiCloudHost := "https://" + u.Host
-
-	account, err := workspace.GetAccount(templatePulumiCloudHost)
-	if err != nil {
-		return workspace.TemplateRepository{}, fmt.Errorf(
-			"looking up pulumi cloud backend %s: %w",
-			templatePulumiCloudHost,
-			err,
-		)
-	}
-
-	if account.AccessToken == "" {
-		return workspace.TemplateRepository{}, fmt.Errorf("no access token found for %s", templatePulumiCloudHost)
-	}
-
-	templateRepository, err := workspace.RetrieveZIPTemplates(templateURL, func(req *http.Request) {
-		req.Header.Set("Authorization", "token "+account.AccessToken)
-	})
-
-	if errors.Is(err, workspace.ErrPulumiCloudUnauthorized) {
-		return workspace.TemplateRepository{}, fmt.Errorf(
-			"unauthorized to access template at %s. You may not have access to this template or token may have expired",
-			templatePulumiCloudHost,
-		)
-	}
-
-	// Caller can handle other errors
-	return templateRepository, err
-}
-
 // isInteractive lets us force interactive mode for testing by setting PULUMI_TEST_INTERACTIVE.
 func isInteractive() bool {
 	test, ok := os.LookupEnv("PULUMI_TEST_INTERACTIVE")
@@ -551,16 +502,15 @@ func NewNewCmd() *cobra.Command {
 		promptRuntimeOptions: promptRuntimeOptions,
 	}
 
-	getTemplates := func(ctx context.Context) ([]workspace.Template, error) {
-		// Attempt to retrieve available templates.
-		repo, err := workspace.RetrieveTemplates(ctx, "", false /*offline*/, workspace.TemplateKindPulumiProject)
-		if err != nil {
-			logging.Warningf("could not retrieve templates: %v", err)
-			return []workspace.Template{}, err
+	getTemplates := func(ctx context.Context) ([]cmdTemplates.Template, io.Closer, error) {
+		scope := cmdTemplates.ScopeAll
+		if args.offline {
+			scope = cmdTemplates.ScopeLocal
 		}
-
-		// Get the list of templates.
-		return repo.Templates()
+		// Attempt to retrieve available templates.
+		s := cmdTemplates.New(ctx, "", scope, workspace.TemplateKindPulumiProject, args.interactive)
+		t, err := s.Templates()
+		return t, s, err
 	}
 
 	cmd := &cobra.Command{
@@ -622,16 +572,17 @@ func NewNewCmd() *cobra.Command {
 				args.templateNameOrURL = cliArgs[0]
 			}
 			if args.listTemplates {
-				templates, err := getTemplates(ctx)
+				templates, closer, err := getTemplates(ctx)
+				defer contract.IgnoreClose(closer)
 				if err != nil {
 					logging.Warningf("could not list templates: %v", err)
 					return err
 				}
 				available, _ := templatesToOptionArrayAndMap(templates)
-				fmt.Println("")
-				fmt.Println("Available Templates:")
+				fmt.Fprintln(cmd.OutOrStdout())
+				fmt.Fprintln(cmd.OutOrStdout(), "Available Templates:")
 				for _, t := range available {
-					fmt.Printf("  %s\n", t)
+					fmt.Fprintf(cmd.OutOrStdout(), "  %s\n", t)
 				}
 				return nil
 			}
@@ -651,7 +602,8 @@ func NewNewCmd() *cobra.Command {
 		// You'd think you could use cmd.Context() here but cobra doesn't set context on the cmd even though
 		// the parent help command has it. If https://github.com/spf13/cobra/issues/2240 gets fixed we can
 		// change back to cmd.Context() here.
-		templates, err := getTemplates(context.Background())
+		templates, closer, err := getTemplates(context.Background())
+		contract.IgnoreClose(closer)
 		if err != nil {
 			logging.Warningf("could not list templates: %v", err)
 			return
@@ -659,8 +611,8 @@ func NewNewCmd() *cobra.Command {
 
 		// If we have any templates, show them.
 		if len(templates) > 0 {
-			fmt.Println()
-			fmt.Printf("There are %d locally installed templates.\n", len(templates))
+			fmt.Fprintln(cmd.OutOrStdout())
+			fmt.Fprintf(cmd.OutOrStdout(), "There are %d available templates.\n", len(templates))
 		}
 	})
 

pkg/cmd/pulumi/newcmd/new_ai_test.go
@@ -30,6 +30,8 @@ func TestErrorsOnNonHTTPBackend(t *testing.T) {
 		DoesProjectExistF: func(ctx context.Context, org string, name string) (bool, error) {
 			return name == projectName, nil
 		},
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
 	})
 
 	testNewArgs := newArgs{

pkg/cmd/pulumi/newcmd/new_test.go
@@ -15,6 +15,7 @@
 package newcmd
 
 import (
+	"bytes"
 	"context"
 	"crypto/rand"
 	"encoding/hex"
@@ -28,6 +29,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/backend"
 	"github.com/pulumi/pulumi/pkg/v3/backend/display"
 	cmdBackend "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/backend"
+	cmdTemplates "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/templates"
 	"github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/ui"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/config"
@@ -252,6 +254,8 @@ func TestCreatingProjectWithExistingPromptedNameFails(t *testing.T) {
 		DoesProjectExistF: func(ctx context.Context, org string, name string) (bool, error) {
 			return name == projectName, nil
 		},
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
 	})
 
 	args := newArgs{
@@ -276,6 +280,8 @@ func TestGeneratingProjectWithExistingArgsSpecifiedNameSucceeds(t *testing.T) {
 		DoesProjectExistF: func(ctx context.Context, org string, name string) (bool, error) {
 			return true, nil
 		},
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
 	})
 
 	// Generate-only command is not creating any stacks, so don't bother with with the name uniqueness check.
@@ -307,6 +313,8 @@ func TestGeneratingProjectWithExistingPromptedNameSucceeds(t *testing.T) {
 		DoesProjectExistF: func(ctx context.Context, org string, name string) (bool, error) {
 			return true, nil
 		},
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
 	})
 
 	// Generate-only command is not creating any stacks, so don't bother with with the name uniqueness check.
@@ -403,6 +411,8 @@ func TestGeneratingProjectWithInvalidPromptedNameFails(t *testing.T) {
 		DoesProjectExistF: func(ctx context.Context, org string, name string) (bool, error) {
 			return true, nil
 		},
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
 	})
 
 	// Generate-only command is not creating any stacks, so don't bother with with the name uniqueness check.
@@ -744,14 +754,14 @@ func TestPulumiNewSetsTemplateTag(t *testing.T) {
 			chdir(t, tempdir)
 			uniqueProjectName := filepath.Base(tempdir) + "test"
 
-			chooseTemplateMock := func(templates []workspace.Template, opts display.Options,
-			) (workspace.Template, error) {
+			chooseTemplateMock := func(templates []cmdTemplates.Template, opts display.Options,
+			) (cmdTemplates.Template, error) {
 				for _, template := range templates {
-					if template.Name == tt.prompted {
+					if template.Name() == tt.prompted {
 						return template, nil
 					}
 				}
-				return workspace.Template{}, errors.New("template not found")
+				return nil, errors.New("template not found")
 			}
 
 			runtimeOptionsMock := func(ctx *plugin.Context, info *workspace.ProjectRuntimeInfo,
@@ -817,4 +827,230 @@ func TestPulumiPromptRuntimeOptions(t *testing.T) {
 	proj := loadProject(t, tempdir)
 	require.Equal(t, 1, len(proj.Runtime.Options()))
 	require.Equal(t, "someValue", proj.Runtime.Options()["someOption"])
+}
+
+//nolint:paralleltest // Sets a global mock backend
+func TestPulumiNewWithOrgTemplates(t *testing.T) {
+	mockBackendInstance(t, &backend.MockBackend{
+		SupportsTemplatesF: func() bool { return true },
+		CurrentUserF: func() (string, []string, *workspace.TokenInformation, error) {
+			return "fred", []string{"org1", "personal"}, nil, nil
+		},
+		ListTemplatesF: func(_ context.Context, orgName string) (apitype.ListOrgTemplatesResponse, error) {
+			switch orgName {
+			case "org1":
+				return apitype.ListOrgTemplatesResponse{
+					OrgHasTemplates: true,
+					Templates: map[string][]*apitype.PulumiTemplateRemote{
+						"github.com/example/foo": {
+							{
+								SourceName:  "Foo",
+								Name:        "template-1",
+								TemplateURL: "github.com/example/foo/template-1",
+								ProjectTemplate: apitype.ProjectTemplate{
+									DisplayName: "Display 1",
+									Description: "Describe 1",
+								},
+							},
+							{
+								SourceName:  "Foo",
+								Name:        "template-2",
+								TemplateURL: "github.com/example/foo/template-2",
+								ProjectTemplate: apitype.ProjectTemplate{
+									DisplayName: "Display 2",
+									Description: "Describe 2",
+								},
+							},
+						},
+					},
+				}, nil
+			case "personal":
+				return apitype.ListOrgTemplatesResponse{OrgHasTemplates: false}, nil
+			default:
+				return apitype.ListOrgTemplatesResponse{}, fmt.Errorf("unknown org %q", orgName)
+			}
+		},
+	})
+
+	newCmd := NewNewCmd()
+	var stdout, stderr bytes.Buffer
+	newCmd.SetOut(&stdout)
+	newCmd.SetErr(&stderr)
+	newCmd.SetArgs([]string{"--list-templates"})
+	err := newCmd.Execute()
+	require.NoError(t, err)
+
+	// Check that the normal prefix is still there
+	assert.Contains(t, stdout.String(), `
+Available Templates:
+`)
+	// Check that our org based templates are there
+	assert.Contains(t, stdout.String(), `
+  template-1                         Describe 1
+  template-2                         Describe 2
+`)
+
+	// Check that normal templates are there
+	assertTemplateContains(t, stdout.String(), `
+  aws-csharp                         A minimal AWS C# Pulumi program
+  aws-fsharp                         A minimal AWS F# Pulumi program
+  aws-go                             A minimal AWS Go Pulumi program
+  aws-java                           A minimal AWS Java Pulumi program
+  aws-javascript                     A minimal AWS JavaScript Pulumi program
+  aws-python                         A minimal AWS Python Pulumi program
+  aws-scala                          A minimal AWS Scala Pulumi program
+  aws-typescript                     A minimal AWS TypeScript Pulumi program
+  aws-visualbasic                    A minimal AWS VB.NET Pulumi program
+  aws-yaml                           A minimal AWS Pulumi YAML program
+`)
+	assert.Equal(t, "", stderr.String())
+}
+
+// TestPulumiNewWithoutPulumiAccessToken checks that we won't error if we run `pulumi new
+// --list-templates` without PULUMI_ACCESS_TOKEN set.
+//
+//nolint:paralleltest // Changes environmental variables
+func TestPulumiNewWithoutPulumiAccessToken(t *testing.T) {
+	t.Setenv("PULUMI_ACCESS_TOKEN", "")
+
+	newCmd := NewNewCmd()
+	var stdout, stderr bytes.Buffer
+	newCmd.SetOut(&stdout)
+	newCmd.SetErr(&stderr)
+	newCmd.SetArgs([]string{"--list-templates"})
+	err := newCmd.Execute()
+	require.NoError(t, err)
+
+	assert.Contains(t, stdout.String(), `
+Available Templates:
+`)
+	assertTemplateContains(t, stdout.String(), `
+  aws-csharp                              A minimal AWS C# Pulumi program
+  aws-fsharp                              A minimal AWS F# Pulumi program
+  aws-go                                  A minimal AWS Go Pulumi program
+  aws-java                                A minimal AWS Java Pulumi program
+  aws-javascript                          A minimal AWS JavaScript Pulumi program
+  aws-python                              A minimal AWS Python Pulumi program
+  aws-scala                               A minimal AWS Scala Pulumi program
+  aws-typescript                          A minimal AWS TypeScript Pulumi program
+  aws-visualbasic                         A minimal AWS VB.NET Pulumi program
+  aws-yaml                                A minimal AWS Pulumi YAML program
+`)
+	assert.Equal(t, "", stderr.String())
+}
+
+//nolint:paralleltest // Sets a global mock backend
+func TestPulumiNewWithoutTemplateSupport(t *testing.T) {
+	mockBackendInstance(t, &backend.MockBackend{
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
+	})
+
+	newCmd := NewNewCmd()
+	var stdout, stderr bytes.Buffer
+	newCmd.SetOut(&stdout)
+	newCmd.SetErr(&stderr)
+	newCmd.SetArgs([]string{"--list-templates"})
+	err := newCmd.Execute()
+	require.NoError(t, err)
+
+	// Check that normal templates are there
+	assert.Contains(t, stdout.String(), `
+Available Templates:
+  aiven-go                           A minimal Aiven Go Pulumi program
+`)
+	assert.Equal(t, "", stderr.String())
+}
+
+//nolint:paralleltest // Sets a global mock backend, changes the directory
+func TestPulumiNewOrgTemplate(t *testing.T) {
+	tempdir := tempProjectDir(t)
+	chdir(t, tempdir)
+	mockBackendInstance(t, &backend.MockBackend{
+		SupportsTemplatesF: func() bool { return true },
+		CurrentUserF: func() (string, []string, *workspace.TokenInformation, error) {
+			return "fred", []string{"org1"}, nil, nil
+		},
+		ListTemplatesF: func(_ context.Context, orgName string) (apitype.ListOrgTemplatesResponse, error) {
+			switch orgName {
+			case "org1":
+				return apitype.ListOrgTemplatesResponse{
+					OrgHasTemplates: true,
+					Templates: map[string][]*apitype.PulumiTemplateRemote{
+						"github.com/example/foo": {
+							{
+								SourceName:  "Foo",
+								Name:        "template-1",
+								TemplateURL: "https://github.com/example/foo/template-1",
+								ProjectTemplate: apitype.ProjectTemplate{
+									DisplayName: "Display 1",
+									Description: "Describe 1",
+								},
+							},
+						},
+					},
+				}, nil
+			default:
+				return apitype.ListOrgTemplatesResponse{}, fmt.Errorf("unknown org %q", orgName)
+			}
+		},
+		DownloadTemplateF: func(_ context.Context, orgName, templateSource string) (backend.TarReaderCloser, error) {
+			if orgName != "org1" {
+				return nil, fmt.Errorf("unknown org %q", orgName)
+			}
+			if templateSource != "https://github.com/example/foo/template-1" {
+				return nil, fmt.Errorf("unknown template source %q", templateSource)
+			}
+
+			return backend.MockTarReader{
+				"Pulumi.yaml": {Content: `name: ${PROJECT}
+description: ${DESCRIPTION}
+runtime: yaml
+template:
+  description: Describe 1
+
+resources:
+  # Create an AWS resource (S3 Bucket)
+  my-bucket:
+    type: aws:s3:BucketV2
+`},
+			}, nil
+		},
+	})
+
+	newCmd := NewNewCmd()
+	var stdout, stderr bytes.Buffer
+	newCmd.SetOut(&stdout)
+	newCmd.SetErr(&stderr)
+	newCmd.SetArgs([]string{"template-1", "--generate-only", "--yes"})
+	err := newCmd.Execute()
+	require.NoError(t, err)
+
+	proj := loadProject(t, tempdir)
+	require.Equal(t, "yaml", proj.Runtime.Name())
+}
+
+// Assert that actual contains the template rows show in expected.
+//
+// This parsing based comparison is necessary since raw string comparison is unstable
+// under insertion due to white-space changes.
+func assertTemplateContains(t *testing.T, actual, expected string) {
+	parse := func(stdout string) []struct{ name, description string } {
+		stdout = strings.TrimPrefix(stdout, `Available Templates:
+`)
+		lines := strings.Split(strings.TrimSpace(stdout), "\n")
+		out := make([]struct{ name, description string }, len(lines))
+		for i, l := range lines {
+			parts := strings.Fields(l)
+			out[i].name = parts[0]
+			out[i].description = strings.Join(parts[1:], " ")
+		}
+		return out
+	}
+
+	expectedP := parse(expected)
+	actualP := parse(actual)
+	for _, e := range expectedP {
+		assert.Contains(t, actualP, e)
+	}
 }

commit 015adff7d80089dcf9ec8e1944cc70e99e8dd55d

Fix the AI prompt flow for pulumi new (#19998)
Fix https://github.com/pulumi/pulumi/issues/19994 which is a regression
from https://github.com/pulumi/pulumi/pull/18627

The change is essentially moving the block which assigns
`args.templateNameOrURL` above the call to `cmdTemplates.New` which uses
`args.templateNameOrURL`. With the existing order,
`args.templateNameOrURL` set to the AI URL was ignored.

The rest of the change is refactoring the AI prompting to a separate
function and a test that mocks that function to a hard-coded value.

pkg/cmd/pulumi/newcmd/new.go
@@ -61,29 +61,33 @@ type chooseTemplateFunc func(templates []cmdTemplates.Template, opts display.Opt
 type runtimeOptionsFunc func(ctx *plugin.Context, info *workspace.ProjectRuntimeInfo, main string,
 	opts display.Options, yes, interactive bool, prompt promptForValueFunc) (map[string]interface{}, error)
 
+type promptForAIProjectURLFunc func(ctx context.Context,
+	ws pkgWorkspace.Context, args newArgs, opts display.Options) (string, error)
+
 type newArgs struct {
-	configArray          []string
-	configPath           bool
-	description          string
-	dir                  string
-	force                bool
-	generateOnly         bool
-	interactive          bool
-	name                 string
-	offline              bool
-	prompt               promptForValueFunc
-	promptRuntimeOptions runtimeOptionsFunc
-	chooseTemplate       chooseTemplateFunc
-	secretsProvider      string
-	stack                string
-	templateNameOrURL    string
-	yes                  bool
-	listTemplates        bool
-	aiPrompt             string
-	aiLanguage           httpstate.PulumiAILanguage
-	templateMode         bool
-	runtimeOptions       []string
-	remoteStackConfig    bool
+	configArray           []string
+	configPath            bool
+	description           string
+	dir                   string
+	force                 bool
+	generateOnly          bool
+	interactive           bool
+	name                  string
+	offline               bool
+	prompt                promptForValueFunc
+	promptRuntimeOptions  runtimeOptionsFunc
+	promptForAIProjectURL promptForAIProjectURLFunc
+	chooseTemplate        chooseTemplateFunc
+	secretsProvider       string
+	stack                 string
+	templateNameOrURL     string
+	yes                   bool
+	listTemplates         bool
+	aiPrompt              string
+	aiLanguage            httpstate.PulumiAILanguage
+	templateMode          bool
+	runtimeOptions        []string
+	remoteStackConfig     bool
 }
 
 func runNew(ctx context.Context, args newArgs) error {
@@ -158,6 +162,14 @@ func runNew(ctx context.Context, args newArgs) error {
 		}
 	}
 
+	if args.templateNameOrURL == "" && args.promptForAIProjectURL != nil {
+		aiURL, err := args.promptForAIProjectURL(ctx, ws, args, opts)
+		if err != nil {
+			return err
+		}
+		args.templateNameOrURL = aiURL
+	}
+
 	// Retrieve the template repo.
 	scope := cmdTemplates.ScopeAll
 	if args.offline {
@@ -166,47 +178,6 @@ func runNew(ctx context.Context, args newArgs) error {
 	templateSource := cmdTemplates.New(ctx,
 		args.templateNameOrURL, scope, workspace.TemplateKindPulumiProject)
 	defer func() { contract.IgnoreError(templateSource.Close()) }()
-
-	if args.templateNameOrURL == "" {
-		// Try to read the current project
-		project, _, err := ws.ReadProject()
-		if err != nil && !errors.Is(err, workspace.ErrProjectNotFound) {
-			return err
-		}
-
-		b, err := cmdBackend.CurrentBackend(ctx, ws, cmdBackend.DefaultLoginManager, project, opts)
-		if err != nil {
-			return err
-		}
-
-		var aiOrTemplate string
-		if shouldPromptForAIOrTemplate(args, b) {
-			aiOrTemplate, err = chooseWithAIOrTemplate(opts)
-		} else {
-			aiOrTemplate = deriveAIOrTemplate(args)
-		}
-		if err != nil {
-			return err
-		}
-		if aiOrTemplate == "ai" {
-			checkedBackend, ok := b.(httpstate.Backend)
-			if !ok {
-				if args.aiLanguage != "" && args.aiPrompt == "" {
-					return errors.New(
-						"--language is used to generate a template with Pulumi AI. " +
-							"Please log in to Pulumi Cloud to use Pulumi AI.\n" +
-							"Use --template to create a project from a template, " +
-							"or no flags to choose one interactively.")
-				}
-				return errors.New("please log in to Pulumi Cloud to use Pulumi AI")
-			}
-			conversationURL, err := runAINew(ctx, args, opts, checkedBackend)
-			if err != nil {
-				return err
-			}
-			args.templateNameOrURL = conversationURL
-		}
-	}
 
 	// List the templates from the repo.
 	templates, err := templateSource.Templates()
@@ -496,9 +467,10 @@ func isInteractive() bool {
 // NewNewCmd creates a New command with default dependencies.
 func NewNewCmd() *cobra.Command {
 	args := newArgs{
-		prompt:               ui.PromptForValue,
-		chooseTemplate:       ChooseTemplate,
-		promptRuntimeOptions: promptRuntimeOptions,
+		prompt:                ui.PromptForValue,
+		chooseTemplate:        ChooseTemplate,
+		promptRuntimeOptions:  promptRuntimeOptions,
+		promptForAIProjectURL: promptForAIProjectURL,
 	}
 
 	getTemplates := func(ctx context.Context) ([]cmdTemplates.Template, io.Closer, error) {
@@ -808,6 +780,47 @@ func promptRuntimeOptions(ctx *plugin.Context, info *workspace.ProjectRuntimeInf
 	}
 
 	return options, nil
+}
+
+func promptForAIProjectURL(ctx context.Context, ws pkgWorkspace.Context, args newArgs,
+	opts display.Options,
+) (string, error) {
+	// Try to read the current project
+	project, _, err := ws.ReadProject()
+	if err != nil && !errors.Is(err, workspace.ErrProjectNotFound) {
+		return "", err
+	}
+
+	b, err := cmdBackend.CurrentBackend(ctx, ws, cmdBackend.DefaultLoginManager, project, opts)
+	if err != nil {
+		return "", err
+	}
+
+	var aiOrTemplate string
+	if shouldPromptForAIOrTemplate(args, b) {
+		aiOrTemplate, err = chooseWithAIOrTemplate(opts)
+	} else {
+		aiOrTemplate = deriveAIOrTemplate(args)
+	}
+	if err != nil {
+		return "", err
+	}
+	if aiOrTemplate != "ai" {
+		return "", nil
+	}
+
+	checkedBackend, ok := b.(httpstate.Backend)
+	if !ok {
+		if args.aiLanguage != "" && args.aiPrompt == "" {
+			return "", errors.New(
+				"--language is used to generate a template with Pulumi AI. " +
+					"Please log in to Pulumi Cloud to use Pulumi AI.\n" +
+					"Use --template to create a project from a template, " +
+					"or no flags to choose one interactively.")
+		}
+		return "", errors.New("please log in to Pulumi Cloud to use Pulumi AI")
+	}
+	return runAINew(ctx, args, opts, checkedBackend)
 }
 
 func makePromptValidator(prompt plugin.RuntimeOptionPrompt) func(string) error {

pkg/cmd/pulumi/newcmd/new_ai_test.go
@@ -19,8 +19,11 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/backend"
+	"github.com/pulumi/pulumi/pkg/v3/backend/display"
 	"github.com/pulumi/pulumi/pkg/v3/util/testutil"
+	pkgWorkspace "github.com/pulumi/pulumi/pkg/v3/workspace"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 //nolint:paralleltest // changes directory
@@ -36,10 +39,11 @@ func TestErrorsOnNonHTTPBackend(t *testing.T) {
 	})
 
 	testNewArgs := newArgs{
-		aiPrompt:        "prompt",
-		aiLanguage:      "typescript",
-		interactive:     true,
-		secretsProvider: "default",
+		aiPrompt:              "prompt",
+		aiLanguage:            "typescript",
+		interactive:           true,
+		secretsProvider:       "default",
+		promptForAIProjectURL: promptForAIProjectURL,
 	}
 
 	assert.ErrorContains(t,
@@ -47,4 +51,40 @@ func TestErrorsOnNonHTTPBackend(t *testing.T) {
 			context.Background(), testNewArgs,
 		),
 		"please log in to Pulumi Cloud to use Pulumi AI")
+}
+
+//nolint:paralleltest // changes directory for process, mocks backendInstance
+func TestGeneratingProjectWithAIPromptSucceeds(t *testing.T) {
+	tempdir := tempProjectDir(t)
+	chdir(t, tempdir)
+
+	testutil.MockBackendInstance(t, &backend.MockBackend{
+		DoesProjectExistF: func(ctx context.Context, org string, name string) (bool, error) {
+			return true, nil
+		},
+		SupportsTemplatesF: func() bool { return false },
+		NameF:              func() string { return "mock" },
+	})
+
+	// Generate-only command is not creating any stacks, so don't bother with with the name uniqueness check.
+	args := newArgs{
+		generateOnly: true,
+		interactive:  true,
+		prompt:       promptMock(projectName, ""),
+		promptForAIProjectURL: func(ctx context.Context, ws pkgWorkspace.Context,
+			args newArgs, opts display.Options,
+		) (string, error) {
+			// Return a plain template name so that we don't have to rely or a hard-coded AI path.
+			// This has the same effect and is good enough for the mock-based testing.
+			return "typescript", nil
+		},
+		secretsProvider:   "default",
+		templateNameOrURL: "", // <-- must be empty to trigger the AI flow
+	}
+
+	err := runNew(context.Background(), args)
+	require.NoError(t, err)
+
+	proj := loadProject(t, tempdir)
+	require.Equal(t, projectName, proj.Name.String())
 }