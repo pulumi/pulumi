7d58e81f343fe8ddd266c245242c07f7106c4b39
Gerardo Gullo <gerardo.gullo@gmail.com>
2025-07-10 17:11:11 +0200

    [issue-20020] Fix stack rename in python automation api

--- sdk/python/lib/pulumi/automation/_stack.py
+++ sdk/python/lib/pulumi/automation/_stack.py
@@ -760,13 +760,22 @@ class Stack:
         args.extend(extra_args)
 
         args.extend(self._remote_args())
-        rename_result = self._run_pulumi_cmd_sync(args, on_output)
 
+        # --- ISSUE #20020 ---
+        # Get summary from stack before renaming
         if self._remote and show_secrets:
             raise RuntimeError("can't enable `showSecrets` for remote workspaces")
 
+        # Summary can be None, this case can happen if the stack was empty and had no history.
         summary = self.info(show_secrets and not self._remote)
-        assert summary is not None
+        # Execute the rename command.
+        rename_result = self._run_pulumi_cmd_sync(args, on_output)
+
+        # After the rename is successful in the backend, the internal state of this
+        # Stack object MUST be updated to reflect the new name
+        self.name = new_stack_name
+        # --- END ISSUE ---
+
         return RenameResult(
             stdout=rename_result.stdout, stderr=rename_result.stderr, summary=summary
         )
