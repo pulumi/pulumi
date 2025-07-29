4e23bb7f2cde66309773561fd07892111fe0e203
Thomas Gummerer <t.gummerer@gmail.com>
2025-07-29 11:19:29 +0200

    WIP

--- sdk/go/common/apitype/journal.go
+++ sdk/go/common/apitype/journal.go
@@ -1,24 +1,49 @@
-// Copyright 2016-2025, Pulumi Corporation.  All rights reserved.
+// Copyright 2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package apitype
 
 type JournalEntryKind int
 
 const (
-	JournalEntryBegin   JournalEntryKind = 0
-	JournalEntrySuccess JournalEntryKind = 1
-	JournalEntryFailure JournalEntryKind = 2
-	JournalEntryOutputs JournalEntryKind = 4
+	JournalEntryBegin          JournalEntryKind = 0
+	JournalEntrySuccess        JournalEntryKind = 1
+	JournalEntryFailure        JournalEntryKind = 2
+	JournalEntryRefreshSuccess JournalEntryKind = 3
+	JournalEntryOutputs        JournalEntryKind = 4
+	JournalEntryRebase         JournalEntryKind = 5
+	JournalEntrySecretsManager JournalEntryKind = 6
 )
 
 type JournalEntry struct {
-	Kind             JournalEntryKind `json:"kind"`
-	OperationID      uint64           `json:"operationID"`         // ID of the operation this journal entry is associated with.
-	DeleteOld        int              `json:"deleteOld"`           // ID for the delete Operation that this journal entry is associated with.
-	DeleteNew        uint64           `json:"deleteNew"`           // ID for the delete Operation that this journal entry is associated with.
-	State            *ResourceV3      `json:"state,omitempty"`     // The resource state associated with this journal entry.
-	Operation        *OperationV2     `json:"operation,omitempty"` // The operation associated with this journal entry, if any.
-	RefreshDeleteURN string           `json:"refreshDeleteURN"`    // The URN of the resource that was deleted by a refresh operation.
+	Kind JournalEntryKind `json:"kind"`
+	// ID of the operation this journal entry is associated with.
+	OperationID uint64 `json:"operationID"`
+	// ID for the delete Operation that this journal entry is associated with.
+	DeleteOld int `json:"deleteOld"`
+	// ID for the delete Operation that this journal entry is associated with.
+	DeleteNew uint64 `json:"deleteNew"`
+	// The resource state associated with this journal entry.
+	State *ResourceV3 `json:"state,omitempty"`
+	// The operation associated with this journal entry, if any.
+	Operation *OperationV2 `json:"operation,omitempty"`
+	// The secrets manager associated with this journal entry, if any.
+	SecretsProvider *SecretsProvidersV1 `json:"secretsProvider,omitempty"`
+	// PendingReplacement is the index of the resource that's to be marked aspending replacement.
+	PendingReplacement int `json:"pendingReplacement,omitempty"`
+	// NewSnapshot is the new snapshot that this journal entry is associated with.
+	NewSnapshot *DeploymentV3 `json:"newSnapshot,omitempty"`
 }
 
 // CreateJournalEntryRequest defines the request body for creating a new journal entry.
