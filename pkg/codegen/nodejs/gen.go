939e323ab1f7f2703859274c46cd508823b70fb3
Julien Poissonnier <julien@caffeine.lu>
2025-08-26 09:51:48 +0200

    Limit @types packages used during local SDK generation
    Generated local SDKs depend directly on typescript because we require the ts compiler to build the package in a postinstall script. The dependency ensures that the compiler is present and at a version that is guaranteed to work for the local SDK.
    
    By default typescript will include any `@types/…` packages. When the project that uses the local SDK includes a `@types/…` package that uses features requiring a more recent typescript compiler, the postinstall script will fail. An example if `@types/jest@30`, which requires typescript 5.2, but local SDKs use 4.3.
    
    To fix this we can pass the `—types` flag to `tsc` to select which `@types/…` packages are included in the compilation and limit it to only those the local SDK depends on, not those of the project.
    
    Fixes https://github.com/pulumi/pulumi/issues/20386

--- pkg/codegen/nodejs/gen.go
+++ pkg/codegen/nodejs/gen.go
@@ -2565,13 +2565,22 @@ func genNPMPackageMetadata(
 // local SDKs declare typescript as a dependencies. When the script is run via a
 // package manager, the package manager will add `node_modules/.bin` to the
 // path, ensuring we run the intended version of `tsc`.
+// We also limit the `@types` packages that are used to those the SDK itself
+// depends on. Otherwise typescript will use all `@types` packages in the
+// project, which can lead to it trying to use type packages that are not
+// compatible with the SDK's typescript version.
 func genPostInstallScript() []byte {
 	return []byte(`const fs = require("node:fs");
 const path = require("node:path");
 const process = require("node:process");
 const { execSync } = require('node:child_process');
+// We want to run "tsc --types node --types something-else ..." for each @types package.
+const packageJSON = JSON.parse(fs.readFileSync("package.json") ?? "{}");
+const deps = Object.keys(packageJSON.dependencies ?? []).concat(Object.keys(packageJSON.devDependencies ?? []));
+const types = deps.filter(d => d.startsWith("@types/")).map(d => d.slice("@types/".length)).join(",");
+const typesFlag = types.length > 0 ? " --types " + types : "";
 try {
-  execSync("tsc");
+  execSync("tsc"+typesFlag);
 } catch (error) {
   console.error("Failed to run 'tsc'", {
     stdout: error.stdout.toString(),
