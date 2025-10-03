bea1348f847d7bbf979e374132af360303d69b10
Julien Poissonnier <julien@caffeine.lu>
2025-10-03 10:29:37 +0200

    fix experimental server

--- sdk/python/lib/pulumi/provider/experimental/server.py
+++ sdk/python/lib/pulumi/provider/experimental/server.py
@@ -664,11 +664,7 @@ def main(args: list[str], version: str, provider: provider.Provider) -> None:
         await server.wait_for_termination()
 
     try:
-        loop = asyncio.get_event_loop()
-        try:
-            loop.run_until_complete(serve())
-        finally:
-            loop.close()
+        asyncio.run(serve())
     except KeyboardInterrupt:
         pass
 
