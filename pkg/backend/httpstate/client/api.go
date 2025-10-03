a7a486b74c293fb2437e2cd830b5be85b11be3b5
Thomas Gummerer <t.gummerer@gmail.com>
2025-10-03 14:10:53 +0200

    show request ID on service errors (#20653)
    When we get errors from the service, we currently have no way of
    correlating them with the logs on the backend. The service already sets
    a `X-Pulumi-Request-ID` header for this purpose, but we don't show it
    anywhere in the CLI.
    
    To facilitate debugging issues, print out the request ID to the user if
    the header is set.

--- pkg/backend/httpstate/client/api.go
+++ pkg/backend/httpstate/client/api.go
@@ -352,7 +352,11 @@ func pulumiAPICall(ctx context.Context,
 		if err != nil {
 			return "", nil, fmt.Errorf("API call failed (%s), could not read response: %w", resp.Status, err)
 		}
-		err = decodeError(respBody, resp.StatusCode, opts)
+		reqID := ""
+		if resp.StatusCode >= 500 {
+			reqID = resp.Header.Get("X-Pulumi-Request-ID")
+		}
+		err = decodeError(respBody, resp.StatusCode, opts, reqID)
 		if resp.StatusCode == 403 {
 			err = backenderr.ForbiddenError{Err: err}
 		}
@@ -363,7 +367,7 @@ func pulumiAPICall(ctx context.Context,
 	return url, resp, nil
 }
 
-func decodeError(respBody []byte, statusCode int, opts httpCallOptions) error {
+func decodeError(respBody []byte, statusCode int, opts httpCallOptions, reqID string) error {
 	if opts.ErrorResponse != nil {
 		if err := json.Unmarshal(respBody, opts.ErrorResponse); err == nil {
 			return opts.ErrorResponse.(error)
@@ -374,6 +378,9 @@ func decodeError(respBody []byte, statusCode int, opts httpCallOptions) error {
 	if err := json.Unmarshal(respBody, &errResp); err != nil {
 		errResp.Code = statusCode
 		errResp.Message = strings.TrimSpace(string(respBody))
+	}
+	if reqID != "" {
+		errResp.Message = fmt.Sprintf("%s (Request ID: %s)", errResp.Message, reqID)
 	}
 	return &errResp
 }
