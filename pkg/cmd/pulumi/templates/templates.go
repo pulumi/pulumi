d81bcd89ec37d8e1453188ff99b01a4b21a695d5
Julien Poissonnier <julien@caffeine.lu>
2025-10-09 13:04:03 +0200

    simplify template display
    The `Template` interface used in the UI does not need to concern itself with project description vs display description. We can simplify the interface and drop some of the confusing properties used to determine the template description.

--- pkg/cmd/pulumi/templates/templates.go
+++ pkg/cmd/pulumi/templates/templates.go
@@ -119,12 +119,12 @@ func (s *Source) Close() error {
 	return errors.Join(errs...)
 }
 
+// A template entry to show in the chooser.
 type Template interface {
 	Name() string
 	Description() string
-	DisplayDescription() string
-	ProjectDescription() string
 	Error() error
+	// Download the template and return an instantiable [workspace.Template] for this template.
 	Download(ctx context.Context) (workspace.Template, error)
 }
 
