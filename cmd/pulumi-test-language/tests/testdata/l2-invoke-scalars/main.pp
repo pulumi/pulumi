b18446c5a4f132d71aea256a23a2b8159bbe5ee0
Fraser Waters <fraser@pulumi.com>
2025-10-10 15:32:48 +0100

    More tests

---
+++ cmd/pulumi-test-language/tests/testdata/l2-invoke-scalars/main.pp
@@ -0,0 +1,11 @@
+output "secret" {
+    value = invoke("scalar-returns:index:invokeSecret", {value="goodbye"})
+}
+
+output "array" {
+    value = invoke("scalar-returns:index:invokeArray", {value="the word"})
+}
+
+output "map" {
+    value = invoke("scalar-returns:index:invokeMap", {value="hello"})
+}
---
+++ sdk/go/pulumi-language-go/testdata/published/projects/l2-invoke-scalars/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"example.com/pulumi-scalar-returns/sdk/go/v21/scalarreturns"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		ctx.Export("secret", scalarreturns.InvokeSecretOutput(ctx, scalarreturns.InvokeSecretOutputArgs{
+			Value: pulumi.String("goodbye"),
+		}, nil))
+		ctx.Export("array", scalarreturns.InvokeArrayOutput(ctx, scalarreturns.InvokeArrayOutputArgs{
+			Value: pulumi.String("the word"),
+		}, nil))
+		ctx.Export("map", scalarreturns.InvokeMapOutput(ctx, scalarreturns.InvokeMapOutputArgs{
+			Value: pulumi.String("hello"),
+		}, nil))
+		return nil
+	})
+}
