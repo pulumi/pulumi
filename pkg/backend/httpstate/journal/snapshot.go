f6bcdd5c53da270150afd0d54a8496c8e7e6c9a1
Bryce Lampe <bryce@pulumi.com>
2025-10-30 16:08:30 -0700

    Add backward compatibility aliases in github.com/pulumi/pulumi/pkg/v3
    To preserve compatibility for existing consumers of
    github.com/pulumi/pulumi/pkg/v3, we are replacing the source module's
    implementation files with compatibility shims that re-export everything from the
    new location.
    
    For each exported symbol in the original module:
    - Types become type aliases (e.g., type User = newpkg.User)
    - Functions become wrapper functions that call through
    - Constants and variables become references to the new location
    
    This approach allows existing code to continue importing from
    github.com/pulumi/pulumi/pkg/v3 without any changes, while internally delegating
    to github.com/pulumi/pulumi/sdk/v3/pkg.
    
    Generated aliases in 474 files.

--- pkg/backend/httpstate/journal/snapshot.go
+++ pkg/backend/httpstate/journal/snapshot.go
@@ -1,73 +1,8 @@
-// Copyright 2025, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package journal
 
-import (
-	"context"
-	"fmt"
-	"sync"
+import journal "github.com/pulumi/pulumi/sdk/v3/pkg/backend/httpstate/journal"
 
-	"github.com/pulumi/pulumi/pkg/v3/backend"
-	"github.com/pulumi/pulumi/pkg/v3/backend/httpstate/client"
-	"github.com/pulumi/pulumi/pkg/v3/engine"
-	"github.com/pulumi/pulumi/pkg/v3/secrets"
-)
-
-var _ engine.Journal = (*cloudJournaler)(nil)
-
-type cloudJournaler struct {
-	context     context.Context         // The context to use for client requests.
-	tokenSource tokenSourceCapability   // A token source for interacting with the service.
-	client      *client.Client          // A backend for communicating with the service
-	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.
-	sm          secrets.Manager         // Secrets manager for encrypting values when serializing the journal entries.
-	wg          sync.WaitGroup          // Wait group to ensure all operations are completed before closing.
+func NewJournaler(ctx context.Context, client *client.Client, update client.UpdateIdentifier, tokenSource tokenSourceCapability, sm secrets.Manager) engine.Journal {
+	return journal.NewJournaler(ctx, client, update, tokenSource, sm)
 }
 
-func (j *cloudJournaler) AddJournalEntry(entry engine.JournalEntry) error {
-	j.wg.Add(1)
-	defer j.wg.Done()
-	serialized, err := backend.SerializeJournalEntry(j.context, entry, j.sm.Encrypter())
-	if err != nil {
-		return fmt.Errorf("serializing journal entry: %w", err)
-	}
-	return j.client.SaveJournalEntry(j.context, j.update, serialized, j.tokenSource)
-}
-
-func (j *cloudJournaler) Close() error {
-	j.wg.Wait() // Wait for all operations to complete before closing.
-	return nil
-}
-
-type tokenSourceCapability interface {
-	GetToken(ctx context.Context) (string, error)
-}
-
-func NewJournaler(
-	ctx context.Context,
-	client *client.Client,
-	update client.UpdateIdentifier,
-	tokenSource tokenSourceCapability,
-	sm secrets.Manager,
-) engine.Journal {
-	journal := &cloudJournaler{
-		context:     ctx,
-		tokenSource: tokenSource,
-		client:      client,
-		update:      update,
-		sm:          sm,
-	}
-	return journal
-}
--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,22 +1,7 @@
-// Copyright 2020-2024, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package syntax
 
-import (
-	"github.com/hashicorp/hcl/v2/hclsyntax"
-)
+import syntax "github.com/pulumi/pulumi/sdk/v3/pkg/codegen/hcl2/syntax"
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}
+var None = syntax.None
+
