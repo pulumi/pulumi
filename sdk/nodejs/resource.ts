08dc286a5c98e37de804b52e79fa3a478417f0c1
Fraser Waters <fraser@pulumi.com>
2025-10-31 12:13:54 +0000

    format

--- sdk/nodejs/resource.ts
+++ sdk/nodejs/resource.ts
@@ -1427,7 +1427,16 @@ export class ComponentResource<TData = any> extends Resource {
     ) {
         // If the PULUMI_NODEJS_SKIP_COMPONENT_INPUTS environment variable is set,
         // we skip sending the inputs to the engine.
-        super(type, name, /*custom:*/ false, process.env.PULUMI_NODEJS_SKIP_COMPONENT_INPUTS ? {} : args, opts, remote, false, packageRef);
+        super(
+            type,
+            name,
+            /*custom:*/ false,
+            process.env.PULUMI_NODEJS_SKIP_COMPONENT_INPUTS ? {} : args,
+            opts,
+            remote,
+            false,
+            packageRef,
+        );
         this.__remote = remote;
         this.__registered = remote || !!opts?.urn;
         this.__data =
