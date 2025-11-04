c1d88797d25a8e9b54d1ee54d90dace39ca35b38
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-11-04 20:10:48 +0530

    fix lint, remove commets and hail tgummer
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- pkg/cmd/pulumi/show/show.go
+++ pkg/cmd/pulumi/show/show.go
@@ -1,3 +1,17 @@
+// Copyright 2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package show
 
 import (
@@ -16,8 +30,7 @@ import (
 
 func ShowCmd() *cobra.Command {
 	var stackName string
-	var rf resourceFilters
-
+	var name string
 	cmd := &cobra.Command{
 		Use:   "show",
 		Short: "show resources in the stack",
@@ -32,29 +45,23 @@ func ShowCmd() *cobra.Command {
 				return err
 			}
 
-			// Read resources from snapshot
 			ss, err := s.Snapshot(ctx, secrets.DefaultProvider)
 			if err != nil {
 				return err
 			}
 			resources := ss.Resources
 			for _, r := range resources {
-				passes, err := resourcePassesFilters(r, &rf)
-				if err != nil {
-					return err
-				}
-				if passes {
+				if strings.Contains(r.URN.Name(), name) {
 					printResourceState(r)
+				} else {
+					continue
 				}
-
 			}
-
 			return nil
 		},
 	}
 	cmd.PersistentFlags().StringVar(&stackName, "stack", "", "the stack for which resources will be shown")
-	cmd.MarkFlagRequired("stack")
-	cmd.PersistentFlags().StringVar(&rf.name, "name", "", "filter resources by name")
+	cmd.PersistentFlags().StringVar(&name, "name", "", "filter resources by name")
 
 	return cmd
 }
@@ -66,7 +73,6 @@ func printResourceState(rs *resource.State) {
 
 	fmt.Println("Properties:")
 	for k, v := range rs.Outputs {
-		// ignore properties starting with __
 		if strings.HasPrefix(string(k), "__") {
 			continue
 		}
@@ -77,7 +83,6 @@ func printResourceState(rs *resource.State) {
 
 func renderPropertyVal(rsp resource.PropertyValue, currIdent string) string {
 	if rsp.IsObject() {
-
 		newIdent := currIdent + "    "
 		var res string
 		objMap := rsp.ObjectValue()
@@ -85,7 +90,6 @@ func renderPropertyVal(rsp resource.PropertyValue, currIdent string) string {
 			res += "\n" + newIdent + string(k) + ": " + renderPropertyVal(v, newIdent)
 		}
 		return res
-
 	}
 	if rsp.IsArray() {
 		res := "\n" + currIdent
@@ -98,20 +102,8 @@ func renderPropertyVal(rsp resource.PropertyValue, currIdent string) string {
 				return renderPropertyVal(v, newIdent)
 			}
 			res += v.String()
-
 		}
 		return res
 	}
 	return rsp.String()
-}
-
-func resourcePassesFilters(rs *resource.State, rf *resourceFilters) (bool, error) {
-	if !strings.Contains(rs.URN.Name(), rf.name) {
-		return false, nil
-	}
-	return true, nil
-}
-
-type resourceFilters struct {
-	name string
 }
