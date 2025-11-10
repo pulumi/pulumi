b5bb058a6440ccb8db08184a63f75e00f8dcf71a
Tom Harding <i.am.tom.harding@gmail.com>
2025-11-10 16:53:41 +0100

    conformance test

---
+++ sdk/go/pulumi-language-go/testdata/published/projects/l2-resource-option-replacement-trigger/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"example.com/pulumi-simple/sdk/go/v2/simple"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		_, err := simple.NewResource(ctx, "replacementTrigger", &simple.ResourceArgs{
+			Value: pulumi.Bool(true),
+		}, pulumi.ReplacementTrigger(pulumi.String("test")))
+		if err != nil {
+			return err
+		}
+		_, err = simple.NewResource(ctx, "notReplacementTrigger", &simple.ResourceArgs{
+			Value: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
