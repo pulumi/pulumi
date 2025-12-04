bf22a04b0463c3108cc4df0a05813c3de1f996d3
Fraser Waters <fraser@pulumi.com>
2025-12-04 15:43:04 +0000

    Enhance the replacement trigger test with unknowns

--- cmd/pulumi-test-language/tests/testdata/l2-resource-option-replacement-trigger/0/main.pp
+++ cmd/pulumi-test-language/tests/testdata/l2-resource-option-replacement-trigger/0/main.pp
@@ -5,6 +5,17 @@ resource "replacementTrigger" "simple:index:Resource" {
     }
 }
 
+resource "unknown" "output:index:Resource" {
+    value = 1
+}
+
+resource "unknownReplacementTrigger" "simple:index:Resource" {
+    value = true
+    options {
+        replacementTrigger = "hellohello"
+    }
+}
+
 resource "notReplacementTrigger" "simple:index:Resource" {
     value = true
 }
