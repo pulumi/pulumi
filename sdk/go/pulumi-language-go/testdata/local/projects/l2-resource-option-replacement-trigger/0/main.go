940be27deaa38452a15dfc511bf24a64ced8d3d5
Fraser Waters <fraser@pulumi.com>
2025-12-04 22:13:46 +0000

    Enhance the replacement trigger test with unknowns (#21139)
    The replacement trigger option can be passed `unknowns` as well as
    normal values, these show up at preview time as replacements. This
    enhances the conformance test to check that an unknown value can be
    passed in and shows a replace at preview time.
    
    Would be good to also extend this test to ensure secrets work correctly,
    but I'll do that after this is merged.

--- sdk/go/pulumi-language-go/testdata/local/projects/l2-resource-option-replacement-trigger/0/main.go
+++ sdk/go/pulumi-language-go/testdata/local/projects/l2-resource-option-replacement-trigger/0/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"example.com/pulumi-output/sdk/go/v23/output"
 	"example.com/pulumi-simple/sdk/go/v2/simple"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -10,6 +11,18 @@ func main() {
 		_, err := simple.NewResource(ctx, "replacementTrigger", &simple.ResourceArgs{
 			Value: pulumi.Bool(true),
 		}, pulumi.ReplacementTrigger(pulumi.Any("test")))
+		if err != nil {
+			return err
+		}
+		_, err = output.NewResource(ctx, "unknown", &output.ResourceArgs{
+			Value: pulumi.Float64(1),
+		})
+		if err != nil {
+			return err
+		}
+		_, err = simple.NewResource(ctx, "unknownReplacementTrigger", &simple.ResourceArgs{
+			Value: pulumi.Bool(true),
+		}, pulumi.ReplacementTrigger(pulumi.Any("hellohello")))
 		if err != nil {
 			return err
 		}
