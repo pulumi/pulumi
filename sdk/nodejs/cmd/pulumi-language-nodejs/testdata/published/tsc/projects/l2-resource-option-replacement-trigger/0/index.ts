7b6fe7979a16ebb44183b5ea93e41233ed18c467
Tom Harding <i.am.tom.harding@gmail.com>
2025-12-09 13:35:58 +0100

    More goldenfiles please

---
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsc/projects/l2-resource-option-replacement-trigger/0/index.ts
@@ -0,0 +1,12 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as output from "@pulumi/output";
+import * as simple from "@pulumi/simple";
+
+const replacementTrigger = new simple.Resource("replacementTrigger", {value: true}, {
+    replacementTrigger: "test",
+});
+const unknown = new output.Resource("unknown", {value: 1});
+const unknownReplacementTrigger = new simple.Resource("unknownReplacementTrigger", {value: true}, {
+    replacementTrigger: "hellohello",
+});
+const notReplacementTrigger = new simple.Resource("notReplacementTrigger", {value: true});
---
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsc/projects/l2-resource-option-replacement-trigger/1/index.ts
@@ -0,0 +1,12 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as output from "@pulumi/output";
+import * as simple from "@pulumi/simple";
+
+const replacementTrigger = new simple.Resource("replacementTrigger", {value: true}, {
+    replacementTrigger: "test2",
+});
+const unknown = new output.Resource("unknown", {value: 2});
+const unknownReplacementTrigger = new simple.Resource("unknownReplacementTrigger", {value: true}, {
+    replacementTrigger: unknown.output,
+});
+const notReplacementTrigger = new simple.Resource("notReplacementTrigger", {value: true});
