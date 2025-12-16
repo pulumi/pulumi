pulumi/pulumi

commit 6dd8a312ca4af2bc349e902379b7f62a10a653dc

Validate config using caching batch encrypt and decrypt (#20674)
To validate the config this PR makes use caching batch encrypt and
decrypt calls introduced in https://github.com/pulumi/pulumi/pull/20578
and https://github.com/pulumi/pulumi/pull/20579

Resolves: https://github.com/pulumi/pulumi/issues/20338

sdk/go/common/workspace/config.go
@@ -67,17 +67,59 @@ type (
 	ProjectConfigKey = string
 )
 
-func validateStackConfigValue(
+func validateStackConfigValues(
 	stackName string,
-	projectConfigKey string,
-	projectConfigType ProjectConfigType,
-	stackValue config.Value,
+	project *Project,
+	stackConfig config.Map,
 	dec config.Decrypter,
 ) error {
 	if dec == nil {
 		return nil
 	}
 
+	decryptedConfig, err := stackConfig.Decrypt(dec)
+	if err != nil {
+		return err
+	}
+
+	keys := make([]string, 0, len(project.Config))
+	for k := range project.Config {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, projectConfigKey := range keys {
+		projectConfigType := project.Config[projectConfigKey]
+
+		key, err := parseConfigKey(project.Name.String(), projectConfigKey)
+		if err != nil {
+			return err
+		}
+
+		stackValue, _, err := stackConfig.Get(key, true)
+		if err != nil {
+			return fmt.Errorf("getting stack config value for key '%v': %w", key.String(), err)
+		}
+
+		if projectConfigType.IsExplicitlyTyped() {
+			decryptedValue := decryptedConfig[key]
+			err := validateStackConfigValue(stackName, projectConfigKey, projectConfigType, stackValue, decryptedValue)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func validateStackConfigValue(
+	stackName string,
+	projectConfigKey string,
+	projectConfigType ProjectConfigType,
+	stackValue config.Value,
+	decryptedValue string,
+) error {
 	// First check if the project says this should be secret, and if so that the stack value is
 	// secure.
 	if projectConfigType.Secret && !stackValue.Secure() {
@@ -88,14 +130,10 @@ func validateStackConfigValue(
 		return validationError
 	}
 
-	value, err := stackValue.Value(dec)
-	if err != nil {
-		return err
-	}
 	// Content will be a JSON string if object is true, so marshal that back into an actual structure
-	var content any = value
+	var content any = decryptedValue
 	if stackValue.Object() {
-		err = json.Unmarshal([]byte(value), &content)
+		err := json.Unmarshal([]byte(decryptedValue), &content)
 		if err != nil {
 			return err
 		}
@@ -142,7 +180,23 @@ func createConfigValue(rawValue any) (config.Value, error) {
 	return config.NewObjectValue(string(configValueJSON)), nil
 }
 
-func envConfigValue(v esc.Value) config.Plaintext {
+func toConfigMap(
+	ctx context.Context, environmentPulumiConfig esc.Value, defaultNamespace string, encrypter config.Encrypter,
+) (config.Map, error) {
+	plaintextMap := map[config.Key]config.Plaintext{}
+	if envMap, ok := environmentPulumiConfig.Value.(map[string]esc.Value); ok {
+		for rawKey, value := range envMap {
+			key, err := parseConfigKey(defaultNamespace, rawKey)
+			if err != nil {
+				return nil, err
+			}
+			plaintextMap[key] = toPlaintext(value)
+		}
+	}
+	return config.EncryptMap(ctx, plaintextMap, encrypter)
+}
+
+func toPlaintext(v esc.Value) config.Plaintext {
 	if v.Unknown {
 		if v.Secret {
 			return config.NewPlaintext(config.PlaintextSecret("[unknown]"))
@@ -171,13 +225,13 @@ func envConfigValue(v esc.Value) config.Plaintext {
 	case []esc.Value:
 		vs := make([]config.Plaintext, len(repr))
 		for i, v := range repr {
-			vs[i] = envConfigValue(v)
+			vs[i] = toPlaintext(v)
 		}
 		return config.NewPlaintext(vs)
 	case map[string]esc.Value:
 		vs := make(map[string]config.Plaintext, len(repr))
 		for k, v := range repr {
-			vs[k] = envConfigValue(v)
+			vs[k] = toPlaintext(v)
 		}
 		return config.NewPlaintext(vs)
 	default:
@@ -196,47 +250,42 @@ func mergeConfig(
 	decrypter config.Decrypter,
 	validate bool,
 ) error {
-	missingConfigurationKeys := make([]string, 0)
 	projectName := project.Name.String()
 
+	envConfig, err := toConfigMap(ctx, stackEnv, projectName, encrypter)
+	if err != nil {
+		return err
+	}
+
+	// First merge the stack environment and the stack config together.
+	for key, envValue := range envConfig {
+		stackValue, foundOnStack, err := stackConfig.Get(key, false)
+		if err != nil {
+			return fmt.Errorf("getting stack config value for key '%v': %w", key.String(), err)
+		}
+
+		if !foundOnStack {
+			err = stackConfig.Set(key, envValue, false)
+		} else {
+			// Uses JSON merge patch semantics: https://datatracker.ietf.org/doc/html/rfc7386
+			// Prefers stack values over env values.
+			merged, mergeErr := stackValue.Merge(envValue)
+			if mergeErr != nil {
+				return fmt.Errorf("merging environment config for key '%v': %w", key.String(), err)
+			}
+			err = stackConfig.Set(key, merged, false)
+		}
+		if err != nil {
+			return fmt.Errorf("setting merged config value for key '%v': %w", key.String(), err)
+		}
+	}
+
+	missingConfigurationKeys := make([]string, 0)
 	keys := make([]string, 0, len(project.Config))
 	for k := range project.Config {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
-
-	// First merge the stack environment and the stack config together.
-	if envMap, ok := stackEnv.Value.(map[string]esc.Value); ok {
-		for rawKey, value := range envMap {
-			key, err := parseConfigKey(projectName, rawKey)
-			if err != nil {
-				return err
-			}
-
-			envValue, err := envConfigValue(value).Encrypt(ctx, encrypter)
-			if err != nil {
-				return err
-			}
-
-			stackValue, foundOnStack, err := stackConfig.Get(key, false)
-			if err != nil {
-				return fmt.Errorf("getting stack config value for key '%v': %w", key.String(), err)
-			}
-
-			if !foundOnStack {
-				err = stackConfig.Set(key, envValue, false)
-			} else {
-				merged, mergeErr := stackValue.Merge(envValue)
-				if mergeErr != nil {
-					return fmt.Errorf("merging environment config for key '%v': %w", key.String(), err)
-				}
-				err = stackConfig.Set(key, merged, false)
-			}
-			if err != nil {
-				return fmt.Errorf("setting merged config value for key '%v': %w", key.String(), err)
-			}
-		}
-	}
 
 	// Next validate the merged config and merge in the project config.
 	for _, projectConfigKey := range keys {
@@ -247,7 +296,7 @@ func mergeConfig(
 			return err
 		}
 
-		stackValue, foundOnStack, err := stackConfig.Get(key, true)
+		_, foundOnStack, err := stackConfig.Get(key, true)
 		if err != nil {
 			return fmt.Errorf("getting stack config value for key '%v': %w", key.String(), err)
 		}
@@ -285,20 +334,20 @@ func mergeConfig(
 
 			continue
 		}
-
-		// Validate stack level value against the config defined at the project level
-		if validate && projectConfigType.IsExplicitlyTyped() {
-			err := validateStackConfigValue(stackName, projectConfigKey, projectConfigType, stackValue, decrypter)
-			if err != nil {
-				return err
-			}
-		}
 	}
 
 	if len(missingConfigurationKeys) > 0 {
 		// there are missing configuration keys in the stack
 		// return them as a single error.
 		return missingStackConfigurationKeysError(missingConfigurationKeys, stackName)
+	}
+
+	// Validate stack level values against the config defined at the project level
+	if validate {
+		err := validateStackConfigValues(stackName, project, stackConfig, decrypter)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil

sdk/go/common/workspace/config_test.go
@@ -1,0 +1,101 @@
+// Copyright 2016-2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package workspace
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/config"
+)
+
+func TestValidateStackConfigValues(t *testing.T) {
+	t.Run("No Decrypter Returns Nil", func(t *testing.T) {
+		t.Parallel()
+		// If decrypter is nil, function should return immediately with no error.
+		project := &Project{Name: "testProject"}
+		stackCfg := config.Map{}
+		err := validateStackConfigValues("stackA", project, stackCfg, nil)
+		require.NoError(t, err)
+	})
+
+	t.Run("Empty Project With Decrypter Returns Nil", func(t *testing.T) {
+		t.Parallel()
+		// Non-nil decrypter but no project config entries -> nothing to validate.
+		project := &Project{Name: "testProject"}
+		stackCfg := config.Map{}
+
+		err := validateStackConfigValues("stackA", project, stackCfg, config.NopDecrypter)
+		require.NoError(t, err)
+	})
+
+	t.Run("Decrypt Error Is Propagated", func(t *testing.T) {
+		t.Parallel()
+		// Decrypter returns an error; validateStackConfigValues should return that error.
+		project := &Project{Name: "testProject"}
+		stackCfg := config.Map{
+			config.MustMakeKey("testProject", "someKey"): config.NewSecureValue("someVal"),
+		}
+		wantErr := "decrypt failed"
+		dec := config.NewErrorCrypter(wantErr)
+
+		err := validateStackConfigValues("stackA", project, stackCfg, dec)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), wantErr)
+	})
+
+	t.Run("Secret Enforced", func(t *testing.T) {
+		t.Parallel()
+		// When project says config is secret, stack value must be secure.
+		projectConfigKey := "proj:secretKey"
+		pct := ProjectConfigType{
+			Secret: true,
+		}
+		// create a non-secure stack value
+		stackVal := config.NewValue("plain")
+		err := validateStackConfigValue("stack1", projectConfigKey, pct, stackVal, "plain")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "must be encrypted")
+	})
+
+	t.Run("Object Unmarshal Error", func(t *testing.T) {
+		t.Parallel()
+		projectConfigKey := "proj:objKey"
+		// object stack value with invalid JSON content should return unmarshal error
+		stackVal := config.NewObjectValue("not-a-json")
+		pct := ProjectConfigType{}
+		err := validateStackConfigValue("stackX", projectConfigKey, pct, stackVal, "not-a-json")
+		require.Error(t, err)
+		// error should be json unmarshal related
+		require.Contains(t, err.Error(), "invalid character")
+	})
+
+	t.Run("Type Mismatch", func(t *testing.T) {
+		t.Parallel()
+		// Project config expects a numeric type but the stack value is a non-numeric string.
+		projectConfigKey := "proj:typeKey"
+		typ := "number"
+		pct := ProjectConfigType{
+			Type: &typ,
+		}
+		// non-numeric stack value
+		stackVal := config.NewValue("not-a-number")
+
+		err := validateStackConfigValue("stackX", projectConfigKey, pct, stackVal, "not-a-number")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "must be of type")
+	})
+}

pkg/cmd/pulumi/config/config.go
@@ -37,6 +37,7 @@ import (
 	cmdStack "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/stack"
 	"github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/ui"
 	"github.com/pulumi/pulumi/pkg/v3/resource/stack"
+	"github.com/pulumi/pulumi/pkg/v3/secrets"
 	"github.com/pulumi/pulumi/pkg/v3/secrets/cloud"
 	"github.com/pulumi/pulumi/pkg/v3/secrets/passphrase"
 	pkgWorkspace "github.com/pulumi/pulumi/pkg/v3/workspace"
@@ -1005,11 +1006,13 @@ func listConfig(
 	}
 
 	var pulumiEnv esc.Value
+	var secretsManager secrets.Manager
 	var envCrypter config.Encrypter
 	if env != nil {
 		pulumiEnv = env.Properties["pulumiConfig"]
 
-		stackEncrypter, state, err := ssml.GetEncrypter(ctx, stack, ps)
+		var state cmdStack.SecretsManagerState
+		secretsManager, state, err = ssml.GetSecretsManager(ctx, stack, ps)
 		if err != nil {
 			return err
 		}
@@ -1019,7 +1022,9 @@ func listConfig(
 				return fmt.Errorf("save stack config: %w", err)
 			}
 		}
-		envCrypter = stackEncrypter
+		// secretsManager's crypter has a cache for crypto operations. As we are encrypting all environment secrets
+		// with the secretManager's encrypter ensure to use its decrypter later on when reading the merged config.
+		envCrypter = secretsManager.Encrypter()
 	}
 
 	stackName := stack.Ref().Name().String()
@@ -1039,17 +1044,19 @@ func listConfig(
 	// By default, we will use a blinding decrypter to show "[secret]". If requested, display secrets in plaintext.
 	decrypter := config.NewBlindingDecrypter()
 	if cfg.HasSecureValue() && showSecrets {
-		stackDecrypter, state, err := ssml.GetDecrypter(ctx, stack, ps)
-		if err != nil {
-			return err
-		}
-		// This may have setup the stack's secrets provider, so save the stack if needed.
-		if state != cmdStack.SecretsManagerUnchanged {
-			if err = cmdStack.SaveProjectStack(ctx, stack, ps); err != nil {
-				return fmt.Errorf("save stack config: %w", err)
+		if secretsManager == nil {
+			var state cmdStack.SecretsManagerState
+			if secretsManager, state, err = ssml.GetSecretsManager(ctx, stack, ps); err != nil {
+				return err
+			}
+			// This may have setup the stack's secrets provider, so save the stack if needed.
+			if state != cmdStack.SecretsManagerUnchanged {
+				if err = cmdStack.SaveProjectStack(ctx, stack, ps); err != nil {
+					return fmt.Errorf("save stack config: %w", err)
+				}
 			}
 		}
-		decrypter = stackDecrypter
+		decrypter = secretsManager.Decrypter()
 	}
 
 	var keys config.KeyArray
@@ -1060,6 +1067,11 @@ func listConfig(
 	}
 	sort.Sort(keys)
 
+	decryptedCfg, err := cfg.Decrypt(decrypter)
+	if err != nil {
+		return fmt.Errorf("could not decrypt configuration values: %w", err)
+	}
+
 	if jsonOut {
 		configValues := make(map[string]configValueJSON)
 		for _, key := range keys {
@@ -1067,10 +1079,7 @@ func listConfig(
 				Secret: cfg[key].Secure(),
 			}
 
-			decrypted, err := cfg[key].Value(decrypter)
-			if err != nil {
-				return fmt.Errorf("could not decrypt configuration value: %w", err)
-			}
+			decrypted := decryptedCfg[key]
 			entry.Value = &decrypted
 
 			if cfg[key].Object() {
@@ -1098,11 +1107,7 @@ func listConfig(
 	} else {
 		rows := []cmdutil.TableRow{}
 		for _, key := range keys {
-			decrypted, err := cfg[key].Value(decrypter)
-			if err != nil {
-				return fmt.Errorf("could not decrypt configuration value: %w", err)
-			}
-
+			decrypted := decryptedCfg[key]
 			rows = append(rows, cmdutil.TableRow{Columns: []string{PrettyKey(key), decrypted}})
 		}
 
@@ -1182,12 +1187,13 @@ func getConfig(
 	}
 
 	var pulumiEnv esc.Value
+	var secretsManager secrets.Manager
 	var envCrypter config.Encrypter
 	if env != nil {
 		pulumiEnv = env.Properties["pulumiConfig"]
 
-		stackEncrypter, state, err := ssml.GetEncrypter(ctx, stack, ps)
-		if err != nil {
+		var state cmdStack.SecretsManagerState
+		if secretsManager, state, err = ssml.GetSecretsManager(ctx, stack, ps); err != nil {
 			return err
 		}
 		// This may have setup the stack's secrets provider, so save the stack if needed.
@@ -1196,7 +1202,9 @@ func getConfig(
 				return fmt.Errorf("save stack config: %w", err)
 			}
 		}
-		envCrypter = stackEncrypter
+		// secretsManager's crypter has a cache for crypto operations. As we are encrypting all environment secrets
+		// with the secretManager's encrypter ensure to use its decrypter later on when reading the merged config.
+		envCrypter = secretsManager.Encrypter()
 	}
 
 	stackName := stack.Ref().Name().String()
@@ -1219,17 +1227,19 @@ func getConfig(
 	if ok {
 		var d config.Decrypter
 		if v.Secure() {
-			var err error
-			var state cmdStack.SecretsManagerState
-			if d, state, err = ssml.GetDecrypter(ctx, stack, ps); err != nil {
-				return fmt.Errorf("could not create a decrypter: %w", err)
-			}
-			// This may have setup the stack's secrets provider, so save the stack if needed.
-			if state != cmdStack.SecretsManagerUnchanged {
-				if err = cmdStack.SaveProjectStack(ctx, stack, ps); err != nil {
-					return fmt.Errorf("save stack config: %w", err)
+			if secretsManager == nil {
+				var state cmdStack.SecretsManagerState
+				if secretsManager, state, err = ssml.GetSecretsManager(ctx, stack, ps); err != nil {
+					return err
+				}
+				// This may have setup the stack's secrets provider, so save the stack if needed.
+				if state != cmdStack.SecretsManagerUnchanged {
+					if err = cmdStack.SaveProjectStack(ctx, stack, ps); err != nil {
+						return fmt.Errorf("save stack config: %w", err)
+					}
 				}
 			}
+			d = secretsManager.Decrypter()
 		} else {
 			d = config.NewPanicCrypter()
 		}

commit 95d64dc46acf376cbdccc9f9698f25013f90ebcd

Fix nested config types not recognized (#21246)
This PR fixes an issue where nested config types have not been
recognized. The issue has been introduced in
https://github.com/pulumi/pulumi/pull/20674 by accessing the batch
decrypted config map instead of the stack config. The batch decrypted
config map is not aware of nested config keys while the stack config is
aware of it. The fix makes use of the caching mechanism introduced
during the batch decrypt work so accessing the stack config does not
introduce performance penalties.

Resolves: https://github.com/pulumi/pulumi/issues/21219

sdk/go/common/workspace/config.go
@@ -77,7 +77,8 @@ func validateStackConfigValues(
 		return nil
 	}
 
-	decryptedConfig, err := stackConfig.Decrypt(dec)
+	// Batch decrypt all stack config values once to warm the cache.
+	_, err := stackConfig.Decrypt(dec)
 	if err != nil {
 		return err
 	}
@@ -102,8 +103,14 @@ func validateStackConfigValues(
 		}
 
 		if projectConfigType.IsExplicitlyTyped() {
-			decryptedValue := decryptedConfig[key]
-			err := validateStackConfigValue(stackName, projectConfigKey, projectConfigType, stackValue, decryptedValue)
+			// We need to use stackValue.Value(dec) here to account for nested values.
+			// We cannot get these from the decrypted config map as it does not handle nested values.
+			// Uses the cached decrypted value from the batch decrypt above.
+			decryptedValue, err := stackValue.Value(dec)
+			if err != nil {
+				return err
+			}
+			err = validateStackConfigValue(stackName, projectConfigKey, projectConfigType, stackValue, decryptedValue)
 			if err != nil {
 				return err
 			}

sdk/go/common/workspace/config_test.go
@@ -57,6 +57,40 @@ func TestValidateStackConfigValues(t *testing.T) {
 		require.Contains(t, err.Error(), wantErr)
 	})
 
+	t.Run("Typed Config Validates", func(t *testing.T) {
+		t.Parallel()
+		intType := integerTypeName
+		stringType := stringTypeName
+		boolType := booleanTypeName
+		project := &Project{
+			Name: "testProject",
+			Config: map[string]ProjectConfigType{
+				"testInt": {
+					Type: &intType,
+				},
+				"testNested1.float": {
+					Type: &intType,
+				},
+				"testNested2.bool": {
+					Type: &boolType,
+				},
+				"testSecret": {
+					Type:   &stringType,
+					Secret: true,
+				},
+			},
+		}
+		stackCfg := config.Map{
+			config.MustMakeKey("testProject", "testInt"):     config.NewTypedValue("1", config.TypeInt),
+			config.MustMakeKey("testProject", "testNested1"): config.NewObjectValue("{\"float\":1.0}"),
+			config.MustMakeKey("testProject", "testNested2"): config.NewObjectValue("{\"bool\":true}"),
+			config.MustMakeKey("testProject", "testSecret"):  config.NewSecureValue("dGVzdFZhbHVl"),
+		}
+
+		err := validateStackConfigValues("stackA", project, stackCfg, config.Base64Crypter)
+		require.NoError(t, err)
+	})
+
 	t.Run("Secret Enforced", func(t *testing.T) {
 		t.Parallel()
 		// When project says config is secret, stack value must be secure.