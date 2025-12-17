6870027a1482f1d4ef2691ad4d54b805563ffebf
Julien Poissonnier <julien@caffeine.lu>
2025-12-17 10:27:20 +0100

    Add doc for sdk/go/pulumi-internal/

---
+++ sdk/go/pulumi-internal/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package pulumiinternal and its subpackages contain internal utilities and helpers for the Pulumi Go SDK.
+//
+// This package is intended for internal use only and should not be imported by user code. The APIs in this package are
+// not subject to any stability guarantees and may change without notice.
+package pulumiinternal
