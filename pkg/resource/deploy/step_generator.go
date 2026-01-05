4c9d4c832cf65857759ed0cab8db5dabe73a809e
Fraser Waters <fraser@pulumi.com>
2026-01-05 15:42:09 -0800

    Revert "Make constraint errors typed"
    This reverts commit 5d88dca768a3a4d0bb2d8e70870c1bc7334777be.

--- pkg/resource/deploy/step_generator.go
+++ pkg/resource/deploy/step_generator.go
@@ -391,13 +391,11 @@ func (sg *stepGenerator) validateSteps(steps []Step) ([]Step, error) {
 				// This op has been attempted, it just might fail its constraint.
 				resourcePlan.Ops = resourcePlan.Ops[1:]
 				if !ConstrainedTo(s.Op(), constraint) {
-					return nil, NewConstraintError(fmt.Errorf(
-						"%v is not allowed by the plan: this resource is constrained to %v", s.Op(), constraint))
+					return nil, fmt.Errorf("%v is not allowed by the plan: this resource is constrained to %v", s.Op(), constraint)
 				}
 			} else {
 				if !ConstrainedTo(s.Op(), OpSame) {
-					return nil, NewConstraintError(fmt.Errorf(
-						"%v is not allowed by the plan: no steps were expected for this resource", s.Op()))
+					return nil, fmt.Errorf("%v is not allowed by the plan: no steps were expected for this resource", s.Op())
 				}
 			}
 		}
@@ -413,7 +411,7 @@ func (sg *stepGenerator) validateSteps(steps []Step) ([]Step, error) {
 				// If the resource is in the plan, add the operation to the plan.
 				resourcePlan.Ops = append(resourcePlan.Ops, s.Op())
 			} else if !ConstrainedTo(s.Op(), OpSame) {
-				return nil, NewConstraintError(fmt.Errorf("Expected a new resource plan for %v", urn))
+				return nil, fmt.Errorf("Expected a new resource plan for %v", urn)
 			}
 		}
 	}
@@ -2263,13 +2261,13 @@ func (sg *stepGenerator) GenerateDeletes(targetsOpt UrnTargets, excludesOpt UrnT
 				resourcePlan.Ops = resourcePlan.Ops[1:]
 
 				if !ConstrainedTo(s.Op(), constraint) {
-					return nil, nil, NewConstraintError(fmt.Errorf(
-						"%v is not allowed by the plan: this resource is constrained to %v", s.Op(), constraint))
+					return nil, nil, fmt.Errorf(
+						"%v is not allowed by the plan: this resource is constrained to %v", s.Op(), constraint)
 				}
 			} else {
 				if !ConstrainedTo(s.Op(), OpSame) {
-					return nil, nil, NewConstraintError(fmt.Errorf(
-						"%v is not allowed by the plan: no steps were expected for this resource", s.Op()))
+					return nil, nil, fmt.Errorf(
+						"%v is not allowed by the plan: no steps were expected for this resource", s.Op())
 				}
 			}
 		}
@@ -3351,28 +3349,4 @@ func unwrapSecrets(value resource.PropertyValue) resource.PropertyValue {
 	}
 
 	return value
-}
-
-func NewConstraintError(err error) *ConstraintError {
-	return &ConstraintError{inner: err}
-}
-
-var _ error = &ConstraintError{}
-
-type ConstraintError struct {
-	inner error
-}
-
-func (e *ConstraintError) Error() string {
-	return e.inner.Error()
-}
-
-func (e *ConstraintError) Unwrap() error {
-	return e.inner
-}
-
-func IsConstraintError(err error) bool {
-	var ce *ConstraintError
-	ok := errors.As(err, &ce)
-	return ok
 }
