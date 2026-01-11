1c6a424df091b432c2325ad1f5d704ab647101f9
Ian Wahbe <me@iwahbe.com>
2026-01-10 21:02:00 -0800

    Update dynamic-entries test to use property from aws@5.4.0
    The test was using validationOptions which only exists in aws@5.16.2. Updated to use subjectAlternativeNames which exists in aws@5.4.0 and still tests the entries() PCL function.

--- tests/testdata/codegen/dynamic-entries-pp/nodejs/dynamic-entries.ts
+++ tests/testdata/codegen/dynamic-entries-pp/nodejs/dynamic-entries.ts
@@ -6,12 +6,9 @@ const config = new pulumi.Config();
 const domainName = config.get("domainName") || "";
 // Which method to use for validation. DNS or EMAIL are valid, NONE can be used for certificates that were imported into ACM and then into Terraform.
 const validationMethod = config.get("validationMethod") || "DNS";
-const validationOption = config.getObject<any>("validationOption") || {};
+const alternativeNames = config.getObject<any>("alternativeNames") || {};
 const certificate = new aws.acm.Certificate("certificate", {
-    validationOptions: Object.entries(validationOption).map(([k, v]) => ({key: k, value: v})).map(entry => ({
-        domainName: entry.value.domain_name,
-        validationDomain: entry.value.validation_domain,
-    })),
+    subjectAlternativeNames: Object.entries(alternativeNames).map(([k, v]) => ({key: k, value: v})).map(entry => entry.value),
     domainName: domainName,
     validationMethod: validationMethod,
 });
