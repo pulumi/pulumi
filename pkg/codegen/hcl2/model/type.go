9771188465234d3b736096f9244559ba428257a6
Fraser Waters <fraser@pulumi.com>
2026-01-16 20:47:30 +0000

    Move AliasType

--- pkg/codegen/hcl2/model/type.go
+++ pkg/codegen/hcl2/model/type.go
@@ -109,13 +109,6 @@ var (
 	StringType = NewOpaqueType("string")
 	// DynamicType represents the set of all values.
 	DynamicType = NewOpaqueType("dynamic")
-	// AliasType represents a type for the alias resource option. Aliases are either a string (single URN) or an object
-	// (with "name" and "parent", etc fields).
-	AliasType = NewUnionType(StringType, NewObjectType(map[string]Type{
-		"name":     NewOptionalType(StringType),
-		"noParent": NewOptionalType(BoolType),
-		"parent":   NewOptionalType(DynamicType),
-	}))
 )
 
 func assignableFrom(dest, src Type, assignableFromImpl func() bool) bool {
