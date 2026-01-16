97eb8bb10afcaf7951c12222ae896b28cf57b717
Fraser Waters <fraser@pulumi.com>
2026-01-16 16:28:12 +0000

    lint

--- cmd/pulumi-test-language/tests/l2_resource_option_alias.go
+++ cmd/pulumi-test-language/tests/l2_resource_option_alias.go
@@ -16,8 +16,6 @@ package tests
 
 import (
 	"github.com/pulumi/pulumi/cmd/pulumi-test-language/providers"
-	"github.com/pulumi/pulumi/pkg/v3/display"
-	"github.com/pulumi/pulumi/pkg/v3/engine"
 	"github.com/pulumi/pulumi/pkg/v3/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 	"github.com/stretchr/testify/assert"
@@ -31,22 +29,17 @@ func init() {
 		},
 		Runs: []TestRun{
 			{
-				Assert: func(l *L,
-					projectDirectory string, err error,
-					snap *deploy.Snapshot, changes display.ResourceChanges,
-					events []engine.Event,
-				) {
-					RequireStackResource(l, err, changes)
+				Assert: func(l *L, args AssertArgs) {
+					RequireStackResource(l, args.Err, args.Changes)
 
-					require.Len(l, snap.Resources, 7, "expected 7 resources in snapshot")
+					require.Len(l, args.Snap.Resources, 7, "expected 7 resources in snapshot")
 				},
 			},
 			{
-				Assert: func(l *L,
-					projectDirectory string, err error,
-					snap *deploy.Snapshot, changes display.ResourceChanges,
-					events []engine.Event,
-				) {
+				Assert: func(l *L, args AssertArgs) {
+					snap := args.Snap
+					changes := args.Changes
+
 					// Don't expect any replacements.
 					require.Equal(l, 0, changes[deploy.OpCreate], "expected no create operations")
 
