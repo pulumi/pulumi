0f4105b54041f4ba9ef141e9f1be37411c8d2c01
Ian Wahbe <me@iwahbe.com>
2026-01-21 14:18:13 +0100

    Language test for `ignoreChanges`
    Adds a language/conformance test for the `ignoreChanges` resource option.
    
    Fixes https://github.com/pulumi/pulumi/issues/21501

---
+++ cmd/pulumi-test-language/tests/l2_resource_option_ignore_changes.go
@@ -0,0 +1,51 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"github.com/pulumi/pulumi/cmd/pulumi-test-language/providers"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	LanguageTests["l2-resource-option-ignore-changes"] = LanguageTest{
+		Providers: []func() plugin.Provider{
+			func() plugin.Provider { return &providers.SimpleProvider{} },
+		},
+		Runs: []TestRun{
+			{
+				Assert: func(l *L, res AssertArgs) {
+					err := res.Err
+					snap := res.Snap
+					changes := res.Changes
+
+					RequireStackResource(l, err, changes)
+
+					// The stack, default provider, and 2 explicit
+					// resources: ignoreChanges and notIgnoreChanges.
+					require.Len(l, snap.Resources, 4, "expected 4 resources in snapshot")
+
+					ignoreChanges := RequireSingleNamedResource(l, snap.Resources, "ignoreChanges")
+					assert.Equal(l, []string{"value"}, ignoreChanges.IgnoreChanges)
+
+					notIgnoreChanges := RequireSingleNamedResource(l, snap.Resources, "notIgnoreChanges")
+					assert.Empty(l, notIgnoreChanges.IgnoreChanges)
+				},
+			},
+		},
+	}
+}
