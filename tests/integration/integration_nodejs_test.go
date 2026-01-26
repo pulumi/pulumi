f011f4a16428e109ea964da1247b49f591924f74
Ian Wahbe <me@iwahbe.com>
2026-01-26 14:49:15 +0100

    Allow noisier `get-schema` output

--- tests/integration/integration_nodejs_test.go
+++ tests/integration/integration_nodejs_test.go
@@ -2982,8 +2982,7 @@ func TestNodejsComponentProviderGetSchema(t *testing.T) {
 	// Run the command from a different, sibling, directory. This ensures that
 	// get-package does not rely on the current working directory.
 	e.CWD = t.TempDir()
-	stdout, stderr := e.RunCommand("pulumi", "package", "get-schema", e.RootPath)
-	require.Empty(t, stderr)
+	stdout, _ := e.RunCommand("pulumi", "package", "get-schema", e.RootPath)
 	var schema map[string]any
 	require.NoError(t, json.Unmarshal([]byte(stdout), &schema))
 	require.Equal(t, "nodejs-component-provider", schema["name"].(string))
