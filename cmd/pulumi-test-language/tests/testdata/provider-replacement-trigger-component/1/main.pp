cda84f9a9d66e748649a37811e440ee23438ba6b
Tom Harding <i.am.tom.harding@gmail.com>
2026-01-26 15:40:16 +0100

    Shrinking

--- cmd/pulumi-test-language/tests/testdata/provider-replacement-trigger-component/1/main.pp
+++ cmd/pulumi-test-language/tests/testdata/provider-replacement-trigger-component/1/main.pp
@@ -5,7 +5,6 @@ resource "res" "conformance-component:index:Simple" {
     }
 }
 
-// Make a simple resource so that plugin detection works.
 resource "simpleResource" "simple:index:Resource" {
     value = false
 }
