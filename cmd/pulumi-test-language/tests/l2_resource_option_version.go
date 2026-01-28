9b0c7333a7c6f71edef5317b4caa36b6d75d529c
Ian Wahbe <me@iwahbe.com>
2026-01-28 15:36:37 +0100

    WIP Language test for version

---
+++ cmd/pulumi-test-language/tests/l2_resource_option_version.go
@@ -0,0 +1,47 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"github.com/pulumi/pulumi/cmd/pulumi-test-language/providers"
+	sdkproviders "github.com/pulumi/pulumi/sdk/v3/go/common/providers"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	LanguageTests["l2-resource-option-version"] = LanguageTest{
+		Providers: []func() plugin.Provider{
+			func() plugin.Provider { return &providers.SimpleProvider{} },
+		},
+		Runs: []TestRun{
+			{
+				Assert: func(l *L, res AssertArgs) {
+					err := res.Err
+					snap := res.Snap
+					changes := res.Changes
+
+					RequireStackResource(l, err, changes)
+
+					require.Lenf(l, snap.Resources, 4, "expected 4 resources in snapshot")
+
+					v1Resource := RequireSingleNamedResource(l, snap.Resources, "withVersion")
+					v2Resource := RequireSingleNamedResource(l, snap.Resources, "withoutVersion")
+				},
+			},
+		},
+	}
+}
