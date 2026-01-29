026a4ff58a73265be2d0a6d993ec2acc30a46455
Tom Harding <i.am.tom.harding@gmail.com>
2026-01-29 19:42:38 +0100

    Make it so

--- pkg/engine/lifecycletest/replace_with_test.go
+++ pkg/engine/lifecycletest/replace_with_test.go
@@ -238,12 +238,14 @@ func TestReplaceWithAndDeletedWith(t *testing.T) {
 	require.Equal(t, "created-id-2", snap.Resources[2].ID.String())
 
 	require.Len(t, created, 2)
-	require.Contains(t, created[0].Name(), "resC")
-	require.Contains(t, created[1].Name(), "resA")
+	createdNames := []string{created[0].Name(), created[1].Name()}
+	require.Contains(t, createdNames, "resC")
+	require.Contains(t, createdNames, "resA")
 
 	require.Len(t, deleted, 2)
-	require.Contains(t, deleted[0].Name(), "resB")
-	require.Contains(t, deleted[1].Name(), "resC")
+	deletedNames := []string{deleted[0].Name(), deleted[1].Name()}
+	require.Contains(t, deletedNames, "resB")
+	require.Contains(t, deletedNames, "resC")
 }
 
 func TestReplaceWithDeleteBeforeReplace(t *testing.T) {
