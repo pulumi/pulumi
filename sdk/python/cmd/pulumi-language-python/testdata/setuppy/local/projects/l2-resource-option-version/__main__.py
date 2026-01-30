d4d3b768fdf183f5cabd6862ef31811b95664e1a
Ian Wahbe <me@iwahbe.com>
2026-01-30 02:31:13 +0100

    Add generated code

---
+++ sdk/python/cmd/pulumi-language-python/testdata/setuppy/local/projects/l2-resource-option-version/__main__.py
@@ -0,0 +1,8 @@
+import pulumi
+import pulumi_simple as simple
+
+with_v1 = simple.Resource("withV1", value=True,
+opts = pulumi.ResourceOptions(version="2.0.0"))
+with_v2 = simple.Resource("withV2", value=False,
+opts = pulumi.ResourceOptions(version="26.0.0"))
+with_default = simple.Resource("withDefault", value=True)
