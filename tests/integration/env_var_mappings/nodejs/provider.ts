34e9f2c811a849fe63397f274fb17240142390b6
guineveresaenger <guinevere@pulumi.com>
2026-02-04 14:23:40 -0800

    remove integration test as per code review

--- tests/integration/env_var_mappings/nodejs/provider.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2026, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
-
-export class TestProvider extends pulumi.ProviderResource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("testprovider", name, {}, opts);
-    }
-}
