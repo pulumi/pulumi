544be552c54847e9c4532535372584d9a25bdb18
Julien Poissonnier <julien@caffeine.lu>
2026-02-06 12:05:53 +0100

    Split Node.js language tests
    In https://github.com/pulumi/pulumi/pull/21367 we split the Python language tests into 3 jobs. Now the Node.js language tests are our slowest job on PRs. Split it into 2.

--- sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
+++ sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
@@ -101,7 +101,8 @@ var expectedFailures = map[string]string{
 	"l2-invoke-options-depends-on": "not implemented yet",
 }
 
-func TestLanguage(t *testing.T) {
+// We should run the nodejs tests twice. Once with tsc and once with ts-node.
+func testLanguage(t *testing.T, forceTsc bool) {
 	// Set PATH to include the local dist directory so policy can run.
 	dist, err := filepath.Abs(filepath.Join("..", "..", "dist"))
 	require.NoError(t, err)
@@ -113,124 +114,129 @@ func TestLanguage(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, local := range []bool{false, true} {
-		// We should run the nodejs tests twice. Once with tsc and once with ts-node.
-		for _, forceTsc := range []bool{false, true} {
-			t.Run(fmt.Sprintf("local=%v/forceTsc=%v", local, forceTsc), func(t *testing.T) {
-				t.Parallel()
+		t.Run(fmt.Sprintf("local=%v", local), func(t *testing.T) {
+			t.Parallel()
 
-				if local {
-					// Local doesn't currently work:
-					//
-					// npm error   stdout: "error TS2688: Cannot find type definition file for 'node'.\n" +
-					// npm error     '  The file is in the program because:\n' +
-					// npm error     "    Entry point of type library 'node' specified in compilerOptions\n",
-					t.Skip("node doesn't currently work with local SDKs")
-				}
+			if local {
+				// Local doesn't currently work:
+				//
+				// npm error   stdout: "error TS2688: Cannot find type definition file for 'node'.\n" +
+				// npm error     '  The file is in the program because:\n' +
+				// npm error     "    Entry point of type library 'node' specified in compilerOptions\n",
+				t.Skip("node doesn't currently work with local SDKs")
+			}
 
-				cancel := make(chan bool)
+			cancel := make(chan bool)
 
-				// Run the language plugin
-				handle, err := rpcutil.ServeWithOptions(rpcutil.ServeOptions{
-					Init: func(srv *grpc.Server) error {
-						host := newLanguageHost(engineAddress, "", forceTsc)
-						pulumirpc.RegisterLanguageRuntimeServer(srv, host)
-						return nil
-					},
-					Cancel: cancel,
-				})
-				require.NoError(t, err)
-
-				// Create a temp project dir for the test to run in
-				rootDir, err := filepath.Abs(t.TempDir())
-				require.NoError(t, err)
-
-				snapshotDir := "./testdata"
-				if local {
-					snapshotDir += "/local"
-				} else {
-					snapshotDir += "/published"
-				}
-				if forceTsc {
-					snapshotDir += "/tsc"
-				} else {
-					snapshotDir += "/tsnode"
-				}
-
-				// Prepare to run the tests
-				prepare, err := engine.PrepareLanguageTests(t.Context(), &testingrpc.PrepareLanguageTestsRequest{
-					LanguagePluginName:   "nodejs",
-					LanguagePluginTarget: fmt.Sprintf("127.0.0.1:%d", handle.Port),
-					TemporaryDirectory:   rootDir,
-					SnapshotDirectory:    snapshotDir,
-					CoreSdkDirectory:     "../..",
-					CoreSdkVersion:       sdk.Version.String(),
-					PolicyPackDirectory:  "testdata/policies",
-					Local:                local,
-					SnapshotEdits: []*testingrpc.PrepareLanguageTestsRequest_Replacement{
-						{
-							Path:        "package\\.json",
-							Pattern:     fmt.Sprintf("pulumi-pulumi-%s\\.tgz", sdk.Version.String()),
-							Replacement: "pulumi-pulumi-CORE.VERSION.tgz",
-						},
-						{
-							Path:        "package\\.json",
-							Pattern:     rootDir + "/artifacts",
-							Replacement: "ROOT/artifacts",
-						},
-					},
-					ProvidersDirectory: "testdata/providers",
-				})
-				require.NoError(t, err)
-
-				for _, tt := range tests.Tests {
-					t.Run(tt, func(t *testing.T) {
-						t.Parallel()
-
-						// We can skip the l1- local tests without any SDK there's nothing new being tested here.
-						if local && strings.HasPrefix(tt, "l1-") {
-							t.Skip("Skipping l1- tests in local mode")
-						}
-
-						if expected, ok := expectedFailures[tt]; ok {
-							t.Skipf("Skipping known failure: %s", expected)
-						}
-
-						// Skip l2-large-string on Node.js 24 https://github.com/nodejs/node/issues/58197
-						// TODO: https://github.com/pulumi/pulumi/issues/19442
-						if tt == "l2-large-string" {
-							cmd := exec.Command("node", "-v")
-							output, err := cmd.Output()
-							require.NoError(t, err)
-
-							var major int
-							_, err = fmt.Sscanf(string(output), "v%d", &major)
-							require.NoError(t, err)
-
-							if major >= 24 {
-								t.Skip("Skipping test on Node.js 24+ due to known regression")
-							}
-						}
-
-						result, err := engine.RunLanguageTest(t.Context(), &testingrpc.RunLanguageTestRequest{
-							Token: prepare.Token,
-							Test:  tt,
-						})
-
-						require.NoError(t, err)
-						for _, msg := range result.Messages {
-							t.Log(msg)
-						}
-						ptesting.LogTruncated(t, "stdout", result.Stdout)
-						ptesting.LogTruncated(t, "stderr", result.Stderr)
-						assert.True(t, result.Success)
-					})
-				}
-
-				t.Cleanup(func() {
-					close(cancel)
-					require.NoError(t, <-handle.Done)
-				})
+			// Run the language plugin
+			handle, err := rpcutil.ServeWithOptions(rpcutil.ServeOptions{
+				Init: func(srv *grpc.Server) error {
+					host := newLanguageHost(engineAddress, "", forceTsc)
+					pulumirpc.RegisterLanguageRuntimeServer(srv, host)
+					return nil
+				},
+				Cancel: cancel,
 			})
-		}
+			require.NoError(t, err)
+
+			// Create a temp project dir for the test to run in
+			rootDir, err := filepath.Abs(t.TempDir())
+			require.NoError(t, err)
+
+			snapshotDir := "./testdata"
+			if local {
+				snapshotDir += "/local"
+			} else {
+				snapshotDir += "/published"
+			}
+			if forceTsc {
+				snapshotDir += "/tsc"
+			} else {
+				snapshotDir += "/tsnode"
+			}
+
+			// Prepare to run the tests
+			prepare, err := engine.PrepareLanguageTests(t.Context(), &testingrpc.PrepareLanguageTestsRequest{
+				LanguagePluginName:   "nodejs",
+				LanguagePluginTarget: fmt.Sprintf("127.0.0.1:%d", handle.Port),
+				TemporaryDirectory:   rootDir,
+				SnapshotDirectory:    snapshotDir,
+				CoreSdkDirectory:     "../..",
+				CoreSdkVersion:       sdk.Version.String(),
+				PolicyPackDirectory:  "testdata/policies",
+				Local:                local,
+				SnapshotEdits: []*testingrpc.PrepareLanguageTestsRequest_Replacement{
+					{
+						Path:        "package\\.json",
+						Pattern:     fmt.Sprintf("pulumi-pulumi-%s\\.tgz", sdk.Version.String()),
+						Replacement: "pulumi-pulumi-CORE.VERSION.tgz",
+					},
+					{
+						Path:        "package\\.json",
+						Pattern:     rootDir + "/artifacts",
+						Replacement: "ROOT/artifacts",
+					},
+				},
+				ProvidersDirectory: "testdata/providers",
+			})
+			require.NoError(t, err)
+
+			for _, tt := range tests.Tests {
+				t.Run(tt, func(t *testing.T) {
+					t.Parallel()
+
+					// We can skip the l1- local tests without any SDK there's nothing new being tested here.
+					if local && strings.HasPrefix(tt, "l1-") {
+						t.Skip("Skipping l1- tests in local mode")
+					}
+
+					if expected, ok := expectedFailures[tt]; ok {
+						t.Skipf("Skipping known failure: %s", expected)
+					}
+
+					// Skip l2-large-string on Node.js 24 https://github.com/nodejs/node/issues/58197
+					// TODO: https://github.com/pulumi/pulumi/issues/19442
+					if tt == "l2-large-string" {
+						cmd := exec.Command("node", "-v")
+						output, err := cmd.Output()
+						require.NoError(t, err)
+
+						var major int
+						_, err = fmt.Sscanf(string(output), "v%d", &major)
+						require.NoError(t, err)
+
+						if major >= 24 {
+							t.Skip("Skipping test on Node.js 24+ due to known regression")
+						}
+					}
+
+					result, err := engine.RunLanguageTest(t.Context(), &testingrpc.RunLanguageTestRequest{
+						Token: prepare.Token,
+						Test:  tt,
+					})
+
+					require.NoError(t, err)
+					for _, msg := range result.Messages {
+						t.Log(msg)
+					}
+					ptesting.LogTruncated(t, "stdout", result.Stdout)
+					ptesting.LogTruncated(t, "stderr", result.Stderr)
+					assert.True(t, result.Success)
+				})
+			}
+
+			t.Cleanup(func() {
+				close(cancel)
+				require.NoError(t, <-handle.Done)
+			})
+		})
 	}
+}
+
+func TestLanguageTSC(t *testing.T) {
+	testLanguage(t, true)
+}
+
+func TestLanguageTSNode(t *testing.T) {
+	testLanguage(t, false)
 }
