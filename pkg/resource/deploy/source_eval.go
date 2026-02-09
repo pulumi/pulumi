e21b2cb0a960e9a7533a58f54839d68f92b37948
guineveresaenger <guinevere@pulumi.com>
2026-02-09 11:33:03 -0800

    No need to call Delete; inputs are passed fresh each time

--- pkg/resource/deploy/source_eval.go
+++ pkg/resource/deploy/source_eval.go
@@ -2507,9 +2507,9 @@ func (rm *resmon) RegisterResource(ctx context.Context,
 		if opts.GetPluginDownloadUrl() != "" {
 			providers.SetProviderURL(props, opts.GetPluginDownloadUrl())
 		}
-		// Set env var mappings if any - this will also reflect removals by user
-		providers.SetEnvironmentVariableMappings(props, req.GetEnvVarMappings())
-
+		if len(req.GetEnvVarMappings()) > 0 {
+			providers.SetEnvironmentVariableMappings(props, req.GetEnvVarMappings())
+		}
 		if req.GetPackageRef() != "" {
 			// If the provider resource has a package ref then we need to set all it's input fields as in
 			// newRegisterDefaultProviderEvent.
