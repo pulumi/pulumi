1c4a87d1d9ebb312e0f9317ad21cdc5473b41273
Ian Wahbe <me@iwahbe.com>
2026-02-12 14:23:18 +0100

    Remove the `pulumi-test-language` module in favor of `pkg`

---
+++ pkg/testing/pulumi-test-language/tests/l2_parallel_resources.go
@@ -0,0 +1,65 @@
+// Copyright 2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"math"
+	"sync"
+
+	"github.com/pulumi/pulumi/pkg/v3/engine"
+	"github.com/pulumi/pulumi/pkg/v3/testing/pulumi-test-language/providers"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	LanguageTests["l2-parallel-resources"] = LanguageTest{
+		Providers: []func() plugin.Provider{
+			func() plugin.Provider {
+				return &providers.SyncProvider{
+					CreateLimit: func() *sync.WaitGroup {
+						w := &sync.WaitGroup{}
+						w.Add(3)
+						return w
+					}(),
+				}
+			},
+		},
+		Runs: []TestRun{
+			{
+				UpdateOptions: engine.UpdateOptions{
+					// Use unlimited parallelism
+					Parallel: math.MaxInt32,
+				},
+				Assert: func(l *L, res AssertArgs) {
+					err := res.Err
+					snap := res.Snap
+					changes := res.Changes
+
+					RequireStackResource(l, err, changes)
+
+					// Check we have 3 resources in the snapshot:
+					// 1 Stack
+					// 1 Provider
+					// 3 blocking resources
+					require.Len(l, snap.Resources, 5, "expected 5 resources in snapshot")
+
+					RequireSingleResource(l, snap.Resources, "pulumi:providers:sync")
+					RequireNResources(l, snap.Resources, "sync:index:Block", 3)
+				},
+			},
+		},
+	}
+}
