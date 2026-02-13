440431f65107f0459752ec4321ee86de99d78ac4
Ian Wahbe <me@iwahbe.com>
2026-02-13 18:17:28 +0100

    Add a language test for ModuleFormat with nested types

--- pkg/testing/pulumi-test-language/tests/testdata/l2-module-format/main.pp
+++ pkg/testing/pulumi-test-language/tests/testdata/l2-module-format/main.pp
@@ -22,3 +22,25 @@ resource "res2" "module-format:mod:Resource" {
 output "out2" {
     value = call(res2, "call", { input = "xx" }).output
 }
+
+// First use the fully specified token to invoke and create a resource.
+resource "res3" "module-format:mod/nested_Resource:Resource" {
+    text = invoke("module-format:mod/nested_concatWorld:concatWorld", {
+        value: "hello",
+    }).result
+}
+
+output "out3" {
+    value = call(res3, "call", { input = "x" }).output
+}
+
+// Next use just the module name as defined by the module format
+resource "res4" "module-format:mod/nested:Resource" {
+    text = invoke("module-format:mod/nested:concatWorld", {
+        value: "goodbye",
+    }).result
+}
+
+output "out4" {
+    value = call(res4, "call", { input = "xx" }).output
+}
