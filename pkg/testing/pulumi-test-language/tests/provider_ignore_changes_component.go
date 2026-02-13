ede3ced79235ac09a4657d306d4cc2d1e4f09401
spider-yamet <needmefordev@gmail.com>
2026-02-13 11:04:24 -0800

    Add language tests for ignore changes resource

---
+++ pkg/testing/pulumi-test-language/tests/provider_ignore_changes_component.go
@@ -0,0 +1,54 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"github.com/pulumi/pulumi/pkg/v3/testing/pulumi-test-language/providers"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	LanguageTests["provider-ignore-changes-component"] = LanguageTest{
+		Providers: []func() plugin.Provider{
+			func() plugin.Provider { return &providers.SimpleProvider{} },
+			func() plugin.Provider { return &providers.ConformanceComponentProvider{} },
+		},
+		LanguageProviders: []string{"conformance-component"},
+		Runs: []TestRun{
+			{
+				Assert: func(l *L, res AssertArgs) {
+					err := res.Err
+					snap := res.Snap
+					changes := res.Changes
+
+					RequireStackResource(l, err, changes)
+
+					// Stack, 2 providers, 2 components, 2 res-child, 1 simpleResource.
+					require.Len(l, snap.Resources, 8, "expected 8 resources in snapshot")
+
+					withIgnore := RequireSingleNamedResource(l, snap.Resources, "withIgnoreChanges")
+					assert.Equal(l, []string{"value"}, withIgnore.IgnoreChanges,
+						"expected component with ignoreChanges to have IgnoreChanges set")
+
+					withoutIgnore := RequireSingleNamedResource(l, snap.Resources, "withoutIgnoreChanges")
+					assert.Empty(l, withoutIgnore.IgnoreChanges,
+						"expected component without ignoreChanges to have empty IgnoreChanges")
+				},
+			},
+		},
+	}
+}
