1b412475dffe467505cdc818fb9c9e3483a8911a
Thomas Gummerer <t.gummerer@gmail.com>
2026-02-13 16:14:38 +0100

    create refresh steps for custom and provider resources
    In `refresh --run-program`, we get a SIE, when a component or provider
    resource is a dependency of another resource that's going to be same
    stepped during the program execution.  This happens because we don't
    generate refresh steps for these resources until after the same step
    happened, which in turn means that the resource that depends on the
    component resource ends up coming before the resource that depends on
    it in the snapshot.
    
    We still generate a refresh step for the component/provider resource
    later in `GenerateRefreshes` as long as there wasn't any step
    modifying it beforehand.
    
    Instead if we just let the resource go through the regular refresh
    cycle, we preserve the snapshot ordering correctly. Note that the
    refresh step is essentially a noop (see
    https://github.com/pulumi/pulumi/blob/master/pkg/resource/deploy/step.go#L1454-L1463),
    so this doesn't change the outcome of the program, though the
    resources will be displayed in the refresh output as refreshed.

--- pkg/resource/deploy/step_generator.go
+++ pkg/resource/deploy/step_generator.go
@@ -755,11 +755,9 @@ func (sg *stepGenerator) generateSteps(event RegisterResourceEvent) ([]Step, boo
 	// If we're doing refreshes then this is the point where we need to fire off a refresh step for this
 	// resource, to call back into GenerateSteps later.
 	//
-	// Only need to do refresh steps here for custom non-provider resources that have an old state.
+	// Only need to do refresh steps here for resources that have an old state.
 	if old != nil &&
-		sg.refresh &&
-		goal.Custom &&
-		!sdkproviders.IsProviderType(goal.Type) {
+		sg.refresh {
 		cts := &promise.CompletionSource[*resource.State]{}
 		// Set up the cts to trigger a continueStepsFromRefresh when it resolves
 		go PanicRecovery(sg.deployment.panicErrs, func() {
