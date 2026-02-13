d7602b5bd8c5c3f1f97ba06a21f375b7050939ff
spider-yamet <needmefordev@gmail.com>
2026-02-13 06:38:25 -0800

    Refactor snapshot files for ubuntu ci

--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsc/projects/provider-depends-on-component/index.ts
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsc/projects/provider-depends-on-component/index.ts
@@ -2,7 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as conformance_component from "@pulumi/conformance-component";
 import * as simple from "@pulumi/simple";
 
-const noDependsOn = new conformance_component.Simple("noDependsOn", { value: true });
-const withDependsOn = new conformance_component.Simple("withDependsOn", { value: true }, { dependsOn: [noDependsOn] });
+// Component with no dependencies (the contrast)
+const noDependsOn = new conformance_component.Simple("noDependsOn", {value: true});
+// Component with dependsOn
+const withDependsOn = new conformance_component.Simple("withDependsOn", {value: true}, {
+    dependsOn: [noDependsOn],
+});
 // Make a simple resource so that plugin detection works.
-const simpleResource = new simple.Resource("simpleResource", { value: false });
+const simpleResource = new simple.Resource("simpleResource", {value: false});
