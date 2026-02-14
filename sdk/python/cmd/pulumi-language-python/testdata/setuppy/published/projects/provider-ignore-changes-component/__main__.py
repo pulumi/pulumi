251b6526536365cf7852c59e971301be00d8d76c
spider-yamet <needmefordev@gmail.com>
2026-02-14 13:54:46 -0800

    Remove snapshot files for fix ubuntu compatibility

--- sdk/python/cmd/pulumi-language-python/testdata/setuppy/published/projects/provider-ignore-changes-component/__main__.py
+++
@@ -1,9 +0,0 @@
-import pulumi
-import pulumi_conformance_component as conformance_component
-import pulumi_simple as simple
-
-with_ignore_changes = conformance_component.Simple("withIgnoreChanges", value=True,
-    opts=pulumi.ResourceOptions(ignore_changes=["value"]))
-without_ignore_changes = conformance_component.Simple("withoutIgnoreChanges", value=True)
-# Make a simple resource so that plugin detection works.
-simple_resource = simple.Resource("simpleResource", value=False)
