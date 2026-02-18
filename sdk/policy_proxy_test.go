pulumi/pulumi

commit 0f0fbfe0fbdec14dcda8ceae09aecabb254c8140

Use RunPlugin instead of policy shim plugins (#21684)
Towards fixing https://github.com/pulumi/pulumi/issues/20402

Passing all config via env vars can run into ARG_MAX limits. Using the
new grpc based RunPlugin approach avoids that, passing config via grpc
instead (which still has limits but are much larger, and don't change
from machine to machine).

To support current NodeJS and Python policy packs we start a proxy
service in the language host so that we can get the stack configuration
via the `StackConfigure` grpc call to the analyser plugin and then use
the information from that to set the legacy environment variables with
which to start the actual policy process up.

This has some extra changes to `Attach` from the first run to watch for
the sub-command exiting before we read a port number off it. In the
first iteration of this change we would hang at that point, now we
correctly close off the stdout pipes and exit the read.

It also tweaks the python RunPlugin to allow no venv for policy packs.
Turns out that while we always defaulted to setting that option when
making a new policy pack a user could remove that option to get the
policy pack to run via the global python install.

sdk/policy_proxy.go
@@ -1,0 +1,259 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sdk
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os/exec"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/promise"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/rpcutil"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// PolicyProxy implements the pulumirpc.AnalyzerServer interface by proxying all requests to a real policy pack. This is
+// used by node and python language hosts to proxy new style engine requests for policy packs (using RunPlugin) to
+// historical policy pack libraries that expected to be called with a informational environment variables set.
+//
+// Nothing new should use this type! New policy pack implementations should implement StackConfigure and Handshake
+// internally as part of the analyzer plugin process.
+type PolicyProxy struct {
+	pulumirpc.UnsafeAnalyzerServer
+
+	stackConfiguration *promise.CompletionSource[*pulumirpc.AnalyzerStackConfigureRequest]
+	client             *promise.CompletionSource[pulumirpc.AnalyzerClient]
+
+	port int
+
+	stdout  io.Writer
+	stdoutR *io.PipeReader
+	stdoutW *io.PipeWriter
+}
+
+// NewPolicyProxy starts a new grpc server implementing the Analyzer service that proxies all requests to the actual
+// policy pack process. The only request that is handled directly is Handshake and StackConfiguration, which are used to
+// start up the actual policy pack process. For compatibility if we get a call to GetAnalyzerInfo without having seen a
+// call for StackConfiguration we will start the policy pack process with no configuration.
+func NewPolicyProxy(ctx context.Context, stdout io.Writer) (*PolicyProxy, io.Writer, error) {
+	stdoutR, stdoutW := io.Pipe()
+	analyzer := &PolicyProxy{
+		stackConfiguration: &promise.CompletionSource[*pulumirpc.AnalyzerStackConfigureRequest]{},
+		client:             &promise.CompletionSource[pulumirpc.AnalyzerClient]{},
+
+		stdout:  stdout,
+		stdoutR: stdoutR,
+		stdoutW: stdoutW,
+	}
+
+	var cancel chan bool
+	done := ctx.Done()
+	if done != nil {
+		cancel = make(chan bool, 1)
+		go func() {
+			<-done
+			cancel <- true
+		}()
+	}
+
+	handle, err := rpcutil.ServeWithOptions(rpcutil.ServeOptions{
+		Cancel: cancel,
+		Init: func(srv *grpc.Server) error {
+			pulumirpc.RegisterAnalyzerServer(srv, analyzer)
+			return nil
+		},
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	analyzer.port = handle.Port
+	logging.V(9).Infof("Started policy proxy on port %d", analyzer.port)
+
+	return analyzer, stdoutW, nil
+}
+
+func (p *PolicyProxy) AwaitConfiguration(ctx context.Context) (*pulumirpc.AnalyzerStackConfigureRequest, error) {
+	logging.V(9).Infof("Awaiting policy pack configuration on port %d", p.port)
+	fmt.Fprintf(p.stdout, "%d\n", p.port)
+	config, err := p.stackConfiguration.Promise().Result(ctx)
+	logging.V(9).Infof("Awaited policy pack configuration %v", config)
+	return config, err
+}
+
+// Attach waits for the subcommand to run and tries to proxy the grpc connection to it.
+func (p *PolicyProxy) Attach(ctx context.Context, cmd *exec.Cmd) error {
+	// We need to wait for cmd to exit and close off stdout when it does so the fscanf doesn't block forever.
+	exit := &promise.CompletionSource[struct{}]{}
+	go func() {
+		err := cmd.Wait()
+		if err == nil {
+			exit.Fulfill(struct{}{})
+		} else {
+			exit.Reject(err)
+		}
+		p.stdoutW.Close()
+		p.stdoutR.Close()
+	}()
+
+	logging.V(9).Infof("Waiting to attach to policy pack")
+	// Read the port number from the subprocess's stdout.
+	var port int
+	_, err := fmt.Fscanf(p.stdoutR, "%d\n", &port)
+	if err != nil {
+		err = fmt.Errorf("could not read policy pack port: %w", err)
+		p.client.Reject(err)
+		return err
+	}
+	logging.V(9).Infof("Attaching to policy pack on port %d", port)
+
+	// Proxy everything else to the normal stdout.
+	go func() {
+		_, err := io.Copy(p.stdout, p.stdoutR)
+		if err != nil {
+			fmt.Fprintf(p.stdout, "error copying stdout: %v\n", err)
+		}
+	}()
+
+	// Attach to the subprocess over gRPC.
+	conn, err := grpc.NewClient(
+		fmt.Sprintf("127.0.0.1:%d", port),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		err = fmt.Errorf("dial policy pack: %w", err)
+		p.client.Reject(err)
+		return err
+	}
+
+	client := pulumirpc.NewAnalyzerClient(conn)
+
+	// Now that we're attached to the subprocess forward it the stack configuration we got earlier.
+	stkcfg, err, has := p.stackConfiguration.Promise().TryResult()
+	contract.AssertNoErrorf(err, "stackConfiguration promise should never be rejected")
+	if has {
+		_, err := client.ConfigureStack(ctx, stkcfg)
+		if err != nil {
+			return fmt.Errorf("policy pack configuration failed: %w", err)
+		}
+	}
+
+	p.client.Fulfill(client)
+	_, err = exit.Promise().Result(ctx)
+	return err
+}
+
+func (p *PolicyProxy) ConfigureStack(
+	ctx context.Context,
+	req *pulumirpc.AnalyzerStackConfigureRequest,
+) (*pulumirpc.AnalyzerStackConfigureResponse, error) {
+	p.stackConfiguration.Fulfill(req)
+	return &pulumirpc.AnalyzerStackConfigureResponse{}, nil
+}
+
+func (p *PolicyProxy) Handshake(
+	ctx context.Context,
+	req *pulumirpc.AnalyzerHandshakeRequest,
+) (*pulumirpc.AnalyzerHandshakeResponse, error) {
+	return &pulumirpc.AnalyzerHandshakeResponse{}, nil
+}
+
+func (p *PolicyProxy) awaitClient(ctx context.Context) (pulumirpc.AnalyzerClient, error) {
+	// The engine sometimes calls others methods before calling StackConfiguration, for example for `policy
+	// publish`, in this case just report no configuration.
+	p.stackConfiguration.Fulfill(nil)
+	logging.V(9).Infof("Awaiting policy pack client")
+	client, err := p.client.Promise().Result(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("policy pack not started: %w", err)
+	}
+	return client, nil
+}
+
+func (p *PolicyProxy) Analyze(
+	ctx context.Context, req *pulumirpc.AnalyzeRequest,
+) (*pulumirpc.AnalyzeResponse, error) {
+	client, err := p.awaitClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.Analyze(ctx, req)
+}
+
+func (p *PolicyProxy) AnalyzeStack(
+	ctx context.Context, req *pulumirpc.AnalyzeStackRequest,
+) (*pulumirpc.AnalyzeResponse, error) {
+	client, err := p.awaitClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.AnalyzeStack(ctx, req)
+}
+
+func (p *PolicyProxy) Remediate(
+	ctx context.Context, req *pulumirpc.AnalyzeRequest,
+) (*pulumirpc.RemediateResponse, error) {
+	client, err := p.awaitClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.Remediate(ctx, req)
+}
+
+func (p *PolicyProxy) GetAnalyzerInfo(
+	ctx context.Context,
+	req *emptypb.Empty,
+) (*pulumirpc.AnalyzerInfo, error) {
+	client, err := p.awaitClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.GetAnalyzerInfo(ctx, req)
+}
+
+func (p *PolicyProxy) GetPluginInfo(
+	ctx context.Context,
+	req *emptypb.Empty,
+) (*pulumirpc.PluginInfo, error) {
+	logging.V(9).Infof("GetPluginInfo")
+	client, err := p.awaitClient(ctx)
+	logging.V(9).Infof("GetPluginInfo: awaitClient returned err=%v", err)
+	if err != nil {
+		return nil, err
+	}
+	info, err := client.GetPluginInfo(ctx, req)
+	logging.V(9).Infof("GetPluginInfo: client.GetPluginInfo returned info=%v, err=%v", info, err)
+	return info, err
+}
+
+func (p *PolicyProxy) Configure(ctx context.Context, req *pulumirpc.ConfigureAnalyzerRequest) (*emptypb.Empty, error) {
+	client, err := p.awaitClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.Configure(ctx, req)
+}
+
+func (p *PolicyProxy) Cancel(ctx context.Context, req *emptypb.Empty) (*emptypb.Empty, error) {
+	client, err := p.awaitClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.Cancel(ctx, req)
+}

.gitignore
@@ -68,13 +68,9 @@ pkg/engine/lifecycletest/testdata/rapid/
 goreleaser
 pulumi-resource-pulumi-nodejs
 pulumi-resource-pulumi-python
-pulumi-analyzer-policy
-pulumi-analyzer-policy-python
 pulumi-language-python-exec
 pulumi-resource-pulumi-nodejs.cmd
 pulumi-resource-pulumi-python.cmd
-pulumi-analyzer-policy.cmd
-pulumi-analyzer-policy-python.cmd
 artifacts/
 goreleaser-downloads/
 goreleaser-prebuilt/

scripts/prep-for-goreleaser.sh
@@ -26,14 +26,8 @@ install_file () {
     done
 }
 
-install_file sdk/nodejs/dist/pulumi-analyzer-policy                         linux   darwin
-install_file sdk/nodejs/dist/pulumi-analyzer-policy.cmd                     windows
-
 install_file sdk/nodejs/dist/pulumi-resource-pulumi-nodejs                  linux   darwin
 install_file sdk/nodejs/dist/pulumi-resource-pulumi-nodejs.cmd              windows
-
-install_file sdk/python/dist/pulumi-analyzer-policy-python                  linux   darwin
-install_file sdk/python/dist/pulumi-analyzer-policy-python.cmd              windows
 
 install_file sdk/python/dist/pulumi-resource-pulumi-python                  linux   darwin
 install_file sdk/python/dist/pulumi-resource-pulumi-python.cmd              windows

sdk/go/common/resource/plugin/analyzer_plugin.go
@@ -142,40 +142,29 @@ func NewPolicyAnalyzer(
 		return res, nil
 	}
 
-	// This first section is a back compatibility bit for the old way of running analyzer plugins where we would look
-	// for a plugin called "pulumi-analyzer-policy-<runtime>" and invoke that plugin with two arguments, the engine
-	// address and the policy pack path. We still do this for python and nodejs, but not for other actual "languages"
-	// (i.e. things with language plugins), but have to leave this in to ensure things like
-	// https://github.com/pulumi/pulumi-policy-opa continue to work (although in time they could probably be moved to
-	// just be language runtimes like the rest).
+	// This first section is a back compatibility bit for the old way of running analyzer plugins where we
+	// would look for a plugin called "pulumi-analyzer-policy-<runtime>" and invoke that plugin with two
+	// arguments, the engine address and the policy pack path. We don't do this for actual "languages" (i.e.
+	// things with language plugins), but have to leave this in to ensure things like
+	// https://github.com/pulumi/pulumi-policy-opa continue to work (although in time they could probably be
+	// moved to just be language runtimes like the rest).
+	if hasPlugin == nil {
+		hasPlugin = func(spec workspace.PluginDescriptor) bool {
+			path, err := workspace.GetPluginPath(
+				ctx.baseContext,
+				ctx.Diag,
+				spec,
+				host.GetProjectPlugins())
+			return err == nil && path != ""
+		}
+	}
+	foundLanguagePlugin := hasPlugin(workspace.PluginDescriptor{Name: proj.Runtime.Name(), Kind: apitype.LanguagePlugin})
 
 	var plug *Plugin
-	var foundLanguagePlugin bool
-	// Try to load the language plugin for the runtime, except for python and node that _for now_ continue using the
-	// legacy behavior.
-	if proj.Runtime.Name() != "python" && proj.Runtime.Name() != "nodejs" {
-		if hasPlugin == nil {
-			hasPlugin = func(spec workspace.PluginDescriptor) bool {
-				path, err := workspace.GetPluginPath(
-					ctx.baseContext,
-					ctx.Diag,
-					spec,
-					host.GetProjectPlugins())
-				return err == nil && path != ""
-			}
-		}
-
-		foundLanguagePlugin = hasPlugin(workspace.PluginDescriptor{Name: proj.Runtime.Name(), Kind: apitype.LanguagePlugin})
-	}
 	if !foundLanguagePlugin {
-		// Couldn't get a language plugin, fall back to the old behavior
-
-		// For historical reasons, the Node.js plugin name is just "policy".
-		// All other languages have the runtime appended, e.g. "policy-<runtime>".
-		policyAnalyzerName := "policy"
-		if !strings.EqualFold(proj.Runtime.Name(), "nodejs") {
-			policyAnalyzerName = "policy-" + proj.Runtime.Name()
-		}
+		// Couldn't get a language plugin, fall back to the old behavior, of trying to run
+		// "pulumi-analyzer-policy-<runtime>".
+		policyAnalyzerName := "policy-" + proj.Runtime.Name()
 
 		// Load the policy-booting analyzer plugin (i.e., `pulumi-analyzer-${policyAnalyzerName}`).
 		var pluginPath string

sdk/go/common/workspace/plugins.go
@@ -1996,9 +1996,7 @@ func IsPluginBundled(kind apitype.PluginKind, name string) bool {
 		(kind == apitype.LanguagePlugin && name == "yaml") ||
 		(kind == apitype.LanguagePlugin && name == "java") ||
 		(kind == apitype.ResourcePlugin && name == "pulumi-nodejs") ||
-		(kind == apitype.ResourcePlugin && name == "pulumi-python") ||
-		(kind == apitype.AnalyzerPlugin && name == "policy") ||
-		(kind == apitype.AnalyzerPlugin && name == "policy-python")
+		(kind == apitype.ResourcePlugin && name == "pulumi-python")
 }
 
 // GetPluginPath finds a plugin's path by its kind, name, and optional version.  It will match the latest version that

sdk/nodejs/Makefile
@@ -50,7 +50,6 @@ build_package:: ensure
 
 build_plugin: ../../bin/pulumi-language-nodejs
 	cp ./dist/pulumi-resource-pulumi-nodejs ../../bin
-	cp ./dist/pulumi-analyzer-policy ../../bin
 
 .PHONY: ../../bin/pulumi-language-nodejs
 ../../bin/pulumi-language-nodejs:
@@ -63,7 +62,6 @@ build:: build_package build_plugin
 
 install_package:: build
 	cp dist/pulumi-resource-pulumi-nodejs* "$(PULUMI_BIN)"
-	cp dist/pulumi-analyzer-policy* "$(PULUMI_BIN)"
 
 install_plugin:: build
 	GOBIN=$(PULUMI_BIN) go install -C cmd/pulumi-language-nodejs \
@@ -135,14 +133,12 @@ dist::
 	go install -C cmd/pulumi-language-nodejs \
 		-ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${LANGHOST_PKG}
 	cp dist/pulumi-resource-pulumi-nodejs "${GOBIN}"
-	cp dist/pulumi-analyzer-policy "${GOBIN}"
 
 brew:: BREW_VERSION := $(shell ../../scripts/get-version HEAD)
 brew::
 	go install -C cmd/pulumi-language-nodejs \
 	   -ldflags "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${VERSION}" ${LANGHOST_PKG}
 	cp dist/pulumi-resource-pulumi-nodejs "$$(go env GOPATH)"/bin/
-	cp dist/pulumi-analyzer-policy "$$(go env GOPATH)"/bin/
 
 publish::
 	bash -c ../../scripts/publish_npm.sh

commit 797454d3a3ae0ca95c77f3fbe7ee6c56b655bdec

Fix hang when loading policy packs
This change fixes a hang that can happen when loading policy packs that use v1.18.1 or older of the policy SDK. The hang was a regression introduced in https://github.com/pulumi/pulumi/pull/21684.

In that change, a new policy proxy was introduced, used to run policy packs. As part of starting up the policy pack, the `ConfigureStack` RPC is called from the proxy. However, the code in the proxy does not gracefully handle `Unimplemented` errors, and policy packs that use v1.18.1 or older of the policy SDK do not have support for `ConfigureStack`. This leads to hangs when such policy packs are loaded.

The fix is to properly ignore `Unimplemented` errors.

Additionally, if there are any errors from `ConfigureStack`, we need to reject the client promise, to avoid hangs waiting on the client to be fulfilled.

sdk/policy_proxy.go
@@ -26,7 +26,9 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/rpcutil"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -150,7 +152,15 @@ func (p *PolicyProxy) Attach(ctx context.Context, cmd *exec.Cmd) error {
 	if has {
 		_, err := client.ConfigureStack(ctx, stkcfg)
 		if err != nil {
-			return fmt.Errorf("policy pack configuration failed: %w", err)
+			st, ok := status.FromError(err)
+			if !ok || st.Code() != codes.Unimplemented {
+				// Reject the client promise so any goroutines waiting in awaitClient can unblock.
+				configErr := fmt.Errorf("policy pack configuration failed: %w", err)
+				p.client.Reject(configErr)
+				return configErr
+			}
+			// codes.Unimplemented is fine -- older policy packs don't implement ConfigureStack.
+			logging.V(7).Infof("PolicyProxy.Attach: ConfigureStack not supported by policy pack, ignoring")
 		}
 	}
 

sdk/policy_proxy_test.go
@@ -1,0 +1,232 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sdk
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net"
+	"os/exec"
+	"testing"
+	"time"
+
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// mockPolicyPackServer is a minimal gRPC AnalyzerServer implementation used for testing PolicyProxy.
+// By default (embedding UnimplementedAnalyzerServer) every RPC returns codes.Unimplemented.
+// Individual test cases override the methods they care about.
+type mockPolicyPackServer struct {
+	pulumirpc.UnimplementedAnalyzerServer
+
+	// configureStackFn, if non-nil, is called by ConfigureStack. When nil the embedded
+	// UnimplementedAnalyzerServer.ConfigureStack is used, which returns codes.Unimplemented —
+	// exactly what old policy-pack SDKs that predate ConfigureStack support do.
+	configureStackFn func(context.Context, *pulumirpc.AnalyzerStackConfigureRequest) (
+		*pulumirpc.AnalyzerStackConfigureResponse, error)
+}
+
+func (m *mockPolicyPackServer) GetPluginInfo(
+	_ context.Context, _ *emptypb.Empty,
+) (*pulumirpc.PluginInfo, error) {
+	return &pulumirpc.PluginInfo{Version: "1.0.0"}, nil
+}
+
+func (m *mockPolicyPackServer) ConfigureStack(
+	ctx context.Context,
+	req *pulumirpc.AnalyzerStackConfigureRequest,
+) (*pulumirpc.AnalyzerStackConfigureResponse, error) {
+	if m.configureStackFn != nil {
+		return m.configureStackFn(ctx, req)
+	}
+	// Delegate to the embedded stub, which returns codes.Unimplemented.
+	return m.UnimplementedAnalyzerServer.ConfigureStack(ctx, req)
+}
+
+// startMockPolicyPackServer starts a gRPC server with srv registered as the AnalyzerServer and
+// returns the port it is listening on. The server is stopped when t ends.
+func startMockPolicyPackServer(t *testing.T, srv *mockPolicyPackServer) int {
+	t.Helper()
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+
+	grpcSrv := grpc.NewServer()
+	pulumirpc.RegisterAnalyzerServer(grpcSrv, srv)
+	go grpcSrv.Serve(lis) //nolint:errcheck
+	t.Cleanup(grpcSrv.Stop)
+
+	return lis.Addr().(*net.TCPAddr).Port
+}
+
+// startLongRunningCmd starts a process that stays alive until the test ends (or it is killed).
+func startLongRunningCmd(t *testing.T) *exec.Cmd {
+	t.Helper()
+	cmd := exec.Command("sleep", "100")
+	require.NoError(t, cmd.Start())
+	t.Cleanup(func() {
+		if cmd.Process != nil {
+			cmd.Process.Kill() //nolint:errcheck
+		}
+	})
+	return cmd
+}
+
+// runAttach runs proxy.Attach in a goroutine and returns a channel that receives the result.
+func runAttach(ctx context.Context, proxy *PolicyProxy, cmd *exec.Cmd) <-chan error {
+	ch := make(chan error, 1)
+	go func() { ch <- proxy.Attach(ctx, cmd) }()
+	return ch
+}
+
+// awaitClientFulfilled waits up to 5 seconds for proxy.client to be resolved and returns it.
+func awaitClientFulfilled(t *testing.T, proxy *PolicyProxy) pulumirpc.AnalyzerClient {
+	t.Helper()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	client, err := proxy.client.Promise().Result(ctx)
+	require.NoError(t, err, "proxy.client promise should be fulfilled without error")
+	require.NotNil(t, client)
+	return client
+}
+
+// TestPolicyProxy_Attach_OldPolicyPackNoConfigureStack tests the fix for issue #21864.
+//
+// Old policy-pack SDKs (e.g. @pulumi/policy <= v1.18.1) do not implement the ConfigureStack RPC.
+// Before the fix, PolicyProxy.Attach returned an error without fulfilling the client promise,
+// causing awaitClient (and thus GetPluginInfo / GetAnalyzerInfo) to block forever, which in turn
+// prevented GracefulStop from completing and hung the engine.
+func TestPolicyProxy_Attach_OldPolicyPackNoConfigureStack(t *testing.T) {
+	t.Parallel()
+
+	// This mock relies on the default UnimplementedAnalyzerServer.ConfigureStack, which returns
+	// codes.Unimplemented — just like an old policy pack that has no knowledge of this RPC.
+	port := startMockPolicyPackServer(t, &mockPolicyPackServer{})
+
+	ctx := context.Background()
+	proxy, stdoutW, err := NewPolicyProxy(ctx, &bytes.Buffer{})
+	require.NoError(t, err)
+
+	// Simulate the engine calling ConfigureStack on the proxy before the policy pack starts.
+	_, err = proxy.ConfigureStack(ctx, &pulumirpc.AnalyzerStackConfigureRequest{})
+	require.NoError(t, err)
+
+	cmd := startLongRunningCmd(t)
+	attachDone := runAttach(ctx, proxy, cmd)
+
+	// Give Attach the mock server port via the pipe. This must happen after runAttach because
+	// io.Pipe is synchronous: the write blocks until Attach's Fscanf reads from the other end.
+	fmt.Fprintf(stdoutW, "%d\n", port)
+
+	// Before the fix this would time out because client was never fulfilled.
+	client := awaitClientFulfilled(t, proxy)
+
+	// Sanity-check: the fulfilled client is usable.
+	info, err := client.GetPluginInfo(ctx, &emptypb.Empty{})
+	require.NoError(t, err)
+	assert.Equal(t, "1.0.0", info.GetVersion())
+
+	// Kill the subprocess to let Attach return.
+	cmd.Process.Kill() //nolint:errcheck
+	<-attachDone       // wait for goroutine to exit; error is expected (killed process)
+}
+
+// TestPolicyProxy_Attach_NewPolicyPackWithConfigureStack verifies that when the policy pack
+// implements ConfigureStack successfully the proxy client is still fulfilled.
+func TestPolicyProxy_Attach_NewPolicyPackWithConfigureStack(t *testing.T) {
+	t.Parallel()
+
+	port := startMockPolicyPackServer(t, &mockPolicyPackServer{
+		configureStackFn: func(_ context.Context, _ *pulumirpc.AnalyzerStackConfigureRequest) (
+			*pulumirpc.AnalyzerStackConfigureResponse, error,
+		) {
+			return &pulumirpc.AnalyzerStackConfigureResponse{}, nil
+		},
+	})
+
+	ctx := context.Background()
+	proxy, stdoutW, err := NewPolicyProxy(ctx, &bytes.Buffer{})
+	require.NoError(t, err)
+
+	_, err = proxy.ConfigureStack(ctx, &pulumirpc.AnalyzerStackConfigureRequest{})
+	require.NoError(t, err)
+
+	cmd := startLongRunningCmd(t)
+	attachDone := runAttach(ctx, proxy, cmd)
+
+	// Write port after starting Attach goroutine so the synchronous pipe write doesn't block.
+	fmt.Fprintf(stdoutW, "%d\n", port)
+
+	client := awaitClientFulfilled(t, proxy)
+
+	info, err := client.GetPluginInfo(ctx, &emptypb.Empty{})
+	require.NoError(t, err)
+	assert.Equal(t, "1.0.0", info.GetVersion())
+
+	cmd.Process.Kill() //nolint:errcheck
+	<-attachDone
+}
+
+// TestPolicyProxy_Attach_ConfigureStackError verifies that when ConfigureStack fails with a
+// non-Unimplemented error the client promise is rejected (not left unresolved), so callers of
+// awaitClient get an error rather than hanging indefinitely.
+func TestPolicyProxy_Attach_ConfigureStackError(t *testing.T) {
+	t.Parallel()
+
+	configureErr := status.Error(codes.Internal, "policy pack misconfigured")
+	port := startMockPolicyPackServer(t, &mockPolicyPackServer{
+		configureStackFn: func(_ context.Context, _ *pulumirpc.AnalyzerStackConfigureRequest) (
+			*pulumirpc.AnalyzerStackConfigureResponse, error,
+		) {
+			return nil, configureErr
+		},
+	})
+
+	ctx := context.Background()
+	proxy, stdoutW, err := NewPolicyProxy(ctx, &bytes.Buffer{})
+	require.NoError(t, err)
+
+	_, err = proxy.ConfigureStack(ctx, &pulumirpc.AnalyzerStackConfigureRequest{})
+	require.NoError(t, err)
+
+	// We don't need a long-running cmd here: Attach returns early on ConfigureStack error before
+	// waiting for the process to exit. Use a no-op process so cmd.Wait() is valid.
+	cmd := exec.Command("true")
+	require.NoError(t, cmd.Start())
+	attachDone := runAttach(ctx, proxy, cmd)
+
+	// Write port after starting Attach goroutine so the synchronous pipe write doesn't block.
+	fmt.Fprintf(stdoutW, "%d\n", port)
+
+	// Attach should return an error.
+	select {
+	case attachErr := <-attachDone:
+		require.ErrorContains(t, attachErr, "policy pack configuration failed")
+	case <-time.After(5 * time.Second):
+		t.Fatal("Attach did not return within timeout after ConfigureStack error")
+	}
+
+	// The client promise should be rejected, not unresolved.
+	clientCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	_, clientErr := proxy.client.Promise().Result(clientCtx)
+	require.ErrorContains(t, clientErr, "policy pack configuration failed")
+}