60f74146dd3fe9096ff84ff893dd31a34db9ed8a
Ian Wahbe <me@iwahbe.com>
2026-02-18 11:31:29 +0100

    Move a missed language test
    It looks like this test merged after we moved `pulumi-test-language`. Since it's
    just a free-floating Go file, it wasn't caught by linters. It causes
    `pulumi-yaml` to fail to upgrade, since there it is nested in a `go.mod` (rooted
    in `pulumi-yaml`) and the code references a non-existent import path.

--- sdk/python/cmd/pulumi-language-python/testdata/setuppy/published/projects/provider-depends-on-component/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/setuppy/published/projects/provider-depends-on-component/__main__.py
@@ -2,7 +2,10 @@ import pulumi
 import pulumi_conformance_component as conformance_component
 import pulumi_simple as simple
 
+# Component with no dependencies (the contrast)
 no_depends_on = conformance_component.Simple("noDependsOn", value=True)
-with_depends_on = conformance_component.Simple("withDependsOn", value=True, opts=pulumi.ResourceOptions(depends_on=[no_depends_on]))
+# Component with dependsOn
+with_depends_on = conformance_component.Simple("withDependsOn", value=True,
+opts = pulumi.ResourceOptions(depends_on=[no_depends_on]))
 # Make a simple resource so that plugin detection works.
 simple_resource = simple.Resource("simpleResource", value=False)
