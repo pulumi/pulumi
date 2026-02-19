f8c9e9af63a001e481d79fa998d47b564ad50806
spider-yamet <needmefordev@gmail.com>
2026-02-19 16:47:37 +0000

    fix: component replaceOnChanges and conformance test snapshots

--- sdk/go/pulumi-language-go/language_test.go
+++ sdk/go/pulumi-language-go/language_test.go
@@ -107,10 +107,9 @@ var expectedFailures = map[string]string{
 	"l1-builtin-can":         "pulumi#18570 Support can in Go program generation",
 
 	// pulumi/pulumi#18345
-	"l1-keyword-overlap":                  "outputs are not cast correctly from pcl to their pulumi types",                                           //nolint:lll
-	"l2-plain":                            "cannot use &plain.DataArgs{…} (value of type *plain.DataArgs) as plain.DataArgs value in struct literal", //nolint:lll
-	"l2-map-keys":                         "cannot use &plain.DataArgs{…} (value of type *plain.DataArgs) as plain.DataArgs value in struct literal", //nolint:lll
-	"l2-discriminated-union":              "pulumi#21829: does not compile",
+	"l1-keyword-overlap":                  "outputs are not cast correctly from pcl to their pulumi types",                                                 //nolint:lll
+	"l2-plain":                            "cannot use &plain.DataArgs{…} (value of type *plain.DataArgs) as plain.DataArgs value in struct literal",       //nolint:lll
+	"l2-map-keys":                         "cannot use &plain.DataArgs{…} (value of type *plain.DataArgs) as plain.DataArgs value in struct literal",       //nolint:lll
 	"l2-component-program-resource-ref":   "pulumi#18140: cannot use ref.Value (variable of type pulumi.StringOutput) as string value in return statement", //nolint:lll
 	"l2-component-component-resource-ref": "pulumi#18140: cannot use ref.Value (variable of type pulumi.StringOutput) as string value in return statement", //nolint:lll
 	"l2-component-call-simple":            "pulumi#18202: syntax error: unexpected / in parameter list; possibly missing comma or )",                       //nolint:lll
@@ -145,6 +144,13 @@ var programOverrides = map[string]*testingrpc.PrepareLanguageTestsRequest_Progra
 	"l2-provider-call-explicit": {
 		Paths: []string{
 			filepath.Join("testdata", "overrides", "l2-provider-call-explicit"),
+		},
+	},
+
+	// TODO: Import paths are incorrect due to schema object types not having canonical tokens.
+	"l2-module-format": {
+		Paths: []string{
+			filepath.Join("testdata", "overrides", "l2-module-format"),
 		},
 	},
 }
--- sdk/python/cmd/pulumi-language-python/testdata/setuppy/published/projects/provider-replace-on-changes-component/0/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/setuppy/published/projects/provider-replace-on-changes-component/0/__main__.py
@@ -2,10 +2,8 @@ import pulumi
 import pulumi_conformance_component as conformance_component
 import pulumi_simple as simple
 
-with_replace_on_changes = conformance_component.Simple(
-    "withReplaceOnChanges",
-    value=True,
-    opts=pulumi.ResourceOptions(replace_on_changes=["value"]),
-)
+with_replace_on_changes = conformance_component.Simple("withReplaceOnChanges", value=True,
+opts = pulumi.ResourceOptions(replace_on_changes=["value"]))
 without_replace_on_changes = conformance_component.Simple("withoutReplaceOnChanges", value=True)
+# Make a simple resource so that plugin detection works.
 simple_resource = simple.Resource("simpleResource", value=False)
