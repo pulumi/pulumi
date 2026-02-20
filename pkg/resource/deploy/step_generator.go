511a9b6da5ffbaca1ea136d935a7e493fdd31e47
Thomas Gummerer <t.gummerer@gmail.com>
2026-02-20 10:16:52 +0100

    review feedback

--- pkg/resource/deploy/step_generator.go
+++ pkg/resource/deploy/step_generator.go
@@ -869,13 +869,15 @@ func (sg *stepGenerator) continueStepsFromRefresh(event ContinueResourceRefreshE
 				sg.skippedCreates[urn] = true
 				return []Step{NewSkippedCreateStep(sg.deployment, event, new)}, false, nil
 			}
-			// We've already refreshed this resource, so we can just trigger the done event (refresh steps never do this
-			// alone) and return no further steps.
-			event.Done(&RegisterResult{
-				State:  event.Old(),
-				Result: ResultStateSuccess,
-			})
-			return []Step{}, false, nil
+			if goal.Custom && !sdkproviders.IsProviderType(goal.Type) {
+				// We've already refreshed this resource, so we can just trigger the done event (refresh steps never do this
+				// alone) and return no further steps.
+				event.Done(&RegisterResult{
+					State:  event.Old(),
+					Result: ResultStateSuccess,
+				})
+			}
+			return []Step{NewSameStep(sg.deployment, event, old, new)}, false, nil
 		}
 	}
 	// If this is a destroy generation we're _always_ going to do a skip create or skip step here for custom
