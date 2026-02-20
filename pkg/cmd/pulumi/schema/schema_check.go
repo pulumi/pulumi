0302cccbd2873414554192e458c0c90303734d7b
bittoby <brianwhitedev1996@gmail.com>
2026-02-20 15:32:44 +0000

    feat: use SchemaFromSchemaSource in pulumi schema check to support any schema source (plugin name, path, or version) not just files

--- pkg/cmd/pulumi/schema/schema_check.go
+++ pkg/cmd/pulumi/schema/schema_check.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2024, Pulumi Corporation.
+// Copyright 2016-2025, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,20 +15,20 @@
 package schema
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/hashicorp/hcl/v2"
-
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 
+	cmdCmd "github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/cmd"
 	"github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/constrictor"
+	"github.com/pulumi/pulumi/pkg/v3/cmd/pulumi/packages"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	pkgWorkspace "github.com/pulumi/pulumi/pkg/v3/workspace"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/env"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
@@ -42,39 +42,40 @@ func newSchemaCheckCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "check",
 		Short: "Check a Pulumi package schema for errors",
-		Long: "Check a Pulumi package schema for errors.\n" +
-			"\n" +
-			"Ensure that a Pulumi package schema meets the requirements imposed by the\n" +
-			"schema spec as well as additional requirements imposed by the supported\n" +
-			"target languages.",
+		Long: `Check a Pulumi package schema for errors.
+
+Ensure that a Pulumi package schema meets the requirements imposed by the
+schema spec as well as additional requirements imposed by the supported
+target languages.
+
+<schema_source> can be a package name, the path to a plugin binary or folder,
+or a JSON/YAML schema file.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			file := args[0]
+			source := args[0]
 
-			// Read from stdin or a specified file
-			reader := os.Stdin
-			if file != "-" {
-				f, err := os.Open(file)
-				if err != nil {
-					return fmt.Errorf("could not open file %v: %w", file, err)
-				}
-				reader = f
-			}
-			schemaBytes, err := io.ReadAll(reader)
+			wd, err := os.Getwd()
 			if err != nil {
-				return fmt.Errorf("failed to read schema: %w", err)
+				return err
 			}
-
-			var pkgSpec schema.PackageSpec
-			if ext := filepath.Ext(file); ext == ".yaml" || ext == ".yml" {
-				err = yaml.Unmarshal(schemaBytes, &pkgSpec)
-			} else {
-				err = json.Unmarshal(schemaBytes, &pkgSpec)
-			}
+			sink := cmdutil.Diag()
+			pctx, err := plugin.NewContext(cmd.Context(), sink, sink, nil, nil, wd, nil, false,
+				nil, schema.NewLoaderServerFromHost)
 			if err != nil {
-				return fmt.Errorf("failed to unmarshal schema: %w", err)
+				return err
+			}
+			defer func() {
+				contract.IgnoreError(pctx.Close())
+			}()
+
+			parameters := &plugin.ParameterizeArgs{Args: args[1:]}
+			spec, _, err := packages.SchemaFromSchemaSource(pctx, source, parameters,
+				cmdCmd.NewDefaultRegistry(cmd.Context(), pkgWorkspace.Instance, nil, cmdutil.Diag(), env.Global()),
+				env.Global(), 0 /* unbounded concurrency */)
+			if err != nil {
+				return err
 			}
 
-			_, diags, err := schema.BindSpec(pkgSpec, nil, schema.ValidationOptions{
+			_, diags, err := schema.BindSpec(*spec, nil, schema.ValidationOptions{
 				AllowDanglingReferences: schemaCheckArgs.allowDanglingReferences,
 			})
 			diagWriter := hcl.NewDiagnosticTextWriter(os.Stderr, nil, 0, true)
@@ -89,10 +90,16 @@ func newSchemaCheckCommand() *cobra.Command {
 
 	constrictor.AttachArguments(cmd, &constrictor.Arguments{
 		Arguments: []constrictor.Argument{
-			{Name: "file"},
+			{Name: "schema-source"},
+			{Name: "provider-parameter"},
 		},
 		Required: 1,
+		Variadic: true,
 	})
+
+	// It's worth mentioning the `--`, as it means that Cobra will stop parsing flags.
+	// In other words, a provider parameter can be `--foo` as long as it's after `--`.
+	cmd.Use = "check <schema-source> [flags] [--] [provider-parameter]..."
 
 	cmd.PersistentFlags().BoolVar(&schemaCheckArgs.allowDanglingReferences, "allow-dangling-references", false,
 		"Whether references to nonexistent types should be considered errors")
