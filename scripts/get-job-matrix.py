94272ff774f6573ae2317c72995a250fae915144
Julien Poissonnier <julien@caffeine.lu>
2026-02-22 18:12:16 +0100

    go1.25

--- scripts/get-job-matrix.py
+++ scripts/get-job-matrix.py
@@ -144,7 +144,7 @@ ALL_PLATFORMS = ["ubuntu-latest", "windows-latest", "macos-latest"]
 
 ALL_VERSION_SET = {
     "dotnet": ["8", "9"],
-    "go": ["1.25.x", "1.26.x"],
+    "go": ["1.25.x"],
     "nodejs": ["20.x", "22.x", "24.x", "25.x"],
     # When updating the minimum Python version here, also update `pyproject.toml`, including the
     # `mypy` and `ruff` sections.
