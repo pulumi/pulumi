b2f40e6c70b9db0f9a2429d0b623dc35bd9df393
spider-yamet <needmefordev@gmail.com>
2026-02-23 18:43:11 +0000

    Remove unnecessary snapshots

--- pkg/testing/pulumi-test-language/tests/testdata/provider-version-component/main.pp
+++ pkg/testing/pulumi-test-language/tests/testdata/provider-version-component/main.pp
@@ -1,21 +1,15 @@
-resource "simpleV2" "pulumi:providers:simple" {
-}
-
 resource "withV22" "conformance-component:index:Simple" {
     value = true
     options {
         version = "22.0.0"
-        providers = {
-            simple = simpleV2
-        }
     }
 }
 
 resource "withDefault" "conformance-component:index:Simple" {
     value = true
-    options {
-        providers = {
-            simple = simpleV2
-        }
-    }
+}
+
+// Ensure the simple plugin is discoverable for this conformance run.
+resource "simpleResource" "simple:index:Resource" {
+    value = false
 }
