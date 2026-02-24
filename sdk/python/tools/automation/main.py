43760340de5623102f369b9591aba0a6a3b55172
Tom Harding <i.am.tom.harding@gmail.com>
2026-02-24 10:42:07 +0100

    Oh boy

--- sdk/python/tools/automation/main.py
+++ sdk/python/tools/automation/main.py
@@ -203,23 +203,6 @@ def main(argv: list[str]) -> int:
         "",
     ]
 
-    header_lines = [
-        "# Copyright 2026, Pulumi Corporation.",
-        "#",
-        '# Licensed under the Apache License, Version 2.0 (the "License");',
-        "# you may not use this file except in compliance with the License.",
-        "# You may obtain a copy of the License at",
-        "#",
-        "#     http://www.apache.org/licenses/LICENSE-2.0",
-        "#",
-        "# Unless required by applicable law or agreed to in writing, software",
-        '# distributed under the License is distributed on an "AS IS" BASIS,',
-        "# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",
-        "# See the License for the specific language governing permissions and",
-        "# limitations under the License.",
-        "",
-    ]
-
     target.write_text("\n".join(header_lines) + code + "\n", encoding="utf-8")
 
     return 0
