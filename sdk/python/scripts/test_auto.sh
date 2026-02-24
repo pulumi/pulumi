d23580b3faf1b5ee02d3013b310193e0886476ac
Julien Poissonnier <julien@caffeine.lu>
2026-02-24 17:35:11 +0100

    r

--- sdk/python/scripts/test_auto.sh
+++ sdk/python/scripts/test_auto.sh
@@ -7,11 +7,7 @@ set -euo pipefail
 mkdir -p ../../junit
 JUNIT_DIR=$(realpath ../../junit)
 
-uv run -m pytest --cov pulumi --cov-append -n auto --junitxml "$JUNIT_DIR/python-test-auto.xml" lib/test/automation \
-    --ignore lib/test/automation/test_fork.py
-
-uv run -m pytest --cov pulumi --cov-append -n 1 --junitxml "$JUNIT_DIR/python-test-auto-fork.xml" \
-    lib/test/automation/test_fork.py
+uv run -m pytest --cov pulumi --cov-append -n auto --junitxml "$JUNIT_DIR/python-test-auto.xml" lib/test/automation
 
 if [[ "$PULUMI_TEST_COVERAGE_PATH" ]]; then
     if [ -e .coverage ]; then
