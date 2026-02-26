224cff74dbf95ec2ecf933891c1cd6b77aaf5238
spider-yamet <needmefordev@gmail.com>
2026-02-26 09:42:15 +0000

    Remove redundant snapshot files

--- sdk/python/cmd/pulumi-language-python/testdata/setuppy/published/projects/provider-replace-on-changes-component/1/__main__.py
+++
@@ -1,9 +0,0 @@
-import pulumi
-import pulumi_conformance_component as conformance_component
-import pulumi_simple as simple
-
-with_replace_on_changes = conformance_component.Simple("withReplaceOnChanges", value=False,
-opts = pulumi.ResourceOptions(replace_on_changes=["value"]))
-without_replace_on_changes = conformance_component.Simple("withoutReplaceOnChanges", value=True)
-# Make a simple resource so that plugin detection works.
-simple_resource = simple.Resource("simpleResource", value=False)
