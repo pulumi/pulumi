f2a591b1d0ed8428c37709c667f6c462d8ebc061
Ian Wahbe <me@iwahbe.com>
2026-03-02 15:08:32 +0100

    Delete `l2-rtti` as redundant (#22022)
    This test covers exactly what `l2-resource-name-type` covers.

--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsnode/projects/l2-rtti/index.ts
+++
@@ -1,6 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as simple from "@pulumi/simple";
-
-const res = new simple.Resource("res", {value: true});
-export const name = pulumi.resourceName(res);
-export const type = pulumi.resourceType(res);
