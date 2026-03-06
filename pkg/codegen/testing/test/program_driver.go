df45f1b7a2deee292532f38deda4b6b5e8b3077b
Ian Wahbe <me@iwahbe.com>
2026-03-06 16:01:57 +0100

    Remove multiline-string programgen test, covered by l1-output-string conformance test
    The l1-output-string language conformance test provides end-to-end
    coverage of multiline strings including heredoc syntax and escaped
    newline sequences.

--- pkg/codegen/testing/test/program_driver.go
+++ pkg/codegen/testing/test/program_driver.go
@@ -288,10 +288,6 @@ var PulumiPulumiProgramTests = []ProgramTest{
 		Description: "Using DependsOn resource option with an array of resources",
 	},
 	{
-		Directory:   "multiline-string",
-		Description: "Multiline string literals",
-	},
-	{
 		Directory:   "config-variables",
 		Description: "Basic program with a bunch of config variables",
 		// TODO[https://github.com/pulumi/pulumi/issues/14957] - object config variables are broken here
