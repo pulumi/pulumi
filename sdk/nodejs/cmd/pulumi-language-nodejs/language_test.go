41925023eb7d6546f3ba2f1b255b04b247cbfe16
Ian Wahbe <me@iwahbe.com>
2026-03-10 15:53:06 +0100

    `l3-for` actually does work in NodeJS

--- sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
+++ sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
@@ -99,7 +99,6 @@ func runTestingHost(t *testing.T) (string, testingrpc.LanguageTestClient) {
 // Add test names here that are expected to fail and the reason why they are failing
 var expectedFailures = map[string]string{
 	"l2-invoke-options-depends-on": "not implemented yet",
-	"l3-for":                       "for expressions are not yet supported in Node.js codegen",
 	"l3-for-resource":              "Property 'value' does not exist on type 'number | Detail'. Did you mean 'valueOf'",
 }
 
