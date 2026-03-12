5591a53f1243968452d62a40d92b323893f87737
Tom 🐈‍⬛ <i.am.tom.harding@gmail.com>
2026-03-12 19:20:53 +0100

    Add CLI error code tests (#22205)
    A couple things: some tests for the error codes so that there's some
    form of documentation in the branch, and a changelog because changing
    exit codes is technically a breaking change. [I have a docs PR to follow
    up on this one](https://github.com/pulumi/docs/pull/17930).
    
    Closes #21716.

---
+++ tests/integration/exit_codes_test.go
@@ -0,0 +1,50 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ints
+
+import (
+	"context"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	ptesting "github.com/pulumi/pulumi/sdk/v3/go/common/testing"
+)
+
+func TestExitCode_StackNotFound(t *testing.T) {
+	t.Parallel()
+
+	e := ptesting.NewEnvironment(t)
+	defer e.DeleteIfNotFailed()
+
+	// Create a minimal project so the DIY file backend can infer the
+	// current project name when parsing stack references.
+	e.WriteTestFile("Pulumi.yaml", `
+name: exit-code-stack-not-found
+runtime: nodejs
+`)
+
+	// Use a local file backend so we don't depend on external services.
+	e.Backend = e.LocalURL()
+
+	cmd := e.SetupCommandIn(context.Background(), e.CWD, "pulumi", "stack", "select", "does-not-exist")
+	err := cmd.Run()
+
+	require.Error(t, err)
+	exitErr, ok := err.(*exec.ExitError)
+	require.True(t, ok)
+	require.Equal(t, 6, exitErr.ExitCode())
+}
