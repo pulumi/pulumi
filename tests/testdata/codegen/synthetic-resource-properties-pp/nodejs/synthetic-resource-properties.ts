418e0a8688cdbc7c21850cc5bca73f785e588f01
Ian Wahbe <me@iwahbe.com>
2026-03-12 12:34:35 +0100

    Remove redundent program tests (#22199)
    These tests are already better covered by conformance tests.

--- tests/testdata/codegen/synthetic-resource-properties-pp/nodejs/synthetic-resource-properties.ts
+++
@@ -1,8 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as synthetic from "@pulumi/synthetic";
-
-const rt = new synthetic.resourceproperties.Root("rt", {});
-export const trivial = rt;
-export const simple = rt.res1;
-export const foo = rt.res1.apply(res1 => res1.obj1?.res2?.obj2);
-export const complex = rt.res1.apply(res1 => res1.obj1?.res2?.obj2?.answer);
