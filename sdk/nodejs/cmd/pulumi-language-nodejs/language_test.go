31e8dc558fe8c3cedd8083dcf9a64d81a1ef0ac0
Julien <julien@caffeine.lu>
2026-03-13 16:47:13 +0100

    Run policy language tests for bun (#22209)
    Implemented in https://github.com/pulumi/pulumi/pull/22206, enable the
    language conformance tests that use policies now.
    
    The polcies are copies of the Node.js policies with modified
    tsconfig.json/package.json/PulumiPolicy.yaml files to suit bun.
    
    Test setup in
    https://github.com/pulumi/pulumi/pull/22209/changes/1df053db8a591d16ef3e0c1bb68e303c3e21a261
    Snapshots added in
    https://github.com/pulumi/pulumi/pull/22209/changes/861cef2adb64dc42a7b6e8f0ed2d4fc745ec9d90
    
    Fixes https://github.com/pulumi/pulumi/issues/22078

--- sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
+++ sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
@@ -146,6 +146,7 @@ func testLanguage(t *testing.T, runtime string, forceTsc bool) {
 			require.NoError(t, err)
 
 			providersDir := "testdata/providers"
+			policyPackDir := "testdata/policies"
 			snapshotDir := "./testdata"
 			if local {
 				snapshotDir += "/local"
@@ -156,6 +157,7 @@ func testLanguage(t *testing.T, runtime string, forceTsc bool) {
 			case "bun":
 				snapshotDir += "/bun"
 				providersDir = "testdata/providers-bun"
+				policyPackDir = "testdata/policies-bun"
 			case "nodejs":
 				if forceTsc {
 					snapshotDir += "/tsc"
@@ -172,7 +174,7 @@ func testLanguage(t *testing.T, runtime string, forceTsc bool) {
 				SnapshotDirectory:    snapshotDir,
 				CoreSdkDirectory:     "../..",
 				CoreSdkVersion:       sdk.Version.String(),
-				PolicyPackDirectory:  "testdata/policies",
+				PolicyPackDirectory:  policyPackDir,
 				Local:                local,
 				SnapshotEdits: []*testingrpc.PrepareLanguageTestsRequest_Replacement{
 					{
@@ -200,9 +202,6 @@ func testLanguage(t *testing.T, runtime string, forceTsc bool) {
 					}
 
 					if runtime == "bun" {
-						if strings.HasPrefix(tt, "policy-") {
-							t.Skip("Skipping policy tests - TODO: https://github.com/pulumi/pulumi/issues/22078")
-						}
 						if tt == "l2-external-enum" || tt == "l2-namespaced-provider" {
 							t.Skip(
 								"On linux bun has trouble resolving indirect dependencies that point to a local file" +
