66aa69e39ab2b7682b8ffcd60f418a75cafefb9f
Fraser Waters <fraser@pulumi.com>
2026-03-13 21:09:42 +0000

    regen testdata

--- sdk/pcl/cmd/pulumi-language-pcl/testdata/round-tripped-project/l3-range-resource-output-traversal/main.pp
+++ sdk/pcl/cmd/pulumi-language-pcl/testdata/round-tripped-project/l3-range-resource-output-traversal/main.pp
@@ -2,9 +2,25 @@ resource "container" "nestedobject:index:Container" {
   inputs = ["alpha", "bravo"]
 }
 
-resource "target" "nestedobject:index:Target" {
+resource "mapContainer" "nestedobject:index:MapContainer" {
+  tags = {
+    "k1": "charlie",
+    "k2": "delta",
+  }
+}
+
+# A resource that ranges over a computed list
+resource "listOutput" "nestedobject:index:Target" {
   options {
     range = container.details
   }
   name = range.value.value
+}
+
+# A resource that ranges over a computed map
+resource "mapOutput" "nestedobject:index:Target" {
+  options {
+    range = mapContainer.tags
+  }
+  name = "${range.key}=>${range.value}"
 }
