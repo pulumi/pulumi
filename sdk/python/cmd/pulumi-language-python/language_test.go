67cff22517216128382e73a3275fa8a2e3ab078b
Fraser Waters <fraser@pulumi.com>
2026-03-19 20:53:15 +0000

    lint

--- sdk/python/cmd/pulumi-language-python/language_test.go
+++ sdk/python/cmd/pulumi-language-python/language_test.go
@@ -104,7 +104,7 @@ var expectedFailures = map[string]string{
 	"l1-builtin-try":                "Temporarily disabled until pr #18915 is submitted",
 	"l1-builtin-can":                "Temporarily disabled until pr #18916 is submitted",
 	"l3-deferred-outputs":           "does not type-check",
-  "l2-resource-config-primitives": "Argument integer to Resource has incompatible type Output[float]",
+	"l2-resource-config-primitives": "Argument integer to Resource has incompatible type Output[float]",
 }
 
 type languageTestConfig struct {
