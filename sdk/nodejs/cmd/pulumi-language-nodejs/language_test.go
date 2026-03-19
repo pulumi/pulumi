4c9cac258f765d577074c607b997d312f1ee5cc5
Julien Poissonnier <julien@caffeine.lu>
2026-03-19 17:56:08 +0100

    Don't apply the secrets filter to true and false
    This is causing more confusion than it helps, and causes issues in tests. I don’t think true or false are very secret values, and CircleCI also does this https://github.com/circleci/circleci-docs/blob/main/docs/guides/modules/ROOT/partials/shared-sections/secrets-masking.adoc.

--- sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
+++ sdk/nodejs/cmd/pulumi-language-nodejs/language_test.go
@@ -206,9 +206,6 @@ func testLanguage(t *testing.T, runtime string, forceTsc bool) {
 								"On linux bun has trouble resolving indirect dependencies that point to a local file" +
 									"https://github.com/pulumi/pulumi/issues/22100")
 						}
-						if strings.HasPrefix(tt, "policy-") {
-							t.Skip("Policy tests are intermittently failing on bun, printing values as [[secret]]")
-						}
 					}
 
 					if expected, ok := expectedFailures[tt]; ok {
