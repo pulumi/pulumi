c1741c706b5728bc53b9e70962e42585375fa244
RohitYandigeri <rohityandigeri2004@gmail.com>
2026-03-22 10:23:19 +0530

    test(mlc): cover getOrganization in provider components

--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/providers/conformance-component/index.ts
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/providers/conformance-component/index.ts
@@ -7,17 +7,22 @@ export interface SimpleArgs {
 
 export class Simple extends pulumi.ComponentResource {
     public readonly value: pulumi.Output<boolean>;
+    public readonly context: pulumi.Output<string>;
 
     constructor(name: string, args: SimpleArgs, opts?: pulumi.ComponentResourceOptions) {
         super("conformance-component:index:Simple", name, args, opts);
 
         this.value = pulumi.output(args.value);
+        this.context = pulumi.output(
+            `${pulumi.getOrganization()}-${pulumi.getProject()}-${pulumi.getStack()}`,
+        );
 
         let res = new simple.Resource(`${name}-child`, {
             value: !this.value,
         }, { parent: this });
 
         this.registerOutputs({
+            context: this.context,
             value: this.value,
         });
     }
