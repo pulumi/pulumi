a2f3ba13aee62bdfd670453e569bf853dc0a2ed6
Fraser Ashyggton <fraser@pulumi.com>
2026-04-01 17:53:15 +0100

    Fix config tests to use int type (#22429)
    Small fix to the config tests to use "int" config values

--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/bun/projects/l3-component-config-primitives/index.ts
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/bun/projects/l3-component-config-primitives/index.ts
@@ -4,19 +4,21 @@ import { PrimitiveComponent } from "./primitiveComponent";
 const config = new pulumi.Config();
 const plainBool = config.requireBoolean("plainBool");
 const plainNumber = config.requireNumber("plainNumber");
+const plainInteger = config.requireNumber("plainInteger");
 const plainString = config.require("plainString");
 const secretBool = config.requireSecretBoolean("secretBool");
 const secretNumber = config.requireSecretNumber("secretNumber");
+const secretInteger = config.requireSecretNumber("secretInteger");
 const secretString = config.requireSecret("secretString");
 const plain = new PrimitiveComponent("plain", {
     boolean: plainBool,
-    float: plainNumber + 0.5,
-    integer: plainNumber,
+    float: plainNumber,
+    integer: plainInteger,
     string: plainString,
 });
 const secret = new PrimitiveComponent("secret", {
     boolean: secretBool,
-    float: secretNumber.apply(secretNumber => secretNumber + 0.5),
-    integer: secretNumber,
+    float: secretNumber,
+    integer: secretInteger,
     string: secretString,
 });
--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsnode/projects/l2-resource-config-primitives/index.ts
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/tsnode/projects/l2-resource-config-primitives/index.ts
@@ -4,14 +4,16 @@ import * as primitive from "@pulumi/primitive";
 const config = new pulumi.Config();
 const plainBool = config.requireBoolean("plainBool");
 const plainNumber = config.requireNumber("plainNumber");
+const plainInteger = config.requireNumber("plainInteger");
 const plainString = config.require("plainString");
 const secretBool = config.requireSecretBoolean("secretBool");
 const secretNumber = config.requireSecretNumber("secretNumber");
+const secretInteger = config.requireSecretNumber("secretInteger");
 const secretString = config.requireSecret("secretString");
 const plain = new primitive.Resource("plain", {
     boolean: plainBool,
-    float: plainNumber + 0.5,
-    integer: plainNumber,
+    float: plainNumber,
+    integer: plainInteger,
     string: plainString,
     numberArray: [
         -1,
@@ -25,8 +27,8 @@ const plain = new primitive.Resource("plain", {
 });
 const secret = new primitive.Resource("secret", {
     boolean: secretBool,
-    float: secretNumber.apply(secretNumber => secretNumber + 0.5),
-    integer: secretNumber,
+    float: secretNumber,
+    integer: secretInteger,
     string: secretString,
     numberArray: [
         -2,
