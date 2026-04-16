0c046722b6960b2c407d514277da16da72b41858
Fraser Waters <fraser@pulumi.com>
2026-04-16 21:58:15 +0100

    Update provider version

--- pkg/testing/pulumi-test-language/providers/builtin_info_component_provider.go
+++ pkg/testing/pulumi-test-language/providers/builtin_info_component_provider.go
@@ -40,7 +40,7 @@ func (p *BuiltinInfoComponentProvider) Pkg() tokens.Package {
 
 func (p *BuiltinInfoComponentProvider) GetPluginInfo(context.Context) (plugin.PluginInfo, error) {
 	return plugin.PluginInfo{
-		Version: &semver.Version{Major: 34},
+		Version: &semver.Version{Major: 36},
 	}, nil
 }
 
@@ -49,7 +49,7 @@ func (p *BuiltinInfoComponentProvider) GetSchema(
 ) (plugin.GetSchemaResponse, error) {
 	pkg := schema.PackageSpec{
 		Name:    "builtin-info-component",
-		Version: "34.0.0",
+		Version: "36.0.0",
 		Resources: map[string]schema.ResourceSpec{
 			"builtin-info-component:index:BuiltinInfo": {
 				ObjectTypeSpec: schema.ObjectTypeSpec{
