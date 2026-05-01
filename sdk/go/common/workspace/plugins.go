ae16a8f9543ffee48109ff50c31c7c47584bfb00
Ian Wahbe <me@iwahbe.com>
2026-05-01 18:05:15 +0100

    Bundle `pcl`
    Stacked on top of https://github.com/pulumi/pulumi/pull/22807.

--- sdk/go/common/workspace/plugins.go
+++ sdk/go/common/workspace/plugins.go
@@ -1992,6 +1992,7 @@ func IsPluginBundled(kind apitype.PluginKind, name string) bool {
 		(kind == apitype.LanguagePlugin && name == "yaml") ||
 		(kind == apitype.LanguagePlugin && name == "java") ||
 		(kind == apitype.LanguagePlugin && name == "hcl") ||
+		(kind == apitype.LanguagePlugin && name == "pcl") ||
 		(kind == apitype.ResourcePlugin && name == "pulumi-nodejs") ||
 		(kind == apitype.ResourcePlugin && name == "pulumi-python")
 }
