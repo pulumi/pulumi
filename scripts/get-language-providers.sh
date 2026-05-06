1ffe9522fbb70ee6b58d43d7b2c51a14725bdf2e
Ian Wahbe <me@iwahbe.com>
2026-05-06 14:17:49 +0200

    Bump the version of pulumi-hcl to v0.2.0

--- scripts/get-language-providers.sh
+++ scripts/get-language-providers.sh
@@ -56,7 +56,7 @@ LANGUAGES=(
   # renovate: datasource=github-releases depName=pulumi/pulumi-yaml
   "yaml v1.33.0"
   # renovate: datasource=github-releases depName=pulumi-labs/pulumi-hcl
-  "hcl v0.0.2 pulumi-labs"
+  "hcl v0.2.0 pulumi-labs"
 )
 
 for i in "${LANGUAGES[@]}"; do
