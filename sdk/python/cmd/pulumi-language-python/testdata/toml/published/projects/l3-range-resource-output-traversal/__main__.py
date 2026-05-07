876922ac31314f2c0322779d07e3484dc7a02e59
Thomas Gummerer <t.gummerer@gmail.com>
2026-05-07 12:24:24 +0200

    update files

--- sdk/python/cmd/pulumi-language-python/testdata/toml/published/projects/l3-range-resource-output-traversal/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/toml/published/projects/l3-range-resource-output-traversal/__main__.py
@@ -1,4 +1,5 @@
 import pulumi
+from typing import Any
 import pulumi_nestedobject as nestedobject
 
 container = nestedobject.Container("container", inputs=[
@@ -11,14 +12,14 @@ map_container = nestedobject.MapContainer("mapContainer", tags={
 })
 # A resource that ranges over a computed list
 list_output = []
-def create_list_output(range_body):
+def create_list_output(range_body: Any):
     for range in [{"key": k, "value": v} for [k, v] in enumerate(range_body)]:
         list_output.append(nestedobject.Target(f"listOutput-{range['key']}", name=range["value"].value))
 
 container.details.apply(create_list_output)
 # A resource that ranges over a computed map
 map_output = []
-def create_map_output(range_body):
+def create_map_output(range_body: Any):
     for range in [{"key": k, "value": v} for [k, v] in sorted((range_body).items())]:
         map_output.append(nestedobject.Target(f"mapOutput-{range['key']}", name=f"{range['key']}=>{range['value']}"))
 
