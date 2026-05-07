12fdf4c306effc2d1808e38ed1d78088576dc3ad
Thomas Gummerer <t.gummerer@gmail.com>
2026-05-07 15:09:15 +0200

    more updates

--- sdk/python/cmd/pulumi-language-python/testdata/toml/local/projects/l3-range/__main__.py
+++ sdk/python/cmd/pulumi-language-python/testdata/toml/local/projects/l3-range/__main__.py
@@ -1,4 +1,5 @@
 import pulumi
+from typing import Any
 import pulumi_nestedobject as nestedobject
 
 config = pulumi.Config()
@@ -6,13 +7,13 @@ num_items = config.require_int("numItems")
 item_list = config.require_object("itemList")
 item_map = config.require_object("itemMap")
 create_bool = config.require_bool("createBool")
-num_resource = []
+num_resource: list[Any] = []
 for range in [{"value": i} for i in range(0, num_items)]:
     num_resource.append(nestedobject.Target(f"numResource-{range['value']}", name=f"num-{range['value']}"))
-list_resource = []
+list_resource: list[Any] = []
 for range in [{"key": k, "value": v} for [k, v] in enumerate(item_list)]:
     list_resource.append(nestedobject.Target(f"listResource-{range['key']}", name=f"{range['key']}:{range['value']}"))
-map_resource = []
+map_resource: list[Any] = []
 for range in [{"key": k, "value": v} for [k, v] in sorted((item_map).items())]:
     map_resource.append(nestedobject.Target(f"mapResource-{range['key']}", name=f"{range['key']}={range['value']}"))
 bool_resource = None
