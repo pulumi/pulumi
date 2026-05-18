pulumi/pulumi

commit 14e6303322d825d20f90f8ed7982f169dc7eb43d

Generate optional input types that accept undefined/None values (#22552)
For optional input properties, rewrite `Optional(Input(T))` to
`Input(Optional(T))` so that the generated types accept `Output<T |
undefined>` (Node.js) and `Output[Optional[T]]` (Python).

The first commit contains the actual code changes, the 2nd commit
updates all the snapshots.

Fixes https://github.com/pulumi/pulumi/issues/6175
Fixes https://github.com/pulumi/pulumi/issues/12105

<s>Wait for https://github.com/pulumi/pulumi/pull/22553 to go out in a
release and update the core SDK version in generated SDKs to match
this.</s>

pkg/codegen/python/gen.go
@@ -2554,15 +2554,34 @@ type typeStringOpts struct {
 func (mod *modContext) typeString(t schema.Type, opts typeStringOpts) string {
 	switch t := t.(type) {
 	case *schema.OptionalType:
+		// Rewrite Optional(Input(T)) to Input(Optional(T)) so that it accepts Output(Optional(T))
+		if lifted := codegen.PushOptionalIntoInput(t); lifted != t {
+			// Clear forDict so the inner Optional renders as Optional, not NotRequired.
+			innerOpts := opts
+			innerOpts.forDict = false
+			result := mod.typeString(lifted, innerOpts)
+			if opts.forDict {
+				return fmt.Sprintf("NotRequired[%s]", result)
+			}
+			return result
+		}
 		typ := mod.typeString(t.ElementType, opts)
 		if opts.forDict {
 			return fmt.Sprintf("NotRequired[%s]", typ)
 		}
 		return fmt.Sprintf("Optional[%s]", typ)
 	case *schema.InputType:
-		typ := mod.typeString(codegen.SimplifyInputUnion(t.ElementType), opts)
+		elem := codegen.SimplifyInputUnion(t.ElementType)
+		typ := mod.typeString(elem, opts)
 		if typ == "Any" {
 			return typ
+		}
+		// When the element is Optional(T) where T renders as "Any", we can drop the Input wrapper
+		// since Any already accepts inputs.
+		if opt, ok := elem.(*schema.OptionalType); ok {
+			if inner := mod.typeString(opt.ElementType, opts); inner == "Any" {
+				return "Optional[Any]"
+			}
 		}
 		return fmt.Sprintf("pulumi.Input[%s]", typ)
 	case *schema.EnumType:
@@ -3478,7 +3497,7 @@ func setDependencies(schema *PyprojectSchema, pkg *schema.Package, dependencies 
 }
 
 // Require the SDK to fall within the same major version.
-var MinimumValidSDKVersion = ">=3.165.0,<4.0.0"
+var MinimumValidSDKVersion = ">=3.231.0,<4.0.0"
 
 // ensureValidPulumiVersion ensures that the Pulumi SDK has an entry.
 // It accepts a list of dependencies

pkg/codegen/nodejs/gen.go
@@ -298,14 +298,25 @@ func tokenToFunctionName(tok string) string {
 func (mod *modContext) typeAst(t schema.Type, input bool, constValue any) tstypes.TypeAst {
 	switch t := t.(type) {
 	case *schema.OptionalType:
+		// Rewrite Optional(Input(T)) to Input(Optional(T)) so that it accepts Output(Optional(T))
+		if lifted := codegen.PushOptionalIntoInput(t); lifted != t {
+			return mod.typeAst(lifted, input, constValue)
+		}
 		return tstypes.Union(
 			mod.typeAst(t.ElementType, input, constValue),
 			tstypes.Identifier("undefined"),
 		)
 	case *schema.InputType:
-		typ := mod.typeString(codegen.SimplifyInputUnion(t.ElementType), input, constValue)
+		elem := codegen.SimplifyInputUnion(t.ElementType)
+		typ := mod.typeString(elem, input, constValue)
 		if typ == "any" {
 			return tstypes.Identifier("any")
+		}
+		// When the element is Optional(T) where T renders as "any" (e.g. Any, JSON), we can drop the Input wrapper.
+		if opt, ok := elem.(*schema.OptionalType); ok {
+			if inner := mod.typeString(opt.ElementType, input, constValue); inner == "any" {
+				return tstypes.Union(tstypes.Identifier("any"), tstypes.Identifier("undefined"))
+			}
 		}
 		return tstypes.Identifier(fmt.Sprintf("pulumi.Input<%s>", typ))
 	case *schema.EnumType:
@@ -438,7 +449,10 @@ func (mod *modContext) genPlainType(w io.Writer, name, comment string,
 
 		sigil, propertyType := "", p.Type
 		if !p.IsRequired() {
-			sigil, propertyType = "?", codegen.RequiredType(p)
+			sigil = "?"
+			if _, isInput := codegen.RequiredType(p).(*schema.InputType); !isInput {
+				propertyType = codegen.RequiredType(p)
+			}
 		}
 
 		typ := mod.typeString(propertyType, input, p.ConstValue)
@@ -462,17 +476,20 @@ func (mod *modContext) genPlainObjectDefaultFunc(w io.Writer, name string,
 			}
 			defaults = append(defaults, fmt.Sprintf("%s: (val.%s) ?? %s", p.Name, p.Name, dv))
 		} else if funcName := mod.provideDefaultsFuncName(p.Type, input); funcName != "" {
-			// ProvideDefaults functions have the form `(Input<shape> | undefined) ->
-			// Output<shape> | undefined`. We need to disallow the undefined. This is safe
-			// because val.%arg existed in the input (type system enforced).
 			var compositeObject string
 			if codegen.IsNOptionalInput(p.Type) {
-				compositeObject = fmt.Sprintf("pulumi.output(val.%s).apply(%s)", p.Name, funcName)
+				if !p.IsRequired() {
+					compositeObject = fmt.Sprintf(
+						"pulumi.output(val.%s).apply(v => v === undefined ? undefined : %s(v))",
+						p.Name, funcName)
+				} else {
+					compositeObject = fmt.Sprintf("pulumi.output(val.%s).apply(%s)", p.Name, funcName)
+				}
 			} else {
 				compositeObject = fmt.Sprintf("%s(val.%s)", funcName, p.Name)
-			}
-			if !p.IsRequired() {
-				compositeObject = fmt.Sprintf("(val.%s ? %s : undefined)", p.Name, compositeObject)
+				if !p.IsRequired() {
+					compositeObject = fmt.Sprintf("(val.%s ? %s : undefined)", p.Name, compositeObject)
+				}
 			}
 			defaults = append(defaults, fmt.Sprintf("%s: %s", p.Name, compositeObject))
 		}
@@ -482,10 +499,6 @@ func (mod *modContext) genPlainObjectDefaultFunc(w io.Writer, name string,
 	if len(defaults) == 0 {
 		return nil
 	}
-	// Generates a function header that looks like this:
-	// export function %sProvideDefaults(val: pulumi.Input<%s> | undefined): pulumi.Output<%s> | undefined {
-	//     const def = (val: LayeredTypeArgs) => ({
-	//         ...val,
 	defaultProvderName := provideDefaultsFuncNameFromName(name)
 	printComment(w, fmt.Sprintf("%s sets the appropriate defaults for %s",
 		defaultProvderName, name), "", indent)
@@ -761,12 +774,15 @@ func (mod *modContext) genResource(w io.Writer, r *schema.Resource) (resourceFil
 			var arg string
 			applyDefaults := func(arg string) string {
 				if name := mod.provideDefaultsFuncName(prop.Type, true /*input*/); name != "" {
-					var body string
 					if codegen.IsNOptionalInput(prop.Type) {
-						body = fmt.Sprintf("pulumi.output(%[2]s).apply(%[1]s)", name, arg)
-					} else {
-						body = fmt.Sprintf("%s(%s)", name, arg)
+						if !prop.IsRequired() {
+							return fmt.Sprintf(
+								"pulumi.output(%[2]s).apply(v => v === undefined ? undefined : %[1]s(v))",
+								name, arg)
+						}
+						return fmt.Sprintf("pulumi.output(%[2]s).apply(%[1]s)", name, arg)
 					}
+					body := fmt.Sprintf("%s(%s)", name, arg)
 					return fmt.Sprintf("(%s ? %s : undefined)", arg, body)
 				}
 				return arg
@@ -1292,12 +1308,17 @@ func (mod *modContext) genFunctionDefinition(w io.Writer, fun *schema.Function, 
 
 				if name := mod.provideDefaultsFuncName(p.Type, true /*input*/); name != "" {
 					if codegen.IsNOptionalInput(p.Type) || !plain {
-						body = fmt.Sprintf("pulumi.output(%s).apply(%s)", body, name)
+						if !p.IsRequired() {
+							body = fmt.Sprintf(
+								"pulumi.output(%s).apply(v => v === undefined ? undefined : %s(v))",
+								body, name)
+						} else {
+							body = fmt.Sprintf("pulumi.output(%s).apply(%s)", body, name)
+						}
 					} else {
 						body = fmt.Sprintf("%s(%s)", name, body)
+						body = fmt.Sprintf("args.%s ? %s : undefined", p.Name, body)
 					}
-
-					body = fmt.Sprintf("args.%s ? %s : undefined", p.Name, body)
 				}
 				fmt.Fprintf(w, "        \"%[1]s\": %[2]s,\n", p.Name, body)
 			}

commit 5e98c2073b9dc6d03ba13a3819e9310636c600bc

Fix usage of ArgsDict types in typed dictionaries
This broke in https://github.com/pulumi/pulumi/pull/22552

Fixes https://github.com/pulumi/pulumi/issues/23246

pkg/codegen/python/gen.go
@@ -2556,10 +2556,16 @@ func (mod *modContext) typeString(t schema.Type, opts typeStringOpts) string {
 	case *schema.OptionalType:
 		// Rewrite Optional(Input(T)) to Input(Optional(T)) so that it accepts Output(Optional(T))
 		if lifted := codegen.PushOptionalIntoInput(t); lifted != t {
-			// Clear forDict so the inner Optional renders as Optional, not NotRequired.
-			innerOpts := opts
-			innerOpts.forDict = false
-			result := mod.typeString(lifted, innerOpts)
+			innerElem := lifted.(*schema.InputType).ElementType.(*schema.OptionalType).ElementType
+			elemStr := mod.typeString(innerElem, opts)
+			if elemStr == "Any" {
+				// Drop the Input wrapper around Optional[Any]: Any already accepts Output[...] values
+				if opts.forDict {
+					return "NotRequired[Any]"
+				}
+				return "Optional[Any]"
+			}
+			result := fmt.Sprintf("pulumi.Input[Optional[%s]]", elemStr)
 			if opts.forDict {
 				return fmt.Sprintf("NotRequired[%s]", result)
 			}

pkg/testing/pulumi-test-language/providers/ref_ref_provider.go
@@ -94,6 +94,12 @@ func (p *RefRefProvider) GetSchema(
 			Ref:  "#/types/ref-ref:index:InnerData",
 		},
 	}
+	dataProperties["optionalInner"] = schema.PropertySpec{
+		TypeSpec: schema.TypeSpec{
+			Type: "ref",
+			Ref:  "#/types/ref-ref:index:InnerData",
+		},
+	}
 	dataRequired := append(typeRequired, "innerData")
 
 	resourceProperties := map[string]schema.PropertySpec{