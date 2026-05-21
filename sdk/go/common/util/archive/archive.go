c37d4f088b5ce935b7328eaca4498a97334b35d9
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-21 08:01:03 -0700

    Use contract.IgnoreClose for gzip.Reader in ExtractTGZ
    Replace bare defer gzr.Close() with defer contract.IgnoreClose(gzr)
    to match the codebase convention used throughout the same file.
    
    Addresses review feedback from Frassle.
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- sdk/go/common/util/archive/archive.go
+++ sdk/go/common/util/archive/archive.go
@@ -115,7 +115,7 @@ func ExtractTGZ(r io.Reader, dir string) error {
 	if err != nil {
 		return fmt.Errorf("uncompressing: %w", err)
 	}
-	defer gzr.Close()
+	defer contract.IgnoreClose(gzr)
 	tr := tar.NewReader(gzr)
 	for {
 		header, err := tr.Next()
