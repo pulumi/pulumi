242a05e29003dbcad03bbe2bfe1745616a7418f0
Ian Wahbe <me@iwahbe.com>
2026-05-27 16:25:19 +0200

    Update test data

--- sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/bun/projects/l3-component-simple/index.ts
+++ sdk/nodejs/cmd/pulumi-language-nodejs/testdata/published/bun/projects/l3-component-simple/index.ts
@@ -1,5 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as simple from "@pulumi/simple";
 import { MyComponent } from "./myComponent";
 
-const someComponent = new MyComponent("someComponent", {input: true});
+const input = new simple.Resource("input", {value: true});
+const someComponent = new MyComponent("someComponent", {input: input.value});
 export const result = someComponent.output;
