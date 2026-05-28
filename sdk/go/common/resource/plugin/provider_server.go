fc6566b9250dbf68102845fbc119be4853acfae7
Ian Wahbe <me@iwahbe.com>
2026-05-28 18:20:59 +0200

    Actually deprecate `ConfigureRequest.variables` (#23367)
    We have claimed in the doc comment that this was deprecated, but we
    should actually do the deprecation.

--- sdk/go/common/resource/plugin/provider_server.go
+++ sdk/go/common/resource/plugin/provider_server.go
@@ -365,7 +365,7 @@ func (p *providerServer) Configure(ctx context.Context,
 		inputs = args
 	} else {
 		inputs = make(resource.PropertyMap)
-		for k, v := range req.GetVariables() {
+		for k, v := range req.GetVariables() { //nolint:staticcheck // maintain backwards compatibility
 			key, err := config.ParseKey(k)
 			if err != nil {
 				return nil, err
