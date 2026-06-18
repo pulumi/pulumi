df251669d26ce828017295d3009607468f57a06e
Fraser Waters <fraser@pulumi.com>
2026-06-18 17:37:40 +0100

    lint

--- pkg/codegen/schema/docs_renderer_test.go
+++ pkg/codegen/schema/docs_renderer_test.go
@@ -33,7 +33,6 @@ func TestRenderDocsToString_PreservesTrailingNewlineBehavior(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
