3a89db79845192e1241f008002dae083d7efb6ad
Fraser Waters <fraser@pulumi.com>
2026-06-25 21:40:44 +0100

    Fix TestRunPlugin

--- tests/integration/run_plugin/go/main.go
+++ tests/integration/run_plugin/go/main.go
@@ -21,8 +21,8 @@ import (
 	"path/filepath"
 
 	pkghost "github.com/pulumi/pulumi/pkg/v3/host"
+	"github.com/pulumi/pulumi/pkg/v3/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
