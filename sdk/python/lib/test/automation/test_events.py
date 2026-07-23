e312b50289f9d0f48f6a8e04a31d4592b75a71ff
Julien Poissonnier <julien@caffeine.lu>
2026-07-23 13:26:17 +0200

    Expose state migration Automation events

--- sdk/python/lib/test/automation/test_events.py
+++ sdk/python/lib/test/automation/test_events.py
@@ -4,8 +4,10 @@ import unittest
 
 from pulumi.automation.events import (
     DiffKind,
+    EngineEvent,
     PolicyEvent,
     PropertyDiff,
+    StateMigrationEvent,
     StepEventMetadata,
 )
 
@@ -52,3 +54,30 @@ class TestPolicyEventFromJson(unittest.TestCase):
         }
         event = PolicyEvent.from_json(data)
         assert event.resource_urn == "urn:pulumi:stack::project::type::name"
+
+
+class TestStateMigrationEventFromJson(unittest.TestCase):
+    """Test state migration payloads are selected and decoded by EngineEvent."""
+
+    def test_state_migration_event_deserialized(self):
+        old_urn = "urn:pulumi:stack::project::old:index:Resource::name"
+        new_urn = "urn:pulumi:stack::project::new:index:Resource::name"
+        event = EngineEvent.from_json(
+            {
+                "sequence": 7,
+                "timestamp": 42,
+                "stateMigrationEvent": {
+                    "urn": "urn:pulumi:stack::project::component:index:Component::name",
+                    "migrated": 2,
+                    "added": [new_urn],
+                    "removed": [old_urn],
+                    "successors": {old_urn: new_urn},
+                },
+            }
+        )
+
+        assert isinstance(event.state_migration_event, StateMigrationEvent)
+        assert event.state_migration_event.migrated == 2
+        assert event.state_migration_event.added == [new_urn]
+        assert event.state_migration_event.removed == [old_urn]
+        assert event.state_migration_event.successors == {old_urn: new_urn}
