0f13eeb6d77d0182b14038f7fc85961a51c38c45
Fraser Waters <fraser@pulumi.com>
2026-07-27 11:57:13 +0100

    Remove locks from Construct/Call in python servers

--- sdk/python/lib/pulumi/provider/server.py
+++ sdk/python/lib/pulumi/provider/server.py
@@ -85,7 +85,6 @@ class ProviderServicer(ResourceProviderServicer):
     engine_address: str
     provider: Provider
     args: list[str]
-    lock: asyncio.Lock
 
     def create_grpc_invalid_properties_status(
         self, message: str, errors: Optional[list[InputPropertyErrorDetails]]
@@ -121,9 +120,6 @@ class ProviderServicer(ResourceProviderServicer):
     async def Construct(
         self, request: proto.ConstructRequest, context
     ) -> proto.ConstructResponse:
-        # Calls to `Construct` and `Call` are serialized because they currently modify globals. When we are able to
-        # avoid modifying globals, we can remove the locking.
-        await self.lock.acquire()
         try:
             return await self._construct(request, context)
         except Exception as e:  # noqa
@@ -150,8 +146,6 @@ class ProviderServicer(ResourceProviderServicer):
                     stack = traceback.extract_tb(e.__traceback__)[:]
                 pretty_stack = "".join(traceback.format_list(stack))
                 raise Exception(f"{str(e)}:\n{pretty_stack}")
-        finally:
-            self.lock.release()
 
     async def _construct(
         self, request: proto.ConstructRequest, context
@@ -349,9 +343,6 @@ class ProviderServicer(ResourceProviderServicer):
         return proto.ConstructResponse(urn=urn, state=state, stateDependencies=deps)
 
     async def Call(self, request: proto.CallRequest, context):
-        # Calls to `Construct` and `Call` are serialized because they currently modify globals. When we are able to
-        # avoid modifying globals, we can remove the locking.
-        await self.lock.acquire()
         try:
             return await self._call(request, context)
         except InputPropertiesError as e:
@@ -366,8 +357,6 @@ class ProviderServicer(ResourceProviderServicer):
             await context.abort_with_status(status)
             # We already aborted at this point
             raise
-        finally:
-            self.lock.release()
 
     async def _call(self, request: proto.CallRequest, context):
         assert isinstance(request, proto.CallRequest), (
@@ -510,7 +499,6 @@ class ProviderServicer(ResourceProviderServicer):
         self.provider = provider
         self.args = args
         self.engine_address = engine_address
-        self.lock = asyncio.Lock()
 
 
 def main(provider: Provider, args: list[str]) -> None:  # args not in use?
