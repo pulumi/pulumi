f4093832e365216df1e32728f36e183778c5d1a1
Fraser Waters <fraser@pulumi.com>
2026-07-31 12:40:47 +0100

    Regen other passing tests

--- sdk/go/pulumi-language-go/language_test.go
+++ sdk/go/pulumi-language-go/language_test.go
@@ -110,7 +110,6 @@ var expectedFailures = map[string]string{
 	"l1-builtin-object":             "entries/lookup emit TODO stubs",
 	"l2-builtin-object":             "entries/lookup emit TODO stubs",
 	"l1-builtin-to-json":            "Go doesn't support output based toJSON",
-	"l2-resource-config-primitives": "cannot convert secretBool (variable of struct type pulumi.BoolOutput) to type pulumi.Bool, etc", //nolint:lll
 	"l2-resource-config-objects":    "cannot convert plainBooleanMap (variable of type string) to type pulumi.BoolMap",
 	"l2-resource-schema-secret":     "does not preserve schema-secret unknown outputs",
 
