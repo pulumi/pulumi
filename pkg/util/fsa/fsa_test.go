38f04d24c0838d44afa6eec81cf56fd8f40e4ba7
Ian Wahbe <me@iwahbe.com>
2026-08-11 10:25:24 -0700

    Basic stuff works

--- pkg/util/fsa/fsa_test.go
+++ pkg/util/fsa/fsa_test.go
@@ -2,10 +2,12 @@ package fsa_test
 
 import (
 	"context"
+	"maps"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/util/fsa"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUnblockedProgression(t *testing.T) {
@@ -15,29 +17,70 @@ func TestUnblockedProgression(t *testing.T) {
 
 	machine := fsa.New[string]()
 	var e1 fsa.Edge
-	n1 := machine.NewNode(func(ctx context.Context, m fsa.FSA[string], edge fsa.Edge, v string) error {
-		assert.Equal(t, "v1", v)
-		actions = append(actions, "node1")
-		return nil
+	n0 := machine.NewNode(func(context.Context, fsa.FSA[string], fsa.Edge, string) error {
+		panic("should not be called")
 	})
 
-	var n2 fsa.Node
-	n2 = machine.NewNode(func(ctx context.Context, m fsa.FSA[string], edge fsa.Edge, v string) error {
-		from, to := fsa.GetEdge(edge)
-		assert.Equal(t, from, n1)
-		assert.Equal(t, to, n2)
+	var n1 fsa.Node
+	n1 = machine.NewNode(func(ctx context.Context, m fsa.FSA[string], edge fsa.Edge, v string) error {
+		from, to := m.GetEdge(edge)
+		assert.Equal(t, from, n0)
+		assert.Equal(t, to, n1)
 		assert.Equal(t, edge, e1)
 		assert.Equal(t, "v1", v)
-		actions = append(actions, "node2")
+		actions = append(actions, "n1")
 		return nil
 	})
 
-	e1 = fsa.NewEdge(func(ctx context.Context, fsa fsa.FSA[Cursor], from, to fsa.Node) (fsa.ConditionResult, error) {
-		assert.Equal(t, n1, from)
-		assert.Equal(t, n2, to)
-		actions = append(actions, "n1->n2")
+	e1 = machine.NewEdge(func(ctx context.Context, m fsa.FSA[string], from, to fsa.Node) (fsa.ConditionResult, error) {
+		assert.Equal(t, n0, from)
+		assert.Equal(t, n1, to)
+		actions = append(actions, "n0->n1")
 		return fsa.ConditionPass, nil
-	}, n1, n2)
+	}, n0, n1)
 
-	fsa.NewCursor("v1", n1)
+	machine.NewCursor("v1", n0)
+
+	require.NoError(t, machine.Progress(t.Context()))
+
+	assert.Equal(t, []string{
+		"n0->n1",
+		"n1",
+	}, actions)
+
+	assert.Equal(t, map[string]fsa.Node{}, maps.Collect(machine.Parked))
+	assert.Equal(t, map[string]fsa.Node{
+		"v1": n1,
+	}, maps.Collect(machine.Cursors))
+}
+
+func TestBlockedProgression(t *testing.T) {
+	t.Parallel()
+
+	machine := fsa.New[string]()
+	n0 := machine.NewNode(func(context.Context, fsa.FSA[string], fsa.Edge, string) error {
+		panic("should not be called")
+	})
+
+	var n1 fsa.Node
+	n1 = machine.NewNode(func(ctx context.Context, m fsa.FSA[string], edge fsa.Edge, v string) error {
+		panic("should not be called")
+	})
+
+	machine.NewEdge(func(ctx context.Context, m fsa.FSA[string], from, to fsa.Node) (fsa.ConditionResult, error) {
+		assert.Equal(t, n0, from)
+		assert.Equal(t, n1, to)
+		return fsa.ConditionFail, nil
+	}, n0, n1)
+
+	machine.NewCursor("v1", n0)
+
+	require.NoError(t, machine.Progress(t.Context()))
+
+	assert.Equal(t, map[string]fsa.Node{
+		"v1": n0,
+	}, maps.Collect(machine.Parked))
+	assert.Equal(t, map[string]fsa.Node{
+		"v1": n0,
+	}, maps.Collect(machine.Cursors))
 }
