e227d0af4d3d2c35a45a171fd8cd8940fb999040
Fraser Ashyggton <fraser@pulumi.com>
2026-08-18 10:51:50 +0000

    Move DetectProject to pkg (#24363)
    Continuing to chip away at moving things to pkg from sdk/go/common.

--- pkg/cmd/pulumi/stack/secrets.go
+++ pkg/cmd/pulumi/stack/secrets.go
@@ -146,11 +146,11 @@ func readStackConfiguration(ctx context.Context, sink diag.Sink, ws pkgWorkspace
 		if configFile != "" {
 			return workspace.LoadProjectStack(sink, project, configFile)
 		}
-		return workspace.DetectProjectStack(sink, stackRef.Name().Q())
+		return pkgWorkspace.DetectProjectStack(sink, stackRef.Name().Q())
 	}
 	ps, err := LoadProjectStack(ctx, sink, project, s, configFile)
 	if configFile == "" && (err != nil || ps == nil) {
-		return workspace.DetectProjectStack(sink, stackRef.Name().Q())
+		return pkgWorkspace.DetectProjectStack(sink, stackRef.Name().Q())
 	}
 
 	return ps, err
