ae44491934980ab03893e68075aa6faeb1cc7c73
Ian Wahbe <me@iwahbe.com>
2026-08-25 07:10:51 +0200

    Regenerate python conformance snapshots for unhyphenated provider docstrings
    The python tokenToName change unhyphenates the member segment, which
    also applies to provider resource docstrings: the provider token's
    member is the package name, so packages with hyphenated names now
    document "Create a SimpleInvoke resource" instead of "Create a
    Simple-invoke resource". Regenerate the SDK snapshots for every
    conformance provider with a hyphenated package name; only the
    l2-kebab-names snapshots had been regenerated before.

--- sdk/python/cmd/pulumi-language-python/testdata/toml/local/sdks/simple-invoke-10.0.0/pulumi_simple_invoke/provider.py
+++ sdk/python/cmd/pulumi-language-python/testdata/toml/local/sdks/simple-invoke-10.0.0/pulumi_simple_invoke/provider.py
@@ -33,7 +33,7 @@ class Provider(pulumi.ProviderResource):
                  opts: Optional[pulumi.ResourceOptions] = None,
                  __props__=None):
         """
-        Create a Simple-invoke resource with the given unique name, props, and options.
+        Create a SimpleInvoke resource with the given unique name, props, and options.
 
         :param str resource_name: The name of the resource.
         :param pulumi.ResourceOptions opts: Options for the resource.
@@ -45,7 +45,7 @@ class Provider(pulumi.ProviderResource):
                  args: Optional[ProviderArgs] = None,
                  opts: Optional[pulumi.ResourceOptions] = None):
         """
-        Create a Simple-invoke resource with the given unique name, props, and options.
+        Create a SimpleInvoke resource with the given unique name, props, and options.
 
         :param str resource_name: The name of the resource.
         :param ProviderArgs args: The arguments to use to populate this resource's properties.
