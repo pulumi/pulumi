fb24db824d58a1d492347e10e9ce30e77ac13329
Julien Poissonnier <julien@caffeine.lu>
2026-09-10 11:48:41 +0200

    Forward caller error hooks to remote component providers
    We missed forwarding the error hooks in the construct options.

--- sdk/go/common/resource/plugin/provider_plugin.go
+++ sdk/go/common/resource/plugin/provider_plugin.go
@@ -2115,6 +2115,7 @@ func (p *provider) Construct(ctx context.Context, req ConstructRequest) (Constru
 		resourceHook.AfterUpdate = req.Options.ResourceHooks[resource.AfterUpdate]
 		resourceHook.BeforeDelete = req.Options.ResourceHooks[resource.BeforeDelete]
 		resourceHook.AfterDelete = req.Options.ResourceHooks[resource.AfterDelete]
+		resourceHook.OnError = req.Options.ResourceHooks[resource.OnError]
 	}
 
 	aliases := make([]*pulumirpc.Alias, len(req.Options.Aliases))
